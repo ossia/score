c430e0f4ad32374273529ae5f80488e85a0d927d
Jaime Chao <jaimitochao@gmail.com>
2013-11-08 13:09:55 +0100

    TimeboxStoreyBar's class.

---
+++ sources/timeboxstoreybar.cpp
@@ -0,0 +1,74 @@
+/*
+Copyright: LaBRI / SCRIME
+
+Authors : Jaime Chao, Clément Bossut (2013-2014)
+
+This software is governed by the CeCILL license under French law and
+abiding by the rules of distribution of free software.  You can  use,
+modify and/ or redistribute the software under the terms of the CeCILL
+license as circulated by CEA, CNRS and INRIA at the following URL
+"http://www.cecill.info".
+
+As a counterpart to the access to the source code and  rights to copy,
+modify and redistribute granted by the license, users are provided only
+with a limited warranty  and the software's author,  the holder of the
+economic rights,  and the successive licensors  have only  limited
+liability.
+
+In this respect, the user's attention is drawn to the risks associated
+with loading,  using,  modifying and/or developing or reproducing the
+software by the user in light of its specific status of free software,
+that may mean  that it is complicated to manipulate,  and  that  also
+therefore means  that it is reserved for developers  and  experienced
+professionals having in-depth computer knowledge. Users are therefore
+encouraged to load and test the software's suitability as regards their
+requirements in conditions enabling the security of their systems and/or
+data to be ensured and,  more generally, to use and operate it in the
+same conditions as regards security.
+
+The fact that you are presently reading this means that you have had
+knowledge of the CeCILL license and that you accept its terms.
+*/
+
+#include "timeboxstoreybar.hpp"
+#include <QGraphicsPixmapItem>
+#include <QComboBox>
+#include <QGraphicsProxyWidget>
+#include <QGraphicsLinearLayout>
+#include <QGraphicsSceneMouseEvent>
+#include <QDebug>
+
+TimeboxStoreyBar::TimeboxStoreyBar(QGraphicsItem *item)
+  : QGraphicsWidget(item)
+{
+
+  //setPreferredSize(parentLayoutItem()->preferredSize());
+  setPreferredSize(parentItem()->boundingRect().size());
+
+  _pButtonAdd = new QGraphicsPixmapItem(QPixmap(":/png/play"), this);
+  _pButtonAdd->setPos(0,0);
+
+  _pComboBox = new QComboBox(); /// @todo Subclass ant create model to do some extra work
+  _pComboBox->setStyleSheet(
+    "QComboBox {"
+    "border: none;"
+    "border-radius: none;"
+    "};"
+    );
+  _pComboBoxProxy = new QGraphicsProxyWidget(this);
+  _pComboBoxProxy->setWidget(_pComboBox);
+  //_pComboBoxProxy->setPos(preferredWidth()-_pComboBoxProxy->size().width(), 0);
+  _pComboBoxProxy->setPos(300,0);
+
+}
+
+
+void TimeboxStoreyBar::mousePressEvent(QGraphicsSceneMouseEvent *event)
+{
+  if(event->button() == Qt::LeftButton) {
+      if(_pButtonAdd->contains(event->pos())) {
+          emit addStorey();
+        }
+    }
+  QGraphicsWidget::mousePressEvent(event);
+}
