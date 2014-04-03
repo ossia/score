34910ebbf213ab917232e92e4dcf9aad6b98b8ba
Jaime Chao <jaimitochao@gmail.com>
2014-04-03 11:39:41 +0200

    Message handler

--- sources/main.cpp
+++ sources/main.cpp
@@ -33,8 +33,29 @@ knowledge of the CeCILL license and that you accept its terms.
 #include <QApplication>
 #include "engine.hpp"
 
+void myMessageOutput(QtMsgType type, const QMessageLogContext &context, const QString &msg)
+{
+    QByteArray localMsg = msg.toLocal8Bit();
+    switch (type) {
+    case QtDebugMsg:
+        fprintf(stderr, "Debug: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        break;
+    case QtWarningMsg:
+        fprintf(stderr, "Warning: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        break;
+    case QtCriticalMsg:
+        fprintf(stderr, "Critical: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        break;
+    case QtFatalMsg:
+        fprintf(stderr, "Fatal: %s (%s:%u, %s)\n", localMsg.constData(), context.file, context.line, context.function);
+        abort();
+    }
+}
+
 int main(int argc, char *argv[])
 {
+  // qInstallMessageHandler(myMessageOutput); /// Uncomment if we want a more verbose msg handler
+
   QApplication app(argc, argv);
   app.setApplicationName("i-score");
   app.setOrganizationName("OSSIA");
