3530703c3eab06b54ab095070ed3f3c49dffe6f3
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2014-11-25 10:25:18 +0100

    Add test cases

--- base/plugins/scenario2/tests/Interval/IntervalModelTests.cpp
+++ base/plugins/scenario2/tests/Interval/IntervalModelTests.cpp
@@ -54,7 +54,8 @@ class IntervalModelTests: public QObject
 		
 		void FindSubProcessTest()
 		{
-			IntervalModel i0{nullptr, nullptr, 0, qApp}; i0.setObjectName("dadada");
+			IntervalModel i0{nullptr, nullptr, 0, qApp}; 
+			i0.setObjectName("OriginalInterval");
 			auto s0 = new ScenarioProcessSharedModel{0, &i0};
 			
 			s0->createInterval(34);
@@ -64,18 +65,42 @@ class IntervalModelTests: public QObject
 			auto s1 = new ScenarioProcessSharedModel{0, i1};
 			auto s2 = new ScenarioProcessSharedModel{1, i1};
 			
-			ObjectPath p{"dadada", 
+			ObjectPath p{"OriginalInterval", 
 						 {
 							{"ScenarioProcessSharedModel", 0},
 							{"IntervalModel", 0},
 							{"ScenarioProcessSharedModel", 1}
 						 }
 						};
-			auto obj = p.find();
-			QCOMPARE(obj, s2);
+			QCOMPARE(p.find(), s2);
 			
+			ObjectPath p2{"OriginalInterval", 
+						 {
+							{"ScenarioProcessSharedModel", 0},
+							{"IntervalModel", 0},
+							{"ScenarioProcessSharedModel", 7}
+						 }
+						};
+			QCOMPARE(p2.find(), static_cast<QObject*>(nullptr));
 			
+			ObjectPath p3{"OriginalInterval", 
+						 {
+							{"ScenarioProcessSharedModel", 0},
+							{"IntervalModel0xBADBAD", 0},
+							{"ScenarioProcessSharedModel", 1}
+						 }
+						};
+			QCOMPARE(p3.find(), static_cast<QObject*>(nullptr));
 			
+			ObjectPath p4{"OriginalInterval", 
+						 {
+							{"ScenarioProcessSharedModel", 0},
+							{"IntervalModel", 0},
+							{"ScenarioProcessSharedModel", 1},
+							{"ScenarioProcessSharedModel", 1}
+						 }
+						};
+			QCOMPARE(p4.find(), static_cast<QObject*>(nullptr));
 		}
 
 };
