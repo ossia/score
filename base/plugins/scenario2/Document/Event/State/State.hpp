7e192a406081a3efe9d89d7df17b560abef54a3a
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2014-12-05 15:52:13 +0100

    Start adding complex data types for State

--- base/plugins/scenario2/Document/Event/State/State.hpp
+++ base/plugins/scenario2/Document/Event/State/State.hpp
@@ -1,6 +1,30 @@
 #pragma once
 #include <QNamedObject>
 
+class DataType { };
+class Impulse : public DataType { }; // ignore QVariant
+class Boolean : public DataType { }; // QVariant = bool
+class Integer : public DataType { }; // QVariant = uint32
+class Decimal : public DataType { }; // QVariant = float
+class String  : public DataType { }; // QVariant = std::string
+class Tuple   : public DataType { }; // QVariant = std::tuple ou struct...
+
+class Domain { };
+enum class AccessMode { Get, Set, Both };
+enum class BoundingMode { Free, Clip, Wrap, Fold };
+struct Message
+{
+		QString address;
+		QVariant value; // On peut utiliser val.type() et val.canConvert() directement...
+		DataType type; // ?
+
+		AccessMode mode{AccessMode::Both};
+		BoundingMode minBoundingMode;
+		BoundingMode maxBoundingMode;
+
+		Domain domain;
+};
+
 class State : public QNamedObject
 {
 		Q_OBJECT
@@ -38,5 +62,6 @@ class FakeState : public State
 
 	private:
 		QString m_address;
+		QVariant val;
 };
 
