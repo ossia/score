740f25f37df9e1e2793fa4dcd72d9fa777d8aa0c
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2014-12-05 10:05:46 +0100

    Fix crash in inspector

--- base/plugins/scenario2/Document/Interval/IntervalPresenter.cpp
+++ base/plugins/scenario2/Document/Interval/IntervalPresenter.cpp
@@ -22,7 +22,7 @@ IntervalPresenter::IntervalPresenter(IntervalModel* model,
 	// Le contentView est child de IntervalView (au sens Qt) mais est accessible via son présenteur.
 	// Le présenteur parent va créer les vues correspondant aux présenteurs enfants
 	// TODO mettre ça dans la doc des classes
-//*
+
 	auto contentView = new IntervalContentView{view};
 
 	// Cas par défaut
@@ -31,25 +31,24 @@ IntervalPresenter::IntervalPresenter(IntervalModel* model,
 														   this};
 
 	m_contentPresenters.push_back(interval_presenter);
-//*/
-	connect(this, SIGNAL(submitCommand(iscore::SerializableCommand*)),
+
+	connect(this,	SIGNAL(submitCommand(iscore::SerializableCommand*)),
 			parent, SIGNAL(submitCommand(iscore::SerializableCommand*)));
 
-	connect(this, SIGNAL(elementSelected(QObject*)),
+	connect(this,	SIGNAL(elementSelected(QObject*)),
 			parent, SIGNAL(elementSelected(QObject*)));
 
 	connect(m_view, &IntervalView::intervalPressed,
-			this, &IntervalPresenter::on_intervalPressed);
+			this,	&IntervalPresenter::on_intervalPressed);
 
 
 	connect(m_view, &IntervalView::addScenarioProcessClicked,
 			[&] ()
-	{
-		auto path = ObjectPath::pathFromObject("BaseIntervalModel", m_model);
-		auto cmd = new AddProcessToIntervalCommand(std::move(path), "Scenario");
-		emit submitCommand(cmd);
-		qDebug("bumpimpuppefsldmsdfsf");
-	});
+		{
+			auto path = ObjectPath::pathFromObject("BaseIntervalModel", m_model);
+			auto cmd = new AddProcessToIntervalCommand(std::move(path), "Scenario");
+			emit submitCommand(cmd);
+		});
 }
 
 IntervalPresenter::~IntervalPresenter()
