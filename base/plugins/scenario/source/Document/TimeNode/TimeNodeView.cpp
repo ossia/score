d913442ff52d1ea2d90f64442a4907c5cca14a30
Nicolas Vuaille <nicolas.vuaille@gmail.com>
2015-02-04 09:50:38 +0100

    correct timeNodes bounding rect

--- base/plugins/scenario/source/Document/TimeNode/TimeNodeView.cpp
+++ base/plugins/scenario/source/Document/TimeNode/TimeNodeView.cpp
@@ -17,11 +17,14 @@ void TimeNodeView::paint(QPainter *painter,
 {
     painter->setPen(QPen(QBrush(QColor(180,0,220)), 5, Qt::SolidLine));
     painter->drawLine(0, m_top, 0, m_bottom);
+
+//    painter->setPen(QPen(QBrush(QColor(0,200,0)), 1, Qt::SolidLine));
+//    painter->drawRect(boundingRect());
 }
 
 QRectF TimeNodeView::boundingRect() const
 {
-    return {0, 0, 20,(qreal) (m_bottom-m_top) };
+    return {-10, (qreal) m_top, 20, (qreal) (m_bottom-m_top) };
 }
 
 void TimeNodeView::setExtremities(int top, int bottom)
