da5afa5596bedae22b6389b9af79e56195eb88c6
Nicolas <nicolas.vuaille@gmail.com>
2015-02-24 10:15:37 +0100

    Ooops, missing files

---
+++ base/plugins/scenario/source/Inspector/Event/EventWidgets/EventShortcut.cpp
@@ -0,0 +1,40 @@
+#include "EventShortcut.hpp"
+
+#include <QPushButton>
+#include <QCheckBox>
+#include <QHBoxLayout>
+
+
+EventShortCut::EventShortCut(QString eventId, QWidget *parent):
+    QWidget{parent}
+{
+    auto groupLay = new QHBoxLayout{};
+    this->setLayout(groupLay);
+
+    // browser button
+    m_eventBtn = new QPushButton{this};
+    m_eventBtn->setText(eventId);
+    m_eventBtn->setFlat(true);
+
+    m_box = new QCheckBox{};
+
+    groupLay->addWidget(m_eventBtn);
+    groupLay->addWidget(m_box);
+
+    connect(m_eventBtn,   &QPushButton::clicked,
+            [=] ()
+    {
+        emit eventSelected(m_eventBtn->text());
+    });
+
+}
+
+bool EventShortCut::isChecked()
+{
+    return m_box->isChecked();
+}
+
+QString EventShortCut::eventName()
+{
+    return m_eventBtn->text();
+}
---
+++ base/plugins/scenario/source/Inspector/Event/EventWidgets/EventShortcut.hpp
@@ -0,0 +1,25 @@
+#pragma once
+#include <QWidget>
+
+class QPushButton;
+class QCheckBox;
+
+class EventShortCut: public QWidget
+{
+    Q_OBJECT
+
+    public:
+
+        EventShortCut(QString eventId, QWidget *parent = 0);
+
+        bool isChecked();
+        QString eventName();
+
+    signals:
+        void eventSelected(QString);
+
+    private:
+        QCheckBox* m_box;
+        QPushButton* m_eventBtn;
+
+};
