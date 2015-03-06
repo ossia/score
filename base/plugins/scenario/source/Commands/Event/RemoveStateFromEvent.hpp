95a5925d8139675bbb8da399722044fcfc9b0785
Nicolas <nicolas.vuaille@gmail.com>
2015-03-06 16:21:25 +0100

    removeStateFromEvent command

--- base/plugins/scenario/source/Commands/Event/RemoveStateFromEvent.hpp
+++ base/plugins/scenario/source/Commands/Event/RemoveStateFromEvent.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include <core/presenter/command/SerializableCommand.hpp>
+#include "tools/ObjectPath.hpp"
 
+class State;
 namespace Scenario
 {
     namespace Command
@@ -10,6 +12,7 @@ namespace Scenario
                 ISCORE_COMMAND
             public:
                 ISCORE_COMMAND_DEFAULT_CTOR(RemoveStateFromEvent, "ScenarioControl")
+                RemoveStateFromEvent(ObjectPath&& eventPath, QString message);
                 virtual void undo() override;
                 virtual void redo() override;
                 virtual bool mergeWith(const Command* other) override;
@@ -17,6 +20,13 @@ namespace Scenario
             protected:
                 virtual void serializeImpl(QDataStream&) const override;
                 virtual void deserializeImpl(QDataStream&) override;
+
+            private:
+                ObjectPath m_path;
+                QString m_message;
+
+                id_type<State> m_stateId {};
+                QByteArray m_serializedState;
         };
 
     }
