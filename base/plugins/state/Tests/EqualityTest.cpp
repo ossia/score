a4d98e74c3cce543dbf591056c9224579c5f0924
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-03-19 14:29:26 +0100

    Add a state test

---
+++ base/plugins/state/Tests/EqualityTest.cpp
@@ -0,0 +1,54 @@
+#include <QTest>
+#include <State/State.hpp>
+#include <State/Message.hpp>
+
+
+class EqualityTest: public QObject
+{
+        Q_OBJECT
+    public:
+
+    private slots:
+
+        void equalityTest()
+        {
+            QMetaType::registerComparators<Message>();
+            QMetaType::registerComparators<MessageList>();
+            qRegisterMetaTypeStreamOperators<Message>();
+            qRegisterMetaTypeStreamOperators<MessageList>();
+            Message m;
+            m.address = "dada";
+            Q_ASSERT(m == m);
+
+            m.value = 5.5;
+            Q_ASSERT(m == m);
+
+            MessageList l1;
+            l1.push_back(m);
+
+            MessageList l2;
+            l2.push_back(m);
+
+            Q_ASSERT(l1 == l2);
+
+            Q_ASSERT(!(l1 != l2));
+            State s1 (l1);
+            State s2 (l2);
+            Q_ASSERT (s1 == s2);
+            Q_ASSERT (!(s1 != s2));
+
+            Message m2;
+            m.address = "dodo";
+            m.value = 3;
+            Q_ASSERT(m != m2);
+            MessageList l3;
+            l3.push_back(m2);
+            Q_ASSERT(l3 != l2);
+
+            State s3(l3);
+            Q_ASSERT(s2 != s3);
+        }
+};
+
+QTEST_MAIN(EqualityTest)
+#include "EqualityTest.moc"
