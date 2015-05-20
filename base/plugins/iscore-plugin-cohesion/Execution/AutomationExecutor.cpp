1e1b349b87a632f1032f5d2505ac114fb2296eb7
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-05-20 15:59:09 +0200

    Execute automations

--- base/plugins/iscore-plugin-cohesion/Execution/AutomationExecutor.cpp
+++ base/plugins/iscore-plugin-cohesion/Execution/AutomationExecutor.cpp
@@ -1,7 +1,36 @@
 #include "AutomationExecutor.hpp"
 
 #include <Automation/AutomationModel.hpp>
+// Device stuff
+#include <Plugin/PanelBase/DeviceExplorerPanelModel.hpp>
+#include <Plugin/Panel/DeviceExplorerModel.hpp>
 #include <Plugin/DeviceExplorerPlugin.hpp>
+#include <Plugin/DocumentPlugin/DeviceDocumentPlugin.hpp>
+
+#include <DeviceExplorer/Node/Node.hpp>
+
+#include <iscore/document/DocumentInterface.hpp>
+#include <core/document/DocumentModel.hpp>
+
+AutomationExecutor::AutomationExecutor(AutomationModel& model):
+    m_model{model}
+{
+    auto doc = iscore::IDocument::documentFromObject(model);
+    auto plug = static_cast<DeviceDocumentPlugin*>(doc->model()->pluginModel("DeviceDocumentPlugin"));
+
+    auto segments = model.address().split("/");
+    m_dev = &plug->list().device(segments[1]);
+
+    // Remove the device name
+    QStringList msg = m_model.address().split("/");
+    msg.removeFirst();
+
+    m_demodel = getNodeFromString(static_cast<DeviceExplorerPanelModel*>(doc->model()->panel("DeviceExplorerPanelModel"))->deviceExplorer()->rootNode(),
+                                  QStringList(msg));
+
+    msg.removeFirst();
+    m.address= msg.join("/").prepend("/");
+}
 
 void AutomationExecutor::start()
 {
@@ -16,14 +45,7 @@ void AutomationExecutor::stop()
 
 void AutomationExecutor::onTick(const TimeValue& time)
 {
-    /*
-    SingletonDeviceList::sendMessage(m_model.address(),
-                                     m_model.value(time));
-                                     */
+    m.value = m_model.value(time) * (m_demodel->maxValue() - m_demodel->minValue()) + m_demodel->minValue();
+    m_dev->sendMessage(m);
 }
 
-AutomationExecutor::AutomationExecutor(AutomationModel& model):
-    m_model{model}
-{
-
-}
--- base/plugins/iscore-plugin-cohesion/Execution/AutomationExecutor.hpp
+++ base/plugins/iscore-plugin-cohesion/Execution/AutomationExecutor.hpp
@@ -1,10 +1,15 @@
 #pragma once
 #include <ProcessInterface/ProcessExecutor.hpp>
+#include <State/Message.hpp>
 class AutomationModel;
-
+class DeviceInterface;
+class Node;
 class AutomationExecutor : public ProcessExecutor
 {
     AutomationModel& m_model;
+    DeviceInterface* m_dev{};
+    Node* m_demodel{};
+    Message m;
 
   public:
     AutomationExecutor(AutomationModel& model);
