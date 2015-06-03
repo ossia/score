3c00e2db081ef25a4f778f044e9fe8dbe4f27ecf
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-06-03 12:00:38 +0200

    Update OSSIA

--- base/plugins/iscore-plugin-ossia/Protocols/Minuit/MinuitDevice.cpp
+++ base/plugins/iscore-plugin-ossia/Protocols/Minuit/MinuitDevice.cpp
@@ -8,8 +8,8 @@ MinuitDevice::MinuitDevice(const DeviceSettings &settings):
 
     auto stgs = settings.deviceSpecificSettings.value<MinuitSpecificSettings>();
     Minuit minuitDeviceParameters{stgs.host.toStdString(),
-                                  stgs.inPort,
-                                  stgs.outPort};
+                stgs.inPort,
+                stgs.outPort};
 
     m_dev = Device::create(minuitDeviceParameters, settings.name.toStdString());
 }
@@ -22,106 +22,106 @@ bool MinuitDevice::canRefresh() const
 // Utility functions to convert from one node to another.
 namespace
 {
-QString valueTypeToString(OSSIA::AddressValue::Type t)
-{
-    switch(t)
+    QString valueTypeToString(OSSIA::AddressValue::Type t)
     {
-        case OSSIA::AddressValue::Type::NONE:
-            return "None";
-        case OSSIA::AddressValue::Type::BOOL:
-            return "Bool";
-        case OSSIA::AddressValue::Type::INT:
-            return "Int";
-        case OSSIA::AddressValue::Type::FLOAT:
-            return "Float";
-        case OSSIA::AddressValue::Type::CHAR:
-            return "Char";
-        case OSSIA::AddressValue::Type::STRING:
-            return "String";
-        case OSSIA::AddressValue::Type::TUPLE:
-            return "Tuple";
-        case OSSIA::AddressValue::Type::GENERIC:
-            return "Generic";
-        default:
-            Q_ASSERT(false);
-            return "";
+        switch(t)
+        {
+            case OSSIA::AddressValue::Type::NONE:
+                return "None";
+            case OSSIA::AddressValue::Type::BOOL:
+                return "Bool";
+            case OSSIA::AddressValue::Type::INT:
+                return "Int";
+            case OSSIA::AddressValue::Type::FLOAT:
+                return "Float";
+            case OSSIA::AddressValue::Type::CHAR:
+                return "Char";
+            case OSSIA::AddressValue::Type::STRING:
+                return "String";
+            case OSSIA::AddressValue::Type::TUPLE:
+                return "Tuple";
+            case OSSIA::AddressValue::Type::GENERIC:
+                return "Generic";
+            default:
+                Q_ASSERT(false);
+                return "";
+        }
     }
-}
 
-IOType accessModeToIOType(OSSIA::Address::AccessMode t)
-{
-    switch(t)
+    IOType accessModeToIOType(OSSIA::Address::AccessMode t)
     {
-        case OSSIA::Address::AccessMode::GET:
-            return IOType::In;
-        case OSSIA::Address::AccessMode::SET:
-            return IOType::Out;
-        case OSSIA::Address::AccessMode::BI:
-            return IOType::InOut;
-        default:
-            Q_ASSERT(false);
-            return IOType::Invalid;
+        switch(t)
+        {
+            case OSSIA::Address::AccessMode::GET:
+                return IOType::In;
+            case OSSIA::Address::AccessMode::SET:
+                return IOType::Out;
+            case OSSIA::Address::AccessMode::BI:
+                return IOType::InOut;
+            default:
+                Q_ASSERT(false);
+                return IOType::Invalid;
+        }
     }
-}
 
 
-void nodeFullName_rec(const OSSIA::Node& node, QStringList& names)
-{
-    names.push_back(QString::fromStdString(node.getName()));
-    try {
-        const OSSIA::Node& n = node.getParent();
-        nodeFullName_rec(n, names);
-    }
-    catch(...)
+    void nodeFullName_rec(const OSSIA::Node& node, QStringList& names)
     {
-        return;
+        names.push_back(QString::fromStdString(node.getName()));
+
+        auto n = node.getParent();
+        if(n)
+            nodeFullName_rec(*n, names);
     }
-}
 
-QString nodeFullName(const OSSIA::Node& node)
-{
-    QStringList names;
-    nodeFullName_rec(node, names);
-
-    std::reverse(names.begin(), names.end());
-
-    auto name = names.join("/");
-    name.prepend("/");
-
-    return name;
-}
-
-AddressSettings extractAddressSettings(const OSSIA::Node& node)
-{
-    AddressSettings s;
-    const auto& addr = node.getAddress();
-
-    s.name = nodeFullName(node);
-    s.valueType = valueTypeToString(addr->getValueType());
-    s.ioType = accessModeToIOType(addr->getAccessMode());
-    return s;
-}
-
-Node* OssiaToDeviceExplorer(const OSSIA::Node& node)
-{
-    Node* n = new Node;
-
-    // 1. Set the parameters
-    n->setAddressSettings(extractAddressSettings(node));
-
-    // 2. Recurse on the children
-    for(const auto& ossia_child : node.children())
+    QString nodeFullName(const OSSIA::Node& node)
     {
-        n->addChild(OssiaToDeviceExplorer(*ossia_child.get()));
+        QStringList names;
+        nodeFullName_rec(node, names);
+
+        std::reverse(names.begin(), names.end());
+
+        auto name = names.join("/");
+        name.prepend("/");
+
+        return name;
     }
 
-    return n;
-}
+    AddressSettings extractAddressSettings(const OSSIA::Node& node)
+    {
+        AddressSettings s;
+        const auto& addr = node.getAddress();
+
+        s.name = nodeFullName(node);
+        if(addr)
+        {
+            s.valueType = valueTypeToString(addr->getValueType());
+            s.ioType = accessModeToIOType(addr->getAccessMode());
+        }
+        return s;
+    }
+
+    Node* OssiaToDeviceExplorer(const OSSIA::Node& node)
+    {
+        Node* n = new Node;
+
+        // 1. Set the parameters
+        n->setAddressSettings(extractAddressSettings(node));
+
+        // 2. Recurse on the children
+        for(const auto& ossia_child : node.children())
+        {
+            n->addChild(OssiaToDeviceExplorer(*ossia_child.get()));
+        }
+
+        return n;
+    }
 }
 
 Node MinuitDevice::refresh()
 {
     Node device;
+
     if(m_dev->updateNamespace())
     {
         // Make a device explorer node from the current state of the device.
