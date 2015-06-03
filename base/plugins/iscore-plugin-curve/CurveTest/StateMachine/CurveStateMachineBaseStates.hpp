5404db10f1bc84283c856dd47bc7ec146c7c2b0c
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-06-03 20:25:14 +0200

    Add a todo

--- base/plugins/iscore-plugin-curve/CurveTest/StateMachine/CurveStateMachineBaseStates.hpp
+++ base/plugins/iscore-plugin-curve/CurveTest/StateMachine/CurveStateMachineBaseStates.hpp
@@ -8,6 +8,7 @@ namespace Curve
 class StateBase : public QState
 {
     public:
+        // TODO can't do this... Instead save ids (and for the point save {idbefore, idafter}
         const QGraphicsItem*  clickedItem{}, *hoveredItem{};
         CurvePoint currentPoint;
 };
