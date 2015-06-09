63ae6e5156fb7f98ec5da5c2df660778d0ef27ee
Pascal Baltazar <pb@zkrx.org>
2015-06-09 19:07:08 +0200

    removing background grid

--- base/plugins/iscore-plugin-scenario/source/Document/BaseElement/Widgets/SizeNotifyingGraphicsView.cpp
+++ base/plugins/iscore-plugin-scenario/source/Document/BaseElement/Widgets/SizeNotifyingGraphicsView.cpp
@@ -8,17 +8,17 @@ SizeNotifyingGraphicsView::SizeNotifyingGraphicsView(QGraphicsScene* parent):
     setAlignment(Qt::AlignTop | Qt::AlignLeft);
     setCacheMode(QGraphicsView::CacheBackground);
 
-    m_graduations = new SceneGraduations{this};
-    scene()->addItem(m_graduations);
+    //m_graduations = new SceneGraduations{this};
+    //scene()->addItem(m_graduations);
 
     m_bg =  qApp->palette("ScenarioPalette").background();
-    m_graduations->setColor(m_bg.color().lighter());
+    //m_graduations->setColor(m_bg.color().lighter());
     this->setBackgroundBrush(m_bg);
 }
 
 void SizeNotifyingGraphicsView::setGrid(QPainterPath&& newGrid)
 {
-    m_graduations->setGrid(std::move(newGrid));
+    //m_graduations->setGrid(std::move(newGrid));
 }
 
 void SizeNotifyingGraphicsView::resizeEvent(QResizeEvent* ev)
