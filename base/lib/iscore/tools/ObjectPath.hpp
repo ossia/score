cb04cd689b329763bce43144337992d0159ccfce
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-06-23 19:13:03 +0200

    Fix build on gcc-4.9

--- base/lib/iscore/tools/ObjectPath.hpp
+++ base/lib/iscore/tools/ObjectPath.hpp
@@ -1,16 +1,19 @@
 #pragma once
+#include <iscore_compiler_detection.hpp>
 #include <iscore/tools/ObjectIdentifier.hpp>
 #include <QVector>
 #include <QPointer>
 
+#if ISCORE_COMPILER_CXX_RELAXED_CONSTEXPR
+#define ISCORE_RELAXED_CONSTEXPR constexpr
+#else
+#define ISCORE_RELAXED_CONSTEXPR
+#endif
 #ifdef ISCORE_DEBUG
 template<typename T, typename U>
-constexpr auto checked_cast(U&& other)
+ISCORE_RELAXED_CONSTEXPR auto checked_cast(U&& other)
 {
-    auto e = dynamic_cast<T>(other);
-    Q_ASSERT(e);
-
-    return e;
+    return Q_ASSERT(dynamic_cast<T>(other)), dynamic_cast<T>(other);
 }
 #else
 #define checked_cast static_cast
