207c0a1bc116b0ddbf47c71a797b22d3555cd11e
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-07-11 11:52:23 +0200

    Update deploy.sh

--- tools/travis/deploy.sh
+++ tools/travis/deploy.sh
@@ -1,13 +1,20 @@
 #!/bin/bash
-
+if [[ "$TRAVIS_BRANCH" != "dev" ]];
+then
+    exit 0
+fi
 cd build
 if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
-	LSB_ID=$(cat /etc/lsb-release | grep DISTRIB_ID | awk -F= "{ print $2 }" | tr "\n" " " | sed "s/ //" | cut -d'=' -f2)
-	LSB_VER=$(cat /etc/lsb-release | grep DISTRIB_RELEASE | awk -F= "{ print $2 }" | tr "\n" " " | sed "s/ //" | cut -d'=' -f2)
-	FILENAME=i-score-0.3.0-$LSB_ID-$LSB_VER
-	GOOD_FILENAME=$(find . -maxdepth 1 -type f -name "$FILENAME.*")
-	DISTRO=i-score_ubuntu12.04
+    if [[ -f /etc/lsb-release ]]; then
+        LSB_ID=$(cat /etc/lsb-release | grep DISTRIB_ID | awk -F= "{ print $2 }" | tr "\n" " " | sed "s/ //" | cut -d'=' -f2)
+        LSB_VER=$(cat /etc/lsb-release | grep DISTRIB_RELEASE | awk -F= "{ print $2 }" | tr "\n" " " | sed "s/ //" | cut -d'=' -f2)
+        FILENAME=i-score-0.3.0-$LSB_ID-$LSB_VER
+        GOOD_FILENAME=$(find . -maxdepth 1 -type f -name "$FILENAME.*")
+    else
+        GOOD_FILENAME=i-score-0.3.0-linux-generic.tar.gz
+    fi
 
+    DISTRO=i-score_ubuntu12.04
 else
   zip -r -9 i-score-0.3.0-OSX.zip bundle/i-score.app
 	GOOD_FILENAME=i-score-0.3.0-OSX.zip
