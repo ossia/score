d51e66624b93452c5631942347d3fc2d3d2e80b0
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-07-11 10:29:40 +0200

    Update deploy.sh

--- tools/travis/deploy.sh
+++ tools/travis/deploy.sh
@@ -5,7 +5,7 @@ if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
 	LSB_ID=$(cat /etc/lsb-release | grep DISTRIB_ID | awk -F= "{ print $2 }" | tr "\n" " " | sed "s/ //" | cut -d'=' -f2)
 	LSB_VER=$(cat /etc/lsb-release | grep DISTRIB_RELEASE | awk -F= "{ print $2 }" | tr "\n" " " | sed "s/ //" | cut -d'=' -f2)
 	FILENAME=i-score-0.3.0-$LSB_ID-$LSB_VER
-	GOOD_FILENAME=$(find build -maxdepth 1 -type f -name "$FILENAME.*")
+	GOOD_FILENAME=$(find . -maxdepth 1 -type f -name "$FILENAME.*")
 	DISTRO=i-score_ubuntu12.04
 
 else
