7809652ffb65e3fd74122b62429f3e42ecbdfcb1
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-07-11 10:29:27 +0200

    Update deploy.sh

---
+++ tools/travis/deploy.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+cd build
+if [[ "$TRAVIS_OS_NAME" = "linux" ]]; then
+	LSB_ID=$(cat /etc/lsb-release | grep DISTRIB_ID | awk -F= "{ print $2 }" | tr "\n" " " | sed "s/ //" | cut -d'=' -f2)
+	LSB_VER=$(cat /etc/lsb-release | grep DISTRIB_RELEASE | awk -F= "{ print $2 }" | tr "\n" " " | sed "s/ //" | cut -d'=' -f2)
+	FILENAME=i-score-0.3.0-$LSB_ID-$LSB_VER
+	GOOD_FILENAME=$(find build -maxdepth 1 -type f -name "$FILENAME.*")
+	DISTRO=i-score_ubuntu12.04
+
+else
+  zip -r -9 i-score-0.3.0-OSX.zip bundle/i-score.app
+	GOOD_FILENAME=i-score-0.3.0-OSX.zip
+	DISTRO=i-score_osx
+fi
+curl -T build/i-score/$GOOD_FILENAME -u$BINTRAY_USER:$BINTRAY_API_KEY https://api.bintray.com/content/ossia/i-score/$DISTRO/0.3/$GOOD_FILENAME
+curl -XPOST -u$BINTRAY_USER:$BINTRAY_API_KEY https://api.bintray.com/content/ossia/i-score/$DISTRO/0.3/publish
+curl -XGET https://bintray.com/ossia/i-score/$DISTRO/0.3
