cc0b661ae9d3c00becd52d2b9a2609ab1f62a186
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-07-29 18:42:35 +0200

    minor

---
+++ base/plugins/iscore-plugin-space/Space/computation.hpp
@@ -0,0 +1,10 @@
+#pragma once
+#include <ginac/ginac.h>
+
+namespace spacelib
+{
+class computation
+{
+
+};
+}
--- base/plugins/iscore-plugin-space/src/Area/AreaPresenter.cpp
+++ base/plugins/iscore-plugin-space/src/Area/AreaPresenter.cpp
@@ -11,7 +11,6 @@ AreaPresenter::AreaPresenter(const AreaModel& model,
     m_model{model},
     m_view{new AreaView{parentview}}
 {
-    qDebug() << Q_FUNC_INFO;
     connect(&m_model, &AreaModel::areaChanged,
             this, &AreaPresenter::on_areaChanged);
 
@@ -32,7 +31,6 @@ void AreaPresenter::update()
 // Il vaut mieux faire comme dans les courbes ou le curvepresenter s'occupe des segments....
 void AreaPresenter::on_areaChanged()
 {
-    qDebug() << Q_FUNC_INFO;
     spacelib::square_renderer<QPointF, RectDevice> renderer;
     renderer.size = {800, 600};
     renderer.render(m_model.area(), m_model.space().space());
