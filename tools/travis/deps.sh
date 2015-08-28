b8545680f9ee97de26405dac2d3ed55dbdcad10e
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-08-28 10:51:25 +0200

    Update deps.sh

--- tools/travis/deps.sh
+++ tools/travis/deps.sh
@@ -19,7 +19,10 @@ case "$TRAVIS_OS_NAME" in
 
 	;;
     osx)
-	brew update; brew update # work around a homebrew bug
+        # work around a homebrew bug
+        set +e
+	brew update; brew update 
+	set -e
         brew install wget
         wget https://www.dropbox.com/s/n3dsifakgzjbsnh/Jamoma-Darwin20150828.zip?dl=0 -O JamomaDarwin20150828.zip
         unzip JamomaDarwin20150828.zip
