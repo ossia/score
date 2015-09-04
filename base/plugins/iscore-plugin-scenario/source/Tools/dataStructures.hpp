81cc84f4fd9a27ed01760a099408b5df0d94e58b
simon j <jamain.simon@gmail.com>
2015-09-04 10:41:08 +0200

    ElementsProperties data struct added to carry date changes

---
+++ base/plugins/iscore-plugin-scenario/source/Tools/dataStructures.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+/*
+This file is used to define simple data structure to simplify the code when needed
+*/
+
+#include <QVector>
+#include <iscore/tools/SettableIdentifier.hpp>
+#include <ProcessInterface/TimeValue.hpp>
+
+struct TimenodeProperties {
+    Id<TimeNodeModel> id;
+    TimeValue oldDate;
+    TimeValue newDate;
+};
+
+struct ConstraintProperties {
+    Id<ConstraintModel> id;
+    TimeValue oldMin;
+    TimeValue newMin;
+    TimeValue oldMax;
+    TimeValue newMax;
+};
+
+struct ElementsProperties {
+    QVector<TimenodeProperties> timenodes;
+    QVector<ConstraintProperties> contraints;
+};
