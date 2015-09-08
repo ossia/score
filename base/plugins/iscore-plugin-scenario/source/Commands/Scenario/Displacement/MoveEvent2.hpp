0c1e00dd78a84bb65344114f849fa4adb3ed56b7
simon j <jamain.simon@gmail.com>
2015-09-08 17:29:01 +0200

    - moveevent command serialization fixed

--- base/plugins/iscore-plugin-scenario/source/Commands/Scenario/Displacement/MoveEvent2.hpp
+++ base/plugins/iscore-plugin-scenario/source/Commands/Scenario/Displacement/MoveEvent2.hpp
@@ -65,6 +65,7 @@ namespace Scenario
                 m_path {std::move(scenarioPath)},
                 m_mode{mode}
                 {
+                    m_eventId = eventId;
                     m_initialDate = newDate;
                     m_oldDate = m_initialDate;
 
@@ -98,120 +99,36 @@ namespace Scenario
                 void
                 undo() override
                 {
+                    auto& scenario = m_path.find();
+
                     m_oldDate = m_initialDate;
 
                     bool useNewValues{false};
+
                     DisplacementPolicy::updatePositions(
-                                m_path.find(),
+                                scenario,
                                 [&] (Process& p, const TimeValue& t){ p.expandProcess(m_mode, t); },
                                 m_savedElementsProperties,
                                 useNewValues);
-                    /*
-                    auto& scenar = m_path.find();
-                    auto& event = scenar.event(m_eventId);
 
-                    TimeValue deltaDate{};
-                    deltaDate = m_oldDate - event.date();
-
-                    StandardDisplacementPolicy::updatePositions(
-                                scenar,
-                                m_movableTimenodes,
-                                m_oldDate - event.date(),
-                                [&] (Process& , const TimeValue& ) { });
-
-                    // Now we have to restore the state of each constraint that might have been modified
-                    // during this command.
-                    for(auto& obj : m_savedConstraints)
-                    {
-                        // 1. Clear the constraint
-                        // TODO Don't use a command since it serializes a ton of unused stuff.
-                        ClearConstraint clear_cmd{Path<ConstraintModel>{obj.first.first}};
-                        clear_cmd.redo();
-
-                        auto& constraint = obj.first.first.find();
-                        // 2. Restore the rackes & processes.
-
-                        // TODO if possible refactor this with ReplaceConstraintContent and ConstraintModel::clone
-                        // Be careful however, the code differs in subtle ways
-                        {
-                            ConstraintModel src_constraint{
-                                    Deserializer<DataStream>{obj.first.second},
-                                    &constraint}; // Temporary parent
-
-                            std::map<const Process*, Process*> processPairs;
-
-                            // Clone the processes
-                            for(const auto& sourceproc : src_constraint.processes)
-                            {
-                                auto newproc = sourceproc.clone(sourceproc.id(), &constraint);
-
-                                processPairs.insert(std::make_pair(&sourceproc, newproc));
-                                constraint.processes.add(newproc);
-                            }
-
-                            // Clone the rackes
-                            for(const auto& sourcerack : src_constraint.racks)
-                            {
-                                // A note about what happens here :
-                                // Since we want to duplicate our process view models using
-                                // the target constraint's cloned shared processes (they might setup some specific data),
-                                // we maintain a pair mapping each original process to their cloned counterpart.
-                                // We can then use the correct cloned process to clone the process view model.
-                                auto newrack = new RackModel{
-                                        sourcerack,
-                                        sourcerack.id(),
-                                        [&] (const SlotModel& source, SlotModel& target)
-                                        {
-                                            for(const auto& lm : source.layers)
-                                            {
-                                                // We can safely reuse the same id since it's in a different slot.
-                                                Process* proc = processPairs[&lm.processModel()];
-                                                // TODO harmonize the order of parameters (source first, then new id)
-                                                target.layers.add(proc->cloneLayer(lm.id(), lm, &target));
-                                            }
-                                        },
-                                        &constraint};
-                                constraint.racks.add(newrack);
-                            }
-                        }
-
-                        // 3. Restore the correct rackes in the constraint view models
-                        for(auto& viewmodel : constraint.viewModels())
-                        {
-                            viewmodel->showRack(obj.second[viewmodel->id()]);
-                        }
-                    }
-                    */
-                    //updateEventExtent(m_eventId, scenar);
+                    updateEventExtent(m_eventId, scenario);
 
                 }
                 virtual
                 void
                 redo() override
                 {
-
+                    auto& scenario = m_path.find();
 
                     bool useNewValues{true};
+
                     DisplacementPolicy::updatePositions(
-                                m_path.find(),
+                                scenario,
                                 [&] (Process& p, const TimeValue& t){ p.expandProcess(m_mode, t); },
                                 m_savedElementsProperties,
                                 useNewValues);
-                    /*
-                    auto& scenar = m_path.find();
-                    auto& event = scenar.event(m_eventId);
 
-                    TimeValue deltaDate{};
-                    deltaDate = m_newDate - event.date();
-
-                    StandardDisplacementPolicy::updatePositions(
-                                scenar,
-                                m_movableTimenodes,
-                                deltaDate,
-                                [&] (Process& p, const TimeValue& t)
-                    { p.expandProcess(m_mode, t); });
-                    */
-                    //updateEventExtent(m_eventId, scenar);
+                    updateEventExtent(m_eventId, scenario);
                 }
 
 
@@ -225,10 +142,12 @@ namespace Scenario
                 void
                 serializeImpl(QDataStream& s) const override
                 {
-                    s << m_path
-                      << m_savedElementsProperties
-                      << (int)m_mode
-                      << m_savedConstraints;
+                    s << m_savedElementsProperties
+                      << m_path
+                      << m_eventId
+                      << m_initialDate
+                      << m_oldDate
+                      << (int)m_mode;
                 }
 
                 virtual
@@ -236,37 +155,26 @@ namespace Scenario
                 deserializeImpl(QDataStream& s) override
                 {
                     int mode;
-                    s >> m_path
-                      >> m_savedElementsProperties
-                      >> mode
-                      >> m_savedConstraints;
+                    s >> m_savedElementsProperties
+                      >> m_path
+                      >> m_eventId
+                      >> m_initialDate
+                      >> m_oldDate
+                      >> mode;
 
-//                    m_mode = static_cast<ExpandMode>(mode);
+                      m_mode = static_cast<ExpandMode>(mode);
                 }
 
                 private:
             private:
-                DisplacementPolicy m_displacementPolicy;
                 ElementsProperties m_savedElementsProperties;
                 Path<ScenarioModel> m_path;
 
+                Id<EventModel> m_eventId;
                 TimeValue m_oldDate; //used to compute the deltaTime
                 TimeValue m_initialDate; //used to compute the deltaTime and respect undo behavior
 
                 ExpandMode m_mode{ExpandMode::Scale};
-
-                QVector<
-                    QPair<
-                        QPair<
-                            Path<ConstraintModel>,
-                            QByteArray
-                        >, // The constraint data
-                        QMap< // Mapping for the view models of this constraint
-                            Id<ConstraintViewModel>,
-                            Id<RackModel>
-                        >
-                     >
-                > m_savedConstraints;
         };
 
     }
