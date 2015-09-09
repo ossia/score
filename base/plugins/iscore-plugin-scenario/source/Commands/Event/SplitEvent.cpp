5e52e31aa2c6431d38a6a1d581c061db2dc7bc1a
Nicolas <nicolas.vuaille@gmail.com>
2015-09-09 10:13:13 +0200

    Start working on splitEvent

---
+++ base/plugins/iscore-plugin-scenario/source/Commands/Event/SplitEvent.cpp
@@ -0,0 +1,24 @@
+#include "SplitEvent.hpp"
+
+using namespace Scenario::Command;
+
+
+void SplitEvent::undo()
+{
+
+}
+
+void SplitEvent::redo()
+{
+
+}
+
+void SplitEvent::serializeImpl(QDataStream &) const
+{
+
+}
+
+void SplitEvent::deserializeImpl(QDataStream &)
+{
+
+}
