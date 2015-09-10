a20ec29fe8ec43796d8f607525613940f7e1d9f0
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-09-10 17:14:45 +0200

    Fix a crash on undo of CreateStateMacro

--- base/plugins/iscore-plugin-scenario/source/Commands/Scenario/Creations/CreateStateMacro.hpp
+++ base/plugins/iscore-plugin-scenario/source/Commands/Scenario/Creations/CreateStateMacro.hpp
@@ -3,17 +3,30 @@
 
 namespace Scenario
 {
-    namespace Command
-    {
-        class CreateStateMacro : public iscore::AggregateCommand
+namespace Command
+{
+/**
+ * @brief The CreateStateMacro class
+ * 
+ * Used to quickly create a state from data coming from outside.
+ * For instance creating a StateModel and adding data inside.
+ * 
+ */
+class CreateStateMacro : public iscore::AggregateCommand
+{
+        ISCORE_COMMAND_DECL_OBSOLETE("CreateStateMacro", "CreateStateMacro")
+    public:
+        CreateStateMacro():
+            AggregateCommand{"ScenarioControl",
+                             commandName(),
+                             description()} { }
+        
+        void undo() override
         {
-                ISCORE_COMMAND_DECL_OBSOLETE("CreateStateMacro", "CreateStateMacro")
-            public:
-                CreateStateMacro():
-                      AggregateCommand{"ScenarioControl",
-                                       commandName(),
-                                       description()} { }
-
-        };
-    }
+            // We only need to undo the creation of the StateModel.
+            m_cmds[0]->undo();
+        }
+        
+};
+}
 }
