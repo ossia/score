1a9ebabab15d051876b2de539340fb919ca3bcf4
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-09-11 13:58:20 +0200

    Stability of address bar

--- base/plugins/iscore-plugin-scenario/source/Document/Constraint/ViewModels/FullView/FullViewConstraintHeader.cpp
+++ base/plugins/iscore-plugin-scenario/source/Document/Constraint/ViewModels/FullView/FullViewConstraintHeader.cpp
@@ -24,39 +24,39 @@ QRectF FullViewConstraintHeader::boundingRect() const
 
 void FullViewConstraintHeader::paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget)
 {
-    int textWidth = m_bar->width();
+    double textWidth = m_bar->width();
 
     // If the centered text is hidden, we put it at the left so that it's on the view.
     // We have to compute the visible part of the header
     auto view = scene()->views().first();
-    int text_left = view->mapFromScene(mapToScene({m_width / 2. - textWidth / 2., 0})).x();
-    int text_right = view->mapFromScene(mapToScene({m_width / 2. + textWidth / 2., 0})).x();
+
+    // Note: if the constraint always has its pos() in (0; 0), we can
+    // safely remove the call to mapToScene.
+    double text_left = view->mapFromScene(mapToScene({m_width / 2. - textWidth / 2., 0})).x();
+    double text_right = view->mapFromScene(mapToScene({m_width / 2. + textWidth / 2., 0})).x();
     double x = 0;
     double min_x = 10;
     double max_x = view->width() - 30;
+
     if(text_left > min_x && text_right < max_x)
     {
         x = m_width / 2. - textWidth / 2.;
     }
-    // TODO both false ??
     else if(text_left < min_x)
     {
         // Compute the pixels needed to add to have top-left at 0
-        x = m_width / 2. - textWidth / 2. - text_left + min_x;
+        x = (m_width - textWidth) / 2. - text_left + min_x;
     }
     else if(text_right > max_x)
     {
         // Compute the pixels needed to add to have top-right at max
-        x = m_width / 2. - textWidth / 2. - text_right + max_x;
+        x = (m_width - textWidth) / 2. - text_right + max_x;
+    }
+    else
+    {
+        return;
     }
 
-    m_bar->setPos(x, 5);
-
-    /*
-    x = std::max(x, 10.);
-    double y = 2.5;
-    double w = m_width - x;
-    double h = ConstraintHeader::headerHeight();
-    painter->drawText(x,y,w,h, Qt::AlignLeft, m_text);
-    */
+    if(std::abs(m_bar->pos().x() - x) > 1)
+        m_bar->setPos(x, 5);
 }
