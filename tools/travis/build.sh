4c3e450e28e16e01f212937cfba441e204b703ac
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-09-27 17:27:55 +0200

    Try to use coverity on master

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -8,21 +8,29 @@ cd build
 export CMAKE_COMMON_FLAGS="-DCMAKE_BUILD_TYPE=$BUILD_TYPE -DISCORE_STATIC_PLUGINS:Bool=$STATIC_PLUGINS -DDEPLOYMENT_BUILD:Bool=$DEPLOYMENT"
 export CTEST_OUTPUT_ON_FAILURE=1
 
-case "$TRAVIS_OS_NAME" in
-  linux)
-    source /opt/qt55/bin/qt55-env.sh
-    /usr/local/bin/cmake -DISCORE_COTIRE:Bool=False $CMAKE_COMMON_FLAGS ..
-
-    if [[ "$DEPLOYMENT_BUILD" = "True" ]];
-    then
-      make package -j2
-    else
-      make -j2
-    fi
-  ;;
-  osx)
-    cmake -DCMAKE_PREFIX_PATH="/usr/local/Cellar/qt5/5.5.0/lib/cmake;$(pwd)/../Jamoma/share/cmake" -DCMAKE_INSTALL_PREFIX=$(pwd)/bundle $CMAKE_COMMON_FLAGS ..
-
-    make install/strip -j2
-  ;;
-esac
+if [[ "$USE_COVERITY" = "False" ]];
+then
+  case "$TRAVIS_OS_NAME" in
+    linux)
+      source /opt/qt55/bin/qt55-env.sh
+      /usr/local/bin/cmake -DISCORE_COTIRE:Bool=False $CMAKE_COMMON_FLAGS ..
+  
+      if [[ "$DEPLOYMENT_BUILD" = "True" ]];
+      then
+        make package -j2
+      else
+        make -j2
+      fi
+    ;;
+    osx)
+      cmake -DCMAKE_PREFIX_PATH="/usr/local/Cellar/qt5/5.5.0/lib/cmake;$(pwd)/../Jamoma/share/cmake" -DCMAKE_INSTALL_PREFIX=$(pwd)/bundle $CMAKE_COMMON_FLAGS ..
+  
+      make install/strip -j2
+    ;;
+  esac
+else
+  source /opt/qt55/bin/qt55-env.sh
+  /usr/local/bin/cmake -DISCORE_COTIRE:Bool=False $CMAKE_COMMON_FLAGS ..
+  
+  eval "$COVERITY_SCAN_BUILD"
+fi
