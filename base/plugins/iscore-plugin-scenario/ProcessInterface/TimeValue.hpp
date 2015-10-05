4025b379fa8e9f2b5929712888e8dbb5d07409b6
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-10-05 09:54:56 +0200

    Try to fix Windows build

--- base/plugins/iscore-plugin-scenario/ProcessInterface/TimeValue.hpp
+++ base/plugins/iscore-plugin-scenario/ProcessInterface/TimeValue.hpp
@@ -29,10 +29,9 @@ class TimeValue_T
             m_impl {0}
         { }
 
-        constexpr TimeValue_T(QTime t)
-        {
-            m_impl = t.msec() + 1000 * t.second() + 60000 * t.minute() + 3600000 * t.hour();
-        }
+        TimeValue_T(QTime t):
+            m_impl{t.msec() + 1000 * t.second() + 60000 * t.minute() + 3600000 * t.hour()}
+        { }
 
 
         // These two overloads are here to please coverity...
