42c635db3fe372bc9878b9377f738f9c0dcd7a95
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2015-12-08 13:44:50 +0100

    remove unused files

--- base/plugins/iscore-plugin-cohesion/Commands/CreateCurvesFromAddressesInConstraints.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include <iscore/command/AggregateCommand.hpp>
-#include <Commands/IScoreCohesionCommandFactory.hpp>
-
-class CreateCurvesFromAddressesInConstraints final : public iscore::AggregateCommand
-{
-        ISCORE_COMMAND_DECL(IScoreCohesionCommandFactoryName(), CreateCurvesFromAddressesInConstraints, "CreateCurvesFromAddressesInConstraints")
-};
