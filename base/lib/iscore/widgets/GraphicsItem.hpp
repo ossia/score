f79543ed4153395aff7f63ee29a4d5ee61fe1aac
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-01-03 16:51:08 +0100

    undo works correctly for AddArea

--- base/lib/iscore/widgets/GraphicsItem.hpp
+++ base/lib/iscore/widgets/GraphicsItem.hpp
@@ -1,6 +1,7 @@
 #pragma once
 #include <iscore_lib_base_export.h>
 class QGraphicsObject;
+class QGraphicsItem;
 /**
  * @brief deleteGraphicsItem Properly delete a QGraphicsObject
  * @param item item to delete
@@ -9,3 +10,4 @@ class QGraphicsObject;
  * item has to be removed from the scene else there will be crashes.
  */
 ISCORE_LIB_BASE_EXPORT void deleteGraphicsObject(QGraphicsObject* item);
+ISCORE_LIB_BASE_EXPORT void deleteGraphicsItem(QGraphicsItem* item);
