eef4dd9e6508ca04b14a995c60645d8e7ccb453c
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-01-12 23:17:59 +0100

    try to fix appveyor

--- CMake/Configurations/developer.cmake
+++ CMake/Configurations/developer.cmake
@@ -1,4 +1,4 @@
 set(CMAKE_CXX_COMPILER /usr/bin/clang++)
-set(CMAKE_PREFIX_PATH /usr/local/jamoma/share/Cmake/Jamoma)
+set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH};/usr/local/jamoma/share/Cmake/Jamoma")
 include(travis/debug)
 include(all-plugins)
