6e6452005c89252dafae076488bf2d7e81070456
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-01-13 13:52:03 +0100

    try to fix osx build

--- tools/travis/configs/osx-source-qt.sh
+++ tools/travis/configs/osx-source-qt.sh
@@ -2,4 +2,5 @@
 
 export QT_PATH=$(dirname $(dirname $(find /usr/local/Cellar/qt5 -name Qt5Config.cmake) ) )
 export CXX=clang++
-export CMAKE_COMMON_FLAGS="$CMAKE_COMMON_FLAGS -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_PREFIX_PATH=\"$QT_PATH;$(pwd)/../../Jamoma/share/cmake\" -DCMAKE_INSTALL_PREFIX=$(pwd)/bundle"
+export CMAKE_PREFIX_PATH="$QT_PATH;$(pwd)/../../Jamoma/share/cmake;$(pwd)/../../../Jamoma/share/cmake"
+export CMAKE_COMMON_FLAGS="$CMAKE_COMMON_FLAGS -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH -DCMAKE_INSTALL_PREFIX=$(pwd)/bundle"
