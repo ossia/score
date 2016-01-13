5e185ca130cdd3f4e515bfdbde7cfc9ae454c97c
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-01-13 14:01:46 +0100

    second osx fix

--- tools/travis/configs/osx-source-qt.sh
+++ tools/travis/configs/osx-source-qt.sh
@@ -2,5 +2,5 @@
 
 export QT_PATH=$(dirname $(dirname $(find /usr/local/Cellar/qt5 -name Qt5Config.cmake) ) )
 export CXX=clang++
-export CMAKE_PREFIX_PATH="$QT_PATH;$(pwd)/../../Jamoma/share/cmake;$(pwd)/../../../Jamoma/share/cmake"
+export CMAKE_PREFIX_PATH="$QT_PATH;$(pwd)/../Jamoma/share/cmake"
 export CMAKE_COMMON_FLAGS="$CMAKE_COMMON_FLAGS -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH -DCMAKE_INSTALL_PREFIX=$(pwd)/bundle"
