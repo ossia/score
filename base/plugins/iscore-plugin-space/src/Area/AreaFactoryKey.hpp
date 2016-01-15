1b15a9708bd9ff02fa37ee37843759871adfff98
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-01-15 10:18:36 +0100

    Work on add-on-ification of i-score

--- base/plugins/iscore-plugin-space/src/Area/AreaFactoryKey.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <QMetaType>
-#include <iscore/plugins/customfactory/StringFactoryKey.hpp>
-namespace Space
-{
-class AreaTag{};
-using AreaFactoryKey = StringKey<AreaTag>;
-}
-Q_DECLARE_METATYPE(Space::AreaFactoryKey)
--- base/plugins/iscore-plugin-space/src/Commands/SpaceCommandFactory.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include <iscore/command/SerializableCommand.hpp>
-
-namespace Space
-{
-const CommandParentFactoryKey& CommandFactoryName();
-}
