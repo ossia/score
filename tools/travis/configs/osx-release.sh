c99301b9940e39acaba8ed7bfed3a2a67a64b649
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-01-17 00:29:45 +0100

    fix osx

--- tools/travis/configs/osx-release.sh
+++ tools/travis/configs/osx-release.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 source "$CONFIG_FOLDER/osx-source-qt.sh"
 
-$CMAKE_BIN -GNinja -DISCORE_CONFIGURATION=release ..
+$CMAKE_BIN -GNinja $CMAKE_COMMON_FLAGS -DISCORE_CONFIGURATION=release ..
 $CMAKE_BIN --build . --target install/strip --config DynamicRelease
--- tools/travis/configs/osx-source-qt.sh
+++ tools/travis/configs/osx-source-qt.sh
@@ -3,4 +3,4 @@
 export QT_PATH=$(dirname $(dirname $(find /usr/local/Cellar/qt5 -name Qt5Config.cmake) ) )
 export CXX=clang++
 export CMAKE_PREFIX_PATH="$QT_PATH;$(pwd)/../Jamoma/share/cmake"
-export CMAKE_COMMON_FLAGS="$CMAKE_COMMON_FLAGS -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH -DCMAKE_INSTALL_PREFIX=$(pwd)/bundle"
+export CMAKE_COMMON_FLAGS="-DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH -DCMAKE_INSTALL_PREFIX=$(pwd)/bundle"
