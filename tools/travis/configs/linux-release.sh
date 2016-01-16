68d9845a6cae8e6cbf4eef0208058ada3e11e3cd
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-01-16 23:50:43 +0100

    try to re-enable unity

--- tools/travis/configs/linux-release.sh
+++ tools/travis/configs/linux-release.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 source "$CONFIG_FOLDER/linux-source-qt.sh"
 
-$CMAKE_BIN $CMAKE_COMMON_FLAGS -DISCORE_CONFIGURATION=release ..
+$CMAKE_BIN -GNinja -DISCORE_CONFIGURATION=release ..
 
 $CMAKE_BIN --build . --target package --config DynamicRelease
