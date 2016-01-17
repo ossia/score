d724d6f11b47636339d5a65c6b9df3b71cbf0539
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-01-17 12:28:39 +0100

    disable unity in developer mode

--- CMake/Configurations/developer.cmake
+++ CMake/Configurations/developer.cmake
@@ -1,4 +1,6 @@
 set(CMAKE_CXX_COMPILER /usr/bin/clang++)
 set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH};/usr/local/jamoma/share/Cmake/Jamoma")
+set(ISCORE_COTIRE_DISABLE_UNITY True)
+
 include(travis/debug)
 include(all-plugins)
