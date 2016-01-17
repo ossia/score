e67a63f0d632d0e2ce169bc310ef9383b18cc71a
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-01-17 15:46:21 +0100

    this is it..?

--- tools/travis/configs/linux-release.sh
+++ tools/travis/configs/linux-release.sh
@@ -1,8 +1,7 @@
 #!/bin/sh
 source "$CONFIG_FOLDER/linux-source-qt.sh"
 
-$CMAKE_BIN -GNinja \
-           -DISCORE_COTIRE_DISABLE_UNITY:Bool=True \
-           -DISCORE_CONFIGURATION=release ..
+$CMAKE_BIN -DISCORE_CONFIGURATION=release ..
 
+$CMAKE_BIN --build . --target all_unity
 $CMAKE_BIN --build . --target package
--- tools/travis/configs/osx-release.sh
+++ tools/travis/configs/osx-release.sh
@@ -3,4 +3,4 @@ source "$CONFIG_FOLDER/osx-source-qt.sh"
 
 $CMAKE_BIN $CMAKE_COMMON_FLAGS -DISCORE_CONFIGURATION=release ..
 $CMAKE_BIN --build . --target all_unity -- -j2
-$CMAKE_BIN --build . --target install/fast
+$CMAKE_BIN --build . --target install/strip/fast
