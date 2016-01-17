3fb23bde36387f696962c9788eb563d916900511
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-01-17 09:48:06 +0100

    dynamic osx unity

--- tools/travis/configs/osx-release.sh
+++ tools/travis/configs/osx-release.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 source "$CONFIG_FOLDER/osx-source-qt.sh"
 
-$CMAKE_BIN -GNinja $CMAKE_COMMON_FLAGS -DISCORE_CONFIGURATION=release ..
-$CMAKE_BIN --build . --target install/strip --config DynamicRelease
+$CMAKE_BIN $CMAKE_COMMON_FLAGS -DISCORE_CONFIGURATION=release ..
+$CMAKE_BIN --build . --target all_unity -- -j2
+$CMAKE_BIN --build . --target install/fast --config DynamicRelease
