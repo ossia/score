2dbaf9c34c24df394d2f48c83c57b9e1e09d720a
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-01-17 15:10:59 +0100

    no unity for linux packages

--- tools/travis/configs/linux-package.sh
+++ tools/travis/configs/linux-package.sh
@@ -1,4 +1,7 @@
 #!/bin/sh
 
-$CMAKE_BIN -GNinja -DCMAKE_PREFIX_PATH="/usr/local/jamoma/share/cmake/Jamoma;/opt/qt5-static/lib/cmake/Qt5" -DISCORE_CONFIGURATION=staticqt-release ..
-$CMAKE_BIN --build . --target package --config StaticRelease
+$CMAKE_BIN -GNinja \
+           -DISCORE_COTIRE_DISABLE_UNITY:Bool=True \
+           -DCMAKE_PREFIX_PATH="/usr/local/jamoma/share/cmake/Jamoma;/opt/qt5-static/lib/cmake/Qt5" -DISCORE_CONFIGURATION=staticqt-release ..
+
+$CMAKE_BIN --build . --target package
