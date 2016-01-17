684105bd6ab8cc0c087ca9903054c520b9cdc108
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-01-17 15:56:03 +0100

    use j2

--- tools/travis/configs/linux-debug.sh
+++ tools/travis/configs/linux-debug.sh
@@ -2,4 +2,4 @@
 source "$CONFIG_FOLDER/linux-source-qt.sh"
 
 $CMAKE_BIN -DISCORE_CONFIGURATION=debug ..
-$CMAKE_BIN --build . --target all_unity
+$CMAKE_BIN --build . --target all_unity -- -j2
--- tools/travis/configs/linux-release.sh
+++ tools/travis/configs/linux-release.sh
@@ -3,5 +3,5 @@ source "$CONFIG_FOLDER/linux-source-qt.sh"
 
 $CMAKE_BIN -DISCORE_CONFIGURATION=release ..
 
-$CMAKE_BIN --build . --target all_unity
-$CMAKE_BIN --build . --target package
+$CMAKE_BIN --build . --target all_unity -- -j2
+$CMAKE_BIN --build . --target package -- -j2
