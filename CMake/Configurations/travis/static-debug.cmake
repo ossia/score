40f512f1fd3069f6b2319b974ae7303e816eb3a2
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-01-17 17:04:07 +0100

    merge components

--- CMake/Configurations/travis/static-debug.cmake
+++ CMake/Configurations/travis/static-debug.cmake
@@ -1,3 +1,2 @@
 include(debug)
-set(ISCORE_MERGE_COMPONENTS False)
 set(ISCORE_STATIC_PLUGINS True)
--- CMake/Configurations/travis/static-release.cmake
+++ CMake/Configurations/travis/static-release.cmake
@@ -1,3 +1,2 @@
 include(release)
-set(ISCORE_MERGE_COMPONENTS False)
 set(ISCORE_STATIC_PLUGINS True)
