9184af0bb8574ea084c61a8986ab60182d21e067
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-01-17 15:46:21 +0100

    this is it..?

--- tools/travis/configs/linux-package.sh
+++ tools/travis/configs/linux-package.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
-$CMAKE_BIN -GNinja \
-           -DISCORE_COTIRE_DISABLE_UNITY:Bool=True \
-           -DCMAKE_PREFIX_PATH="/usr/local/jamoma/share/cmake/Jamoma;/opt/qt5-static/lib/cmake/Qt5" -DISCORE_CONFIGURATION=staticqt-release ..
+$CMAKE_BIN -DCMAKE_PREFIX_PATH="/usr/local/jamoma/share/cmake/Jamoma;/opt/qt5-static/lib/cmake/Qt5" -DISCORE_CONFIGURATION=staticqt-release ..
 
+$CMAKE_BIN --build . --target all_unity
 $CMAKE_BIN --build . --target package
