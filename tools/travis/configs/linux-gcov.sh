ffcb193323476c21de722cb32b28309e6883f1a3
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-01-28 23:03:09 +0100

    try to fix coverage

--- tools/travis/configs/linux-gcov.sh
+++ tools/travis/configs/linux-gcov.sh
@@ -10,6 +10,7 @@ export DISPLAY=:99.0
 sh -e /etc/init.d/xvfb start
 sleep 3
 cp -rf ../Tests/testdata .
-$CMAKE_BIN --build . --target iscore_test_coverage
-lcov --compat-libtool --directory .. --capture --output-file coverage.info --no-external
+$CMAKE_BIN --build . --target iscore_test_coverage_unity
+# lcov --compat-libtool --directory .. --capture --output-file coverage.info --no-external
+mv coverage.info.cleaned coverage.info
 coveralls-lcov --repo-token jjoMcOyOg9R05XT3aVysqTcsL1gyAc9tF coverage.info
