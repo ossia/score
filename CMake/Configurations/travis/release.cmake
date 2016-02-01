12c1ff52376f113e188871b8f63f00ad8094a844
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-02-01 11:11:45 +0100

    allow i-score to work on older versions

--- CMake/Configurations/travis/release.cmake
+++ CMake/Configurations/travis/release.cmake
@@ -2,5 +2,5 @@ set(CMAKE_BUILD_TYPE Release)
 set(ISCORE_COTIRE True)
 set(DEPLOYMENT_BUILD True)
 set(ISCORE_MERGE_COMPONENTS True)
-
+set(CMAKE_OSX_DEPLOYMENT_TARGET 10.9)
 include(default-plugins)
