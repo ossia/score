ef5ef36e2106e2816089e4b681f8251526964644
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-02-19 09:53:32 +0100

    use clang for C compiler

--- CMake/Configurations/developer.cmake
+++ CMake/Configurations/developer.cmake
@@ -1,3 +1,4 @@
+set(CMAKE_C_COMPILER /usr/bin/clang)
 set(CMAKE_CXX_COMPILER /usr/bin/clang++)
 set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH};/usr/jamoma/share/cmake/Jamoma")
 set(ISCORE_COTIRE_DISABLE_UNITY True)
