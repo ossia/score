353e0e2a35d1bdb0397835f2d45e218e3b70ac66
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-02-24 11:07:44 +0100

    Fix a potential crash

--- base/plugins/iscore-plugin-ossia/OSSIA/iscore2OSSIA.cpp
+++ base/plugins/iscore-plugin-ossia/OSSIA/iscore2OSSIA.cpp
@@ -49,7 +49,7 @@
 
 class NodeNotFoundException : public std::exception
 {
-        const State::Address& m_addr;
+        State::Address m_addr;
     public:
         NodeNotFoundException(const State::Address& n):
             m_addr{n}
