732d3c406c38265838977434ddee988d57fff691
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-02-29 18:42:39 +0100

    work on skinning

--- base/plugins/iscore-plugin-curve/Curve/CurveStyle.hpp
+++ base/plugins/iscore-plugin-curve/Curve/CurveStyle.hpp
@@ -4,11 +4,11 @@ namespace Curve
 {
 struct Style
 {
-        QColor Point{128, 215, 62};
-        QColor PointSelected{233, 208, 89};
+        QColor Point{128, 215, 62}; // Tender3
+        QColor PointSelected{233, 208, 89}; // Emphasis2
 
-        QColor Segment{199, 31, 44};
-        QColor SegmentSelected{216, 178, 24};
-        QColor SegmentDisabled{127, 127, 127};
+        QColor Segment{199, 31, 44}; // Tender1
+        QColor SegmentSelected{216, 178, 24}; // Tender2
+        QColor SegmentDisabled{127, 127, 127}; // Gray
 };
 }
