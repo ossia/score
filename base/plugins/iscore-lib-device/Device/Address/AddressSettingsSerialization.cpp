c5010057baba2c6fb52816e24c422ea1becefa41
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-03-21 18:59:54 +0100

    serialization

--- base/plugins/iscore-lib-device/Device/Address/AddressSettingsSerialization.cpp
+++ base/plugins/iscore-lib-device/Device/Address/AddressSettingsSerialization.cpp
@@ -51,6 +51,55 @@ void Visitor<Writer<DataStream>>::writeTo(Device::AddressSettingsCommon& n)
 
 
 template<>
+void Visitor<Reader<JSONObject>>::readFrom(const Device::AddressSettingsCommon& n)
+{
+    // Metadata
+    m_obj[iscore::StringConstant().ioType] = Device::IOTypeStringMap()[n.ioType];
+    m_obj[iscore::StringConstant().ClipMode] = Device::ClipModeStringMap()[n.clipMode];
+    m_obj[iscore::StringConstant().Unit] = n.unit;
+
+    m_obj[iscore::StringConstant().RepetitionFilter] = n.repetitionFilter;
+    m_obj[iscore::StringConstant().RefreshRate] = n.rate;
+
+    m_obj[iscore::StringConstant().Priority] = n.priority;
+
+    QJsonArray arr;
+    for(auto& str : n.tags)
+        arr.append(str);
+    m_obj[iscore::StringConstant().Tags] = arr;
+
+    // Value, domain and type
+    readFrom(n.value);
+    m_obj[iscore::StringConstant().Domain] = DomainToJson(n.domain);
+}
+
+
+template<>
+void Visitor<Writer<JSONObject>>::writeTo(Device::AddressSettingsCommon& n)
+{
+    n.ioType = Device::IOTypeStringMap().key(m_obj[iscore::StringConstant().ioType].toString());
+    n.clipMode = Device::ClipModeStringMap().key(m_obj[iscore::StringConstant().ClipMode].toString());
+    n.unit = m_obj[iscore::StringConstant().Unit].toString();
+
+    n.repetitionFilter = m_obj[iscore::StringConstant().RepetitionFilter].toBool();
+    n.rate = m_obj[iscore::StringConstant().RefreshRate].toInt();
+
+    n.priority = m_obj[iscore::StringConstant().Priority].toInt();
+
+    auto arr = m_obj[iscore::StringConstant().Tags].toArray();
+    for(auto&& elt : arr)
+        n.tags.append(elt.toString());
+
+    writeTo(n.value);
+    // TODO doesn't handle multi-type variants.
+    if(m_obj.contains(iscore::StringConstant().Type))
+    {
+        n.domain = Device::JsonToDomain(m_obj[iscore::StringConstant().Domain].toObject(), m_obj[iscore::StringConstant().Type].toString());
+    }
+}
+
+
+template<>
 ISCORE_LIB_DEVICE_EXPORT void Visitor<Reader<DataStream>>::readFrom(const Device::AddressSettings& n)
 {
     readFrom(static_cast<const Device::AddressSettingsCommon&>(n));
@@ -86,50 +135,27 @@ ISCORE_LIB_DEVICE_EXPORT void Visitor<Writer<DataStream>>::writeTo(Device::FullA
 template<>
 ISCORE_LIB_DEVICE_EXPORT void Visitor<Reader<JSONObject>>::readFrom(const Device::AddressSettings& n)
 {
+    readFrom(static_cast<const Device::AddressSettingsCommon&>(n));
     m_obj[iscore::StringConstant().Name] = n.name;
-
-    // Metadata
-    m_obj[iscore::StringConstant().ioType] = Device::IOTypeStringMap()[n.ioType];
-    m_obj[iscore::StringConstant().ClipMode] = Device::ClipModeStringMap()[n.clipMode];
-    m_obj[iscore::StringConstant().Unit] = n.unit;
-
-    m_obj[iscore::StringConstant().RepetitionFilter] = n.repetitionFilter;
-    m_obj[iscore::StringConstant().RefreshRate] = n.rate;
-
-    m_obj[iscore::StringConstant().Priority] = n.priority;
-
-    QJsonArray arr;
-    for(auto& str : n.tags)
-        arr.append(str);
-    m_obj[iscore::StringConstant().Tags] = arr;
-
-    // Value, domain and type
-    readFrom(n.value);
-    m_obj[iscore::StringConstant().Domain] = DomainToJson(n.domain);
 }
 
 template<>
 ISCORE_LIB_DEVICE_EXPORT void Visitor<Writer<JSONObject>>::writeTo(Device::AddressSettings& n)
 {
+    writeTo(static_cast<Device::AddressSettingsCommon&>(n));
     n.name = m_obj[iscore::StringConstant().Name].toString();
+}
 
-    n.ioType = Device::IOTypeStringMap().key(m_obj[iscore::StringConstant().ioType].toString());
-    n.clipMode = Device::ClipModeStringMap().key(m_obj[iscore::StringConstant().ClipMode].toString());
-    n.unit = m_obj[iscore::StringConstant().Unit].toString();
+template<>
+ISCORE_LIB_DEVICE_EXPORT void Visitor<Reader<JSONObject>>::readFrom(const Device::FullAddressSettings& n)
+{
+    readFrom(static_cast<const Device::AddressSettingsCommon&>(n));
+    m_obj[iscore::StringConstant().Address] = toJsonObject(n.address);
+}
 
-    n.repetitionFilter = m_obj[iscore::StringConstant().RepetitionFilter].toBool();
-    n.rate = m_obj[iscore::StringConstant().RefreshRate].toInt();
-
-    n.priority = m_obj[iscore::StringConstant().Priority].toInt();
-
-    auto arr = m_obj[iscore::StringConstant().Tags].toArray();
-    for(auto&& elt : arr)
-        n.tags.append(elt.toString());
-
-    writeTo(n.value);
-    // TODO doesn't handle multi-type variants.
-    if(m_obj.contains(iscore::StringConstant().Type))
-    {
-        n.domain = Device::JsonToDomain(m_obj[iscore::StringConstant().Domain].toObject(), m_obj[iscore::StringConstant().Type].toString());
-    }
+template<>
+ISCORE_LIB_DEVICE_EXPORT void Visitor<Writer<JSONObject>>::writeTo(Device::FullAddressSettings& n)
+{
+    writeTo(static_cast<Device::AddressSettingsCommon&>(n));
+    n.address = fromJsonObject<State::Address>(m_obj[iscore::StringConstant().Address].toObject());
 }
