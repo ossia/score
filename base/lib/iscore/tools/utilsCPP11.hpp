2fbe17bcae6e0b1688d6575d21b5cc8b3191f854
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-03-24 12:32:15 +0100

    fix a potential crash

--- base/lib/iscore/tools/utilsCPP11.hpp
+++ base/lib/iscore/tools/utilsCPP11.hpp
@@ -8,7 +8,8 @@
 template <typename Vector, typename Functor>
 void vec_erase_remove_if(Vector& v, Functor&& f)
 {
-    v.erase(std::remove_if(std::begin(v), std::end(v), f), std::end(v));
+    auto it = std::remove_if(std::begin(v), std::end(v), f);
+    v.erase(it, std::end(v));
 }
 
 template<typename Container>
