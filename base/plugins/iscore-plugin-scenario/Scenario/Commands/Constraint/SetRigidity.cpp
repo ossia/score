53c0e0988da059eacac578030363f44d43d7399d
Nicolas <nicolas.vuaille@gmail.com>
2016-04-01 17:21:49 +0200

    Fix 167

--- base/plugins/iscore-plugin-scenario/Scenario/Commands/Constraint/SetRigidity.cpp
+++ base/plugins/iscore-plugin-scenario/Scenario/Commands/Constraint/SetRigidity.cpp
@@ -19,12 +19,9 @@ SetRigidity::SetRigidity(
     m_path {constraintPath},
     m_rigidity {rigid}
 {
-    // We suppose that this command is never called with rigid == current state of the constraint.
-//    if(rigid)  // it is currently not rigid so min & max are set -> TODO : WHY ??
     // TODO make a class that embodies the logic for the relationship between rigidity and min/max.
     // Also, min/max are indicative if rigid, they can still be set but won't be used.
     auto& constraint = m_path.find();
-    ISCORE_ASSERT(constraint.duration.isRigid() != rigid);
 
     m_oldMinDuration = constraint.duration.minDuration();
     m_oldMaxDuration = constraint.duration.maxDuration();
