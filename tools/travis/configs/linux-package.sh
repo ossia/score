9bda3ddc2ef2751d72a1870a811d7ec04c2eadf0
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-04-06 16:17:41 +0200

    use more recent boost in ubuntu

--- tools/travis/configs/linux-package.sh
+++ tools/travis/configs/linux-package.sh
@@ -2,7 +2,7 @@
 export CC=gcc-5
 export CXX=g++-5
 
-$CMAKE_BIN -DCMAKE_PREFIX_PATH="/usr/local/jamoma/share/cmake/Jamoma;/opt/qt5-static/lib/cmake/Qt5" -DISCORE_CONFIGURATION=staticqt-release ..
+$CMAKE_BIN -DBOOST_ROOT="/opt/boost_1_60_0/" -DCMAKE_PREFIX_PATH="/usr/local/jamoma/share/cmake/Jamoma;/opt/qt5-static/lib/cmake/Qt5" -DISCORE_CONFIGURATION=staticqt-release ..
 
 $CMAKE_BIN --build . --target all_unity -- -j2
 $CMAKE_BIN --build . --target package -- -j2
