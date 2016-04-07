7f2670a629df318374d52e7a8a9ec4c5953d5cf6
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-04-08 00:04:51 +0200

    fix libsndfile

--- CMake/modules/FindSndfile.cmake
+++ CMake/modules/FindSndfile.cmake
@@ -1,71 +1,51 @@
-# - Find SNDFILE
-# Find the native SNDFILE includes and library
-# This module defines
-#  SNDFILE_INCLUDE_DIR, where to find sndfile.h, etc.
-#  SNDFILE_LIBRARIES, the libraries needed to use SNDFILE.
-#  SNDFILE_FOUND, If false, do not try to use SNDFILE.
-# also defined, but not for general use are
-#  SNDFILE_LIBRARY, where to find the SNDFILE library.
+# - Find sndfile
+# Find the native sndfile includes and libraries
+#
+#  SNDFILE_INCLUDE_DIR - where to find sndfile.h, etc.
+#  SNDFILE_LIBRARIES   - List of libraries when using libsndfile.
+#  SNDFILE_FOUND       - True if libsndfile found.
 
-if(WIN32)
-  FIND_PATH(SNDFILE_INCLUDE_DIR sndfile.h
-		PATHS ${CMAKE_CURRENT_LIST_DIR}/../../../DSP/extensions/SoundfileLib/libsndfile)
+if(NO_LIBSNDFILE)
+    #if(NOT SNDFILE_FIND_QUIETLY)
+    #	message(STATUS "FindSndfile: sndfile deactivated (NO_LIBSNDFILE)")
+    #endif()
+    set(SNDFILE_FOUND False)
+    set(SNDFILE_INCLUDE_DIR "nowhere")  # for onceonly check above
+    set(SNDFILE_LIBRARIES "")
+    add_definitions("-DNO_LIBSNDFILE")
+elseif (SNDFILE_INCLUDE_DIR AND SNDFILE_LIBRARY)
+    set(SNDFILE_LIBRARIES ${SNDFILE_LIBRARY})
+    set(SNDFILE_FOUND TRUE)
+elseif (APPLE)
+    set(SNDFILE_FOUND TRUE)
+    set(SNDFILE_INCLUDE_DIR ${CMAKE_CURRENT_LIST_DIR}/../external_libraries/libsndfile/)
+    set(SNDFILE_LIBRARIES ${CMAKE_CURRENT_LIST_DIR}/../platform/mac/lib/scUBlibsndfile.a)
+    add_definitions("-isystem ${CMAKE_CURRENT_LIST_DIR}/../external_libraries/libsndfile")
 
-  FIND_LIBRARY(SNDFILE_LIBRARY NAMES sndfile-jamoma sndfile-1 libsndfile-1
-		 PATHS ${CMAKE_CURRENT_LIST_DIR}/../../../DSP/extensions/SoundfileLib/libsndfile)
+    # TODO on non-apple platforms, we need to be able to test for >=1018.
+    # (On apple it is known true, because we bundle a later version.)
+    add_definitions("-DLIBSNDFILE_1018")
 
 else()
-  FIND_PATH(SNDFILE_INCLUDE_DIR sndfile.h)
-  FIND_LIBRARY(SNDFILE_LIBRARY NAMES sndfile)
+    find_path(SNDFILE_INCLUDE_DIR sndfile.h
+        PATHS "$ENV{ProgramFiles}/Mega-Nerd/libsndfile/include"
+    )
+
+    find_library(SNDFILE_LIBRARY NAMES sndfile sndfile-1 libsndfile libsndfile-1
+        PATHS "$ENV{ProgramFiles}/Mega-Nerd/libsndfile/lib"
+    )
+
+    # Handle the QUIETLY and REQUIRED arguments and set SNDFILE_FOUND to TRUE if
+    # all listed variables are TRUE.
+    include(FindPackageHandleStandardArgs)
+    find_package_handle_standard_args(Sndfile DEFAULT_MSG
+        SNDFILE_LIBRARY SNDFILE_INCLUDE_DIR)
+
+    if(SNDFILE_FOUND)
+        set(SNDFILE_LIBRARIES ${SNDFILE_LIBRARY})
+    else(SNDFILE_FOUND)
+        set(SNDFILE_LIBRARIES)
+    endif(SNDFILE_FOUND)
+
 endif()
-
-# handle the QUIETLY and REQUIRED arguments and set SNDFILE_FOUND to TRUE if
-# all listed variables are TRUE
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(SNDFILE DEFAULT_MSG SNDFILE_LIBRARY SNDFILE_INCLUDE_DIR)
-
-IF(SNDFILE_FOUND)
-  SET(SNDFILE_LIBRARIES ${SNDFILE_LIBRARY})
-
-  GET_FILENAME_COMPONENT(SNDFILE_LINK_DIRECTORIES ${SNDFILE_LIBRARY} PATH)
-ELSE()
-  FIND_PATH(SNDFILE_INCLUDE_DIR sndfile.h
-    HINTS
-    $ENV{SNDFILE_DIR}
-    ${SNDFILE_INCLUDE_WIN32_PATH}
-    PATHS
-    ${JamomaCore_SOURCE_DIR}/DSP/extensions/SoundfileLib/libsndfile
-    /usr/include
-    /usr/local/include
-    /opt/local/include
-  )
-
-  FIND_LIBRARY(SNDFILE_LIBRARY libsndfile-1
-    HINTS
-    $ENV{SNDFILE_DIR}
-    ${SNDFILE_LIB_WIN32_PATH}
-    PATHS
-    ${JamomaCore_SOURCE_DIR}/DSP/extensions/SoundfileLib/libsndfile
-    /usr/lib
-    /usr/local/lib
-    /opt/local/lib
-  )
-
-  set(SNDFILE_LIBRARIES ${SNDFILE_LIBRARY})
-  set(SNDFILE_INCLUDE_DIR ${SNDFILE_INCLUDE_DIR})
-ENDIF(SNDFILE_FOUND)
-
-# Deprecated declarations.
-SET (NATIVE_SNDFILE_INCLUDE_PATH ${SNDFILE_INCLUDE_DIR} )
-IF(SNDFILE_LIBRARY)
-  GET_FILENAME_COMPONENT (NATIVE_SNDFILE_LIB_PATH ${SNDFILE_LIBRARY} PATH)
-ENDIF(SNDFILE_LIBRARY)
-
-if(SNDFILE_LIBRARIES)
-  #osx_check_architecture(SNDFILE ${SNDFILE_LIBRARIES})
-  if(NOT SNDFILE_DYLIB_ARCHS_MATCH_REQUIRED_ARCHS)
-    set(SNDFILE_FOUND False)
-  endif()
-endif()
-
-MARK_AS_ADVANCED(SNDFILE_LIBRARY SNDFILE_INCLUDE_DIR SNDFILE_LINK_DIRECTORIES)
+mark_as_advanced(SNDFILE_INCLUDE_DIR SNDFILE_LIBRARY)
