d7fc2e18f38327c1e5e852e343abfa42547c59e8
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-04-10 01:45:03 +0200

    don't conflict with libaudiostream portaudio

--- CMake/Configurations/sanitized.cmake
+++ CMake/Configurations/sanitized.cmake
@@ -3,7 +3,7 @@ set(CMAKE_CXX_COMPILER /usr/bin/g++)
 set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH};/usr/jamoma/share/cmake/Jamoma")
 set(ISCORE_COTIRE_DISABLE_UNITY True)
 set(ISCORE_SANITIZE True)
-set(ISCORE_DISABLE_ADDONS True)
+#set(ISCORE_DISABLE_ADDONS True)
 include(travis/static-debug)
 include(all-plugins)
 
