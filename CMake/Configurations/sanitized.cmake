eef8b3b6634ce3020ee5b580936e940285dfbc97
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-05-05 16:10:26 +0200

    minor fixes

--- CMake/Configurations/sanitized.cmake
+++ CMake/Configurations/sanitized.cmake
@@ -1,5 +1,5 @@
-set(CMAKE_C_COMPILER /usr/bin/gcc)
-set(CMAKE_CXX_COMPILER /usr/bin/g++)
+set(CMAKE_C_COMPILER /usr/bin/clang)
+set(CMAKE_CXX_COMPILER /usr/bin/clang++)
 set(CMAKE_PREFIX_PATH "${CMAKE_PREFIX_PATH};/usr/jamoma/share/cmake/Jamoma")
 set(ISCORE_COTIRE_DISABLE_UNITY True)
 set(ISCORE_SANITIZE True)
