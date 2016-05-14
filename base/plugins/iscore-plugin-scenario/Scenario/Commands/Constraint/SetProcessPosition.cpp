219842b487787d5934441fee5ad69a24ed5008d1
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-05-14 18:35:30 +0200

    set process position command

--- base/plugins/iscore-plugin-scenario/Scenario/Commands/Constraint/SetProcessPosition.cpp
+++ base/plugins/iscore-plugin-scenario/Scenario/Commands/Constraint/SetProcessPosition.cpp
@@ -6,6 +6,40 @@ namespace Scenario
 {
 namespace Command
 {
+SetProcessPosition::SetProcessPosition(
+        Scenario::ConstraintModel& cst,
+        const Id<Process::ProcessModel>& proc,
+        const Id<Process::ProcessModel>& proc2) :
+    m_path{cst},
+    m_proc{proc},
+    m_proc2{proc2}
+{
+
+}
+
+
+void SetProcessPosition::undo() const
+{
+    redo();
+}
+
+void SetProcessPosition::redo() const
+{
+    auto& cst = m_path.find();
+    cst.processes.relocate(m_proc, m_proc2);
+}
+
+void SetProcessPosition::serializeImpl(DataStreamInput& s) const
+{
+    s << m_path << m_proc << m_proc2;
+}
+
+void SetProcessPosition::deserializeImpl(DataStreamOutput& s)
+{
+    s >> m_path >> m_proc >> m_proc2;
+}
+
+
 SwapProcessPosition::SwapProcessPosition(
         Scenario::ConstraintModel& cst,
         const Id<Process::ProcessModel>& proc,
--- base/plugins/iscore-plugin-scenario/Scenario/Commands/Constraint/SetProcessPosition.hpp
+++ base/plugins/iscore-plugin-scenario/Scenario/Commands/Constraint/SetProcessPosition.hpp
@@ -16,6 +16,27 @@ namespace Scenario
 class ConstraintModel;
 namespace Command
 {
+class SetProcessPosition final : public iscore::SerializableCommand
+{
+        ISCORE_COMMAND_DECL(ScenarioCommandFactoryName(), SetProcessPosition, "Set process position")
+
+    public:
+        SetProcessPosition(
+                Scenario::ConstraintModel& cst,
+                const Id<Process::ProcessModel>& proc,
+                const Id<Process::ProcessModel>& proc2);
+
+        void undo() const override;
+        void redo() const override;
+
+    protected:
+        void serializeImpl(DataStreamInput& s) const override;
+        void deserializeImpl(DataStreamOutput& s) override;
+
+    private:
+        Path<Scenario::ConstraintModel> m_path;
+        Id<Process::ProcessModel> m_proc, m_proc2;
+};
 class SwapProcessPosition final : public iscore::SerializableCommand
 {
         ISCORE_COMMAND_DECL(ScenarioCommandFactoryName(), SwapProcessPosition, "Set process position")
