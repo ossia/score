c00e1a05f8a41aaafdc7a6a1da84665935ba8c60
mchauvat <mchauvat@enseirb-matmeca.fr>
2016-05-23 15:34:51 +0200

    adding track panel view, the breathtaking sequel

--- base/lib/iscore/widgets/QmlContainerPanel.hpp
+++ base/lib/iscore/widgets/QmlContainerPanel.hpp
@@ -4,7 +4,9 @@
 #include <QString>
 #include <QUrl>
 #include <iscore_lib_base_export.h>
+#include <QSize>
 
+class QVBoxLayout;
 class QQuickWidget;
 
 class ISCORE_LIB_BASE_EXPORT QMLContainerPanel : public QWidget {
@@ -13,16 +15,23 @@ public:
     QMLContainerPanel(QMLContainerPanel* container, QWidget* parent = nullptr);
 
     virtual void setSource(const QString&);
-    QString source();
-    virtual bool isCollapsed();
+    QString source() const;
+    virtual bool isCollapsed() const;
+    const QSize internalSize() const;
+    void setInternalSize(const QSize& s);
+    void setInternalSize(const int& w, const int& h);
 
     ~QMLContainerPanel();
 
 public slots:
-    virtual void collapse(bool);
+    virtual void collapse();
+    virtual QQuickWidget* container();
+    virtual QWidget* rootWidget();
 
 private:
     QString m_source;
     QWidget* m_widget;
     QQuickWidget* m_qcontainer;
+    QAction* m_collapse;
+    QVBoxLayout* m_layout;
 };
