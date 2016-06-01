14d53a5535f5b95d24373f32e34a79b8b0dcb717
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-06-02 01:52:23 +0200

    work on appimage

--- base/app/i-score.sh
+++ base/app/i-score.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-LD_LIBRARY_PATH=.:/usr/lib/i-score ./i-score
+if [ -f ./i-score ]; then
+    LD_LIBRARY_PATH=.:/usr/lib/i-score ./i-score
+else
+    LD_LIBRARY_PATH=lib:lib/i-score bin/i-score
+fi
