5ec04ab249a691b1bc8b44c7885c0cf6c2c925ce
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-06-02 02:12:58 +0200

    more work on appiamge

--- base/app/i-score.sh
+++ base/app/i-score.sh
@@ -2,5 +2,6 @@
 if [ -f ./i-score ]; then
     LD_LIBRARY_PATH=.:/usr/lib/i-score ./i-score
 else
+    export QT_XKB_CONFIG_ROOT=/usr/share/X11/xkb
     LD_LIBRARY_PATH=lib:lib/i-score bin/i-score
 fi
