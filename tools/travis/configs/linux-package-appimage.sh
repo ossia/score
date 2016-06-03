8c130636f15251ab925603221ff66d659ada79cc
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-06-03 18:02:01 +0200

    fix appimage

--- tools/travis/configs/linux-package-appimage.sh
+++ tools/travis/configs/linux-package-appimage.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 docker pull iscore/i-score-package-linux
-docker run --name buildvm iscore/i-score-package-linux Recipe
+docker run --name buildvm iscore/i-score-package-linux /bin/bash Recipe
 docker cp buildvm:/i-score.AppImage i-score.AppImage
