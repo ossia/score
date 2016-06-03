77cca3ef405854ca9ca6745908669c221cc1fbeb
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-06-03 17:29:11 +0200

    work on docker

---
+++ tools/travis/configs/linux-package-appimage.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+docker pull iscore/i-score-package-linux
+docker run --name buildvm iscore/i-score-package-linux Recipe
+docker cp buildvm:/i-score.AppImage i-score.AppImage
--- tools/travis/deps.sh
+++ tools/travis/deps.sh
@@ -3,22 +3,12 @@
 git submodule update --init --recursive
 export ISCORE_PLUGINS_TO_BUILD=("iscore-addon-csp")
 
-# Set-up the plug-ins
-#(
-#cd base/addons
-#for plugin in "${ISCORE_PLUGINS_TO_BUILD[@]}"
-#do
-#    git clone "https://github.com/OSSIA/$plugin"
-#done
-
-#for plugin in */
-#do
-#  (
-#    cd $plugin
-#    git submodule update --init --recursive
-#  )
-#done
-#)
+# In this case everything is built in docker
+if [[ "$CONF" == "linux-package-appimage" ]];
+then
+    exit 0
+fi
+    
 # Install the deps
 case "$TRAVIS_OS_NAME" in
   linux)
@@ -44,36 +34,19 @@ case "$TRAVIS_OS_NAME" in
       sudo apt-get install -qq qt56-meta-full libboost1.55-dev
     fi
 
-#   wget https://www.dropbox.com/s/fiujf6l95g9nrjl/gcc5.3.deb?dl=1 -O gcc.deb
-#   sudo dpkg --force-overwrite -i gcc.deb
-
     wget https://www.dropbox.com/s/ysnozd2sqre7x2d/cmake-3.4.1-Linux-x86_64.deb?dl=1 -O cmake.deb
     sudo dpkg --force-overwrite -i cmake.deb
 
     wget https://www.dropbox.com/s/0pmy14zlpqpyaq6/JamomaCore-0.6-dev-Linux.deb?dl=1 -O jamoma.deb
     sudo dpkg -i jamoma.deb
-
-    # sudo update-alternatives --install /usr/bin/g++ g++ /usr/bin/g++-5 1000
   ;;
   osx)
     set +e
-
 
     brew install wget gnu-tar
     wget https://www.dropbox.com/s/ycl6tmct75po1n6/homebrew-cache.tar.gz?dl=1 -O homebrew-cache.tar.gz
     gtar xhzf homebrew-cache.tar.gz --directory /usr/local/Cellar
     brew link --force boost cmake ninja qt5 wget
-
-    # ./tools/travis/dl-homebrew.sh
-
-#     if [ ! -f ~/travis-cache/homebrew-cache.tar.gz ]; then
- #      brew update
-  #     brew install wget
-   #    brew install cmake qt5 ninja
-  #     brew upgrade boost
- #    fi
-
-#     ./tools/travis/cache-homebrew.sh
 
     wget https://www.dropbox.com/s/t155m8wt2cp075k/JamomaDarwin20151108.zip?dl=1 -O JamomaDarwin20151108.zip
     unzip JamomaDarwin20151108.zip
