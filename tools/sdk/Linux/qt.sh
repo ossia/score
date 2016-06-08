8dbc715f89673b83ceb13e2f1a4d7c44739eaa23
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-06-08 22:54:20 +0200

    work

---
+++ tools/sdk/Linux/qt.sh
@@ -0,0 +1,11 @@
+#!/bin/bash -eux
+
+cd /image
+
+NPROC=$(nproc)
+
+yum install perl-version libxcb libxcb-devel xcb-util xcb-util-devel
+
+git clone https://github.com/qtproject/qt5
+cd qt5
+perl init-repository 
