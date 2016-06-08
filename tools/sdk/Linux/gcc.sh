b90bc8c4e22db492701b3e8c533604a2984225d5
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-06-08 23:56:53 +0200

    work

--- tools/sdk/Linux/gcc.sh
+++ tools/sdk/Linux/gcc.sh
@@ -4,11 +4,9 @@ cd /image
 
 NPROC=$(nproc)
 
-GCC=gcc-6.1.0
-
 mkdir gcc-build
 (
   cd gcc-build
-  ../$GCC/configure --enable-languages=c,c++,lto --enable-gold --enable-plugin --disable-multilib --disable-nls
+  ../combined/configure --enable-languages=c,c++,lto --enable-gold --enable-plugin --disable-multilib --disable-nls
   make
 )
