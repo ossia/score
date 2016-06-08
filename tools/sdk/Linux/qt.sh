c9092827c5c5c8c6c0ff7317c738c45e1b37cdcd
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-06-08 23:22:52 +0200

    work

--- tools/sdk/Linux/qt.sh
+++ tools/sdk/Linux/qt.sh
@@ -6,7 +6,8 @@ NPROC=$(nproc)
 
 yum install perl-version libxcb libxcb-devel xcb-util xcb-util-devel fontconfig-devel libX11-devel libXrender-devel libXi-devel git
 
-git clone https://github.com/qtproject/qt5
+git clone https://code.qt.io/qt/qt5.git
+
 cd qt5
 git checkout 5.6.1
-perl init-repository --module-subset=default,-qtwebkit,-qtwebkit-examples,-qtwebengine,-qt3d,-qtcanvas3d,-qtactiveqt,-qtandroidextras,-qtcharts,-qtconnectivity,-qtdatavis3d,-qtenginio,-qtgamepad,-qtgraphicaleffects,-qtlocation,-qtmacextras,-qtpurchasing,-qtqa,-qtscxml,-qtscript,-qtsensors,-qtserialport,-qtvirtualkeyboard,-qtwayland,-qtwebchannel,-qtwebview,-qtwinextras,-qtx11extras,-qtxmlpatterns
+perl init-repository --module-subset=qtbase,qtimageformats,qtsvg,qtwebsockets,qttranslations,qtrepotools
