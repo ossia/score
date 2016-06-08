c6234c96b38feb23c5ccc16b639933164e401731
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-06-08 23:05:25 +0200

    work

--- tools/sdk/Linux/gcc.sh
+++ tools/sdk/Linux/gcc.sh
@@ -1,4 +1,4 @@
-#!/bin/bash -eux
+#!/bin/bash
 
 cd /image
 
--- tools/sdk/Linux/qt.sh
+++ tools/sdk/Linux/qt.sh
@@ -4,7 +4,7 @@ cd /image
 
 NPROC=$(nproc)
 
-yum install perl-version libxcb libxcb-devel xcb-util xcb-util-devel
+yum install perl-version libxcb libxcb-devel xcb-util xcb-util-devel fontconfig-devel libX11-devel libXrender-devel libXi-devel git
 
 git clone https://github.com/qtproject/qt5
 cd qt5
