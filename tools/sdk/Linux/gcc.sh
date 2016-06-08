8c676c20a4b2af8a78dfbf1d01e6cfc579e5ef88
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-06-08 22:42:54 +0200

    work on generic docker image

---
+++ tools/sdk/Linux/gcc.sh
@@ -0,0 +1,45 @@
+#!/bin/bash -eux
+
+cd /image
+
+NPROC=$(nproc)
+BINUTILS=binutils-2.26
+MPC=mpc-1.0.3
+MPFR=mpfr-3.1.4
+GMP=gmp-6.1.0
+GCC=gcc-6.1.0
+ISL=isl-0.16.1
+CLOOG=cloog-0.18.4
+
+wget "http://isl.gforge.inria.fr/$ISL.tar.bz2"
+wget "https://ftp.gnu.org/gnu/gcc/$GCC/$GCC.tar.bz2"
+wget "https://gmplib.org/download/gmp/$GMP.tar.lz"
+wget "ftp://ftp.gnu.org/gnu/mpc/$MPC.tar.gz"
+wget "http://www.mpfr.org/mpfr-current/$MPFR.tar.bz2"
+wget "http://ftp.gnu.org/gnu/binutils/$BINUTILS.tar.bz2"
+wget "http://www.bastoul.net/cloog/pages/download/count.php3?url=./$CLOOG.tar.gz"
+tar xaf "$GMP.tar.lz"
+tar xaf "$MPC.tar.gz"
+tar xaf "$MPFR.tar.gz"
+tar xaf "$ISL.tar.bz2"
+tar xaf "$CLOOG.tar.gz"
+tar xaf "$BINUTILS.tar.bz2"
+tar xaf "$GCC.tar.bz2"
+
+(
+  cd $GCC
+  ln -s ../$GMP gmp
+  ln -s ../$MPC mpc
+  ln -s ../$MPFR mpfr
+  ln -s ../$ISL isl
+  ln -s ../$CLOOG cloog
+  for file in ../$BINUTILS/* ; do ln -s "${file}" ; done
+)
+
+mkdir gcc-build
+(
+  cd gcc-build
+  ../$GCC/configure --enable-languages=c,c++,lto --enable-gold --enable-plugin --disable-multilib --disable-nls
+  make -j$NPROC
+  sudo make -j$NPROC install
+)
