19c3f355ea1f45f50272b650ec561db0acadf751
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-06-08 23:41:17 +0200

    work

--- tools/sdk/Linux/qt.sh
+++ tools/sdk/Linux/qt.sh
@@ -4,7 +4,7 @@ cd /image
 
 NPROC=$(nproc)
 
-yum install perl-version libxcb libxcb-devel xcb-util xcb-util-devel fontconfig-devel libX11-devel libXrender-devel libXi-devel git
+yum install perl-version libxcb libxcb-devel xcb-util xcb-util-devel fontconfig-devel libX11-devel libXrender-devel libXi-devel git openssl-devel
 
 git clone https://code.qt.io/qt/qt5.git
 
@@ -14,15 +14,49 @@ git clone https://code.qt.io/qt/qt5.git
   perl init-repository --module-subset=qtbase,qtimageformats,qtsvg,qtwebsockets,qttranslations,qtrepotools,qtdeclarative
 )
 
+export CC=/usr/local/bin/gcc
+export CXX=/usr/local/bin/g++
+
 mkdir qt5-build
 (
   cd qt5-build
   ../qt5/configure -release \
                    -opensource \
                    -confirm-license \
-                   -qt-zlib \
-                   -no-mtdev \
                    -nomake examples \
                    -nomake tests \
+                   -no-qml-debug \
+                   -qt-zlib \
+                   -no-mtdev \
+                   -no-journald \
+                   -no-syslog \
+                   -no-gif \
+                   -qt-libpng \
+                   -qt-libjpeg \
+                   -qt-freetype \
+                   -qt-harfbuzz \
+                   -openssl \
+                   -libproxy \
+                   -qt-pcre \
+                   -qt-xcb \
+                   -qt-xkbcommon-x11 \
+                   -no-xinput2 \
+                   -glib \
+                   -no-pulseaudio \
+                   -no-alsa \
+                   -no-gtkstyle \
+                   -no-compile-examples \
+                   -no-cups \
+                   -no-iconv \
+                   -no-tslib \
+                   -no-icu \
+                   -no-pch \
+                   -ltcg \
+                   -dbus-linked \
+                   -no-gstreamer \
+                   -no-system-proxies
+                   
+  make -j$NPROC
+  make install -j$NPROC
                    
 )
