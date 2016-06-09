492b7796f18b7b5ce5e275abc1421bf06b992857
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-06-09 18:10:58 +0200

    minor

--- base/lib/iscore/tools/NotifyingMap.hpp
+++ base/lib/iscore/tools/NotifyingMap.hpp
@@ -56,6 +56,7 @@ class NotifyingMap
             removing(elt);
             m_map.remove(elt.id());
             removed(elt);
+            elt.setParent(nullptr);
             elt.deleteLater();
         }
 
@@ -71,6 +72,7 @@ class NotifyingMap
             removing(elt);
             m_map.remove(it);
             removed(elt);
+            elt.setParent(nullptr);
             elt.deleteLater();
         }
 
