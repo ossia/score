37793c3161676250763fcda3b9fc2c8821b66ef3
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-06-11 23:22:09 +0200

    triggers are false by default. Fix #226

--- base/plugins/iscore-plugin-scenario/Scenario/Document/TimeNode/Trigger/TriggerModel.cpp
+++ base/plugins/iscore-plugin-scenario/Scenario/Document/TimeNode/Trigger/TriggerModel.cpp
@@ -10,7 +10,12 @@ namespace Scenario
 TriggerModel::TriggerModel(const Id<TriggerModel>& id, QObject* parent):
     IdentifiedObject<TriggerModel>{id, Metadata<ObjectKey_k, TriggerModel>::get(), parent}
 {
-
+    m_expression.push_back(
+    State::Expression{
+            State::Relation{
+                State::RelationMember{State::Value::fromValue(true)},
+                State::Relation::Equal,
+                State::RelationMember{State::Value::fromValue(false)}}, &m_expression});
 }
 
 State::Trigger TriggerModel::expression() const
