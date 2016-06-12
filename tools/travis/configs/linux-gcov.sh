357d3a9680f546e1337ab88060ca45bf88b5ff50
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-06-12 13:42:14 +0200

    wokr on travis

--- tools/travis/configs/linux-gcov.sh
+++ tools/travis/configs/linux-gcov.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 source "$CONFIG_FOLDER/linux-source-qt.sh"
 
-$CMAKE_BIN -DISCORE_CONFIGURATION=linux-gcov ..
+$CMAKE_BIN -DBOOST_ROOT="$BOOST_ROOT" -DISCORE_CONFIGURATION=linux-gcov ..
 $CMAKE_BIN --build . --target all_unity -- -j2
 
 echo "$PWD"
