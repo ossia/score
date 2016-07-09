54dc7c244e9ad9c4f51ee65ea08fec2e203fef2f
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-07-09 22:12:25 +0200

    add missing files

---
+++ base/plugins/iscore-plugin-ossia/OSSIA/LocalTree/LocalTreeComponent.hpp
@@ -0,0 +1,33 @@
+#pragma once
+#include <OSSIA/LocalTree/NameProperty.hpp>
+#include <OSSIA/LocalTree/Scenario/MetadataParameters.hpp>
+#include <iscore/component/Component.hpp>
+#include <OSSIA/LocalTree/LocalTreeDocumentPlugin.hpp>
+namespace Ossia
+{
+namespace LocalTree
+{
+template<typename Component_T>
+class Component :
+        public Component_T
+{
+    public:
+        template<typename... Args>
+        Component(OSSIA::Node& n, ModelMetadata& m, Args&&... args):
+            Component_T{std::forward<Args>(args)...},
+            m_thisNode{n, m, this}
+        {
+            make_metadata_node(m, *m_thisNode.node, m_properties, this);
+        }
+
+        auto& node() const
+        { return m_thisNode.node; }
+
+    protected:
+        MetadataNamePropertyWrapper m_thisNode;
+        std::vector<std::unique_ptr<BaseProperty>> m_properties;
+};
+
+using CommonComponent = Component<iscore::GenericComponent<DocumentPlugin>>;
+}
+}
