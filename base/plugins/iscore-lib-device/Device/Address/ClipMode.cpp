b0e19d74bc3ad5e1e901668ecefdca1b00a90ebf
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-08-17 11:25:53 +0200

    add low/high clip modes

--- base/plugins/iscore-lib-device/Device/Address/ClipMode.cpp
+++ base/plugins/iscore-lib-device/Device/Address/ClipMode.cpp
@@ -7,12 +7,13 @@
 
 namespace Device
 {
-
-static const QMap<ClipMode, QString> clipmodemap{
+const QMap<ClipMode, QString> clipmodemap{
     {{ClipMode::Free, QObject::tr("Free")},
      {ClipMode::Clip, QObject::tr("Clip")},
-     {ClipMode::Fold, QObject::tr("Fold")},     
+     {ClipMode::Fold, QObject::tr("Fold")},
      {ClipMode::Wrap, QObject::tr("Wrap")},
+     {ClipMode::Low, QObject::tr("Low")},
+     {ClipMode::High, QObject::tr("High")},
     }
 };
 const QMap<ClipMode, QString> &ClipModeStringMap()
