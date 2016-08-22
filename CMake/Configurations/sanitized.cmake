2f1e5d295b6d3ec1be66064c8174faea76299e40
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-08-22 20:06:44 +0200

    don't use clang for fsanitize due to a current bug in glib...

--- CMake/Configurations/sanitized.cmake
+++ CMake/Configurations/sanitized.cmake
@@ -1,5 +1,5 @@
-set(CMAKE_C_COMPILER /usr/bin/clang)
-set(CMAKE_CXX_COMPILER /usr/bin/clang++)
+# set(CMAKE_C_COMPILER /usr/bin/clang)
+# set(CMAKE_CXX_COMPILER /usr/bin/clang++)
 set(ISCORE_COTIRE_DISABLE_UNITY True)
 set(ISCORE_SANITIZE True)
 #set(ISCORE_DISABLE_ADDONS True)
