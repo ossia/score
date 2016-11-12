dd306bc26352bd6c5d8c51b3569c06b6ab8e9f1b
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-11-12 15:12:09 +0100

    should fix DLL builds

---
+++ base/plugins/iscore-plugin-curve/Curve/Palette/CurvePalette.cpp
@@ -0,0 +1,105 @@
+#include "CurvePalette.hpp"
+
+namespace Curve
+{
+
+ToolPalette::ToolPalette(const iscore::DocumentContext& ctx, Presenter& pres):
+    GraphicsSceneToolPalette{*pres.view().scene()},
+    m_presenter{pres},
+    m_selectTool{*this, ctx},
+    m_createTool{*this, ctx},
+    m_setSegmentTool{*this, ctx}
+{
+}
+
+Presenter& ToolPalette::presenter() const
+{
+    return m_presenter;
+}
+
+Curve::EditionSettings& ToolPalette::editionSettings() const
+{
+    return m_presenter.editionSettings();
+}
+
+const Model& ToolPalette::model() const
+{
+    return m_presenter.model();
+}
+
+void ToolPalette::on_pressed(QPointF point)
+{
+    scenePoint = point;
+    auto curvePoint = ScenePointToCurvePoint(m_presenter.view().mapFromScene(point));
+    switch(editionSettings().tool())
+    {
+        case Curve::Tool::Create:
+            m_createTool.on_pressed(point, curvePoint);
+            break;
+        case Curve::Tool::Select:
+            m_selectTool.on_pressed(point, curvePoint);
+            break;
+        case Curve::Tool::SetSegment:
+            m_setSegmentTool.on_pressed(point, curvePoint);
+            break;
+        default:
+            break;
+    }
+}
+
+void ToolPalette::on_moved(QPointF point)
+{
+    scenePoint = point;
+    auto curvePoint = ScenePointToCurvePoint(m_presenter.view().mapFromScene(point));
+    switch(editionSettings().tool())
+    {
+        case Curve::Tool::Create:
+            m_createTool.on_moved(point, curvePoint);
+            break;
+        case Curve::Tool::Select:
+            m_selectTool.on_moved(point, curvePoint);
+            break;
+        case Curve::Tool::SetSegment:
+            m_setSegmentTool.on_moved(point, curvePoint);
+            break;
+        default:
+            break;
+    }
+}
+
+void ToolPalette::on_released(QPointF point)
+{
+    scenePoint = point;
+    auto curvePoint = ScenePointToCurvePoint(m_presenter.view().mapFromScene(point));
+    switch(editionSettings().tool())
+    {
+        case Curve::Tool::Create:
+            m_createTool.on_released(point, curvePoint);
+            break;
+        case Curve::Tool::Select:
+            m_selectTool.on_released(point, curvePoint);
+            break;
+        case Curve::Tool::SetSegment:
+            m_setSegmentTool.on_released(point, curvePoint);
+            break;
+        default:
+            break;
+    }
+}
+
+void ToolPalette::on_cancel()
+{
+    m_createTool.on_cancel();
+    m_selectTool.on_cancel();
+    m_setSegmentTool.on_cancel();
+}
+
+void ToolPalette::activate(Curve::Tool)
+{
+
+}
+void ToolPalette::desactivate(Curve::Tool)
+{
+
+}
+}
