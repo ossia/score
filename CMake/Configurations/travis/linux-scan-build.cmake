6b453a8aa342cc795e7a5fa693dc3acf0b78ed08
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-11-13 22:45:20 +0100

    update thirdparties

--- CMake/Configurations/travis/linux-scan-build.cmake
+++ CMake/Configurations/travis/linux-scan-build.cmake
@@ -1,5 +1,7 @@
 set(CMAKE_BUILD_TYPE Release)
 
+set(DISABLE_COTIRE True CACHE BOOL "No cotire" FORCE)
+set(OSSIA_SANITIZE True CACHE BOOL "Sanitize ossia" FORCE)
 set(ISCORE_COTIRE False)
 set(DEPLOYMENT_BUILD False)
 set(ISCORE_COVERAGE False)
