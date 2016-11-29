03fc3b588ad999648961c8eed07c241b269bfa33
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-11-29 17:52:56 +0100

    try to fix deployment problem

--- tools/travis/configs/linux-package-appimage.sh
+++ tools/travis/configs/linux-package-appimage.sh
@@ -2,4 +2,16 @@
 
 docker pull iscore/i-score-package-linux
 docker run --name buildvm iscore/i-score-package-linux /bin/bash Recipe
-docker cp buildvm:/i-score.AppImage i-score.AppImage
+docker cp buildvm:/i-score.AppDir.txz .
+
+tar xaf i-score.AppDir.txz
+
+cp ../CMake/Deployment/Linux/AppImage/AppRun i-score.AppDir/
+chmod a+rwx i-score.AppDir/AppRun
+
+wget "https://github.com/probonopd/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
+chmod a+x appimagetool-x86_64.AppImage
+
+./appimagetool-x86_64.AppImage "$APP.AppDir" "i-score.AppImage"
+
+chmod a+rwx i-score.AppImage
