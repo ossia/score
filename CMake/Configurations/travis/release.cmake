9927977194f77766a7116a6f5dbf4b6f52b3fcee
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-12-03 19:45:08 +0100

    disable lto on appveyor

--- CMake/Configurations/travis/release.cmake
+++ CMake/Configurations/travis/release.cmake
@@ -1,6 +1,9 @@
 set(CMAKE_BUILD_TYPE Release)
 set(ISCORE_COTIRE True)
 set(DEPLOYMENT_BUILD True)
+
+if(NOT DEFINED ISCORE_ENABLE_LTO)
 set(ISCORE_ENABLE_LTO True)
+endif()
 
 include(default-plugins)
