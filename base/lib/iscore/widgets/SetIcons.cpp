08c9689a318990c47facee7b2fe0b2387c0c83a3
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2016-12-03 21:20:01 +0100

    minor

--- base/lib/iscore/widgets/SetIcons.cpp
+++ base/lib/iscore/widgets/SetIcons.cpp
@@ -3,11 +3,11 @@
 void setIcons(QAction* action, const QString& iconOn, const QString& iconOff)
 {
   QIcon icon;
-  icon.addPixmap(iconOn, QIcon::Mode::Selected);
-  icon.addPixmap(iconOn, QIcon::Mode::Active);
-  icon.addPixmap(iconOn, QIcon::Mode::Normal, QIcon::State::On);
+  icon.addFile(iconOn, QSize{}, QIcon::Mode::Selected);
+  icon.addFile(iconOn, QSize{}, QIcon::Mode::Active);
+  icon.addFile(iconOn, QSize{}, QIcon::Mode::Normal, QIcon::State::On);
 
-  icon.addPixmap(iconOff, QIcon::Mode::Normal);
+  icon.addFile(iconOff, QSize{}, QIcon::Mode::Normal);
   action->setIcon(icon);
 }
 
@@ -19,12 +19,12 @@ QIcon makeIcons(const QString& iconOn, const QString& iconOff)
 QIcon genIconFromPixmaps(const QString& iconOn, const QString& iconOff)
 {
   QIcon icon;
-  icon.addPixmap(iconOn, QIcon::Mode::Selected);
-  icon.addPixmap(iconOn, QIcon::Mode::Active);
-  icon.addPixmap(iconOn, QIcon::Mode::Normal, QIcon::State::On);
-  icon.addPixmap(iconOn, QIcon::Mode::Selected, QIcon::State::On);
-  icon.addPixmap(iconOn, QIcon::Mode::Active, QIcon::State::On);
+  icon.addFile(iconOn, QSize{}, QIcon::Mode::Selected);
+  icon.addFile(iconOn, QSize{}, QIcon::Mode::Active);
+  icon.addFile(iconOn, QSize{}, QIcon::Mode::Normal, QIcon::State::On);
+  icon.addFile(iconOn, QSize{}, QIcon::Mode::Selected, QIcon::State::On);
+  icon.addFile(iconOn, QSize{}, QIcon::Mode::Active, QIcon::State::On);
 
-  icon.addPixmap(iconOff, QIcon::Mode::Normal);
+  icon.addFile(iconOff, QSize{}, QIcon::Mode::Normal);
   return icon;
 }
