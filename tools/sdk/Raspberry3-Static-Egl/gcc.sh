3519159e0f03de757388583f08a2104adec70ee1
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-01-06 10:04:37 +0100

    update

--- tools/sdk/Raspberry3-Static-Egl/gcc.sh
+++ tools/sdk/Raspberry3-Static-Egl/gcc.sh
@@ -13,13 +13,26 @@ mkdir -p /opt/gcc-6-temp
 mkdir -p gcc-build
 (
   cd gcc-build
-  ../combined/configure --enable-languages=c,c++,lto --enable-gold --enable-plugins --enable-plugin --disable-nls --enable-werror=no --with-float=hard --with-arch=armv7-a --with-fpu=vfp --prefix=/opt/gcc-6-temp --build=arm-linux-gnueabihf --host=arm-linux-gnueabihf --target=arm-linux-gnueabihf 
+  ../combined/configure --enable-languages=c,c++,lto \
+                        --enable-gold \
+                        --enable-plugins \
+                        --enable-plugin \
+                        --disable-nls \
+                        --enable-werror=no \
+                        --with-float=hard \
+                        --with-arch=armv8-a \
+                        --with-tune=cortex-a53 \
+                        --with-fpu=crypto-neon-fp-armv8 \
+                        --prefix=/opt/gcc-6-temp \
+                        --build=arm-linux-gnueabihf \
+                        --host=arm-linux-gnueabihf \
+                        --target=arm-linux-gnueabihf
+
   make -j$NPROC && make install-strip
 )
 rm -rf gcc-build
 
 # Now rebuild all the binutils with LTO
-
 mkdir -p /opt/gcc-6
 mkdir gcc-build-2
 (
@@ -28,8 +41,24 @@ mkdir gcc-build-2
   export CXX=/opt/gcc-6-temp/bin/g++
   export LD_LIBRARY_PATH=/opt/gcc-6-temp/lib
 
-  ../combined/configure --enable-languages=c,c++,lto --enable-gold --enable-plugins --enable-plugin --disable-nls --enable-werror=no --with-build-config=bootstrap-lto --enable-checking=none --with-float=hard  --with-arch=armv7-a --with-fpu=vfp --prefix=/opt/gcc-6 --with-build-time-tools=/opt/gcc-6-temp/bin --build=arm-linux-gnueabihf --host=arm-linux-gnueabihf --target=arm-linux-gnueabihf 
-  make BOOT_CFLAGS="-O3 -g0 -march=armv8-a -mtune=cortex-a53 -mfpu=crypto-neon-fp-armv8" -j$NPROC && make install-strip
+  ../combined/configure --enable-languages=c,c++,lto \
+                        --enable-gold \
+                        --enable-plugins \
+                        --enable-plugin \
+                        --disable-nls \
+                        --enable-werror=no \
+                        --with-build-config=bootstrap-lto \
+                        --enable-checking=none \
+                        --with-float=hard \
+                        --with-arch=armv8-a \
+                        --with-tune=cortex-a53 \
+                        --prefix=/opt/gcc-6 \
+                        --with-build-time-tools=/opt/gcc-6-temp/bin \
+                        --build=arm-linux-gnueabihf \
+                        --host=arm-linux-gnueabihf \
+                        --target=arm-linux-gnueabihf
+
+  make BOOT_CFLAGS="-O3 -g0" -j$NPROC && make install-strip
 )
 
 rm -rf /opt/gcc-6-temp
