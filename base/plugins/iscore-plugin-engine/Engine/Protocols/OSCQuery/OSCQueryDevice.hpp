6c689c382e8537fd8ab7b9feb9bdde56ab0503f4
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-02-13 09:47:34 +0100

    update

--- base/plugins/iscore-plugin-engine/Engine/Protocols/OSCQuery/OSCQueryDevice.hpp
+++ base/plugins/iscore-plugin-engine/Engine/Protocols/OSCQuery/OSCQueryDevice.hpp
@@ -1,12 +1,11 @@
 #pragma once
 #include <Engine/Protocols/OSSIADevice.hpp>
-#include <nano_observer.hpp>
 
 namespace Engine
 {
 namespace Network
 {
-class OSCQueryDevice final : public OwningOSSIADevice, public Nano::Observer
+class OSCQueryDevice final : public OwningOSSIADevice
 {
 public:
   OSCQueryDevice(const Device::DeviceSettings& settings);
