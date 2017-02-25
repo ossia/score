df6718b8c3cfe43dca7e8e006d36085b27114150
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-02-25 15:40:58 +0100

    fix #392

--- base/plugins/iscore-plugin-recording/Recording/Commands/Record.hpp
+++ base/plugins/iscore-plugin-recording/Recording/Commands/Record.hpp
@@ -10,13 +10,20 @@ class Record final : public iscore::AggregateCommand
 public:
   void undo() const override
   {
+    const int N = m_cmds.size();
     // Undo 1
-    auto it = m_cmds.begin();
-    std::advance(it, 1);
-    (*it)->undo();
+    if(N >= 2)
+    {
+      auto it = m_cmds.begin();
+      std::advance(it, 1);
+      (*it)->undo();
+    }
 
-    // Undo 0
-    (*m_cmds.begin())->undo();
+    if(N >= 1)
+    {
+      // Undo 0
+      (*m_cmds.begin())->undo();
+    }
   }
 };
 }
