64dbd7e9545edcc9786db8424d7b57ff300501f6
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-03-09 20:15:09 +0100

    try to reduce build times

--- tools/travis/deps.sh
+++ tools/travis/deps.sh
@@ -1,7 +1,5 @@
 #!/bin/bash -eux
 
-# export ISCORE_PLUGINS_TO_BUILD=("iscore-addon-csp")
-
 # In this case everything is built in docker
 if [[ "$CONF" == "linux-package-appimage" ]];
 then
@@ -11,33 +9,22 @@ fi
 # Install the deps
 case "$TRAVIS_OS_NAME" in
   linux)
+    sudo wget https://github.com/OSSIA/iscore-sdk/releases/download/6.0-osx/boost_1_63_0.tar.bz2 -O /opt/boost.tar.bz2 &
+    
+    wget https://cmake.org/files/v3.8/cmake-3.8.0-rc1-Linux-x86_64.tar.gz -O cmake-linux.tgz &
+    
+    echo 'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-4.0 main' | sudo tee /etc/apt/sources.list.d/llvm.list
     sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 1397BC53640DB551
 
-    if [[ "$CONF" != "linux-package" ]];
-    then
-      sudo add-apt-repository --yes ppa:beineri/opt-qt58-trusty
-    fi
+    sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
+    sudo add-apt-repository --yes ppa:beineri/opt-qt58-trusty
 
     sudo apt-get update -qq
-    sudo apt-get install -qq libasound-dev ninja-build gcovr lcov
+    sudo apt-get install -qq --force-yes g++-6 binutils libasound-dev ninja-build gcovr lcov qt58-meta-full clang-4.0 lld-4.0
     
-
-    sudo wget https://downloads.sourceforge.net/project/boost/boost/1.63.0/boost_1_63_0.tar.bz2 -O /opt/boost.tar.bz2
+    wait wget || true
     (cd /opt; sudo tar xaf boost.tar.bz2; sudo mv boost_* boost ; sudo chmod -R a+rwx boost)
 
-    if [[ "$CONF" == "linux-package" ]];
-    then
-      sudo wget https://www.dropbox.com/s/a8w8o3mu0jfr3t8/qt5.6-static-release.tar.xz?dl=1 -O /opt/qt5.6-static-release.tgz
-      (cd /opt; sudo tar xaf qt5.6-static-release.tgz)
-      sudo apt-get install -qq libxcb-icccm4-dev libxi-dev libxcb-image0-dev libxcb-keysyms1-dev libxcb-xkb-dev libxcb-render-util0-dev libxinerama-dev libxcb-xinerama0-dev libudev-dev
-    else
-      sudo apt-get install -qq qt58-meta-full
-    fi
-    
-    wget https://github.com/OSSIA/iscore-sdk/releases/download/6.0-osx/gcc.deb -O gcc.deb
-    sudo dpkg --force-all -i gcc.deb
-
-    wget https://cmake.org/files/v3.7/cmake-3.7.2-Linux-x86_64.tar.gz -O cmake-linux.tgz
     tar xaf cmake-linux.tgz
     mv cmake-*-x86_64 cmake
   ;;
