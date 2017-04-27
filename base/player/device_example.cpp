529a587fadf411b8d43b1b690ca66a9166b70403
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-04-27 18:20:51 +0200

    example documentation

--- base/player/device_example.cpp
+++ base/player/device_example.cpp
@@ -1,25 +1,31 @@
 #include "player.hpp"
 #include <ossia/network/generic/generic_device.hpp>
 
-int main(int argc, char** argv)
+int main()
 {
-    ossia::net::generic_device dev;
-    dev.set_name("OSCdevice");
+  // Create a device
+  ossia::net::generic_device dev;
 
-    auto address = ossia::net::create_node(dev, "/foo/bar").create_address(ossia::val_type::FLOAT);
-    address->add_callback([] (const ossia::value& val) {
-       std::cerr << val << std::endl;
-    });
+  // The name has to match one of the devices in the loaded file
+  dev.set_name("OSCdevice");
 
-    //if(argc > 1)
-    {
-        iscore::Player p;
-        p.load("/tmp/device.scorejson");
-        p.registerDevice(dev);
+  // Add a custom callback on the device
+  auto address = ossia::net::create_node(dev, "/foo/bar").create_address(ossia::val_type::FLOAT);
+  address->add_callback([] (const ossia::value& val) {
+    std::cerr << val << std::endl;
+  });
 
-        p.play();
-        std::this_thread::sleep_for(std::chrono::seconds(5));
-        p.stop();
-    }
-    return 0;
+  // Create a player and load a file
+  iscore::Player p;
+  p.load("/tmp/device.scorejson");
+
+  // The device will replace the current implementation that was loaded
+  p.registerDevice(dev);
+
+  // Execution occurs in a separate thread
+  p.play();
+  std::this_thread::sleep_for(std::chrono::seconds(5));
+  p.stop();
+
+  return 0;
 }
