2b6bb7b6e09561d6a16cd30cb636d19b898fe618
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-04-28 18:13:43 +0200

    disable lto on travis

--- CMake/Configurations/travis/release.cmake
+++ CMake/Configurations/travis/release.cmake
@@ -5,5 +5,8 @@ set(DEPLOYMENT_BUILD True)
 if(NOT DEFINED ISCORE_ENABLE_LTO)
 set(ISCORE_ENABLE_LTO True)
 endif()
+if(UNIX AND EXISTS "/etc/centos-release")
+  set(ISCORE_ENABLE_LTO False)
+endif()
 
 include(default-plugins)
