d4ab3e550e0157c1773372e911d41ff03caf8d08
Antoine Villeret <antoine.villeret@gmail.com>
2017-05-14 10:21:42 +0200

    First commit of i-score Pd object

---
+++ base/player/i-score-pd/iscore-pd.cpp
@@ -0,0 +1,65 @@
+#include <sstream>
+#include "player.hpp"
+
+extern "C" {
+#include <cicm_wrapper.h>
+}
+
+namespace iscore {
+namespace player {
+
+struct t_iscore
+{
+    t_eobj     obj; // pd object - always placed first in the object's struct
+    iscore::Player p;
+};
+
+static t_eclass *iscore_class;
+
+static void iscore_bang(t_iscore* x){
+
+}
+
+static void iscore_float(t_iscore* x, t_float f){
+    if (f > 0.){
+       x->p.play();
+    } else {
+       x->p.stop();
+    }
+}
+
+static void iscore_load(t_iscore* x, t_symbol* s){
+    try{
+      x->p.load(s->s_name);
+    } catch (std::exception& e){
+        pd_error(x,"can't open file %s: %s", s->s_name, e.what());
+    }
+}
+
+static void *iscore_new(t_symbol *name, int argc, t_atom *argv)
+{
+    t_iscore *x = (t_iscore *)eobj_new(iscore_class);
+    return (x);
+}
+
+static void iscore_free(t_iscore *x){
+
+}
+
+extern "C" void setup_i0x2dscore(void)
+{
+    t_eclass* c = eclass_new("i-score",
+                           (method)iscore_new, (method)iscore_free,
+                           sizeof(t_iscore), CLASS_DEFAULT, A_GIMME, 0);
+
+    if (c){
+        eclass_addmethod(c, (method) iscore_bang,  "bang",     A_NULL,   0);
+        eclass_addmethod(c, (method) iscore_float, "float",    A_FLOAT,  0);
+        eclass_addmethod(c, (method) iscore_load,  "load",     A_SYMBOL, 0);
+    }
+    iscore_class = c;
+}
+
+
+}
+}
