a2d7660f6de045966883ba223db1db30e101f851
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-07-24 09:42:48 +0200

    fix linux build... should investigate why gcc won't build on travis

--- tools/travis/configs/linux-source-qt.sh
+++ tools/travis/configs/linux-source-qt.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set +eux
-export CC=/usr/bin/gcc-7
-export CXX=/usr/bin/g++-7
+export CC=/usr/bin/clang-4.0
+export CXX=/usr/bin/clang++-4.0
 export BOOST_ROOT=/opt/boost
 export CMAKE_COMMON_FLAGS="-DOSSIA_DISABLE_COTIRE=1"
 QT_ENV_SCRIPT=$(find /opt -name 'qt*-env.sh')
