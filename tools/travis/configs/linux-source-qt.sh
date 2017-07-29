4c983f547beee904e9c8a37c348f8ab34f8b8f1e
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-07-29 17:25:47 +0200

    try other build

--- tools/travis/configs/linux-source-qt.sh
+++ tools/travis/configs/linux-source-qt.sh
@@ -3,7 +3,7 @@ set +eux
 export CC=/usr/bin/clang-4.0
 export CXX=/usr/bin/clang++-4.0
 export BOOST_ROOT=/opt/boost
-export CMAKE_COMMON_FLAGS="-DOSSIA_DISABLE_COTIRE=1"
+export CMAKE_COMMON_FLAGS="-GNinja"
 QT_ENV_SCRIPT=$(find /opt -name 'qt*-env.sh')
 source $QT_ENV_SCRIPT
 
