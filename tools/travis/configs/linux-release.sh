6d158fe6eb9e5ef724447abf99ce297c2089a844
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-08-08 13:01:53 +0200

    update version

--- tools/travis/configs/linux-release.sh
+++ tools/travis/configs/linux-release.sh
@@ -4,4 +4,4 @@ source "$CONFIG_FOLDER/linux-source-qt.sh"
 $CMAKE_BIN -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" -DBOOST_ROOT="$BOOST_ROOT" -DISCORE_CONFIGURATION=release $CMAKE_COMMON_FLAGS -DCMAKE_SKIP_RPATH=ON ..
 
 $CMAKE_BIN --build . --target all_unity -- -j2
-$CMAKE_BIN --build . --target package -- -j2
+# $CMAKE_BIN --build . --target package -- -j2
