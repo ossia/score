8cd6bb50eb478817d0de4445699ed831c6570c71
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-09-12 23:31:09 +0200

    should fix linux build

--- tools/travis/configs/linux-package-appimage.sh
+++ tools/travis/configs/linux-package-appimage.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 docker pull ossia/score-package-linux
-docker run --name buildvm ossia/score-package-linux /bin/bash Recipe
+docker run --name buildvm -v "$(pwd)"/../CMake/Deployment/Linux/AppImage/Recipe:/Recipe ossia/score-package-linux /bin/bash /Recipe
+#docker run --name buildvm ossia/score-package-linux /bin/bash Recipe
 docker cp buildvm:/score.AppDir.txz .
 
 tar xaf score.AppDir.txz
@@ -12,6 +13,6 @@ chmod a+rwx score.AppDir/AppRun
 wget "https://github.com/probonopd/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
 chmod a+x appimagetool-x86_64.AppImage
 
-./appimagetool-x86_64.AppImage "score.AppDir" "Score.AppImage"
+./appimagetool-x86_64.AppImage -n "score.AppDir" "Score.AppImage"
 
 chmod a+rwx Score.AppImage
