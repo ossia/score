b8e3d72a0b2fd31775efbd888bbdf35711a6928d
Antoine Villeret <antoine.villeret+dev@gmail.com>
2017-11-29 00:02:25 +0100

    revert commit c391e5ed826746f99c90a19c70ebc4b96debf9c1)

--- tools/travis/configs/linux-static-debug.sh
+++ tools/travis/configs/linux-static-debug.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-source `readlink -e ${0%/*}/linux-source-qt.sh`
+source "$CONFIG_FOLDER/linux-source-qt.sh"
 
 $CMAKE_BIN -DCMAKE_C_COMPILER="$CC" -DCMAKE_CXX_COMPILER="$CXX" -DBOOST_ROOT="$BOOST_ROOT" -DSCORE_CONFIGURATION=static-debug ..
 $CMAKE_BIN --build . --target all_unity -- -j2
--- tools/travis/configs/osx-package.sh
+++ tools/travis/configs/osx-package.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-source "${0%/*}/osx-source-qt.sh"
+source "$CONFIG_FOLDER/osx-source-qt.sh"
 
 $CMAKE_BIN $CMAKE_COMMON_FLAGS -DSCORE_CONFIGURATION=static-release ..
 $CMAKE_BIN --build . --target all_unity -- -j2
