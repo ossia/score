af05176206ac4d287234a775a36992ffee9da2ec
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-12-04 00:47:44 +0100

    load - save VSTs ; make kontakt work

---
+++ base/app/mac_main.m
@@ -0,0 +1,13 @@
+#include <Foundation/Foundation.h>
+
+NSAutoreleasePool* mac_init_pool()
+{
+  return nullptr;
+  //NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+  //return pool;
+}
+
+void mac_finish_pool(NSAutoreleasePool* pool)
+{
+  //[pool release];
+}
--- base/plugins/score-plugin-media/Media/Effect/VST/AppleUI/vst2x.mm
+++ base/plugins/score-plugin-media/Media/Effect/VST/AppleUI/vst2x.mm
@@ -42,8 +42,11 @@ void show_vst2_editor(AEffect* effect, uint16_t width, uint16_t height)
 void hide_vst2_editor(AEffect* effect)
 {
   effect->dispatcher(effect, effEditClose, 0, 0, nullptr, 0);
-  auto window = reinterpret_cast<QMacCocoaViewContainer*>(effect->resvd2);
-  window->close();
-  window->deleteLater();
-  effect->resvd2 = 0;
+  if(effect->resvd2)
+  {
+    auto container = reinterpret_cast<QMacCocoaViewContainer*>(effect->resvd2);
+    container->close();
+    container->deleteLater();
+    effect->resvd2 = 0;
+  }
 }
