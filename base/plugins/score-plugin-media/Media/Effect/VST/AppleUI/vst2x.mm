b82fb60d499edf23a074aeb23bdebb3926db2f15
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-12-04 12:09:41 +0100

    fix vst close crash

--- base/plugins/score-plugin-media/Media/Effect/VST/AppleUI/vst2x.mm
+++ base/plugins/score-plugin-media/Media/Effect/VST/AppleUI/vst2x.mm
@@ -5,7 +5,18 @@
 #include <iostream>
 #include <QMacCocoaViewContainer>
 #include <aeffect.h>
+#include <QDialog>
 #include "vstwindow.h"
+#include <QHBoxLayout>
+struct VSTDialog: public QDialog
+{
+    VSTDialog(QWidget* w)
+    {
+      auto lay = new QHBoxLayout(this);
+      lay->setContentsMargins(0, 0, 0, 0);
+      lay->addWidget(w);
+    }
+};
 // TODO have a look at https://github.com/alex-weej/Evilnote/blob/master/vsteditorwidget.mm
 void show_vst2_editor(AEffect* effect, uint16_t width, uint16_t height)
 {
@@ -16,9 +27,12 @@ void show_vst2_editor(AEffect* effect, uint16_t width, uint16_t height)
     container->show();
     return;
   }
+  NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
 
-  NSView *view = [[NSView alloc] initWithFrame: NSMakeRect(0, 0, width, height)];
+  NSView *view = [[NSView alloc] initWithFrame: NSMakeRect(0, 0, width, height)] ;
+
   container = new QMacCocoaViewContainer{view};
+  auto diag = new VSTDialog{container};
   container->resize(width, height);
   container->window()->setWindowFlags(Qt::Window | Qt::WindowCloseButtonHint);
   container->window()->setWindowFlag(Qt::WindowMinimizeButtonHint, false);
@@ -34,19 +48,20 @@ void show_vst2_editor(AEffect* effect, uint16_t width, uint16_t height)
   [view setFrame:frame];
 
   effect->dispatcher(effect, effEditOpen, 0, 0, (void*)view, 0);
-  container->show();
+  diag->show();
 
   effect->resvd2 = reinterpret_cast<VstIntPtr>(container);
+  [pool release];
 }
 
 void hide_vst2_editor(AEffect* effect)
 {
-  effect->dispatcher(effect, effEditClose, 0, 0, nullptr, 0);
   if(effect->resvd2)
   {
     auto container = reinterpret_cast<QMacCocoaViewContainer*>(effect->resvd2);
-    container->close();
-    container->deleteLater();
+    ((QDialog*)container->parent())->close();
+    delete container->parent();
     effect->resvd2 = 0;
   }
+  effect->dispatcher(effect, effEditClose, 0, 0, nullptr, 0);
 }
