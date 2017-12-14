0f94bed00de251c9967f45564646ac1148a5454b
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-12-14 11:57:28 +0100

    fix static osx builds

--- CMake/Deployment/OSX/set_rpath.sh
+++ CMake/Deployment/OSX/set_rpath.sh
@@ -17,7 +17,10 @@ do
   done   
 done
 install_name_tool -add_rpath @executable_path/plugins "$1/MacOS/score"
+install_name_tool -add_rpath @executable_path/../Frameworks "$1/MacOS/score"
 
 # Also fixup for the QtQuick dylibs
 find "$1/Resources/qml" -name '*.dylib' -exec rm {} \;
+find "$1" -name '*dSYM' -exec rm {} \;
+find "$1" -name '*_debug' -exec rm {} \;
 ln -s "$1/PlugIns/quick/libqtquick2plugin.dylib" "$1/Resources/qml/QtQuick.2"
