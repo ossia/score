2138e73f0dc596a95d2154e19ab45276386a22dd
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2017-12-19 22:53:50 +0100

    minor

--- base/plugins/score-plugin-media/Media/Effect/Inspector/EffectInspector.cpp
+++ base/plugins/score-plugin-media/Media/Effect/Inspector/EffectInspector.cpp
@@ -34,167 +34,196 @@ namespace Effect
 {
 
 InspectorWidget::InspectorWidget(
-        const Effect::ProcessModel &object,
-        const score::DocumentContext &doc,
-        QWidget *parent):
-    InspectorWidgetDelegate_T {object, parent},
-    m_dispatcher{doc.commandStack}
+    const Effect::ProcessModel &object,
+    const score::DocumentContext &doc,
+    QWidget *parent):
+  InspectorWidgetDelegate_T {object, parent},
+  m_dispatcher{doc.commandStack}
+, m_ctx{doc}
 {
-    setObjectName("EffectInspectorWidget");
+  setObjectName("EffectInspectorWidget");
 
-    auto lay = new QVBoxLayout;
-    m_list = new QListWidget;
-    lay->addWidget(m_list);
-    con(process(), &Effect::ProcessModel::effectsChanged,
-        this, &InspectorWidget::recreate);
-
-
-    connect(m_list, &QListWidget::itemDoubleClicked,
-            this, [=] (QListWidgetItem* item) {
-        // Make a text dialog to edit faust program.
-        auto id = item->data(Qt::UserRole).value<Id<EffectModel>>();
-        auto proc = &process().effects().at(id);
-        proc->hideUI();
-        proc->showUI();
-    }, Qt::QueuedConnection);
-
-    recreate();
+  auto lay = new QVBoxLayout;
+  m_list = new QListWidget;
+  lay->addWidget(m_list);
+  con(process(), &Effect::ProcessModel::effectsChanged,
+      this, &InspectorWidget::recreate);
 
 
-    // Add an effect
-    {
-      auto add = new QPushButton{tr("Add (Score)")};
-      connect(add, &QPushButton::pressed, this, [&] {
+  connect(m_list, &QListWidget::itemDoubleClicked,
+          this, [=] (QListWidgetItem* item) {
+    // Make a text dialog to edit faust program.
+    auto id = item->data(Qt::UserRole).value<Id<EffectModel>>();
+    auto proc = &process().effects().at(id);
+    proc->hideUI();
+    proc->showUI();
+  }, Qt::QueuedConnection);
 
-        auto& base_fxs = doc.app.interfaces<EffectFactoryList>();
-        auto dialog = new Scenario::AddProcessDialog<EffectFactoryList>(base_fxs, this);
+  m_list->setContextMenuPolicy(Qt::DefaultContextMenu);
+  recreate();
 
-        dialog->on_okPressed = [&] (const auto& proc)  {
-          m_dispatcher.submitCommand(
-                      new Commands::InsertEffect{process(), proc, "", process().effects().size()});
-        };
-        dialog->launchWindow();
-        dialog->deleteLater();
-      });
-      lay->addWidget(add);
-    }
-    {
+  // Add an effect
+  {
+    auto add = new QPushButton{tr("Add (Score)")};
+    connect(add, &QPushButton::pressed,
+            this, [&] { add_score(cur_pos()); });
+    lay->addWidget(add);
+  }
+  {
 #if defined(HAS_FAUST)
     m_add = new QPushButton{tr("Add (Faust)")};
     connect(m_add, &QPushButton::pressed,
-            this, [=] () {
-        m_dispatcher.submitCommand(
-                    new Commands::InsertEffect{
-                        process(),
-                        FaustEffectFactory::static_concreteKey(),
-                        "",
-                        0});
-    });
+            this, [&] { add_faust(cur_pos()); });
 
     lay->addWidget(m_add);
 #endif
-    }
+  }
 
-    {
+  {
 #if defined(LILV_SHARED)
     auto add_lv2 = new QPushButton{tr("Add (LV2)")};
     connect(add_lv2, &QPushButton::pressed,
-            this, [=] () {
-        auto& world = score::AppComponents().applicationPlugin<Media::ApplicationPlugin>().lilv;
-
-        auto plugs = world.get_all_plugins();
-
-        QStringList items;
-
-        auto it = plugs.begin();
-        while(!plugs.is_end(it))
-        {
-            auto plug = plugs.get(it);
-            items.push_back(plug.get_name().as_string());
-            it = plugs.next(it);
-        }
-
-        auto res = QInputDialog::getItem(
-                    this,
-                    tr("Select a plug-in"), tr("Select a LV2 plug-in"),
-                    items, 0, false);
-
-        if(!res.isEmpty())
-        {
-            m_dispatcher.submitCommand(
-                        new Commands::InsertEffect{
-                            process(),
-                            Media::LV2::LV2EffectModel::static_concreteKey(),
-                            res,
-                            process().effects().size()});
-        }
-    });
+            this, [&] { add_lv2(cur_pos()); });
 
     lay->addWidget(add_lv2);
 #endif
-    }
+  }
 
 #if defined(HAS_VST2)
-    {
-      auto add_vst = new QPushButton{tr("Add (VST 2)")};
-      connect(add_vst, &QPushButton::pressed,
-              this, [=] () {
+  {
+    auto add_vst = new QPushButton{tr("Add (VST 2)")};
+    connect(add_vst, &QPushButton::pressed,
+            this, [&] { add_vst2(cur_pos()); });
 
-        QString defaultPath;
+    lay->addWidget(add_vst);
+  }
+#endif
+
+  // Remove an effect
+  // Effects changed
+  // Effect list
+  // Double-click : open editor window.
+
+  this->setLayout(lay);
+}
+
+void InspectorWidget::add_score(std::size_t pos)
+{
+  auto& base_fxs = m_ctx.app.interfaces<EffectFactoryList>();
+  auto dialog = new Scenario::AddProcessDialog<EffectFactoryList>(base_fxs, this);
+
+
+  dialog->on_okPressed = [&] (const auto& proc)  {
+    m_dispatcher.submitCommand(
+          new Commands::InsertEffect{process(), proc, "", pos});
+  };
+  dialog->launchWindow();
+  dialog->deleteLater();
+}
+
+void InspectorWidget::add_lv2(std::size_t pos)
+{
+#if defined(LILV_SHARED)
+  auto& world = score::AppComponents().applicationPlugin<Media::ApplicationPlugin>().lilv;
+
+  auto plugs = world.get_all_plugins();
+
+  QStringList items;
+
+  auto it = plugs.begin();
+  while(!plugs.is_end(it))
+  {
+    auto plug = plugs.get(it);
+    items.push_back(plug.get_name().as_string());
+    it = plugs.next(it);
+  }
+
+  auto res = QInputDialog::getItem(
+               this,
+               tr("Select a plug-in"), tr("Select a LV2 plug-in"),
+               items, 0, false);
+
+  if(!res.isEmpty())
+  {
+    m_dispatcher.submitCommand(
+          new Commands::InsertEffect{
+            process(),
+            Media::LV2::LV2EffectModel::static_concreteKey(),
+            res,
+            pos});
+  }
+#endif
+}
+void InspectorWidget::add_faust(std::size_t pos)
+{
+#if defined(HAS_FAUST)
+  m_dispatcher.submitCommand(
+        new Commands::InsertEffect{
+          process(),
+          FaustEffectFactory::static_concreteKey(),
+          "",
+          pos});
+#endif
+}
+
+void InspectorWidget::add_vst2(std::size_t pos)
+{
+#if defined(HAS_VST2)
+
+  QString defaultPath;
 #if defined(__APPLE__)
-        defaultPath = "/Library/Audio/Plug-Ins/VST";
+  defaultPath = "/Library/Audio/Plug-Ins/VST";
 #elif defined(__linux__)
-        defaultPath = "/usr/lib/vst";
+  defaultPath = "/usr/lib/vst";
 #endif
-        auto res = QFileDialog::getOpenFileName(
-                     this,
-                     tr("Select a VST plug-in"), defaultPath,
-                     "VST (*.dll *.so *.vst *.dylib)");
+  auto res = QFileDialog::getOpenFileName(
+               this,
+               tr("Select a VST plug-in"), defaultPath,
+               "VST (*.dll *.so *.vst *.dylib)");
 
-        if(!res.isEmpty())
-        {
-          m_dispatcher.submitCommand(
-                new Commands::InsertEffect{
-                  process(),
-                  Media::VST::VSTEffectModel::static_concreteKey(),
-                  res,
-                  process().effects().size()});
-        }
-      });
-
-      lay->addWidget(add_vst);
-    }
+  if(!res.isEmpty())
+  {
+    m_dispatcher.submitCommand(
+          new Commands::InsertEffect{
+            process(),
+            Media::VST::VSTEffectModel::static_concreteKey(),
+            res, pos});
+  }
 #endif
+}
 
-    // Remove an effect
-    // Effects changed
-    // Effect list
-    // Double-click : open editor window.
+void InspectorWidget::addRequested(int pos)
+{
+}
 
-    this->setLayout(lay);
+int InspectorWidget::cur_pos()
+{
+  if(m_list->currentRow() >= 0)
+    return m_list->currentRow();
+  return process().effects().size();
 }
 struct ListWidgetItem :
-        public QObject,
-        public QListWidgetItem
+    public QObject,
+    public QListWidgetItem
 {
-    public:
-        using QListWidgetItem::QListWidgetItem;
+  public:
+    using QListWidgetItem::QListWidgetItem;
 
 };
 
 void InspectorWidget::recreate()
 {
-    m_list->clear();
+  m_list->clear();
 
-    for(const auto& fx : process().effects())
-    {
-        auto item = new ListWidgetItem(fx.prettyName(), m_list);
+  for(const auto& fx : process().effects())
+  {
+    auto item = new ListWidgetItem(fx.prettyName(), m_list);
 
-        con(fx.metadata(), &score::ModelMetadata::LabelChanged,
-            item, [=] (const auto& txt) { item->setText(txt); });
-        item->setData(Qt::UserRole, QVariant::fromValue(fx.id()));
-        m_list->addItem(item);
-    }
+    con(fx.metadata(), &score::ModelMetadata::LabelChanged,
+        item, [=] (const auto& txt) { item->setText(txt); });
+    item->setData(Qt::UserRole, QVariant::fromValue(fx.id()));
+    m_list->addItem(item);
+  }
 }
 }
 }
--- base/plugins/score-plugin-media/Media/Effect/Inspector/EffectInspector.hpp
+++ base/plugins/score-plugin-media/Media/Effect/Inspector/EffectInspector.hpp
@@ -3,7 +3,7 @@
 #include <Process/Inspector/ProcessInspectorWidgetDelegateFactory.hpp>
 #include <score/command/Dispatchers/CommandDispatcher.hpp>
 #include <Media/Effect/EffectProcessModel.hpp>
-
+#include <QMenu>
 class QListWidget;
 class QPushButton;
 
@@ -12,25 +12,34 @@ namespace Media
 namespace Effect
 {
 class InspectorWidget final :
-        public Process::InspectorWidgetDelegate_T<ProcessModel>
+    public Process::InspectorWidgetDelegate_T<ProcessModel>
 {
-    public:
-        explicit InspectorWidget(
-                const ProcessModel& object,
-                const score::DocumentContext& doc,
-                QWidget* parent);
+  public:
+    explicit InspectorWidget(
+        const ProcessModel& object,
+        const score::DocumentContext& doc,
+        QWidget* parent);
 
-    private:
-        void recreate();
-        QListWidget* m_list{};
-        QPushButton* m_add{};
-        CommandDispatcher<> m_dispatcher;
+  private:
+    void add_score(std::size_t pos);
+    void add_lv2(std::size_t pos);
+    void add_vst2(std::size_t pos);
+    void add_faust(std::size_t pos);
+    void recreate();
+    QListWidget* m_list{};
+    QPushButton* m_add{};
+    CommandDispatcher<> m_dispatcher;
+    const score::DocumentContext& m_ctx;
+    void addRequested(int pos);
+    int cur_pos();
+
+    QMenu m_contextMenu;
 };
 
 class InspectorFactory final :
-        public Process::InspectorWidgetDelegateFactory_T<ProcessModel, InspectorWidget>
+    public Process::InspectorWidgetDelegateFactory_T<ProcessModel, InspectorWidget>
 {
-        SCORE_CONCRETE("cc8ceff3-ef93-4b73-865a-a9f870d6e898")
+    SCORE_CONCRETE("cc8ceff3-ef93-4b73-865a-a9f870d6e898")
 };
 }
 }
