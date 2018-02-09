42524ca1de6442b9065de17aecdbc450f212bafb
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-02-09 13:02:10 +0100

    rescale midi

--- base/plugins/score-plugin-midi/Midi/Commands/ScaleNotes.cpp
+++ base/plugins/score-plugin-midi/Midi/Commands/ScaleNotes.cpp
@@ -43,4 +43,45 @@ void ScaleNotes::deserializeImpl(DataStreamOutput& s)
 {
   s >> m_model >> m_toScale >> m_delta;
 }
+
+
+RescaleMidi::RescaleMidi(
+    const ProcessModel& model,
+    double delta)
+    : m_model{model}, m_delta{delta}
+{
+  m_old.reserve(model.notes.size());
+  for(auto& note : model.notes)
+    m_old.push_back({note.id(), note.noteData()});
+}
+
+void RescaleMidi::undo(const score::DocumentContext& ctx) const
+{
+  auto& model = m_model.find(ctx);
+  model.notes.clear();
+  for(auto& note : m_old)
+  {
+    model.notes.add(new Note{note.first, note.second, &model});
+  }
+}
+
+void RescaleMidi::redo(const score::DocumentContext& ctx) const
+{
+  auto& model = m_model.find(ctx);
+  for (auto& note : model.notes)
+  {
+    note.setStart(note.start() * m_delta);
+    note.setDuration(note.duration() * m_delta);
+  }
+}
+
+void RescaleMidi::serializeImpl(DataStreamInput& s) const
+{
+  s << m_model << m_old << m_delta;
+}
+
+void RescaleMidi::deserializeImpl(DataStreamOutput& s)
+{
+  s >> m_model >> m_old >> m_delta;
+}
 }
