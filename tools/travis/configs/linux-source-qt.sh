ca2929ddab55c02873cdc9b6f75f5d0f050b1028
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-03-08 20:26:25 +0100

    find portaudio on ubuntu

--- tools/travis/configs/linux-source-qt.sh
+++ tools/travis/configs/linux-source-qt.sh
@@ -3,7 +3,7 @@ set +eux
 export CC=/usr/bin/clang-6.0
 export CXX=/usr/bin/clang++-6.0
 export BOOST_ROOT=/opt/boost
-export CMAKE_COMMON_FLAGS="-GNinja"
+export CMAKE_COMMON_FLAGS="-GNinja -DPORTAUDIO_ONLY_DYNAMIC=1"
 QT_ENV_SCRIPT=$(find /opt -name 'qt*-env.sh')
 source $QT_ENV_SCRIPT
 
