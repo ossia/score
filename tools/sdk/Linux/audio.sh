d78792fa4840899910a6189a7316e87f9461ae65
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-04-27 18:37:25 +0200

    wip docker

--- tools/sdk/Linux/audio.sh
+++ tools/sdk/Linux/audio.sh
@@ -2,7 +2,9 @@
 wget -nv https://ffmpeg.org/releases/ffmpeg-4.0.tar.bz2
 tar -xaf ffmpeg-4.0.tar.bz2
 cd ffmpeg-4.0
-./configure --arch=x86_64 --cpu=x86_64 --disable-doc --disable-ffmpeg --disable-ffplay --disable-debug --prefix=/usr/local --pkg-config-flags="--static" --enable-gpl --enable-version3 --disable-x86asm --disable-openssl --disable-protocols --disable-securetransport --disable-videotoolbox --disable-network --disable-iconv
+./configure --arch=x86_64 --cpu=x86_64 --disable-doc --disable-ffmpeg --disable-ffplay --disable-debug --prefix=/usr/local --pkg-config-flags="--static" --enable-gpl --enable-version3 --disable-x86asm --disable-openssl --disable-protocols --disable-securetransport --disable-videotoolbox --disable-network --disable-iconv --disable-lzma
+make -j8
+make install
 
 yum -y install cmake alsa-lib-devel
 wget -nv http://www.portaudio.com/archives/pa_snapshot.tgz
@@ -12,5 +14,6 @@ cmake .. -DCMAKE_BUILD_TYPE=Release -DCMAKE_INSTALL_PREFIX=/usr/local
 make -j8
 make install
 cp libportaudio_static.a /usr/local/lib
+cp libportaudio_static.a /usr/local/lib64
 
 # jack-audio-connection-kit-devel
