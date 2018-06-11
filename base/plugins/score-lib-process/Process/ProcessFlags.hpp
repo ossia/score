14561bc494a9e320fc63574e63a2abaf6976480c
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-06-11 16:38:52 +0200

    fix dumb bug

--- base/plugins/score-lib-process/Process/ProcessFlags.hpp
+++ base/plugins/score-lib-process/Process/ProcessFlags.hpp
@@ -14,22 +14,22 @@ class EffectFactory;
 enum ProcessFlags : int64_t
 {
   //! Can be loaded as a process of an interval
-  SupportsTemporal = 0x1,
+  SupportsTemporal = 1,
 
   //! Can be loaded in an effect chain
-  SupportsEffectChain = 0x2,
+  SupportsEffectChain = 2,
 
   //! Can be loaded in a state
-  SupportsState = 0x4,
+  SupportsState = 4,
 
   //! Action from the user required upon creation
-  RequiresCustomData = 0x8,
+  RequiresCustomData = 8,
 
   //! When created in an interval, go on the top slot or in a new slot
-  PutInNewSlot = 0x16,
+  PutInNewSlot = 16,
 
   //! The process won't change if the parent duration changes (eg it's a filter)
-  TimeIndependent = 0x32,
+  TimeIndependent = 32,
 
   SupportsLasting = SupportsTemporal | SupportsEffectChain,
   ExternalEffect = SupportsLasting | RequiresCustomData | TimeIndependent,
