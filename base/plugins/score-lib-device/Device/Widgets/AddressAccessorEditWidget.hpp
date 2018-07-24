72ac4be6be61fa2d297e616a5d56bb579fae54c5
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-07-24 19:42:42 +0200

    refactor, port list

---
+++ base/plugins/score-lib-device/Device/Widgets/AddressAccessorEditWidget.hpp
@@ -0,0 +1,40 @@
+#pragma once
+#include <Device/Address/AddressSettings.hpp>
+#include <wobjectdefs.h>
+#include <QWidget>
+#include <score_lib_device_export.h>
+
+class QLineEdit;
+namespace Device
+{
+class NodeBasedItemModel;
+}
+namespace Device
+{
+class SCORE_LIB_DEVICE_EXPORT AddressAccessorEditWidget final
+    : public QWidget
+{
+  W_OBJECT(AddressAccessorEditWidget)
+public:
+  AddressAccessorEditWidget(Device::NodeBasedItemModel& model, QWidget* parent);
+
+  void setAddress(const State::AddressAccessor& addr);
+  void setFullAddress(Device::FullAddressAccessorSettings&& addr);
+
+  const Device::FullAddressAccessorSettings& address() const;
+
+  QString addressString() const;
+
+  void dragEnterEvent(QDragEnterEvent* event) override;
+  void dropEvent(QDropEvent*) override;
+public:
+  void addressChanged(const Device::FullAddressAccessorSettings& arg_1) E_SIGNAL(SCORE_LIB_DEVICE_EXPORT, addressChanged, arg_1);
+
+private:
+  void customContextMenuEvent(const QPoint& p);
+
+  QLineEdit* m_lineEdit{};
+  Device::FullAddressAccessorSettings m_address;
+  Device::NodeBasedItemModel& m_model;
+};
+}
---
+++ base/plugins/score-lib-process/Process/Commands/EditPort.cpp
@@ -0,0 +1,34 @@
+#include <Process/Commands/EditPort.hpp>
+#include <Process/Dataflow/Port.hpp>
+#include <score/model/path/PathSerialization.hpp>
+namespace Process
+{
+
+ChangePortAddress::ChangePortAddress(
+    const Process::Port& p, State::AddressAccessor addr)
+    : m_model{p}, m_old{p.address()}, m_new{std::move(addr)}
+{
+}
+
+void ChangePortAddress::undo(const score::DocumentContext& ctx) const
+{
+  auto& m = m_model.find(ctx);
+  m.setAddress(m_old);
+}
+
+void ChangePortAddress::redo(const score::DocumentContext& ctx) const
+{
+  auto& m = m_model.find(ctx);
+  m.setAddress(m_new);
+}
+
+void ChangePortAddress::serializeImpl(DataStreamInput& s) const
+{
+  s << m_model << m_old << m_new;
+}
+
+void ChangePortAddress::deserializeImpl(DataStreamOutput& s)
+{
+  s >> m_model >> m_old >> m_new;
+}
+}
