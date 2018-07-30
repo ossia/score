6619306c16c852e72b5c009f7cdb228dba187c0a
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-07-30 13:39:20 +0200

    add licenses in about

--- base/lib/core/presenter/AboutDialog.cpp
+++ base/lib/core/presenter/AboutDialog.cpp
@@ -12,7 +12,7 @@
 #include <map>
 #include <score_git_info.hpp>
 #include <score/widgets/Pixmap.hpp>
-
+#include <score_licenses.hpp>
 namespace score {
 AboutDialog::AboutDialog(QWidget *parent) :
   QDialog(parent),
@@ -43,9 +43,81 @@ AboutDialog::AboutDialog(QWidget *parent) :
   }
 
   // map
-  std::map<QString, QString> map;
-  map["Qt"] = "GNU General Public License v3";
-  map["Boost"] = "Boost License";
+  struct License
+  {
+    QString url;
+    QString license;
+  };
+
+  std::map<QString, License> map;
+  map["Score"] = License{"https://ossia.io", score_LICENSE};
+
+  map["libossia"] = License{"https://ossia.io", ossia_LICENSE};
+
+  map["Qt"] = License{"https://www.qt.io"
+      , "GNU General Public License v3"};
+
+  map["Boost"] = License{"https://www.boost.org"
+      , "Boost Software License 1.0"};
+
+  // In libossia
+  map["Asio"] = License{"https://github.com/chriskohlhoff/asio", Asio_LICENSE};
+  map["CMake"] = License{"https://www.cmake.org", "BSD 3-clause License"};
+  map["TinySpline"] = License{"https://github.com/msteinbeck/tinyspline", "MIT License"};
+  map["GSL"] = License{"https://github.com/Microsoft/GSL", GSL_LICENSE};
+
+  map["RtMidi17"] = License{"https://github.com/jcelerier/RtMidi17",
+                            "Based on RtMidi (https://github.com/thestk/rtmidi) and "
+                            "ModernMIDI (https://github.com/ddiakopoulos/ModernMIDI)\n"
+                            "RtMidi license: " rtmidi_LICENSE
+                            "\n\nModernMidi license: " modernmidi_LICENSE
+  };
+  map["Servus"] = License{"https://github.com/jcelerier/Servus", "Based on https://github.com/HBPVIS/Servus\n" servus_LICENSE};
+
+  map["SmallFunction"] = License{"https://github.com/jcelerier/SmallFunction"
+      , "Based on https://github.com/LoopPerfect/smallfunction\n" smallfun_LICENSE};
+
+  map["bitset2"] = License{"https://github.com/ClaasBontus/bitset2", bitset2_LICENSE};
+  map["Brigand"] = License{"https://github.com/edouarda/brigand", Brigand_LICENSE};
+  map["Chobo"] = License{"https://github.com/Chobolabs/chobo-shl", chobo_LICENSE};
+  map["ConcurrentQueue"] = License{"https://github.com/cameron314/concurrentqueue", concurrentqueue_LICENSE};
+  map["ReaderWriterQueue"] = License{"https://github.com/cameron314/readerwriterqueue", readerwriterqueue_LICENSE};
+  map["flat"] = License{"https://github.com/jcelerier/flat", "Based on https://github.com/pubby/flat\n" flat_LICENSE};
+  map["flat_hash_map"] = License{"https://github.com/jcelerier/flat_hash_map"
+      , "Based on https://github.com/skarupke/flat_hash_map\n" };
+  map["fmt"] = License{"https://github.com/fmtlib/fmt", fmt_LICENSE};
+  map["frozen"] = License{"https://github.com/serge-sans-paille/frozen", frozen_LICENSE};
+  map["hopscotch-map"] = License{"https://github.com/tessil/hopscotch-map", hopscotchmap_LICENSE};
+  map["multi_index"] = License{"https://github.com/jcelerier/multi_index", multiindex_LICENSE};
+  map["nano-signal-slot"] = License{"https://github.com/jcelerier/nano-signal-slot", nanosignal_LICENSE};
+  map["OSCPack"] = License{"https://github.com/jcelerier/oscpack", "Boost License"};
+  map["Pure Data"] = License{"https://github.com/pure-data/pure-data.git", pd_LICENSE};
+  map["pybind11"] = License{"https://github.com/pybind/pybind11", pybind11_LICENSE};
+  map["rapidjson"] = License{"https://github.com/miloyip/rapidjson", rapidjson_LICENSE};
+  map["spdlog"] = License{"https://github.com/gabime/spdlog", spdlog_LICENSE};
+  map["variant"] = License{"https://github.com/eggs-cpp/variant", variant_LICENSE};
+  map["verdigris"] = License{"https://github.com/jcelerier/verdigris", verdigris_LICENSE};
+  map["weakjack"] = License{"https://github.com/jcelerier/weakjack"
+                          , "Based on https://github.com/x42/weakjack\nGNU General Public License version 2 (or later)"};
+  map["websocketpp"] = License{"https://github.com/jcelerier/websocketpp", websocketpp_LICENSE};
+  map["whereami"] = License{"https://github.com/gpakosz/whereami", whereami_LICENSE};
+
+  // In score
+  map["QMenuView"] = License{"https://github.com/pvanek/qlipper/blob/master/qmenuview", "CeCILL license. \nXINX\nCopyright Ulrich Van Den Hekke, (2007-2011)\nxinx@shadoware.org"};
+  map["QProgressIndicator"] = License{"https://github.com/jcelerier/QProgressIndicator", qprogressindicator_LICENSE}; // TODO based on ...
+  map["Qt-Color-Widgets"] = License{"https://github.com/jcelerier/Qt-Color-Widgets.git", qtcolorwidgets_LICENSE};
+  map["PSIMPL"] = License{"http://psimpl.sf.net", "MPL 1.1"};
+  map["miniz"] = License{"https://github.com/richgel999/miniz", "MIT License"};
+  map["desktopqqc2style"] = License{"https://anongit.kde.org/scratch/mart/desktopqqc2style.git", "MIT License"};
+
+  // TODO ifdefs
+  map["SDL"] = License{"", "Boost License"};
+  map["Faust"] = License{"https://faust.grame.fr", "GNU General Public License"};
+  map["PortAudio"] = License{"https://www.portaudio.com", portaudio_LICENSE};
+  map["libJACK"] = License{"https://jackaudio.org", "GNU Lesser General Public License"};
+  map["Phidgets"] = License{"https://www.phidgets.com/", "Boost License"};
+  map["FFMPEG"] = License{"https://ffmpeg.org", "GNU General Public License v3"};
+  map["libusb"] = License{"https://libusb.info/", "GNU Lesser General Public License v2.1 or any later version"};
 
   // software list
   auto softwareList = new QListWidget{this};
@@ -97,12 +169,15 @@ AboutDialog::AboutDialog(QWidget *parent) :
   auto license = new QPlainTextEdit{this};
   license->move(280,230);
   license->resize(185,183);
-  license->setFont(m_catamaranFont);
+  QFont smallCata = m_catamaranFont;
+  smallCata.setPointSize(m_catamaranFont.pointSize() - 2);
+  license->setFont(smallCata);
   license->setReadOnly(true);
   connect(softwareList,&QListWidget::currentTextChanged,this,
           [=] (const QString& currentText)
   {
-    license->setPlainText(map.at(currentText));
+    auto& lic = map.at(currentText);
+    license->setPlainText(lic.url + "\n\n" + lic.license);
   });
 }
 
