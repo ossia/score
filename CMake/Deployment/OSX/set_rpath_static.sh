c575a951f914b4f6a279634c94ae9fb1cf814214
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-08-29 01:12:29 +0200

    app bundle stuff

--- CMake/Deployment/OSX/set_rpath_static.sh
+++ CMake/Deployment/OSX/set_rpath_static.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 install_name_tool -add_rpath @executable_path/../Frameworks "$1/MacOS/score"
+install_name_tool -add_rpath @executable_path/../Frameworks "$1/MacOS/ossia-score-vstpuppet"
 
 # Also fixup for the QtQuick dylibs
 find "$1" -name '*dSYM' -exec rm -rf {} \;
