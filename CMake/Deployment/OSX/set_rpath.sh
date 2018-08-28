303754df739459ff18300e3e4a45d40c6bc59911
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-08-29 01:12:29 +0200

    app bundle stuff

--- CMake/Deployment/OSX/set_rpath.sh
+++ CMake/Deployment/OSX/set_rpath.sh
@@ -1,23 +1,25 @@
-#!/bin/bash 
+#!/bin/bash
 PLUGIN_FOLDER=$1/MacOS/plugins
-for PLUGIN in $PLUGIN_FOLDER/*.dylib; 
+for PLUGIN in $PLUGIN_FOLDER/*.dylib;
 do
   OTOOL_OUTPUT=$(otool -L "$PLUGIN")
   LIBS=$(echo "$OTOOL_OUTPUT" | grep 'libscore_plugin' | grep -v ':' | awk '{print $1}')
 
   echo "Fixing $PLUGIN :"
   install_name_tool -id "@rpath/$PLUGIN" "$PLUGIN"
-  
+
   for LIB in $LIBS
   do
         FIXED_LIB=$(echo "$LIB" | perl -pe 's|(.*?libscore_)|\@rpath/libscore_|')
         echo $LIB
         echo $FIXED_LIB
         install_name_tool -change "$LIB" "$FIXED_LIB" "$PLUGIN"
-  done   
+  done
 done
 install_name_tool -add_rpath @executable_path/plugins "$1/MacOS/score"
 install_name_tool -add_rpath @executable_path/../Frameworks "$1/MacOS/score"
+install_name_tool -add_rpath @executable_path/plugins "$1/MacOS/ossia-score-vstpuppet"
+install_name_tool -add_rpath @executable_path/../Frameworks "$1/MacOS/ossia-score-vstpuppet"
 
 # Also fixup for the QtQuick dylibs
 find "$1/Resources/qml" -name '*.dylib' -exec rm {} \;
