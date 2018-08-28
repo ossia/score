29c252548171935e925c32f7bf7ef977dbeea954
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-08-29 01:15:23 +0200

    for now don't remember execution settings - only allow them for experimentation

--- base/plugins/score-plugin-engine/Execution/Settings/ExecutorModel.cpp
+++ base/plugins/score-plugin-engine/Execution/Settings/ExecutorModel.cpp
@@ -50,6 +50,15 @@ Model::Model(QSettings& set, const score::ApplicationContext& ctx)
     : m_clockFactories{ctx.interfaces<ClockFactoryList>()}
 {
   score::setupDefaultSettings(set, Parameters::list(), *this);
+  // TODO
+  // for now set the unsafe options to safe values on each start
+  setClock(Parameters::Clock.def);
+  setScheduling(Parameters::Scheduling.def);
+  setOrdering(Parameters::Ordering.def);
+  setMerging(Parameters::Merging.def);
+  setCommit(Parameters::Commit.def);
+  setTick(Parameters::Tick.def);
+  setScoreOrder(Parameters::ScoreOrder.def);
 }
 
 std::unique_ptr<Clock>
