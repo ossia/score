93ad25b25291f9f45e9f8b3cd8bb32a1b8e0b592
aliefhooghe <aliefhooghe@enseirb-matmeca.fr>
2018-09-20 15:09:09 +0200

    new Artnet Protocol integration in score started

---
+++ base/plugins/score-plugin-engine/Protocols/Artnet/ArtnetProtocolFactory.cpp
@@ -0,0 +1,60 @@
+
+#include "ArtnetProtocolFactory.hpp"
+
+#include "ArtnetDevice.hpp"
+#include "ArtnetProtocolSettingsWidget.hpp"
+#include "ArtnetSpecificSettings.hpp"
+
+#include <QObject>
+
+namespace Engine::Network {
+
+QString ArtnetProtocolFactory::prettyName() const
+{
+  return QObject::tr("Artnet");
+}
+
+Device::DeviceInterface* ArtnetProtocolFactory::makeDevice(
+    const Device::DeviceSettings& settings, const score::DocumentContext& ctx)
+{
+  return new ArtnetDevice{settings};
+}
+
+const Device::DeviceSettings& ArtnetProtocolFactory::defaultSettings() const
+{
+  static const Device::DeviceSettings& settings = [&]() {
+    Device::DeviceSettings s;
+    s.protocol = concreteKey();
+    s.name = "Artnet";
+    ArtnetSpecificSettings settings;
+    s.deviceSpecificSettings = QVariant::fromValue(settings);
+    return s;
+  }();
+
+  return settings;
+}
+
+Device::ProtocolSettingsWidget* ArtnetProtocolFactory::makeSettingsWidget()
+{
+  return new ArtnetProtocolSettingsWidget;
+}
+
+QVariant ArtnetProtocolFactory::makeProtocolSpecificSettings(
+    const VisitorVariant& visitor) const
+{
+  return makeProtocolSpecificSettings_T<ArtnetSpecificSettings>(visitor);
+}
+
+void ArtnetProtocolFactory::serializeProtocolSpecificSettings(
+    const QVariant& data, const VisitorVariant& visitor) const
+{
+  serializeProtocolSpecificSettings_T<ArtnetSpecificSettings>(data, visitor);
+}
+
+bool ArtnetProtocolFactory::checkCompatibility(
+    const Device::DeviceSettings& a, const Device::DeviceSettings& b) const
+{
+  return false; //  TODO
+}
+
+}
