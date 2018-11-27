de5ec67ee88b3ee72db8653d49706ed1aa4a6518
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-11-27 18:26:39 +0100

    Many build fixes
    update api
    
    update api
    
    try to fix stuff
    
    wip
    
    add linux dep
    
    add mac dep
    
    minor
    
    should fix builds
    
    oops
    
    oops 2
    
    try to fix more builds
    
    update api
    
    should fix ubuntu release build...
    
    disable joystick / wiimote for now
    
    should fix all builds
    
    update version
    
    strip correct exe

--- tools/travis/deps.sh
+++ tools/travis/deps.sh
@@ -9,22 +9,34 @@ fi
 # Install the deps
 case "$TRAVIS_OS_NAME" in
   linux)
-    sudo wget https://github.com/OSSIA/score-sdk/releases/download/sdk8/boost.tar.bz2 -O /opt/boost.tar.bz2 &
+    sudo chmod -R a+rwx /opt
+    wget https://github.com/OSSIA/score-sdk/releases/download/sdk8/boost.tar.bz2 -O /opt/boost.tar.bz2 &
 
-    wget -nv https://cmake.org/files/v3.11/cmake-3.11.3-Linux-x86_64.tar.gz -O cmake-linux.tgz &
+    wget -nv https://cmake.org/files/v3.13/cmake-3.13.0-Linux-x86_64.tar.gz -O cmake-linux.tgz &
 
-    echo 'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-6.0 main' | sudo tee /etc/apt/sources.list.d/llvm.list
+    echo 'deb http://apt.llvm.org/trusty/ llvm-toolchain-trusty-7 main' | sudo tee /etc/apt/sources.list.d/llvm.list
     sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 1397BC53640DB551
+    sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 15CF4D18AF4F7421
 
     sudo add-apt-repository --yes ppa:jonathonf/ffmpeg-3
     sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
-    sudo add-apt-repository --yes ppa:beineri/opt-qt-5.10.1-trusty
+    sudo add-apt-repository --yes ppa:beineri/opt-qt596-trusty
 
     sudo apt-get update -qq
-    sudo apt-get install -qq --force-yes g++-7 binutils libasound-dev ninja-build gcovr lcov qt510-meta-minimal qt510svg qt510quickcontrols2 qt510websockets qt510serialport qt510multimedia libgl1-mesa-dev libavcodec-dev libavutil-dev libavfilter-dev libavformat-dev libswresample-dev portaudio19-dev clang-6.0 lld-6.0
+    sudo apt-get install -qq --force-yes \
+        g++-8 binutils libasound-dev ninja-build \
+        gcovr lcov qt59-meta-minimal qt59svg \
+        qt59quickcontrols2 qt59websockets qt59serialport \
+        qt59multimedia libgl1-mesa-dev \
+        libavcodec-dev libavutil-dev libavfilter-dev libavformat-dev libswresample-dev \
+        portaudio19-dev clang-7 lld-7 \
+        libbluetooth-dev \
+        libsdl2-dev libsdl2-2.0-0 libglu1-mesa-dev libglu1-mesa \
+        qt59base qt59declarative qt59xmlpatterns libgles2-mesa-dev \
+        libavahi-compat-libdnssd-dev
 
     wait wget || true
-    (cd /opt; sudo tar xaf boost.tar.bz2; sudo mv boost_* boost ; sudo chmod -R a+rwx boost)
+    (cd /opt; tar xaf boost.tar.bz2; mv boost_* boost ; chmod -R a+rwx boost)
 
     tar xaf cmake-linux.tgz
     mv cmake-*-x86_64 cmake
@@ -45,7 +57,7 @@ case "$TRAVIS_OS_NAME" in
 
     brew unlink cmake
     brew link --force boost ninja qt5 cmake
-    brew install portaudio
+    brew install portaudio sdl2
 
     set -e
   ;;
