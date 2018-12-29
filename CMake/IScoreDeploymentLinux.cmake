c2c3f38a21590e24dd92b8a6dca4931899f82450
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2018-12-29 20:28:31 +0100

    fix debian package generation

--- CMake/IScoreDeploymentLinux.cmake
+++ CMake/IScoreDeploymentLinux.cmake
@@ -75,18 +75,27 @@ install(FILES "${PROJECT_BINARY_DIR}/ossia-score.desktop"
 install(FILES "${CMAKE_SOURCE_DIR}/base/lib/resources/ossia-score.png"
         DESTINATION share/pixmaps
         COMPONENT OssiaScore)
+
 # Score-v2.0.0-a17-Ubuntu-18.04-amd64.deb
-set(CPACK_PACKAGE_FILE_NAME "score-${CPACK_PACKAGE_VERSION}-${CPACK_SYSTEM_NAME}")
 set(CPACK_PACKAGING_INSTALL_PREFIX "")
+
+set(CPACK_DEBIAN_PACKAGE_NAME "ossia-score")
+set(CPACK_DEBIAN_OSSIASCORE_PACKAGE_NAME "ossia-score")
+set(CPACK_DEBIAN_FILE_NAME "ossia-score_${CPACK_PACKAGE_VERSION}_${CPACK_DEBIAN_PACKAGE_ARCHITECTURE}.deb")
+
 set(CPACK_DEBIAN_PACKAGE_MAINTAINER "ossia devs <ossia.collective@gmail.com>")
 set(CPACK_DEBIAN_PACKAGE_HOMEPAGE "https://ossia.io")
 set(CPACK_DEBIAN_PACKAGE_PROVIDES "ossia-score")
 if(SCORE_STATIC_QT)
-  set(CPACK_DEBIAN_PACKAGE_DEPENDS "libgl1-mesa-glx, libpng16-16, libc6, zlib1g, libpcre2-16-0, libdbus-1-3, libjpeg62-turbo, libfontconfig1, libfreetype6, libx11-xcb1, libxcb-render-util0, libxcb-render0, libxcb-xkb1, libxrender1, libxext6, libx11-xcb1, libsm6, libxcb-sync1, libxcb-xfixes0, libxcb-xinerama0, libxcb-randr0, libxcb-image0, libxcb-shm0, libxcb-keysyms1, libxcb-icccm4, libxcb-shape0, libxcb1, libxkbcommon-x11-0, libxkbcommon0, libxcb-glx0, libasound2, libudev1")
+  #set(CPACK_DEBIAN_PACKAGE_DEPENDS "libgl1-mesa-glx, libpng16-16, libc6, zlib1g, libpcre2-16-0, libdbus-1-3, libjpeg62-turbo, libfontconfig1, libfreetype6, libx11-xcb1, libxcb-render-util0, libxcb-render0, libxcb-xkb1, libxrender1, libxext6, libx11-xcb1, libsm6, libxcb-sync1, libxcb-xfixes0, libxcb-xinerama0, libxcb-randr0, libxcb-image0, libxcb-shm0, libxcb-keysyms1, libxcb-icccm4, libxcb-shape0, libxcb1, libxkbcommon-x11-0, libxkbcommon0, libxcb-glx0, libasound2, libudev1")
 else()
   set(CPACK_DEBIAN_PACKAGE_DEPENDS "libqt5core5a, libqt5gui5, libqt5svg5, libqt5xml5, libqt5network5, libqt5serialport5, libqt5quickcontrols2-5, libtbb2, libportaudio2, libjack0, libavahi-client3, libavcodec57, libavdevice57, libavfilter6, libavformat57, libswresample2, libqt5websockets5, liblilv-0-0, libsuil-0-0")
 endif()
 set(CPACK_DEBIAN_PACKAGE_SECTION "sound")
 set(CPACK_DEB_COMPONENT_INSTALL ON)
+set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
+set(CPACK_STRIP_FILES "bin/ossia-score;bin/ossia-score-vstpuppet")
+
+
 set(CPACK_COMPONENTS_ALL OssiaScore)
 endif()
