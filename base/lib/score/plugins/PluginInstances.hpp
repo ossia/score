55d37fc0ab192f8f6f20e064ab16b89c4cb322a2
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2019-01-03 18:06:10 +0100

    add a case for jit-ed plug-ins

--- base/lib/score/plugins/PluginInstances.hpp
+++ base/lib/score/plugins/PluginInstances.hpp
@@ -12,7 +12,20 @@ SCORE_LIB_BASE_EXPORT
 std::vector<Plugin_QtInterface*>& staticPlugins();
 }
 
-#if defined(SCORE_STATIC_PLUGINS)
+#if defined(SCORE_JIT_ID)
+
+#define SCORE_JIT_PASTER(x,y) x ## _ ## y
+#define SCORE_JIT_EVALUATOR(x,y) SCORE_JIT_PASTER(x,y)
+#define SCORE_JIT_NAME(fun) SCORE_JIT_EVALUATOR(fun, SCORE_JIT_ID)
+
+#define SCORE_EXPORT_PLUGIN(classname)                                         \
+  extern "C" Q_DECL_EXPORT score::Plugin_QtInterface* SCORE_JIT_NAME(plugin_instance) () \
+  {                                                                            \
+    static classname p;                                                        \
+    return &p;                                                                 \
+  }
+
+#elif defined(SCORE_STATIC_PLUGINS)
 #define SCORE_EXPORT_PLUGIN(classname)
 #else
 #define SCORE_EXPORT_PLUGIN(classname)                                  \
