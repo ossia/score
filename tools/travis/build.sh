94d39239dd48d0ab421a4475791ca3739ab3ac84
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2019-03-24 06:31:13 -0700

    wip on fix osx

--- tools/travis/build.sh
+++ tools/travis/build.sh
@@ -4,15 +4,6 @@ export PS4='+ ${FUNCNAME[0]:+${FUNCNAME[0]}():}line ${LINENO}: '
 # Note : to make the tests work under travis, they have to be changed in order not to require QApplication but only QCoreApplication
 #    - LD_LIBRARY_PATH=/usr/lib64 make ExperimentalTest
 
-
-case "$TRAVIS_OS_NAME" in
-  linux)
-  export CMAKE_BIN=$(readlink -f "$(find cmake/bin -name cmake -type f )")
-  ;;
-  osx)
-  export CMAKE_BIN=$(which cmake)
-  ;;
-esac
 export CTEST_OUTPUT_ON_FAILURE=1
 
 mkdir -p build
--- tools/travis/configs/osx-source-qt.sh
+++ tools/travis/configs/osx-source-qt.sh
@@ -6,7 +6,9 @@ if [[ -d "/usr/local/Cellar/qt" ]]; then
 elif [[ -d "/usr/local/Cellar/qt5" ]]; then
   export QT_PATH=$(dirname $(dirname $(find /usr/local/Cellar/qt5 -name Qt5Config.cmake) ) )
 fi
-export BOOST_PATH=$(dirname $(find /usr/local/Cellar/boost -name INSTALL_RECEIPT.json))
+
+export CMAKE_BIN=/opt/score-sdk/cmake/*/bin/cmake
+export BOOST_PATH=$(dirname $(find /opt/score-sdk/boost -name INSTALL_RECEIPT.json))
 export CXX=clang++
 export CMAKE_PREFIX_PATH="$QT_PATH"
 export CMAKE_COMMON_FLAGS="-DBOOST_ROOT=$BOOST_PATH -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH -DCMAKE_INSTALL_PREFIX=$(pwd)/bundle -DCMAKE_OSX_DEPLOYMENT_TARGET=10.12 -DOSSIA_SDK=/opt/score-sdk"
