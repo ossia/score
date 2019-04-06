017a9ce4c787e27e47c8b2bdc91203a83ff86394
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2019-04-06 09:29:39 +0200

    Update StyledButton.hpp

--- src/lib/score/widgets/StyledButton.hpp
+++ src/lib/score/widgets/StyledButton.hpp
@@ -1,10 +1,11 @@
 #pragma once
 #include <QPushButton>
 #include <wobjectdefs.h>
+#include <score_lib_base_export.h>
 
 namespace score
 {
-  class StyledButton : public QPushButton
+  class SCORE_LIB_BASE_EXPORT StyledButton : public QPushButton
   {
       W_OBJECT(StyledButton)
   public:
