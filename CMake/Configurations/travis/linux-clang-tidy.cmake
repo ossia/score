cb0fc2bc053c2d73b58e0f477dbc34180bcd8618
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2019-04-06 20:47:18 +0200

    try to enforce audio plug-ins

--- CMake/Configurations/travis/linux-clang-tidy.cmake
+++ CMake/Configurations/travis/linux-clang-tidy.cmake
@@ -2,7 +2,7 @@ set(CMAKE_BUILD_TYPE Debug CACHE INTERNAL "")
 set(SCORE_COTIRE False)
 set(DEPLOYMENT_BUILD False)
 set(SCORE_COVERAGE False)
-set(SCORE_AUDIO_PLUGINS True)
+set(SCORE_AUDIO_PLUGINS True CACHE INTERNAL "")
 
 set(CMAKE_CXX_CLANG_TIDY "/usr/bin/clang-tidy"
   "-checks=-*,clang-analyzer-*,performance-*"
