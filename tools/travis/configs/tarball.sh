46d8228415fb39304b8fa8cbd1a2090ee16de06e
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2019-04-07 21:00:32 +0200

    try to make a debian release

--- tools/travis/configs/tarball.sh
+++ tools/travis/configs/tarball.sh
@@ -1,10 +1,32 @@
 #!/bin/sh
 cd ..
 rm -rf .git
-rm -rf API/3rdparty/CicmWrapper
-rm -rf API/3rdparty/pure-data
-rm -rf API/3rdparty/pybind11
-rm -rf API/3rdparty/jni_hpp
-rm -rf API/3rdparty/max-sdk
+find . -name '.git' -exec rm -rf {} \;
+rm -rf 3rdparty/libossia/.git
+rm -rf 3rdparty/libossia/3rdparty/CicmWrapper
+
+cp 3rdparty/libossia/3rdparty/pure-data/LICENSE.txt pd-license.txt
+cp 3rdparty/libossia/3rdparty/pure-data/portaudio/portaudio/LICENSE.txt portaudio-license.txt
+rm -rf 3rdparty/libossia/3rdparty/pure-data
+mkdir -p 3rdparty/libossia/3rdparty/pure-data/portaudio/portaudio/
+mv portaudio-license.txt 3rdparty/libossia/3rdparty/pure-data/portaudio/portaudio/LICENSE.txt 
+mv pd-license.txt 3rdparty/libossia/3rdparty/pure-data/LICENSE.txt 
+
+mv 3rdparty/libossia/3rdparty/pybind11/LICENSE pybind11-license.txt
+rm -rf 3rdparty/libossia/3rdparty/pybind11
+mkdir -p 3rdparty/libossia/3rdparty/pybind11
+mv  pybind11-license.txt 3rdparty/libossia/3rdparty/pybind11/ LICENSE
+
+rm -rf 3rdparty/libossia/3rdparty/jni_hpp
+rm -rf 3rdparty/libossia/3rdparty/max-sdk
+rm -rf 3rdparty/libossia/3rdparty/concurrentqueue/benchmarks
+rm -rf 3rdparty/libossia/3rdparty/concurrentqueue/build
+rm -rf 3rdparty/libossia/3rdparty/concurrentqueue/test
+rm -rf 3rdparty/libossia/3rdparty/tbb/examples
+rm -rf 3rdparty/libossia/3rdparty/exprtk/*test*
+rm -rf Documentation/Doc_sources
+rm -rf Documentation/Doxygen
 
 tar caf ossia-score.tar.xz ./*
+
+#gpg --default-key ... -ab ossia-score.tar.xz 
