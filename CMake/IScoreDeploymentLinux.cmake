fced1c62fa18068fdbb761e861bc321b9a9cdd21
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2019-04-07 21:00:32 +0200

    try to make a debian release

--- CMake/IScoreDeploymentLinux.cmake
+++ CMake/IScoreDeploymentLinux.cmake
@@ -30,6 +30,21 @@ if(EXISTS /etc/lsb-release)
     set(LSB_ID "")
     set(LSB_VER "")
   endif()
+elseif(EXISTS /usr/bin/lsb_release)
+  execute_process(
+    COMMAND lsb_release -s -i
+    COMMAND tr "\n" " "
+    COMMAND sed "s/ //"
+    OUTPUT_VARIABLE LSB_ID
+    RESULT_VARIABLE LSB_ID_RESULT
+    )
+  execute_process(
+    COMMAND lsb_release -s -r
+    COMMAND tr "\n" " "
+    COMMAND sed "s/ //"
+    OUTPUT_VARIABLE LSB_VER
+    RESULT_VARIABLE LSB_VER_RESULT
+    )
 endif()
 
 
@@ -101,13 +116,50 @@ set(CPACK_DEBIAN_PACKAGE_PROVIDES "ossia-score")
 if(SCORE_STATIC_QT)
   #set(CPACK_DEBIAN_PACKAGE_DEPENDS "libgl1-mesa-glx, libpng16-16, libc6, zlib1g, libpcre2-16-0, libdbus-1-3, libjpeg62-turbo, libfontconfig1, libfreetype6, libx11-xcb1, libxcb-render-util0, libxcb-render0, libxcb-xkb1, libxrender1, libxext6, libx11-xcb1, libsm6, libxcb-sync1, libxcb-xfixes0, libxcb-xinerama0, libxcb-randr0, libxcb-image0, libxcb-shm0, libxcb-keysyms1, libxcb-icccm4, libxcb-shape0, libxcb1, libxkbcommon-x11-0, libxkbcommon0, libxcb-glx0, libasound2, libudev1")
 else()
-  set(CPACK_DEBIAN_PACKAGE_DEPENDS "libqt5core5a, libqt5gui5, libqt5svg5, libqt5xml5, libqt5network5, libqt5serialport5, libqt5quickcontrols2-5, libtbb2, libportaudio2, libjack0, libavahi-client3, libavcodec57, libavdevice57, libavfilter6, libavformat57, libswresample2, libqt5websockets5, liblilv-0-0, libsuil-0-0")
+  execute_process(
+    COMMAND dpkg -l
+    COMMAND grep libavcodec
+    COMMAND awk '{ print $2 }'
+    COMMAND cut -d ':' -f 1
+    COMMAND grep -v dev
+    COMMAND  's/libavcodec//'
+    COMMAND tr "\n" " "
+    COMMAND sed "s/ //"
+    OUTPUT_VARIABLE AVCODEC_VER
+    RESULT_VARIABLE AVCODEC_VER_RESULT
+  )
+  execute_process(
+    COMMAND dpkg -l
+    COMMAND grep libavfilter
+    COMMAND awk '{ print $2 }'
+    COMMAND cut -d ':' -f 1
+    COMMAND grep -v dev
+    COMMAND  's/libavfilter//'
+    COMMAND tr "\n" " "
+    COMMAND sed "s/ //"
+    OUTPUT_VARIABLE AVFILTER_VER
+    RESULT_VARIABLE AVFILTER_VER_RESULT
+  )
+  execute_process(
+    COMMAND dpkg -l
+    COMMAND grep libswresample
+    COMMAND awk '{ print $2 }'
+    COMMAND cut -d ':' -f 1
+    COMMAND grep -v dev
+    COMMAND  's/libswresample//'
+    COMMAND tr "\n" " "
+    COMMAND sed "s/ //"
+    OUTPUT_VARIABLE SWRESAMPLE_VER
+    RESULT_VARIABLE SWRESAMPLE_VER_RESULT
+  )
+  set(CPACK_DEBIAN_PACKAGE_DEPENDS "libqt5core5a, libqt5gui5, libqt5svg5, libqt5xml5, libqt5network5, libqt5serialport5, libqt5quickcontrols2-5, libavahi-client3, libqt5websockets5, liblilv-0-0, libsuil-0-0")
+  if(NOT AVCODEC_VER_RESULT EQUAL 1)
+    set(CPACK_DEBIAN_PACKAGE_DEPENDS "${CPACK_DEBIAN_PACKAGE_DEPENDS}, libavcodec${AVCODEC_VER}, libavdevice${AVCODEC_VER}, libavfilter${AVFILTER_VER}, libavformat${AVCODEC_VER}, libswresample${SWRESAMPLE_VER}")
+  endif()
 endif()
 set(CPACK_DEBIAN_PACKAGE_SECTION "sound")
 set(CPACK_DEB_COMPONENT_INSTALL ON)
 set(CPACK_DEBIAN_PACKAGE_SHLIBDEPS ON)
 set(CPACK_STRIP_FILES "bin/ossia-score;bin/ossia-score-vstpuppet")
-
-
 set(CPACK_COMPONENTS_ALL OssiaScore)
 endif()
---
+++ tools/travis/configs/linux-debian-release.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+apt -yy update && apt -yy full-upgrade
+
+apt -yy install git build-essential libsdl2-dev cmake qt5-default qtbase5-dev qtbase5-dev-tools qt5-image-formats-plugins qtdeclarative5-dev qtdeclarative5-dev-tools qttools5-dev qttools5-dev-tools g++-8 libavahi-compat-libdnssd-dev libavahi-client-dev libavahi-core-dev liblilv-dev libsuil-dev libjack-jackd2-dev libavcodec-dev libavdevice-dev  libavfilter-dev libavformat-dev libavutil-dev libswresample-dev libswscale-dev libbluetooth-dev libqt5websockets5-dev libqt5serialport5-dev libqt5svg5-dev qtquickcontrols2-5-dev  wget
+
+wget -nv http://www.portaudio.com/archives/pa_snapshot.tgz
+tar xaf pa_snapshot.tgz
+
+sed -i '378i TARGET_INCLUDE_DIRECTORIES(portaudio_static PUBLIC "$<INSTALL_INTERFACE:include>")' portaudio/CMakeLists.txt
+sed -i '307d' portaudio/CMakeLists.txt # SET(PA_LIBRARY_DEPENDENCIES ${PA_LIBRARY_DEPENDENCIES} ${ALSA_LIBRARIES})
+sed -i '306d' portaudio/CMakeLists.txt # SET(PA_PKGCONFIG_LDFLAGS "${PA_PKGCONFIG_LDFLAGS} -lasound")
+sed -i '305d' portaudio/CMakeLists.txt
+
+sed -i '305i  SET(PA_PRIVATE_COMPILE_DEFINITIONS ${PA_PRIVATE_COMPILE_DEFINITIONS} PA_USE_ALSA PA_ALSA_DYNAMIC)' portaudio/CMakeLists.txt
+sed -i '305i  SET(PA_PKGCONFIG_LDFLAGS "${PA_PKGCONFIG_LDFLAGS} ${CMAKE_DL_LIBS}")' portaudio/CMakeLists.txt
+      
+      
+cd portaudio/build
+
+$CMAKE .. \
+ -DCMAKE_BUILD_TYPE=Release \
+ -DCMAKE_POSITION_INDEPENDENT_CODE=1 \
+ -DPA_BUILD_SHARED=Off \
+ -DPA_USE_JACK=Off \
+ -DCMAKE_INSTALL_PREFIX=/usr
+
+make -j$NPROC
+make install
+cd /
+rm -rf /portaudio
+
+git clone --recursive -j16 https://github.com/OSSIA/score
+mkdir build
+cd build
+cmake -Wno-dev ../score -DSCORE_CONFIGURATION=static-release -DSCORE_CONFIGURATION=static-release -DDEPLOYMENT_BUILD=1 -DCMAKE_SKIP_RPATH=ON -DCMAKE_INSTALL_PREFIX="/usr" 
+make all_unity -j12
+cpack -G DEB 
