7e4171d0c15e086a75013bf9370089d1f151940c
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2019-04-07 21:03:19 +0200

    try to make a debian release

--- CMake/IScoreDeploymentLinux.cmake
+++ CMake/IScoreDeploymentLinux.cmake
@@ -115,47 +115,6 @@ set(CPACK_DEBIAN_PACKAGE_HOMEPAGE "https://ossia.io")
 set(CPACK_DEBIAN_PACKAGE_PROVIDES "ossia-score")
 if(SCORE_STATIC_QT)
   #set(CPACK_DEBIAN_PACKAGE_DEPENDS "libgl1-mesa-glx, libpng16-16, libc6, zlib1g, libpcre2-16-0, libdbus-1-3, libjpeg62-turbo, libfontconfig1, libfreetype6, libx11-xcb1, libxcb-render-util0, libxcb-render0, libxcb-xkb1, libxrender1, libxext6, libx11-xcb1, libsm6, libxcb-sync1, libxcb-xfixes0, libxcb-xinerama0, libxcb-randr0, libxcb-image0, libxcb-shm0, libxcb-keysyms1, libxcb-icccm4, libxcb-shape0, libxcb1, libxkbcommon-x11-0, libxkbcommon0, libxcb-glx0, libasound2, libudev1")
-else()
-  execute_process(
-    COMMAND dpkg -l
-    COMMAND grep libavcodec
-    COMMAND awk '{ print $2 }'
-    COMMAND cut -d ':' -f 1
-    COMMAND grep -v dev
-    COMMAND  's/libavcodec//'
-    COMMAND tr "\n" " "
-    COMMAND sed "s/ //"
-    OUTPUT_VARIABLE AVCODEC_VER
-    RESULT_VARIABLE AVCODEC_VER_RESULT
-  )
-  execute_process(
-    COMMAND dpkg -l
-    COMMAND grep libavfilter
-    COMMAND awk '{ print $2 }'
-    COMMAND cut -d ':' -f 1
-    COMMAND grep -v dev
-    COMMAND  's/libavfilter//'
-    COMMAND tr "\n" " "
-    COMMAND sed "s/ //"
-    OUTPUT_VARIABLE AVFILTER_VER
-    RESULT_VARIABLE AVFILTER_VER_RESULT
-  )
-  execute_process(
-    COMMAND dpkg -l
-    COMMAND grep libswresample
-    COMMAND awk '{ print $2 }'
-    COMMAND cut -d ':' -f 1
-    COMMAND grep -v dev
-    COMMAND  's/libswresample//'
-    COMMAND tr "\n" " "
-    COMMAND sed "s/ //"
-    OUTPUT_VARIABLE SWRESAMPLE_VER
-    RESULT_VARIABLE SWRESAMPLE_VER_RESULT
-  )
-  set(CPACK_DEBIAN_PACKAGE_DEPENDS "libqt5core5a, libqt5gui5, libqt5svg5, libqt5xml5, libqt5network5, libqt5serialport5, libqt5quickcontrols2-5, libavahi-client3, libqt5websockets5, liblilv-0-0, libsuil-0-0")
-  if(NOT AVCODEC_VER_RESULT EQUAL 1)
-    set(CPACK_DEBIAN_PACKAGE_DEPENDS "${CPACK_DEBIAN_PACKAGE_DEPENDS}, libavcodec${AVCODEC_VER}, libavdevice${AVCODEC_VER}, libavfilter${AVFILTER_VER}, libavformat${AVCODEC_VER}, libswresample${SWRESAMPLE_VER}")
-  endif()
 endif()
 set(CPACK_DEBIAN_PACKAGE_SECTION "sound")
 set(CPACK_DEB_COMPONENT_INSTALL ON)
