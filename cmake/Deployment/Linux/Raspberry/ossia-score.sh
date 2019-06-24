dae9d2e68a87a674135c8b9752356a276d0ee4d6
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2019-06-24 13:16:39 +0200

    Raspberry build scripts

---
+++ cmake/Deployment/Linux/Raspberry/ossia-score.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -eux
+SOURCE="${BASH_SOURCE[0]}"
+while [ -h "$SOURCE" ]; do # resolve $SOURCE until the file is no longer a symlink
+  DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+  SOURCE="$(readlink "$SOURCE")"
+  [[ $SOURCE != /* ]] && SOURCE="$DIR/$SOURCE" # if $SOURCE was a relative symlink, we need to resolve it relative to the path where the symlink file was located
+done
+DIR="$( cd -P "$( dirname "$SOURCE" )" >/dev/null 2>&1 && pwd )"
+DIR="$(dirname $DIR)"
+export LD_LIBRARY_PATH="$DIR/lib"
+
+exec "$DIR/bin/ossia-score"
