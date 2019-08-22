0873278a90b76d951e7200b1ade5ac75c7a28309
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2019-08-22 19:10:54 +0200

    fix #966

---
+++ src/plugins/score-plugin-scenario/Scenario/Process/ScenarioSelection.hpp
@@ -0,0 +1,16 @@
+#pragma once
+#include <Scenario/Process/ScenarioModel.hpp>
+#include <score/selection/Selection.hpp>
+
+namespace Scenario
+{
+inline bool selectionHasScenarioElements(const Selection& sel)
+{
+  if(sel.empty())
+    return false;
+  if(qobject_cast<const Scenario::ProcessModel*>(sel.begin()->data()))
+    return false;
+
+  return true;
+}
+}
