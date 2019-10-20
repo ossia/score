f7e34e37d376e18ec097fa42957c9ecb42d50b9f
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2019-10-20 13:56:35 +0200

    Respect CMAKE_BUILD_TYPE, and disable LTO on Fedora

--- cmake/Configurations/travis/release.cmake
+++ cmake/Configurations/travis/release.cmake
@@ -8,8 +8,13 @@ set(SCORE_AUDIO_PLUGINS True CACHE INTERNAL "")
 if(NOT DEFINED SCORE_ENABLE_LTO)
 set(SCORE_ENABLE_LTO True)
 endif()
+
+# TODO LTO is broken on many platforms. 
+# With gcc-9.2 and binutils 2.32 it seems ok so revisit when more distros use that
 if(UNIX)
   if(EXISTS "/etc/centos-release")
+    set(SCORE_ENABLE_LTO False)
+  elseif(EXISTS "/etc/fedora-release")
     set(SCORE_ENABLE_LTO False)
   elseif(CMAKE_SYSTEM_PROCESSOR MATCHES "arm")
     set(SCORE_ENABLE_LTO False)
