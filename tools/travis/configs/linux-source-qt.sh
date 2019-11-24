278a460586a22bf985541cb1e3d358672a8450e6
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2019-11-24 21:28:23 +0100

    Update travis versions

--- tools/travis/configs/linux-source-qt.sh
+++ tools/travis/configs/linux-source-qt.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 set +eux
-export CC=/usr/bin/clang-8
-export CXX=/usr/bin/clang++-8
+export CC=/usr/bin/clang-9
+export CXX=/usr/bin/clang++-9
 export CMAKE_BIN=$(readlink -f "$(find ../cmake-latest/bin -name cmake -type f )")
 export CMAKE_COMMON_FLAGS="-GNinja -DPORTAUDIO_ONLY_DYNAMIC=1 -DOSSIA_USE_FAST_LINKER=1"
 QT_ENV_SCRIPT=$(find /opt -name 'qt*-env.sh')
--- tools/travis/deps.sh
+++ tools/travis/deps.sh
@@ -18,29 +18,27 @@ case "$TRAVIS_OS_NAME" in
     sudo apt-get update -qq
     sudo apt-get install wget software-properties-common
 
-    wget -nv https://cmake.org/files/v3.14/cmake-3.14.3-Linux-x86_64.tar.gz -O cmake-linux.tgz &
-
-    echo 'deb http://apt.llvm.org/xenial/ llvm-toolchain-xenial-8 main' | sudo tee /etc/apt/sources.list.d/llvm.list
+    wget -nv https://github.com/Kitware/CMake/releases/download/v3.16.0-rc4/cmake-3.16.0-rc4-Linux-x86_64.tar.gz -O cmake-linux.tgz &
+    echo 'deb http://apt.llvm.org/bionic/ llvm-toolchain-bionic-9 main' | sudo tee /etc/apt/sources.list.d/llvm.list
     sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 1397BC53640DB551
     sudo apt-key adv --recv-keys --keyserver keyserver.ubuntu.com 15CF4D18AF4F7421
 
-    sudo add-apt-repository --yes ppa:jonathonf/ffmpeg-3
     sudo add-apt-repository --yes ppa:ubuntu-toolchain-r/test
-    sudo add-apt-repository --yes ppa:beineri/opt-qt-5.12.3-xenial
+    sudo add-apt-repository --yes ppa:beineri/opt-qt-5.13.2-bionic
 
     sudo apt-get update -qq
     sudo apt-get install -qq --force-yes \
-        g++-8 binutils libasound-dev ninja-build \
+        g++-9 binutils libasound-dev ninja-build \
         gcovr lcov \
-        qt512-meta-minimal qt512svg qt512quickcontrols2 qt512websockets qt512serialport \
-        qt512base qt512declarative \
+        qt513-meta-minimal qt513svg qt513quickcontrols2 qt513websockets qt513serialport \
+        qt513base qt513declarative \
         libgl1-mesa-dev \
         libavcodec-dev libavutil-dev libavfilter-dev libavformat-dev libswresample-dev \
         portaudio19-dev clang-8 lld-8 \
         libbluetooth-dev \
         libsdl2-dev libsdl2-2.0-0 libglu1-mesa-dev libglu1-mesa \
         libgles2-mesa-dev \
-        libavahi-compat-libdnssd-dev
+        libavahi-compat-libdnssd-dev libsamplerate0-dev
 
     wait wget || true
 
@@ -51,13 +49,13 @@ case "$TRAVIS_OS_NAME" in
     set +e
 
     brew update
-    brew install gnu-tar xz
+    brew install qt cmake portaudio ffmpeg ninja libsamplerate
 
-    SDK_ARCHIVE=homebrew-cache.txz
-    wget -nv https://github.com/OSSIA/score-sdk/releases/download/sdk12/$SDK_ARCHIVE -O $SDK_ARCHIVE
-    sudo mkdir -p /opt/score-sdk
-    sudo chmod -R a+rwx /opt/score-sdk
-    gtar xhaf $SDK_ARCHIVE --directory /opt/score-sdk
+#    SDK_ARCHIVE=homebrew-cache.txz
+#    wget -nv https://github.com/OSSIA/score-sdk/releases/download/sdk12/$SDK_ARCHIVE -O $SDK_ARCHIVE
+#    sudo mkdir -p /opt/score-sdk
+#    sudo chmod -R a+rwx /opt/score-sdk
+#    gtar xhaf $SDK_ARCHIVE --directory /opt/score-sdk
 
     set -e
   ;;
