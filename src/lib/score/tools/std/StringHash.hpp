21eb1ad410b6becef1dcbb551465078a9a07d76f
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2019-11-25 19:53:51 +0100

    5.14 fix

--- src/lib/score/tools/std/StringHash.hpp
+++ src/lib/score/tools/std/StringHash.hpp
@@ -1,9 +1,9 @@
 #pragma once
+#include <QtGlobal>
+#if QT_VERSION < QT_VERSION_CHECK(5, 14, 0)
 #include <QHashFunctions>
 #include <QString>
-
 #include <functional>
-
 #include <ossia-config.hpp>
 
 // TODO merge String.hpp here
@@ -15,3 +15,4 @@ struct hash<QString>
   std::size_t operator()(const QString& path) const { return qHash(path); }
 };
 }
+#endif
