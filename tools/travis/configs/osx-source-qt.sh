3c4ce314f498182e7af719160a0304284700e9d9
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2019-12-20 10:36:23 +0100

    Fix osx brew

--- tools/travis/configs/osx-source-qt.sh
+++ tools/travis/configs/osx-source-qt.sh
@@ -8,7 +8,18 @@ elif [[ -d "/usr/local/Cellar/qt5" ]]; then
 fi
 
 export CMAKE_BIN=$(find /opt/score-sdk -type f -perm +111 -name cmake)
+if [[ "x$CMAKE_BIN" == "x" ]]; then
+  export CMAKE_BIN=$(find /usr/local/bin -type f -perm +111 -name cmake)
+fi
 export CC=clang
 export CXX=clang++
 export CMAKE_PREFIX_PATH="$QT_PATH"
-export CMAKE_COMMON_FLAGS="-DBOOST_ROOT=/opt/score-sdk/boost -DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH -DCMAKE_INSTALL_PREFIX=$(pwd)/bundle -DCMAKE_OSX_DEPLOYMENT_TARGET=10.12 -DOSSIA_SDK=/opt/score-sdk"
+export CMAKE_COMMON_FLAGS="-DCMAKE_C_COMPILER=/usr/bin/clang -DCMAKE_CXX_COMPILER=/usr/bin/clang++ -DCMAKE_PREFIX_PATH=$CMAKE_PREFIX_PATH -DCMAKE_INSTALL_PREFIX=$(pwd)/bundle -DCMAKE_OSX_DEPLOYMENT_TARGET=10.12"
+
+if [[ -d "/opt/ossia-sdk" ]]; then
+  export CMAKE_COMMON_FLAGS="$CMAKE_COMMON_FLAGS -DOSSIA_SDK=/opt/ossia-sdk"
+elif [[ -d "/opt/score-sdk" ]]; then
+  export CMAKE_COMMON_FLAGS="$CMAKE_COMMON_FLAGS -DOSSIA_SDK=/opt/score-sdk"
+elif [[ -d "/opt/score-sdk-osx" ]]; then
+  export CMAKE_COMMON_FLAGS="$CMAKE_COMMON_FLAGS -DOSSIA_SDK=/opt/score-sdk-osx"
+fi
