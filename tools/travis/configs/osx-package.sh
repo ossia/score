97698d46ed6d7dbecc68c1bfb7f141d9775d8aa5
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2019-12-22 12:45:32 +0100

    macos fix

--- tools/travis/configs/osx-package.sh
+++ tools/travis/configs/osx-package.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 source "$CONFIG_FOLDER/osx-source-qt.sh"
 
-$CMAKE_BIN $CMAKE_COMMON_FLAGS -DSCORE_CONFIGURATION=static-release ..
-$CMAKE_BIN --build . --target all_unity -- -j2
-$CMAKE_BIN --build . --target install/strip/fast -- -j2
+xcrun $CMAKE_BIN $CMAKE_COMMON_FLAGS -DSCORE_CONFIGURATION=static-release ..
+xcrun $CMAKE_BIN --build . --target all_unity -- -j2
+xcrun $CMAKE_BIN --build . --target install/strip/fast -- -j2
