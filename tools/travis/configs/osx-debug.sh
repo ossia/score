fc7bf83b12f70d5a11044c74881df1dc93aca50c
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2019-12-22 12:45:32 +0100

    macos fix

--- tools/travis/configs/osx-debug.sh
+++ tools/travis/configs/osx-debug.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 source "$CONFIG_FOLDER/osx-source-qt.sh"
 
-$CMAKE_BIN $CMAKE_COMMON_FLAGS -DSCORE_CONFIGURATION=debug ..
-$CMAKE_BIN --build . --target all_unity -- -j2
+xcrun $CMAKE_BIN $CMAKE_COMMON_FLAGS -DSCORE_CONFIGURATION=debug ..
+xcrun $CMAKE_BIN --build . --target all_unity -- -j2
