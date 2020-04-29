5068009ad1bdc4530f4094137b842112dcea0529
Akané Lévy <levyakane@gmail.com>
2020-04-29 22:15:19 +0200

    animate trigger

--- src/plugins/score-plugin-scenario/Scenario/Document/TimeSync/TriggerView.hpp
+++ src/plugins/score-plugin-scenario/Scenario/Document/TimeSync/TriggerView.hpp
@@ -4,7 +4,6 @@
 #include <score/widgets/MimeData.hpp>
 
 #include <QGraphicsItem>
-#include <QGraphicsPixmapItem>
 #include <QMimeData>
 
 #include <score_plugin_scenario_export.h>
@@ -17,14 +16,13 @@ namespace Scenario
 {
 class SCORE_PLUGIN_SCENARIO_EXPORT TriggerView final
     : public QObject
-    , public QGraphicsPixmapItem
+    , public QGraphicsItem
 {
   W_OBJECT(TriggerView)
   Q_INTERFACES(QGraphicsItem)
 
 public:
   TriggerView(QGraphicsItem* parent);
-
   static const constexpr int Type = ItemType::Trigger;
   int type() const final override { return Type; }
 
@@ -36,16 +34,29 @@ public:
   void dropReceived(const QPointF& pos, const QMimeData& arg_2)
       W_SIGNAL(dropReceived, pos, arg_2);
 
+  QRectF boundingRect() const;
+  void paint(QPainter *painter, const QStyleOptionGraphicsItem *option, QWidget *widget) override;
+
 protected:
   void dropEvent(QGraphicsSceneDragDropEvent* event) override;
-
+  void nextFrame();
 private:
   void mousePressEvent(QGraphicsSceneMouseEvent*) override;
 
   void hoverEnterEvent(QGraphicsSceneHoverEvent*) override;
   void hoverLeaveEvent(QGraphicsSceneHoverEvent*) override;
 
+  void updatePixmap();
+
+  void onWaitStart();
+  void onWaitEnd();
+
   bool m_selected: 1;
   bool m_hovered: 1;
+  bool m_waiting: 1;
+
+  QPixmap m_currentPixmap;
+  int m_currentFrame;
+  int m_frameDirection;
 };
 }
