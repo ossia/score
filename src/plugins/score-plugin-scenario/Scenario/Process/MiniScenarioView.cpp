6d104e757c413be914ec90b6899dda073de7a0f4
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-05-10 15:29:02 +0200

    fix minimap interval colors

--- src/plugins/score-plugin-scenario/Scenario/Process/MiniScenarioView.cpp
+++ src/plugins/score-plugin-scenario/Scenario/Process/MiniScenarioView.cpp
@@ -26,15 +26,20 @@ MiniScenarioView::MiniScenarioView(const ProcessModel& sc, QGraphicsItem* p)
 void MiniScenarioView::paint_impl(QPainter* p) const
 {
   auto& skin = Process::Style::instance();
-  const auto h = height() - 8;
+  const auto h = 12.;
 
   for (const Scenario::IntervalModel& c : m_scenario.intervals)
   {
-    p->setPen(skin.MiniScenarioPen(c.metadata().getColor().getBrush()));
+    const auto& col = c.metadata().getColor();
+    if(&col.getBrush() == &skin.IntervalDefaultBackground())
+      p->setPen(skin.MiniScenarioPen(skin.IntervalHeaderText()));
+    else
+      p->setPen(skin.MiniScenarioPen(c.metadata().getColor().getBrush()));
+
     auto def = c.duration.defaultDuration().toPixels(zoom());
     auto st = c.date().toPixels(zoom());
     auto y = c.heightPercentage();
-    p->drawLine(QPointF{st, 4. + y * h}, QPointF{st + def, 4. + y * h});
+    p->drawLine(QPointF{st, 1. + y * h}, QPointF{st + def, 1. + y * h});
   }
 }
 }
