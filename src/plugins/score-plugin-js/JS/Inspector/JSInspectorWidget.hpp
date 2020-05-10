fd774b54c087d3fb1b58dbb6b0161c8c9283e210
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-05-10 18:54:33 +0200

    harmonize js inspector with the rest

--- src/plugins/score-plugin-js/JS/Inspector/JSInspectorWidget.hpp
+++ src/plugins/score-plugin-js/JS/Inspector/JSInspectorWidget.hpp
@@ -1,5 +1,6 @@
 #pragma once
 #include <JS/JSProcessModel.hpp>
+#include <Process/Inspector/GenericProcessInspector.hpp>
 #include <Process/Inspector/ProcessInspectorWidgetDelegate.hpp>
 
 #include <score/command/Dispatchers/CommandDispatcher.hpp>
@@ -8,46 +9,16 @@
 
 #include <verdigris>
 
-class JSEdit;
 class QWidget;
 class QLabel;
 class QTableWidget;
 namespace JS
 {
-struct JSWidgetBase
-{
-  JSWidgetBase(const score::CommandStackFacade& st) : m_dispatcher{st} {}
-
-  template <typename Widg, typename T>
-  void init(Widg* self, T& model);
-  void on_modelChanged(const QString& script);
-
-  JSEdit* m_edit{};
-  QLabel* m_errorLabel{};
-  QString m_script;
-
-  CommandDispatcher<> m_dispatcher;
-};
-
 class InspectorWidget final
-    : public Process::InspectorWidgetDelegate_T<JS::ProcessModel>,
-      public JSWidgetBase
+    : public Process::GenericInspectorWidget<JS::ProcessModel>
 {
-  W_OBJECT(InspectorWidget)
-  friend struct JSWidgetBase;
-
 public:
-  explicit InspectorWidget(
-      const JS::ProcessModel& object,
-      const score::DocumentContext& context,
-      QWidget* parent);
-
-public:
-  void pressed() W_SIGNAL(pressed);
-
-private:
-  void on_textChange(const QString& newText);
-  void updateControls(const score::DocumentContext&);
-  QWidget* m_ctrlWidg{};
+  using GenericInspectorWidget::GenericInspectorWidget;
 };
+
 }
