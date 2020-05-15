20e586651115fd090c6499b07d26ceccb8e109c2
Akané Lévy <levyakane@gmail.com>
2020-05-15 16:51:00 +0200

    Make hand cursor open and close when grabbing the interval header

--- src/plugins/score-plugin-scenario/Scenario/Document/Interval/IntervalHeader.cpp
+++ src/plugins/score-plugin-scenario/Scenario/Document/Interval/IntervalHeader.cpp
@@ -14,16 +14,14 @@ void IntervalHeader::setWidth(double width)
   prepareGeometryChange();
   m_width = width;
   auto& skin = score::Skin::instance();
-  if (this->cursor().shape() != skin.CursorOpenHand.shape())
-    this->setCursor(skin.CursorOpenHand);
+  this->setCursor(skin.CursorOpenHand);
   update();
 }
 
 void IntervalHeader::mousePressEvent(QGraphicsSceneMouseEvent* event)
 {
   auto& skin = score::Skin::instance();
-  if (this->cursor().shape() != skin.CursorClosedHand.shape())
-    this->setCursor(skin.CursorClosedHand);
+  this->setCursor(skin.CursorClosedHand);
   m_view->mousePressEvent(event);
   event->accept();
 }
@@ -37,8 +35,7 @@ void IntervalHeader::mouseMoveEvent(QGraphicsSceneMouseEvent* event)
 void IntervalHeader::mouseReleaseEvent(QGraphicsSceneMouseEvent* event)
 {
   auto& skin = score::Skin::instance();
-  if (this->cursor().shape() != skin.CursorOpenHand.shape())
-    this->setCursor(skin.CursorOpenHand);
+  this->setCursor(skin.CursorOpenHand);
   m_view->mouseReleaseEvent(event);
   event->accept();
 }
