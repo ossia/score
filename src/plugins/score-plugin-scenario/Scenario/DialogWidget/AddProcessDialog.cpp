267e373df277c12a07ba847302172cf1b010d9ec
Akané Lévy <levyakane@gmail.com>
2020-05-17 19:10:28 +0200

    Restyled the add process window

--- src/plugins/score-plugin-scenario/Scenario/DialogWidget/AddProcessDialog.cpp
+++ src/plugins/score-plugin-scenario/Scenario/DialogWidget/AddProcessDialog.cpp
@@ -1,9 +1,9 @@
 #include "AddProcessDialog.hpp"
 
-#include <QDialogButtonBox>
-#include <QHBoxLayout>
+#include <QGridLayout>
 #include <QListWidget>
 #include <QPushButton>
+#include <QLabel>
 
 #include <set>
 namespace Scenario
@@ -22,18 +22,34 @@ AddProcessDialog::AddProcessDialog(
     : QDialog{parent}, m_factoryList{plist}, m_flags{acceptable}
 {
   setWindowTitle(tr("Add process"));
-  auto lay = new QHBoxLayout;
+  auto lay = new QGridLayout;
+  lay->setSpacing(4);
   this->setLayout(lay);
+
+  auto categoriesLabel = new QLabel{tr("Categories"),this};
+  auto titleFont = categoriesLabel->font();
+  titleFont.setBold(true);
+  titleFont.setPixelSize(14);
+  categoriesLabel->setFont(titleFont);
+  categoriesLabel->setAlignment(Qt::AlignHCenter);
+  lay->addWidget(categoriesLabel,0,0);
   m_categories = new QListWidget;
-  lay->addWidget(m_categories);
+  lay->addWidget(m_categories,1,0,-1,1);
+
+  auto processLabel = new QLabel{tr("Process"),this};
+  processLabel->setFont(titleFont);
+  processLabel->setAlignment(Qt::AlignHCenter);
+  lay->addWidget(processLabel,0,1);
+
   m_processes = new QListWidget;
-  lay->addWidget(m_processes);
-  auto buttonBox = new QDialogButtonBox{};
-  auto add = new QPushButton{"+"};
-  buttonBox->addButton(add, QDialogButtonBox::ActionRole);
-  buttonBox->addButton(QDialogButtonBox::StandardButton::Close);
-  buttonBox->setOrientation(Qt::Vertical);
-  lay->addWidget(buttonBox);
+  lay->addWidget(m_processes,1,1,-1,1);
+
+  auto add = new QPushButton{"+", this};
+  auto btnFont = add->font();
+  btnFont.setPixelSize(28);
+  add->setFont(btnFont);
+  add->setSizePolicy(QSizePolicy::Preferred, QSizePolicy::Expanding);
+  lay->addWidget(add,1,2,-1,1);
 
   connect(
       m_categories, &QListWidget::currentTextChanged, this, &AddProcessDialog::updateProcesses);
@@ -59,12 +75,6 @@ AddProcessDialog::AddProcessDialog(
   connect(m_processes, &QListWidget::itemDoubleClicked, this, accept_item);
   connect(add, &QPushButton::clicked, [=] { accept_item(m_processes->currentItem()); });
 
-  connect(buttonBox, &QDialogButtonBox::accepted, this, [=] {
-    accept_item(m_processes->currentItem());
-    QDialog::close();
-  });
-
-  connect(buttonBox, &QDialogButtonBox::rejected, this, &QDialog::reject);
   setup();
   hide();
 }
