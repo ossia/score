6eda996a84c99b572466939baee482adce93ebdd
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-05-20 01:49:11 +0200

    Various small appimage generation fixes

--- cmake/Deployment/Linux/AppImage/build.sh
+++ cmake/Deployment/Linux/AppImage/build.sh
@@ -3,6 +3,7 @@ sudo docker build --squash --compress --force-rm  -f Dockerfile.llvm -t ossia/sc
 sudo docker rm llvm-build-vm
 sudo docker run --name llvm-build-vm -v "$(pwd)"/Recipe.llvm:/Recipe -w="/" -e TRAVIS_COMMIT=`git rev-parse HEAD` ossia/score-linux-llvm /bin/bash /Recipe
 sudo docker cp llvm-build-vm:/score.AppDir.txz .
+tar xaf score.AppDir.txz
 cp AppRun score.AppDir/
 ./appimagetool-x86_64.AppImage -n score.AppDir Score.AppImage
 
