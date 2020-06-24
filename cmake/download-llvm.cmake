ddd08ba8c07c8d73af2ebb78524e54bc9b4bf191
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-06-24 21:47:31 +0200

    Try to set up github actions for windows builds

---
+++ cmake/download-llvm.cmake
@@ -0,0 +1,2 @@
+#file(DOWNLOAD "https://github.com/mstorsjo/llvm-mingw/releases/download/20200325/llvm-mingw-20200325-x86_64.zip" "./llvm.zip")
+file(DOWNLOAD "https://github.com/ninja-build/ninja/releases/download/v1.10.0/ninja-win.zip" "./ninja.zip")
