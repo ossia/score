b2728e794926a2352ff41ace9a50cd8ae6d71167
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-06-27 12:27:56 +0200

    Add error checking to serialization of TimeVal

--- src/plugins/score-lib-process/Process/TimeValueSerialization.hpp
+++ src/plugins/score-lib-process/Process/TimeValueSerialization.hpp
@@ -31,12 +31,41 @@ struct TSerializer<DataStream, TimeVal>
 template <>
 struct TSerializer<JSONObject, TimeVal>
 {
-  static void readFrom(JSONObject::Serializer& s, const TimeVal& tv) { s.stream.Int64(tv.impl); }
+  static void readFrom(JSONObject::Serializer& s, const TimeVal& tv)
+  {
+    if(Q_UNLIKELY(tv.impl > ossia::time_value::infinite_min))
+    {
+      s.stream.Int64(ossia::time_value::infinity);
+    }
+    else if(Q_UNLIKELY(tv.impl < 0))
+    {
+      qDebug() << "Warning: saving a time_value < 0. This likely indicates a bug: " << tv.impl;
+      s.stream.Int64(0);
+    }
+    else
+    {
+      s.stream.Int64(tv.impl);
+    }
+  }
 
   static void writeTo(JSONObject::Deserializer& s, TimeVal& tv)
   {
     using namespace std;
     using namespace std::literals;
-    tv.impl = s.base.GetInt64();
+    if(Q_LIKELY(s.base.IsInt64()))
+    {
+      tv.impl = s.base.GetInt64();
+    }
+    else if(s.base.IsUint64())
+    {
+      // Note: there is likely a rapidjson bug there...
+      qDebug() << "Warning: loading a value > to the maximum of an int64_t: " << s.base.GetUint64();
+      tv.impl = ossia::time_value::infinity;
+    }
+    else
+    {
+      qDebug() << "Warning: could not load a TimeVal";
+      tv.impl = 0;
+    }
   }
 };
