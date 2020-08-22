04e66fc1f506e22735a727cc6bf8e67822768ae5
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-08-22 15:59:33 +0200

    Fix options not available on macOS

--- .cninja/developer.cmake
+++ .cninja/developer.cmake
@@ -1,11 +1,15 @@
 cninja_require(lld)
 cninja_require(linkerwarnings)
 cninja_require(debugsyms)
+cninja_require(debugmode)
 cninja_require(debugsplit)
 cninja_require(score-warnings)
 
 set_cache(SCORE_PCH True)
 set_cache(SCORE_DYNAMIC_PLUGINS True)
 
-string(APPEND CMAKE_C_FLAGS_INIT " -ggnu-pubnames -fdebug-types-section")
-string(APPEND CMAKE_CXX_FLAGS_INIT " -ggnu-pubnames -fdebug-types-section")
+if(UNIX AND NOT APPLE)
+  string(APPEND CMAKE_C_FLAGS_INIT " -ggnu-pubnames -fdebug-types-section")
+  string(APPEND CMAKE_CXX_FLAGS_INIT " -ggnu-pubnames -fdebug-types-section")
+endif()
+
