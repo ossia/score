17cba594bd706cb2f9801577075e303db559c673
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-09-02 11:05:50 +0200

    Update version

--- tools/travis/deploy.sh
+++ tools/travis/deploy.sh
@@ -4,15 +4,16 @@ then
     exit 0
 fi
 
-cd build
+
 
 case "$CONF" in
   osx-package)
-    cd bundle
-    zip -r -9 "Score-$TRAVIS_TAG-macOS.zip" Score.app
+    cd /Users/travis/build/ossia/score/bundle/
+    zip -r -9 "Score-$TRAVIS_TAG-macOS.zip" score.app
     mv "Score-$TRAVIS_TAG-macOS.zip" ..
   ;;
   linux-package-appimage)
+    cd build
     mv "Score.AppImage" "Score-$TRAVIS_TAG-amd64.AppImage"
   ;;
   tarball)
