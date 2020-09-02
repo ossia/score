26a76dd8c7ab9fd052d7e3c7f582bbac2bee0562
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-09-02 22:55:48 +0200

    Fix appimage

--- .cninja/appimage.cmake
+++ .cninja/appimage.cmake
@@ -4,9 +4,8 @@ set_cache(DEPLOYMENT_BUILD 1)
 set_cache(CMAKE_SKIP_RPATH 1)
 set_cache(BUILD_SHARED_LIBS OFF)
 set_cache(CMAKE_FIND_LIBRARY_SUFFIXES .a)
-set_cache(CMAKE_LINK_SEARCH_END_STATIC ON)
 
 add_linker_flags(" -static-libgcc -static-libstdc++")
 
-string(APPEND CMAKE_CXX_STANDARD_LIBRARIES " -lc++abi -pthread")
+string(APPEND CMAKE_CXX_STANDARD_LIBRARIES " -pthread")
 
