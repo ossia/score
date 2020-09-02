83ed6a89461f982b90a959c63d59d138a462c4af
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-09-02 21:55:52 +0200

    Fix appimage build

--- cmake/Deployment/Linux/AppImage/build.sh
+++ cmake/Deployment/Linux/AppImage/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
-sudo docker build --squash --compress --force-rm  -f Dockerfile.llvm -t ossia/score-linux-llvm . 
-sudo docker rm -f llvm-build-vm
-sudo docker run --name llvm-build-vm -v "$(pwd)"/Recipe.llvm:/Recipe -w="/" -e TRAVIS_COMMIT=`git rev-parse HEAD` ossia/score-linux-llvm /bin/bash /Recipe
-sudo docker cp llvm-build-vm:/score.AppDir.txz .
+docker build --squash --compress --force-rm  -f Dockerfile.llvm -t ossia/score-linux-llvm . 
+docker rm -f llvm-build-vm
+docker run --name llvm-build-vm -v "$(pwd)"/Recipe.llvm:/Recipe -w="/" -e TRAVIS_COMMIT=`git rev-parse HEAD` ossia/score-linux-llvm /bin/bash /Recipe
+docker cp llvm-build-vm:/score.AppDir.txz .
 tar xaf score.AppDir.txz
 cp AppRun score.AppDir/
 ./appimagetool-x86_64.AppImage -n score.AppDir Score.AppImage
