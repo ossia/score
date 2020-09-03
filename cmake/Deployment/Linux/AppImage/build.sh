e5935b9b3039878e28212e612c10075cb7b53f1d
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-09-03 11:13:42 +0200

    work on appimaage...

--- cmake/Deployment/Linux/AppImage/build.sh
+++ cmake/Deployment/Linux/AppImage/build.sh
@@ -1,7 +1,22 @@
 #!/bin/sh
-docker build --squash --compress --force-rm  -f Dockerfile.llvm -t ossia/score-linux-llvm . 
+export SOURCE_DIR=$(git rev-parse --show-toplevel)
+
+rm -rf *AppDir*
+rm -rf *AppImage
+rm -rf build
+mkdir build
+
+#docker build --squash --compress --force-rm  -f Dockerfile.llvm -t ossia/score-linux-llvm . 
 docker rm -f llvm-build-vm
-docker run --name llvm-build-vm -v "$(pwd)"/Recipe.llvm:/Recipe -w="/" -e TRAVIS_COMMIT=`git rev-parse HEAD` ossia/score-linux-llvm /bin/bash /Recipe
+
+docker run --name llvm-build-vm -it \
+           -v "$(pwd)"/Recipe.llvm:/Recipe \
+           --mount type=bind,source="$SOURCE_DIR",target=/score \
+           --mount type=bind,source="$(pwd)/build",target=/build \
+           -w="/" \
+           ossia/score-linux-llvm \
+           /bin/bash Recipe
+
 docker cp llvm-build-vm:/score.AppDir.txz .
 tar xaf score.AppDir.txz
 cp AppRun score.AppDir/
