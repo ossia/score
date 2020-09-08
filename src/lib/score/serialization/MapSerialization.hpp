f2313062bd5556e65de0213aa8d50eec60ef5178
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-09-08 21:34:54 +0200

    Add serialization for fast_hash_map

--- src/lib/score/serialization/MapSerialization.hpp
+++ src/lib/score/serialization/MapSerialization.hpp
@@ -35,6 +35,38 @@ struct TSerializer<DataStream, ossia::fast_hash_map<T, U>>
 };
 
 template <typename T, typename U>
+struct TSerializer<JSONObject, ossia::fast_hash_map<T, U>>
+{
+  using type = ossia::fast_hash_map<T, U>;
+  using pair_type = typename type::value_type;
+  static void readFrom(JSONObject::Serializer& s, const type& obj)
+  {
+    s.stream.StartArray();
+    for (const auto& pair : obj)
+    {
+      s.stream.StartArray();
+      s.readFrom(pair.first);
+      s.readFrom(pair.second);
+      s.stream.EndArray();
+    }
+    s.stream.EndArray();
+  }
+
+  static void writeTo(JSONObject::Deserializer& s, type& obj)
+  {
+    const auto& arr = s.base.GetArray();
+    for(const auto& elt : arr)
+    {
+      const auto& pair = elt.GetArray();
+      pair_type v;
+      v.first <<= JsonValue{pair[0]};
+      v.second <<= JsonValue{pair[1]};
+      obj.insert(std::move(v));
+    }
+  }
+};
+
+template <typename T, typename U>
 struct TSerializer<DataStream, ossia::flat_map<T, U>>
 {
   using type = ossia::flat_map<T, U>;
