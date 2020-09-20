afe10814106e77225175bda133a893c18d5f5b10
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-09-20 09:53:36 +0200

    try to make everything upload automatically for good

--- tools/travis/deploy.sh
+++ tools/travis/deploy.sh
@@ -5,18 +5,24 @@ then
 fi
 
 
-
+export TAG=$(echo $TRAVIS_TAG | tr -d v)
 case "$CONF" in
   osx-package)
     cd /Users/travis/build/ossia/score/bundle/
-    zip -r -9 "Score-$TRAVIS_TAG-macOS.zip" score.app
-    mv "Score-$TRAVIS_TAG-macOS.zip" ..
+    mkdir /Users/travis/build/ossia/score/deploy
+
+    zip -r -9 "score.zip" score.app
+    mv "score.zip" /Users/travis/build/ossia/score/deploy/"ossia score-$TAG-macOS.zip"
   ;;
   linux-package-appimage)
     cd /home/travis/build/ossia/score/
-    mv "Score.AppImage" "Score-$TRAVIS_TAG-amd64.AppImage"
+    mkdir /home/travis/build/ossia/score/deploy
+    mv "Score.AppImage" "deploy/ossia score-$TAG-linux-amd64.AppImage"
   ;;
   tarball)
-    mv "ossia-score.tar.xz" "Score-$TRAVIS_TAG-src.tar.xz"
+    cd /home/travis/build/ossia/score/
+    mkdir /home/travis/build/ossia/score/deploy
+    mv "ossia-score.tar.xz" "deploy/ossia score-$TAG-src.tar.xz"
+    mv "ossia-score.tar.xz.asc" "deploy/ossia score-$TAG-src.tar.xz.asc"
   ;;
 esac
