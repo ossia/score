92813d23f8a762a13581f278ffed37e0fd314235
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-10-03 13:53:38 +0200

    Minor appimage improvements

--- .cninja/appimage.cmake
+++ .cninja/appimage.cmake
@@ -5,6 +5,7 @@ set_cache(CMAKE_SKIP_RPATH 1)
 set_cache(BUILD_SHARED_LIBS OFF)
 set_cache(CMAKE_FIND_LIBRARY_SUFFIXES .a)
 set_cache(SCORE_INSTALL_HEADERS ON)
+set_cache(OSSIA_STATIC_EXPORT ON)
 
 add_linker_flags(" -static-libgcc -static-libstdc++ -Wl,--version-script,/score/cmake/Deployment/Linux/AppImage/version")
 
--- cmake/Deployment/Linux/AppImage/build.sh
+++ cmake/Deployment/Linux/AppImage/build.sh
@@ -7,9 +7,9 @@ sudo rm -rf build
 mkdir build
 
 docker build --squash --compress --force-rm  -f Dockerfile.llvm -t ossia/score-linux-llvm . 
-docker rm -f llvm-build-vm
 
-docker run --name llvm-build-vm -it \
+
+docker run --rm --name llvm-build-vm -it \
            -v "$(pwd)"/Recipe.llvm:/Recipe \
            --mount type=bind,source="$SOURCE_DIR",target=/score \
            --mount type=bind,source="$(pwd)/build",target=/build \
@@ -17,11 +17,11 @@ docker run --name llvm-build-vm -it \
            ossia/score-linux-llvm \
            /bin/bash Recipe
 
-sudo chown -R $(whoami) build
 wget "https://github.com/probonopd/AppImageKit/releases/download/continuous/appimagetool-x86_64.AppImage"
 chmod a+x appimagetool-x86_64.AppImage
 wget "https://github.com/probonopd/AppImageKit/releases/download/continuous/AppRun-x86_64"
 chmod a+x AppRun-x86_64
+sudo chown -R $(whoami) build
 cp AppRun-x86_64 build/score.AppDir/AppRun
 ./appimagetool-x86_64.AppImage -n build/score.AppDir Score.AppImage
 
