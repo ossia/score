0ecb8d1f4db056b2dad1b8e56b94063f5e61b460
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-10-04 15:16:53 +0200

    Improve macOS deployment

--- cmake/ScoreDeploymentOSX.cmake
+++ cmake/ScoreDeploymentOSX.cmake
@@ -3,32 +3,17 @@ if(APPLE)
 set_target_properties(
   ${APPNAME}
   PROPERTIES
-    MACOSX_BUNDLE_INFO_STRING "score, an interactive sequencer for the intermedia arts"
-    MACOSX_BUNDLE_GUI_IDENTIFIER "org.score"
+    MACOSX_BUNDLE_INFO_STRING "ossia score, an interactive sequencer for the intermedia arts"
+    MACOSX_BUNDLE_GUI_IDENTIFIER "io.ossia.score"
     MACOSX_BUNDLE_LONG_VERSION_STRING "${SCORE_VERSION}"
     MACOSX_BUNDLE_BUNDLE_NAME "score"
     MACOSX_BUNDLE_SHORT_VERSION_STRING "${SCORE_VERSION}"
     MACOSX_BUNDLE_BUNDLE_VERSION "${SCORE_VERSION}"
-    MACOSX_BUNDLE_COPYRIGHT "The score team"
+    MACOSX_BUNDLE_COPYRIGHT "ossia.io"
     MACOSX_BUNDLE_ICON_FILE "score.icns"
     MACOSX_BUNDLE_INFO_PLIST "${CMAKE_CURRENT_SOURCE_DIR}/Info.plist.in"
 )
 
-if(TARGET ${APPNAME}_unity)
-set_target_properties(
-  ${APPNAME}_unity
-  PROPERTIES
-    MACOSX_BUNDLE_INFO_STRING "score, an interactive sequencer for the intermedia arts"
-    MACOSX_BUNDLE_GUI_IDENTIFIER "org.score"
-    MACOSX_BUNDLE_LONG_VERSION_STRING "${SCORE_VERSION}"
-    MACOSX_BUNDLE_BUNDLE_NAME "score"
-    MACOSX_BUNDLE_SHORT_VERSION_STRING "${SCORE_VERSION}"
-    MACOSX_BUNDLE_BUNDLE_VERSION "${SCORE_VERSION}"
-    MACOSX_BUNDLE_COPYRIGHT "The score team"
-    MACOSX_BUNDLE_ICON_FILE "score.icns"
-    MACOSX_BUNDLE_INFO_PLIST "${CMAKE_CURRENT_SOURCE_DIR}/Info.plist.in"
-)
-endif()
 # Copy our dylibs if necessary
 if(NOT SCORE_STATIC_PLUGINS)
     set(SCORE_BUNDLE_PLUGINS_FOLDER "${CMAKE_INSTALL_PREFIX}/${APPNAME}.app/Contents/MacOS/plugins/")
@@ -37,21 +22,11 @@ if(NOT SCORE_STATIC_PLUGINS)
       add_custom_command(
         TARGET ${APPNAME} POST_BUILD
         COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${theTarget}> ${SCORE_BUNDLE_PLUGINS_FOLDER})
-
-      if(TARGET ${theTarget}_unity)
-        add_custom_command(
-          TARGET ${APPNAME}_unity POST_BUILD
-          COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${theTarget}_unity> ${SCORE_BUNDLE_PLUGINS_FOLDER})
-      endif()
     endfunction()
 
     # Copy score plugins into the app bundle
     add_custom_command(TARGET ${APPNAME} POST_BUILD
                        COMMAND mkdir -p ${CMAKE_INSTALL_PREFIX}/${APPNAME}.app/Contents/MacOS/plugins/)
-  if(TARGET ${APPNAME}_unity)
-    add_custom_command(TARGET ${APPNAME}_unity POST_BUILD
-               COMMAND mkdir -p ${CMAKE_INSTALL_PREFIX}/${APPNAME}.app/Contents/MacOS/plugins/)
-  endif()
 
     foreach(plugin ${SCORE_PLUGINS_LIST})
       score_copy_osx_plugin(${plugin})
@@ -60,8 +35,14 @@ endif()
 
 # set-up Qt stuff.
 # Remember to set CMAKE_INSTALL_PREFIX on the CMake command line.
-get_target_property(QT_LIBRARY_DIR Qt5::Core LOCATION)
-get_filename_component(QT_LIBRARY_DIR ${QT_LIBRARY_DIR} PATH)
+get_target_property(QT_LIBRARY_FILE Qt5::Core LOCATION)
+if("${QT_LIBRARY_FILE}" MATCHES "\.a$")
+    set(QT_STATIC 1)
+else()
+    set(QT_STATIC 0)
+endif()
+
+get_filename_component(QT_LIBRARY_DIR ${QT_LIBRARY_FILE} PATH)
 get_filename_component(QT_LIBRARY_DIR "${QT_LIBRARY_DIR}/.." ABSOLUTE)
 set(QT_PLUGINS_DIR "${Qt5Widgets_DIR}/../../../plugins")
 set(QT_QML_PLUGINS_DIR "${Qt5Widgets_DIR}/../../../qml")
@@ -85,7 +66,9 @@ Qml2Imports = Resources/qml
 [Platforms]
 CocoaArguments=fontengine=freetype
 \")
-" )
+"
+COMPONENT OssiaScore
+)
 
 # set-up Faust stuff
 if(EXISTS "${CMAKE_BINARY_DIR}/src/plugins/score-plugin-media/faustlibs-prefix/src/faustlibs")
@@ -94,6 +77,7 @@ if(EXISTS "${CMAKE_BINARY_DIR}/src/plugins/score-plugin-media/faustlibs-prefix/s
       "${CMAKE_BINARY_DIR}/src/plugins/score-plugin-media/faustlibs-prefix/src/faustlibs/"
     DESTINATION
       "${APPNAME}.app/Contents/Frameworks/Faust.framework"
+    COMPONENT OssiaScore
       PATTERN ".git" EXCLUDE
       PATTERN "doc" EXCLUDE
       PATTERN "*.html" EXCLUDE
@@ -106,7 +90,18 @@ if(EXISTS "${CMAKE_BINARY_DIR}/src/plugins/score-plugin-media/faustlibs-prefix/s
    )
 endif()
 
-if(SCORE_STATIC_PLUGINS)
+if(QT_STATIC)
+    install(CODE "
+        set(BU_CHMOD_BUNDLE_ITEMS ON)
+        include(BundleUtilities)
+        fixup_bundle(
+          \"\${CMAKE_INSTALL_PREFIX}/${APPNAME}.app\"
+          \"\"
+          \"\")
+
+        " COMPONENT OssiaScore)
+
+elseif(SCORE_STATIC_PLUGINS)
     install(CODE "
         file(GLOB_RECURSE QTPLUGINS \"\${CMAKE_INSTALL_PREFIX}/${plugin_dest_dir}/*.dylib\")
         file(GLOB_RECURSE QMLPLUGINS \"\${CMAKE_INSTALL_PREFIX}/${qml_dest_dir}/*.dylib\")
@@ -121,7 +116,7 @@ if(SCORE_STATIC_PLUGINS)
       execute_process(COMMAND
                 \"${SCORE_ROOT_SOURCE_DIR}/cmake/Deployment/OSX/set_rpath_static.sh\"
                 \"${CMAKE_INSTALL_PREFIX}/score.app/Contents\")
-        " COMPONENT Runtime)
+        " COMPONENT OssiaScore)
 else()
     set(CMAKE_INSTALL_RPATH "plugins")
     foreach(plugin ${SCORE_PLUGINS_LIST})
