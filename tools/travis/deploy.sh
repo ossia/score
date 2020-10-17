76827bd5f64a09f6caa8a84c05ca26a714562735
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-10-17 14:34:41 +0200

    more work on entitlements

--- tools/travis/deploy.sh
+++ tools/travis/deploy.sh
@@ -7,6 +7,9 @@ fi
 export TAG=$(echo $TRAVIS_TAG | tr -d v)
 case "$CONF" in
   osx-package)
+    brew install graphicsmagick imagemagick npm
+    npm install --global create-dmg
+
     cd /Users/travis/build/ossia/score/bundle/
     mkdir /Users/travis/build/ossia/score/deploy
 
@@ -14,25 +17,45 @@ case "$CONF" in
     security unlock-keychain -p travis build.keychain
     codesign \
       --entitlements /Users/travis/build/ossia/score/src/app/entitlements.plist \
+      --deep \
       --force \
       --timestamp \
+      --options=runtime \
       --sign "ossia.io" \
       score.app
 
     # Create a .dmg
-    brew install graphicsmagick imagemagick
-    npm install --global create-dmg
     cp /Users/travis/build/ossia/score/LICENSE.txt license.txt
+    security unlock-keychain -p travis build.keychain
     create-dmg 'score.app'
     ls
 
     # Notarize the .dmg
+    security unlock-keychain -p travis build.keychain
     xcrun altool --notarize-app \
                  -t osx \
                  -f *.dmg \
                  --primary-bundle-id "io.ossia.score" \
                  -u jeanmichael.celerier@gmail.com \
-                 -p "@env:MAC_ALTOOL_PASSWORD"
+                 -p "@env:MAC_ALTOOL_PASSWORD" > altool.log
+
+    REQ_UUID=$(cat altool.log | grep Request | awk ' { print $3 } ')
+
+    # Wait until the notarization process completes to staple the result to the dmg file
+    sleep 30
+    for seconds in 30 30 30 30 30; do
+      RES=$(xcrun altool --notarization-info $REQ_UUID -u  jeanmichael.celerier@gmail.com -p "@env:MAC_ALTOOL_PASSWORD" --output-format xml)
+      if [[ $(echo "$RES" | grep Approved) ]]; then
+        xcrun stapler staple *.dmg
+        echo "Stapling successful !"
+        break
+      elif [[ $(echo "$RES" | grep -i progress) ]]; then
+        sleep $seconds
+      else
+        echo "Stapling failed ! Displaying log..."
+        echo "$RES"
+      fi
+    done
 
     mv *.dmg "/Users/travis/build/ossia/score/deploy/ossia score-$TAG-macOS.dmg"
     mv "mac-sdk.zip" "/Users/travis/build/ossia/score/deploy/"
