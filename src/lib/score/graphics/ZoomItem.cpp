7a92fb47bc3646e59a78f0222cf9e4cfc4db3871
Akané Lévy <levyakane@gmail.com>
2020-10-18 15:34:40 +0200

    updated zoom icons

--- src/lib/score/graphics/ZoomItem.cpp
+++ src/lib/score/graphics/ZoomItem.cpp
@@ -10,14 +10,17 @@ namespace score
 ZoomItem::ZoomItem(QGraphicsItem* parent)
   : QGraphicsItem{parent}
 {
-  static const auto plus_pixmap = score::get_pixmap(":/icons/zoom/plus.png");
-  static const auto minus_pixmap = score::get_pixmap(":/icons/zoom/minus.png");
-  static const auto center_pixmap = score::get_pixmap(":/icons/zoom/center.png");
-  auto zplus = new score::QGraphicsPixmapButton{plus_pixmap, plus_pixmap, this};
-  auto zminus = new score::QGraphicsPixmapButton{minus_pixmap, minus_pixmap, this};
-  zminus->setPos({0, 15});
-  auto zcenter = new score::QGraphicsPixmapButton{center_pixmap, center_pixmap, this};
-  zcenter->setPos({0, 30});
+  static const auto plus_pixmap_on = score::get_pixmap(":/icons/zoom/plus_on.png");
+  static const auto plus_pixmap_off = score::get_pixmap(":/icons/zoom/plus_off.png");
+  static const auto minus_pixmap_on = score::get_pixmap(":/icons/zoom/minus_on.png");
+  static const auto minus_pixmap_off = score::get_pixmap(":/icons/zoom/minus_off.png");
+  static const auto center_pixmap_on = score::get_pixmap(":/icons/zoom/center_on.png");
+  static const auto center_pixmap_off = score::get_pixmap(":/icons/zoom/center_off.png");
+  auto zplus = new score::QGraphicsPixmapButton{plus_pixmap_on, plus_pixmap_off, this};
+  auto zminus = new score::QGraphicsPixmapButton{minus_pixmap_on, minus_pixmap_off, this};
+  zminus->setPos({0, 18});
+  auto zcenter = new score::QGraphicsPixmapButton{center_pixmap_on, center_pixmap_off, this};
+  zcenter->setPos({0, 36});
 
   setFlag(ItemHasNoContents, true);
 
