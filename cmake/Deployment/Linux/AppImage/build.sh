d42dd535d5dbe00cddcf55f2a7b9d3a5931da6a6
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-11-24 20:27:30 +0100

    Update appimage sdk

--- cmake/Deployment/Linux/AppImage/build.sh
+++ cmake/Deployment/Linux/AppImage/build.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 sudo rm -rf *AppDir*
 sudo rm -rf *AppImage
-sudo rm -rf build
-mkdir build
+sudo rm -rf /tmp/build
+mkdir /tmp/build
 
 docker build --squash --compress --force-rm  -f Dockerfile.llvm -t ossia/score-linux-llvm . 
 
@@ -10,7 +10,7 @@ docker build --squash --compress --force-rm  -f Dockerfile.llvm -t ossia/score-l
 docker run --rm -it \
            -v "$(pwd)"/Recipe.llvm:/Recipe \
            --mount type=bind,source=$(git rev-parse --show-toplevel),target=/score \
-           --mount type=bind,source="$(pwd)/build",target=/build \
+           --mount type=bind,source="/tmp/build",target=/build \
            -w="/" \
            ossia/score-linux-llvm \
            /bin/bash Recipe
@@ -19,7 +19,8 @@ wget "https://github.com/probonopd/AppImageKit/releases/download/continuous/appi
 chmod a+x appimagetool-x86_64.AppImage
 wget "https://github.com/probonopd/AppImageKit/releases/download/continuous/AppRun-x86_64"
 chmod a+x AppRun-x86_64
-sudo chown -R $(whoami) build
+sudo chown -R $(whoami) /tmp/build
 cp AppRun-x86_64 build/score.AppDir/AppRun
-./appimagetool-x86_64.AppImage -n build/score.AppDir Score.AppImage
+cp -rf /tmp/build/score.AppDir .
+./appimagetool-x86_64.AppImage -n score.AppDir Score.AppImage
 
