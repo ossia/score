3388ea4ad3cc00bc5c4a8ad32139c4ee11557daf
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-12-04 15:56:21 +0100

    Fix various soundfile bugs

--- src/plugins/score-plugin-media/Media/Sound/SoundComponent.cpp
+++ src/plugins/score-plugin-media/Media/Sound/SoundComponent.cpp
@@ -13,8 +13,22 @@
 #include <ossia/dataflow/nodes/sound_ref.hpp>
 #include <ossia/detail/pod_vector.hpp>
 
+namespace
+{
+
+static std::unique_ptr<ossia::resampler> make_resampler(const Media::Sound::ProcessModel& element) noexcept
+{
+  auto res = std::make_unique<ossia::resampler>();
+  const auto channels = element.file()->channels();
+  const auto sampleRate = element.file()->sampleRate();
+  res->reset(0, element.stretchMode(), channels, sampleRate);
+  return res;
+}
+
+}
 namespace Media
 {
+
 class SoundComponentSetup
 {
 public:
@@ -26,56 +40,45 @@ public:
     struct
     {
       Execution::SoundComponent& component;
-      void operator()() const noexcept { construct_dummy(component); }
+      void operator()() const noexcept
+      {
+        // Note : we need to construct a dummy node in case there is nothing,
+        // because else the parent interval will destroy the component so
+        // even if the sound is created afterwards there won't be any component
+        // anymore
+
+        auto node = std::make_shared<ossia::nodes::dummy_node>();
+        component.node = node;
+        component.m_ossia_process = std::make_shared<ossia::sound_process>(node);
+      }
       void operator()(const std::shared_ptr<Media::AudioFile::LibavReader>& r) const noexcept
       {
-        construct_ffmpeg(r, component);
+        Execution::Transaction commands{component.system()};
+
+        auto node = std::make_shared<ossia::nodes::sound_ref>();
+        component.node = node;
+        component.m_ossia_process = std::make_shared<ossia::sound_process>(node);
+        update_ref(node, r, component, commands);
+
+        commands.run_all();
       }
       void operator()(const Media::AudioFile::MmapReader& r) const noexcept
       {
-        construct_drwav(r, component);
+        Execution::Transaction commands{component.system()};
+
+        auto node = std::make_shared<ossia::nodes::sound_mmap>();
+        component.node = node;
+        component.m_ossia_process = std::make_shared<ossia::sound_process>(node);
+        update_mmap(node, r, component, commands);
+
+        commands.run_all();
       }
     } _{component};
 
     ossia::apply(_, handle->m_impl);
   }
 
-  // Note : we need to construct a dummy node in case there is nothing,
-  // because else the parent interval will destroy the component so
-  // even if the sound is created afterwards there won't be any component
-  // anymore
-  static void construct_dummy(Execution::SoundComponent& component)
-  {
-    auto node = std::make_shared<ossia::nodes::dummy_node>();
-    component.node = node;
-    if (!component.m_ossia_process)
-      component.m_ossia_process = std::make_shared<ossia::node_process>(node);
-  }
-
-  static void construct_ffmpeg(
-      const std::shared_ptr<Media::AudioFile::LibavReader>& r,
-      Execution::SoundComponent& component)
-  {
-    auto node = std::make_shared<ossia::nodes::sound_ref>();
-    component.node = node;
-    if (!component.m_ossia_process)
-      component.m_ossia_process = std::make_shared<ossia::sound_process>(node);
-
-    recompute_ffmpeg(r, component);
-  }
-
-  static void
-  construct_drwav(const Media::AudioFile::MmapReader& r, Execution::SoundComponent& component)
-  {
-    auto node = std::make_shared<ossia::nodes::sound_mmap>();
-    component.node = node;
-    if (!component.m_ossia_process)
-      component.m_ossia_process = std::make_shared<ossia::sound_process>(node);
-
-    recompute_drwav(r, component);
-  }
-
-  static void recompute(Execution::SoundComponent& component)
+  static void update(Execution::SoundComponent& component)
   {
     Sound::ProcessModel& element = component.process();
     auto handle = element.file();
@@ -86,89 +89,99 @@ public:
       void operator()() const noexcept { return; }
       void operator()(const std::shared_ptr<Media::AudioFile::LibavReader>& r) const noexcept
       {
-        recompute_ffmpeg(r, component);
+        Execution::Transaction commands{component.system()};
+        auto old_node = component.node;
+
+        if(auto n = std::dynamic_pointer_cast<ossia::nodes::sound_ref>(old_node))
+        {
+          update_ref(n, r, component, commands);
+        }
+        else
+        {
+          n = std::make_shared<ossia::nodes::sound_ref>();
+          update_ref(n, r, component, commands);
+          component.system().setup.unregister_node(component.process(), old_node, commands);
+          component.system().setup.register_node(component.process(), component.node, commands);
+          component.system().setup.replace_node(component.OSSIAProcessPtr(), component.node, commands);
+          component.nodeChanged(old_node, component.node, commands);
+        }
+
+        commands.run_all();
       }
       void operator()(const Media::AudioFile::MmapReader& r) const noexcept
       {
-        recompute_drwav(r, component);
+        if(!r.wav)
+          return;
+
+        Execution::Transaction commands{component.system()};
+        auto old_node = component.node;
+
+        if(auto n = std::dynamic_pointer_cast<ossia::nodes::sound_mmap>(old_node))
+        {
+          update_mmap(n, r, component, commands);
+        }
+        else
+        {
+          n = std::make_shared<ossia::nodes::sound_mmap>();
+          update_mmap(n, r, component, commands);
+          component.system().setup.unregister_node(component.process(), old_node, commands);
+          component.system().setup.register_node(component.process(), component.node, commands);
+          component.system().setup.replace_node(component.OSSIAProcessPtr(), component.node, commands);
+          component.nodeChanged(old_node, component.node, commands);
+        }
+
+        commands.run_all();
       }
     } _{component};
 
     ossia::apply(_, handle->m_impl);
   }
-  static void recompute_ffmpeg(
+
+  static void update_ref(
+      const std::shared_ptr<ossia::nodes::sound_ref>& n,
       const std::shared_ptr<Media::AudioFile::LibavReader>& r,
-      Execution::SoundComponent& component)
+      Execution::SoundComponent& component,
+      Execution::Transaction& commands)
   {
     auto& p = component.process();
-    auto old_node = component.node;
-    auto n = std::dynamic_pointer_cast<ossia::nodes::sound_ref>(old_node);
-    if (n)
-    {
-      component.in_exec([n,
-                         data = r->handle,
-                         channels = r->decoder.channels,
-                         sampleRate = r->decoder.convertedSampleRate,
-                         tempo = component.process().nativeTempo(),
-                         mode = component.process().stretchMode(),
-                         upmix = p.upmixChannels(),
-                         start = p.startChannel()] {
-        n->set_sound(std::move(data), channels, sampleRate);
-        n->set_start(start);
-        n->set_upmix(upmix);
-        n->set_stretch_mode(mode);
-        n->set_native_tempo(tempo);
-      });
-    }
-    else
-    {
-      construct_ffmpeg(r, component);
-      Execution::Transaction commands{component.system()};
-      component.system().setup.unregister_node(component.process(), old_node, commands);
-      component.system().setup.register_node(component.process(), component.node, commands);
-      component.system().setup.replace_node(component.OSSIAProcessPtr(), component.node, commands);
-      component.nodeChanged(old_node, component.node, commands);
-
-      commands.run_all();
-    }
+    commands.push_back([n,
+                       data = r->handle,
+                       channels = r->decoder.channels,
+                       sampleRate = r->decoder.convertedSampleRate,
+                       tempo = component.process().nativeTempo(),
+                       res = make_resampler(component.process()),
+                       upmix = p.upmixChannels(),
+                       start = p.startChannel()] () mutable{
+      n->set_sound(std::move(data), channels, sampleRate);
+      n->set_start(start);
+      n->set_upmix(upmix);
+      n->set_resampler(std::move(*res));
+      n->set_native_tempo(tempo);
+    });
   }
+
   static void
-  recompute_drwav(const Media::AudioFile::MmapReader& r, Execution::SoundComponent& component)
+  update_mmap(
+      const std::shared_ptr<ossia::nodes::sound_mmap>& n,
+      const Media::AudioFile::MmapReader& r,
+      Execution::SoundComponent& component,
+      Execution::Transaction& commands)
   {
-    if (!r.wav)
-      return;
-    Sound::ProcessModel& p = component.process();
-
-    auto old_node = component.node;
-    auto n = std::dynamic_pointer_cast<ossia::nodes::sound_mmap>(old_node);
-    if (n)
-    {
-      component.in_exec(
+    auto& p = component.process();
+    commands.push_back(
           [n,
-           data = r.wav,
-           channels = r.wav.channels(),
-           tempo = component.process().nativeTempo(),
-           mode = component.process().stretchMode(),
-           upmix = p.upmixChannels(),
-           start = p.startChannel()]() mutable {
-            n->set_sound(std::move(data));
-            n->set_start(start);
-            n->set_upmix(upmix);
-            n->set_stretch_mode(mode);
-            n->set_native_tempo(tempo);
-          });
-    }
-    else
-    {
-      construct_drwav(r, component);
-      Execution::Transaction commands{component.system()};
-      component.system().setup.unregister_node(component.process(), old_node, commands);
-      component.system().setup.register_node(component.process(), component.node, commands);
-      component.system().setup.replace_node(component.OSSIAProcessPtr(), component.node, commands);
-      component.nodeChanged(old_node, component.node, commands);
-
-      commands.run_all();
-    }
+          data = r.wav,
+          channels = r.wav.channels(),
+          tempo = component.process().nativeTempo(),
+          res = make_resampler(component.process()),
+          upmix = p.upmixChannels(),
+          start = p.startChannel()]() mutable {
+      n->set_sound(std::move(data));
+      n->set_start(start);
+      n->set_upmix(upmix);
+      n->set_resampler(std::move(*res));
+      n->set_native_tempo(tempo);
+    });
   }
 };
 }
@@ -189,11 +202,11 @@ SoundComponent::SoundComponent(
   connect(
       &element, &Media::Sound::ProcessModel::fileChanged, this, &SoundComponent::on_fileChanged);
 
-  auto node_action = [this](auto f) {
-    if (auto node = std::dynamic_pointer_cast<ossia::nodes::sound_ref>(this->node))
-      in_exec([node, f] { f(*node); });
-    else if (auto node = std::dynamic_pointer_cast<ossia::nodes::sound_mmap>(this->node))
-      in_exec([node, f] { f(*node); });
+  auto node_action = [this](auto&& f) {
+    if (auto n_ref = std::dynamic_pointer_cast<ossia::nodes::sound_ref>(this->node))
+      in_exec([n_ref, ff=std::move(f)] () mutable { ff(*n_ref); });
+    else if (auto n_mmap = std::dynamic_pointer_cast<ossia::nodes::sound_mmap>(this->node))
+      in_exec([n_mmap, ff=std::move(f)] () mutable { ff(*n_mmap); });
   };
 
   con(element, &Media::Sound::ProcessModel::startChannelChanged, this, [=, &element] {
@@ -206,7 +219,9 @@ SoundComponent::SoundComponent(
     node_action([start = element.nativeTempo()](auto& node) { node.set_native_tempo(start); });
   });
   con(element, &Media::Sound::ProcessModel::stretchModeChanged, this, [=, &element] {
-    node_action([start = element.stretchMode()](auto& node) { node.set_stretch_mode(start); });
+    node_action([r = make_resampler(element)](auto& node) mutable {
+      node.set_resampler(std::move(*r));
+    });
   });
 
   if (auto& file = element.file())
@@ -229,7 +244,7 @@ void SoundComponent::on_fileChanged()
 
 void SoundComponent::recompute()
 {
-  Media::SoundComponentSetup{}.recompute(*this);
+  Media::SoundComponentSetup{}.update(*this);
 }
 
 SoundComponent::~SoundComponent() { }
