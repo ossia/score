a3e050ad6e31cdcf39cd555128db6f1fc5381da1
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-12-23 12:23:10 +0100

    ci work

--- ci/bionic.deps.sh
+++ ci/bionic.deps.sh
@@ -17,6 +17,7 @@ sudo apt-get install -qq --force-yes \
      libfftw3-dev \
      libsuil-dev liblilv-dev lv2-dev \
      qtbase5-dev qtdeclarative5-dev libqt5serialport5-dev libqt5websockets5-dev libqt5opengl5-dev \
+     qtbase5-private-dev qtdeclarative5-private-dev \
      libbluetooth-dev libsdl2-dev libsdl2-2.0-0 \
      libglu1-mesa-dev libglu1-mesa libgles2-mesa-dev \
      libavahi-compat-libdnssd-dev libsamplerate0-dev \
