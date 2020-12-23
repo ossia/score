a302af4e931f2c49a68615f653db06a6180deb59
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-12-23 23:23:10 +0100

    add arch to github workflows

---
+++ ci/archlinux.deps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash -eux
+
+pacman -S --noconfirm \
+   cmake ninja gcc llvm clang boost \
+   qt5-base qt5-imageformats qt5-websockets qt5-serialport qt5-declarative qt5-tools \
+   ffmpeg portaudio jack2 lv2 suil lilv sdl2 alsa-lib \
+   avahi fftw bluez-libs
+
