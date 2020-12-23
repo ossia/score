2665e64cf67384467513e177772fdcb8bb717c72
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-12-23 23:52:26 +0100

    arch package needs icu

--- ci/archlinux.deps.sh
+++ ci/archlinux.deps.sh
@@ -2,7 +2,7 @@
 
 pacman -S --noconfirm \
    cmake ninja gcc llvm clang boost \
-   qt5-base qt5-imageformats qt5-websockets qt5-serialport qt5-declarative qt5-tools \
+   qt5-base qt5-imageformats qt5-websockets qt5-serialport qt5-declarative qt5-tools icu \
    ffmpeg portaudio jack2 lv2 suil lilv sdl2 alsa-lib \
    avahi fftw bluez-libs
 
