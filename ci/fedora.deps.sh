d6103ccc78260d85fb925958430427a44e086926
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2020-12-23 23:29:28 +0100

    try to fix fedora prompt?

--- ci/fedora.deps.sh
+++ ci/fedora.deps.sh
@@ -1,12 +1,12 @@
 #!/bin/bash -eux
 
 # Needed for ffmpeg-devel
-dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
-dnf install -y https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+dnf -y install https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm
+dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
 
 # TODO fix SDL2-devel SDL2-static -fPIC issue
 
-dnf install -y \
+dnf -y install \
      cmake ninja-build \
      ffmpeg-devel llvm-devel \
      alsa-lib-devel portaudio-devel \
