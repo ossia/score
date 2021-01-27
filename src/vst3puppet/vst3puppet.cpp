641e6f84f5d5e8817f67802efa165e252cb512bc
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-01-27 22:41:36 +0100

    Add a binary to scan vst3 plug-ins

---
+++ src/vst3puppet/vst3puppet.cpp
@@ -0,0 +1,121 @@
+#include <public.sdk/source/vst/hosting/module.h>
+#include <public.sdk/source/vst/hosting/plugprovider.h>
+#include <public.sdk/source/vst/hosting/hostclasses.h>
+#include <pluginterfaces/base/funknown.h>
+#include <pluginterfaces/vst/ivstcomponent.h>
+#include <pluginterfaces/vst/ivstaudioprocessor.h>
+
+#include <QGuiApplication>
+#include <QJsonDocument>
+#include <QJsonObject>
+#include <QJsonArray>
+#include <QUrl>
+#include <QTimer>
+#include <QFile>
+#include <QWebSocket>
+#include <iostream>
+#include <set>
+#include <QWindow>
+using namespace Steinberg;
+QString load_vst(const QString& path, int id)
+{
+  try
+  {
+    bool isFile = QFile(QUrl(path).toString(QUrl::PreferLocalFile)).exists();
+    if (!isFile)
+    {
+      std::cerr << "Invalid path: " << path.toStdString() << std::endl;
+      return {};
+    }
+
+    std::string err;
+    auto module = VST3::Hosting::Module::create(path.toStdString(), err);
+
+    if (!module)
+    {
+      std::cerr << "Failed to load VST3 " << path.toStdString() <<  err << std::endl;
+    }
+
+    const auto& info = module->getFactory().info();
+    QJsonArray arr;
+    for(const auto& cls : module->getFactory().classInfos())
+    {
+      if (cls.category() == kVstAudioEffectClass)
+      {
+        QJsonObject obj;
+
+        obj["Author"] = QString::fromStdString(cls.vendor());
+        obj["PrettyName"] = QString::fromStdString(cls.name());
+        obj["Subcategories"] = QString::fromStdString(cls.subCategoriesString());
+        obj["Version"] = QString::fromStdString(cls.version());
+        obj["UID"] = QString::fromStdString(cls.ID().toString());
+        obj["Path"] = path;
+        obj["Request"] = id;
+
+        arr.push_back(obj);
+      }
+    }
+    return QJsonDocument{arr}.toJson();
+  }
+  catch (const std::runtime_error& e)
+  {
+    std::cerr << e.what() << std::endl;
+  }
+  return {};
+}
+
+int main(int argc, char** argv)
+{
+  if (argc > 1)
+  {
+    int id = 0;
+    if(argc > 2) {
+        id = QString(argv[2]).toInt();
+    }
+    QGuiApplication app(argc, argv);
+    QWindow w;
+    w.setWidth(1);
+    w.setHeight(1);
+    w.setFlag(Qt::FramelessWindowHint);
+    w.setFlag(Qt::X11BypassWindowManagerHint);
+    w.show();
+
+    QWebSocket socket;
+
+    bool socket_ready{}, vst_ready{};
+    QString json_ret;
+
+    auto onReady = [&] {
+      if(socket_ready && vst_ready) {
+        socket.sendTextMessage(json_ret);
+        socket.flush();
+        socket.close();
+        app.exit(json_ret.isEmpty() ? 1 : 0);
+      }
+    };
+
+    QTimer::singleShot(32, [&] {
+      json_ret = load_vst(argv[1], id);
+      std::cout << json_ret.toStdString();
+      vst_ready = true;
+      onReady();
+    });
+
+    QObject::connect(&socket, &QWebSocket::connected,
+            &app, [&] {
+      socket_ready = true;
+      onReady();
+    });
+
+    QObject::connect(&socket, qOverload<QAbstractSocket::SocketError>(&QWebSocket::error), &app,
+                     [&] { qDebug() << socket.errorString(); app.exit(1); });
+    QObject::connect(&socket, &QWebSocket::disconnected, &app,
+                     [&] { qDebug() << socket.errorString(); app.exit(1); });
+
+    QTimer::singleShot(10000, [&] { qDebug() << "timeout"; qApp->exit(1); });
+
+    socket.open(QUrl("ws://127.0.0.1:37587"));
+    app.exec();
+  }
+  return 1;
+}
