6d0189d345f1b2b0951b1bfcd9a83482129b9c93
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-03-01 11:30:18 +0100

    Try to add wasm build to CI

---
+++ ci/wasm.build.sh
@@ -0,0 +1,50 @@
+#!/bin/bash -eux
+
+mkdir -p /build || true
+cd /build
+
+export QT=/opt/ossia-sdk-wasm/qt5
+
+cmake -GNinja $SCORE_DIR \
+   -DCMAKE_C_FLAGS="-O3 -g0" \
+   -DCMAKE_CXX_FLAGS="-O3 -g0" \
+   -DCMAKE_CXX_STANDARD=17 \
+   -DCMAKE_BUILD_TYPE=Release \
+   -DCMAKE_UNITY_BUILD=1 \
+   -DCMAKE_TOOLCHAIN_FILE=/opt/ossia-sdk-wasm/emsdk/upstream/emscripten/cmake/Modules/Platform/Emscripten.cmake \
+   -DOSSIA_PCH=0 \
+   -DSCORE_PCH=0 \
+   -DQt5_DIR=$QT/lib/cmake/Qt5 \
+   -DQt5Core_DIR=$QT/lib/cmake/Qt5Core \
+   -DQt5DeviceDiscoverySupport_DIR=$QT/lib/cmake/Qt5DeviceDiscoverySupport \
+   -DQt5EdidSupport_DIR=$QT/lib/cmake/Qt5EdidSupport \
+   -DQt5EglSupport_DIR=$QT/lib/cmake/Qt5EglSupport \
+   -DQt5EventDispatcherSupport_DIR=$QT/lib/cmake/Qt5EventDispatcherSupport \
+   -DQt5FbSupport_DIR=$QT/lib/cmake/Qt5FbSupport \
+   -DQt5FontDatabaseSupport_DIR=$QT/lib/cmake/Qt5FontDatabaseSupport \
+   -DQt5Gui_DIR=$QT/lib/cmake/Qt5Gui \
+   -DQt5Multimedia_DIR=$QT/lib/cmake/Qt5Multimedia \
+   -DQt5MultimediaQuick_DIR=$QT/lib/cmake/Qt5MultimediaQuick \
+   -DQt5MultimediaWidgets_DIR=$QT/lib/cmake/Qt5MultimediaWidgets \
+   -DQt5Network_DIR=$QT/lib/cmake/Qt5Network \
+   -DQt5OpenGL_DIR=$QT/lib/cmake/Qt5OpenGL \
+   -DQt5OpenGLExtensions_DIR=$QT/lib/cmake/Qt5OpenGLExtensions \
+   -DQt5PlatformCompositorSupport_DIR=$QT/lib/cmake/Qt5PlatformCompositorSupport \
+   -DQt5Qml_DIR=$QT/lib/cmake/Qt5Qml \
+   -DQt5QmlDevTools_DIR=$QT/lib/cmake/Qt5QmlDevTools \
+   -DQt5QmlImportScanner_DIR=$QT/lib/cmake/Qt5QmlImportScanner \
+   -DQt5QmlModels_DIR=$QT/lib/cmake/Qt5QmlModels \
+   -DQt5Quick_DIR=$QT/lib/cmake/Qt5Quick \
+   -DQt5QuickCompiler_DIR=$QT/lib/cmake/Qt5QuickCompiler \
+   -DQt5QuickParticles_DIR=$QT/lib/cmake/Qt5QuickParticles \
+   -DQt5QuickShapes_DIR=$QT/lib/cmake/Qt5QuickShapes \
+   -DQt5QuickWidgets_DIR=$QT/lib/cmake/Qt5QuickWidgets \
+   -DQt5ServiceSupport_DIR=$QT/lib/cmake/Qt5ServiceSupport \
+   -DQt5ThemeSupport_DIR=$QT/lib/cmake/Qt5ThemeSupport \
+   -DQt5WebSockets_DIR=$QT/lib/cmake/Qt5WebSockets \
+   -DQt5Widgets_DIR=$QT/lib/cmake/Qt5Widgets \
+   -DQt5Xml_DIR=$QT/lib/cmake/Qt5Xml \
+   -DQt5Zlib_DIR=$QT/lib/cmake/Qt5Zlib
+
+
+cmake --build .
---
+++ ci/wasm.deps.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -eux
+
+sudo apt-get update -qq
+sudo apt-get install wget
+
+export SDK_ARCHIVE=sdk-wasm.tar.xz
+wget -nv https://github.com/ossia/score-sdk/releases/download/sdk18/$SDK_ARCHIVE -O $SDK_ARCHIVE
+
+sudo mkdir -p /opt/ossia-sdk-wasm
+sudo chown -R $(whoami) /opt/ossia-sdk-wasm
+sudo chmod -R a+rwx /opt/ossia-sdk-wasm
+tar xaf $SDK_ARCHIVE --strip-components=2 --directory /opt/ossia-sdk-wasm/
