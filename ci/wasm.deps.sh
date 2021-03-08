560a3b4136ce0454ef77b64aa89e71168574d54d
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-03-08 10:13:21 +0100

    add missing build tool to wasm

--- ci/wasm.deps.sh
+++ ci/wasm.deps.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 
 sudo apt-get update -qq
-sudo apt-get install wget
+sudo apt-get install wget ninja-build
 
 export SDK_ARCHIVE=sdk-wasm.tar.xz
 wget -nv https://github.com/ossia/score-sdk/releases/download/sdk18/$SDK_ARCHIVE -O $SDK_ARCHIVE
