bf8f59492180099d08cf03cf8aa3af3d40e826ae
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-03-08 10:44:06 +0100

    add missing build deps for some ci configurations

--- ci/raspberrypi.deps.sh
+++ ci/raspberrypi.deps.sh
@@ -2,7 +2,7 @@
 
 # Note: this is run from an ArchLinux container due to too recent glibc
 pacman -Syyu --noconfirm
-pacman -S wget ninja --noconfirm
+pacman -S wget ninja cmake tar --noconfirm
 
 export SDK_ARCHIVE=sdk-rpi.tar.xz
 wget -nv https://github.com/ossia/score-sdk/releases/download/sdk19/$SDK_ARCHIVE -O $SDK_ARCHIVE
--- ci/wasm.deps.sh
+++ ci/wasm.deps.sh
@@ -2,7 +2,7 @@
 
 # Note: this is run from an ArchLinux container due to too recent glibc
 pacman -Syyu --noconfirm
-pacman -S wget ninja --noconfirm
+pacman -S wget ninja cmake which python tar git --noconfirm
 
 export SDK_ARCHIVE=sdk-wasm.tar.xz
 wget -nv https://github.com/ossia/score-sdk/releases/download/sdk19/$SDK_ARCHIVE -O $SDK_ARCHIVE
