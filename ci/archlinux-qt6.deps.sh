f9193208a7bcb24f43d9b8bcfabed9ce4d2c1a48
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-04-04 17:37:59 +0200

    Add CI for Qt 6

---
+++ ci/archlinux-qt6.deps.sh
@@ -0,0 +1,15 @@
+#!/bin/bash -eux
+
+pacman -S --noconfirm \
+   cmake ninja gcc llvm clang boost \
+   icu \
+   ffmpeg portaudio jack2 lv2 suil lilv sdl2 alsa-lib \
+   avahi fftw bluez-libs \
+   tar xz wget
+
+wget https://github.com/ossia/sdk/releases/download/sdk20/qt6-debug.tar.xz
+tar xaf qt6-debug.tar.xz
+ls
+mkdir -p /opt/
+mv qt6 /opt/
+
