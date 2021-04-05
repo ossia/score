bd409284fe2d8da6190466fa780bdd77da468432
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-04-05 10:43:20 +0200

    add more needed qt6 deps

--- ci/archlinux-qt6.deps.sh
+++ ci/archlinux-qt6.deps.sh
@@ -6,8 +6,13 @@ pacman -S --noconfirm \
    ffmpeg portaudio jack2 lv2 suil lilv sdl2 alsa-lib \
    avahi fftw bluez-libs \
    tar xz wget \
+   brotli double-conversion zstd glib2 libb2 pcre2 \
    libxkbcommon libxkbcommon-x11 \
-   vulkan-headers vulkan-icd-loader 
+   vulkan-headers vulkan-icd-loader \
+   mesa freetype2 harfbuzz fontconfig libglvnd \
+   libdrm tslib udev zstd \
+   xcb-proto xcb-util xcb-util-cursor xcb-util-image \
+   xcb-util-keysyms xcb-util-renderutil xcb-util-wm
 
 wget https://github.com/ossia/sdk/releases/download/sdk20/qt6-debug.tar.xz
 tar xaf qt6-debug.tar.xz
