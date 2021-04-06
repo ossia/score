b96a9c999927390b9abacc36ccf465c259127f7e
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-04-06 14:02:58 +0200

    try to make travis builds faster

--- ci/focal.build.sh
+++ ci/focal.build.sh
@@ -8,7 +8,7 @@ cmake .. \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=install \
   -DSCORE_DYNAMIC_PLUGINS=1 \
-  -DSCORE_PCH=1
+  -DCMAKE_UNITY_BUILD=1
 
 cmake --build .
 cmake --build . --target install
