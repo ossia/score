d03b8f464c3831ba3b4606b2cb6fe56835fb50ae
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-05-30 23:18:55 +0200

    add missing flag for jit on win32..

--- ci/win32.build.cmd
+++ ci/win32.build.cmd
@@ -5,7 +5,6 @@ set PATH=%PATH%;c:\ossia-sdk\llvm\bin
 cmake -GNinja %BUILD_SOURCESDIRECTORY% ^
   -DCMAKE_C_COMPILER=c:/ossia-sdk/llvm/bin/clang.exe ^
   -DCMAKE_CXX_COMPILER=c:/ossia-sdk/llvm/bin/clang++.exe ^
-  -DCMAKE_BUILD_TYPE=Release ^
   -DOSSIA_SDK=c:\ossia-sdk ^
   -DCMAKE_INSTALL_PREFIX=install ^
   -DCMAKE_PREFIX_PATH="c:/ossia-sdk/qt5-static;c:/ossia-sdk/llvm-libs;c:/ossia-sdk/SDL2;c:/ossia-sdk" ^
@@ -13,6 +12,7 @@ cmake -GNinja %BUILD_SOURCESDIRECTORY% ^
   -DCMAKE_UNITY_BUILD=1 ^
   -DOSSIA_STATIC_EXPORT=1 ^
   -DSCORE_INSTALL_HEADERS=1 ^
+  -DCMAKE_EXE_LINKER_FLAGS="-Wl,--export-all-symbols" ^
   -DDEPLOYMENT_BUILD=1
 
 cmake --build .
