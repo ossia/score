b505b4aa41946b8c71fa30e2e4b03675b534ce95
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-08-12 16:37:30 +0200

    Improve VST2 timing

--- src/plugins/score-plugin-vst/Vst/Node.hpp
+++ src/plugins/score-plugin-vst/Vst/Node.hpp
@@ -105,7 +105,9 @@ public:
     static const constexpr double ppq_reference = 960.;
 
     auto& time_info = fx->info;
-    time_info.samplePos = tk.date.impl;
+    // TODO this isn't accurate when tempo becomes slower !
+    // We need to track the actual number of audio buffers played throuh an interval
+    time_info.samplePos = tk.start_date_to_physical(st.modelToSamples());
     time_info.sampleRate = st.sampleRate();
     time_info.nanoSeconds = st.currentDate() - st.startDate();
     time_info.ppqPos = tk.musical_start_position * ppq_reference;
