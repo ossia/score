022ba6174cff8c5e160428680aa114c99ae86a73
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2021-10-01 13:26:09 +0200

    Try to fix NDI plug-in build with the linux JIT

--- src/plugins/score-plugin-gfx/Gfx/GfxExecContext.hpp
+++ src/plugins/score-plugin-gfx/Gfx/GfxExecContext.hpp
@@ -6,59 +6,24 @@
 
 #include <concurrentqueue.h>
 
+#include <score_plugin_gfx_export.h>
+
 namespace Gfx
 {
 
-class GfxExecutionAction final : public Execution::ExecutionAction
+class SCORE_PLUGIN_GFX_EXPORT GfxExecutionAction final : public Execution::ExecutionAction
 {
   SCORE_CONCRETE("06f48270-35a4-44d2-929a-e67b8e2904f5")
 public:
-  GfxExecutionAction(GfxContext& w)
-      : ui{&w}
-  {
-    prev_edges.reserve(100);
-  }
-  GfxContext* ui{};
+  GfxExecutionAction(GfxContext& w);
 
-  void startTick(const ossia::audio_tick_state& st) override { }
-
-  void setEdge(port_index source, port_index sink)
-  {
-    incoming_edges.enqueue({source, sink});
-  }
-
-  void endTick(const ossia::audio_tick_state& st) override
-  {
-    std::atomic_thread_fence(std::memory_order_seq_cst);
-
-    edges_cache.clear();
-    edges_cache.reserve(
-        std::max(prev_edges.size(), incoming_edges.size_approx()));
-
-    edge e;
-    while (incoming_edges.try_dequeue(e))
-    {
-      edges_cache.push_back(e);
-    }
-    if (edges_cache != prev_edges)
-    {
-      std::sort(edges_cache.begin(), edges_cache.end());
-      if (edges_cache != prev_edges)
-      {
-        {
-          std::lock_guard l{ui->edges_lock};
-
-          ui->new_edges.container.assign(
-              edges_cache.begin(), edges_cache.end());
-        }
-
-        std::swap(edges_cache, prev_edges);
-        ui->edges_changed = true;
-      }
-    }
-  }
+  void startTick(const ossia::audio_tick_state& st) override;
+  void setEdge(port_index source, port_index sink);
+  void endTick(const ossia::audio_tick_state& st) override;
 
   using edge = std::pair<port_index, port_index>;
+
+  GfxContext* ui{};
   std::vector<edge> prev_edges;
   std::vector<edge> edges_cache;
   using edge_queue = moodycamel::ConcurrentQueue<edge>;
---
+++ src/plugins/score-plugin-gfx/Gfx/GfxExecutionAction.cpp
@@ -0,0 +1,51 @@
+#include <Gfx/GfxExecContext.hpp>
+namespace Gfx
+{
+
+GfxExecutionAction::GfxExecutionAction(GfxContext& w)
+  : ui{&w}
+{
+  prev_edges.reserve(100);
+}
+
+void GfxExecutionAction::startTick(const ossia::audio_tick_state& st)
+{
+
+}
+
+void GfxExecutionAction::setEdge(port_index source, port_index sink)
+{
+  incoming_edges.enqueue({source, sink});
+}
+
+void GfxExecutionAction::endTick(const ossia::audio_tick_state& st)
+{
+  std::atomic_thread_fence(std::memory_order_seq_cst);
+
+  edges_cache.clear();
+  edges_cache.reserve(
+        std::max(prev_edges.size(), incoming_edges.size_approx()));
+
+  edge e;
+  while (incoming_edges.try_dequeue(e))
+  {
+    edges_cache.push_back(e);
+  }
+  if (edges_cache != prev_edges)
+  {
+    std::sort(edges_cache.begin(), edges_cache.end());
+    if (edges_cache != prev_edges)
+    {
+      {
+        std::lock_guard l{ui->edges_lock};
+
+        ui->new_edges.container.assign(
+              edges_cache.begin(), edges_cache.end());
+      }
+
+      std::swap(edges_cache, prev_edges);
+      ui->edges_changed = true;
+    }
+  }
+}
+}
