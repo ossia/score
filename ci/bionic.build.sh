1acabdb2b4291bbe6334bfd748f1e204c82ae861
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2022-01-02 16:44:58 +0100

    [ci] Ubuntu woes

--- ci/bionic.build.sh
+++ ci/bionic.build.sh
@@ -8,7 +8,8 @@ cmake .. \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=install \
   -DSCORE_DYNAMIC_PLUGINS=1 \
-  -DCMAKE_UNITY_BUILD=1
+  -DCMAKE_UNITY_BUILD=1 \
+  -DCMAKE_EXE_LINKER_FLAGS="-lstdc++fs" # Needed for VST3
 
 cmake --build .
 cmake --build . --target install
