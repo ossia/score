43f55cbf33227703ac1bc543d3bbae6fa21ed6eb
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2022-01-02 14:19:04 +0100

    [ci] unbust buster

--- ci/buster.build.sh
+++ ci/buster.build.sh
@@ -5,11 +5,15 @@ export SCORE_DIR=$PWD
 mkdir -p /build || true
 cd /build
 
+# Debian Buster does not support building KFR
+
 cmake $SCORE_DIR \
   -GNinja \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=install \
   -DSCORE_DYNAMIC_PLUGINS=1 \
+  -DOSSIA_DISABLE_KFR=1 \
+  -DOSSIA_FFT_FFTW=1 \
   -DSCORE_PCH=1
 
 cmake --build .
