dbe0de8ab0b3220222eaeba29fd547b06709240e
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2022-01-26 16:50:32 +0100

    [vst3] Handle score global scale factor

--- src/plugins/score-plugin-vst3/Vst3/UI/WindowContainer.hpp
+++ src/plugins/score-plugin-vst3/Vst3/UI/WindowContainer.hpp
@@ -1,7 +1,8 @@
 #pragma once
 #include <QDialog>
 #include <QWindow>
-
+#include <QDebug>
+#include <QApplication>
 #include <pluginterfaces/gui/iplugview.h>
 
 namespace vst3
@@ -27,6 +28,17 @@ struct WindowContainer
   QWidget* container{};
   vst3::PlugFrame* frame{};
 
+  double qtScaleFactor{1.};
+
+  WindowContainer()
+  {
+    double r = qEnvironmentVariable("QT_SCALE_FACTOR").toDouble();
+    if(r > 0.1)
+    {
+      qtScaleFactor = 1. / r;
+    }
+  }
+
   auto setSizeFromQt(
       Steinberg::IPlugView& view,
       const Steinberg::ViewRect& r,
@@ -34,6 +46,8 @@ struct WindowContainer
   {
     int w = r.getWidth();
     int h = r.getHeight();
+    int qw = r.getWidth() * qtScaleFactor;
+    int qh = r.getHeight() * qtScaleFactor;
 
     if(w == 0 || h == 0)
     {
@@ -42,20 +56,20 @@ struct WindowContainer
 
     if (view.canResize() == Steinberg::kResultTrue)
     {
-      parentWindow.resize(QSize{w, h});
+      parentWindow.resize(QSize{qw, qh});
     }
     else
     {
-      parentWindow.setFixedSize(QSize{w, h});
+      parentWindow.setFixedSize(QSize{qw, qh});
     }
     if (qwindow)
     {
-      qwindow->resize(w, h);
+      qwindow->resize(qw, qh);
     }
     if (container)
     {
       container->move(0, 0);
-      container->setFixedSize(w, h);
+      container->setFixedSize(qw, qh);
     }
 
     return std::make_pair(w, h);
@@ -73,22 +87,25 @@ struct WindowContainer
     Steinberg::ViewRect r;
     r.top = 0;
     r.left = 0;
-    r.right = sz.width();
-    r.bottom = sz.height();
+    r.right = sz.width() * 2;
+    r.bottom = sz.height() * 2;
     view.checkSizeConstraint(&r);
 
     int w = r.getWidth();
     int h = r.getHeight();
-    parentWindow.resize(QSize(w, h));
+    int qw = r.getWidth() * qtScaleFactor;
+    int qh = r.getHeight() * qtScaleFactor;
+
+    parentWindow.resize(QSize(qw, qh));
 
     if (qwindow)
     {
-      qwindow->resize(w, h);
+      qwindow->resize(qw, qh);
     }
     if (container)
     {
       container->move(0, 0);
-      container->setFixedSize(w, h);
+      container->setFixedSize(qw, qh);
     }
 
     view.onSize(&r);
@@ -101,35 +118,37 @@ struct WindowContainer
   {
     int w = r.getWidth();
     int h = r.getHeight();
+    int qw = r.getWidth() * qtScaleFactor;
+    int qh = r.getHeight() * qtScaleFactor;
 
     if(w == 0 || h == 0)
     {
       view.onSize(&r);
-      return std::make_pair(w,h);
+      return std::make_pair(qw, qh);
     }
 
     if (view.canResize() == Steinberg::kResultTrue)
     {
-      parentWindow.resize(QSize{w, h});
+      parentWindow.resize(QSize{qw, qh});
     }
     else
     {
-      parentWindow.setFixedSize(QSize{w, h});
+      parentWindow.setFixedSize(QSize{qw, qh});
     }
 
     if (qwindow)
     {
-      qwindow->resize(w, h);
+      qwindow->resize(qw, qh);
     }
     if (container)
     {
       container->move(0, 0);
-      container->setFixedSize(w, h);
+      container->setFixedSize(qw, qh);
     }
 
     view.onSize(&r);
 
-    return std::make_pair(w, h);
+    return std::make_pair(qw, qh);
   }
 };
 
