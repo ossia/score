6b530751b685904a85a9e54e623347c99fbad94d
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2022-04-22 18:10:33 +0200

    [ci] Try to update macOS to Monterey

--- ci/osx-brew.deps.sh
+++ ci/osx-brew.deps.sh
@@ -3,4 +3,4 @@
 set +e
 
 export HOMEBREW_NO_AUTO_UPDATE=1
-brew install ninja qt5 boost cmake ffmpeg portaudio jack fftw
+brew install ninja qt boost cmake ffmpeg fftw portaudio jack fftw sdl lv2 lilv suil freetype
