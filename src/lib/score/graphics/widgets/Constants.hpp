42485c52de91778e4cbeefe19ac510c56e72591b
Pia Baltazar <pia@baltazars.org>
2022-05-31 18:15:39 +0200

    initial work on range slider

--- src/lib/score/graphics/widgets/Constants.hpp
+++ src/lib/score/graphics/widgets/Constants.hpp
@@ -6,6 +6,7 @@ static const constexpr QRectF defaultSliderSize{0., 0., 60., 23.};
 static const constexpr QRectF defaultKnobSize{0., 0., 35., 35.};
 static const constexpr QRectF defaultCheckBoxSize{0., 0., 20., 20.};
 static const constexpr QRectF defaultToggleSize{0., 0., 60., 20.};
+static const constexpr QRectF defaultRangeSliderSize{0., 0., 60., 23.};
 
 struct DoubleSpinboxWithEnter;
 struct DefaultGraphicsSliderImpl;
---
+++ src/lib/score/graphics/widgets/QGraphicsRangeSlider.cpp
@@ -0,0 +1,14 @@
+#include <score/graphics/widgets/QGraphicsRangeSlider.hpp>
+#include <score/model/Skin.hpp>
+
+#include <QGraphicsSceneMouseEvent>
+#include <QPainter>
+
+#include <wobjectimpl.h>
+W_OBJECT_IMPL(score::QGraphicsRangeSlider);
+
+QGraphicsRangeSlider::QGraphicsRangeSlider(QObject *parent)
+    : QObject{parent}
+{
+
+}
