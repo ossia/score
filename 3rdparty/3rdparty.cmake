3efb843f9d05211605df2e9807a0aff41f3cb318
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2022-06-12 22:54:51 +0200

    [wasm] Disable unneeded 3rdparty deps

--- 3rdparty/3rdparty.cmake
+++ 3rdparty/3rdparty.cmake
@@ -1,7 +1,12 @@
 include(3rdparty/libossia.cmake)
+
+add_subdirectory(3rdparty/QCodeEditor)
+disable_qt_plugins(QCodeEditor)
+
+if(EMSCRIPTEN)
+  return()
+endif()
 include(3rdparty/shmdata.cmake)
 include(3rdparty/sndfile.cmake)
 include(3rdparty/snappy.cmake)
 
-add_subdirectory(3rdparty/QCodeEditor)
-disable_qt_plugins(QCodeEditor)
