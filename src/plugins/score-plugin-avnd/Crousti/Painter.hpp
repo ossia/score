a419e4433a23e29c8f13e96e17e2c874e8601dea
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2022-06-26 11:41:21 +0200

    [avnd] Add update calls after mouse events

--- src/plugins/score-plugin-avnd/Crousti/Painter.hpp
+++ src/plugins/score-plugin-avnd/Crousti/Painter.hpp
@@ -1,10 +1,12 @@
 #pragma once
+#include <score/model/Skin.hpp>
+
 #include <avnd/concepts/painter.hpp>
+#include <halp/texture.hpp>
+
 #include <QPainter>
 #include <QGraphicsItem>
 #include <QGraphicsSceneMouseEvent>
-#include <halp/texture.hpp>
-
 #include <QGradient>
 #include <QPolygon>
 
@@ -14,197 +16,196 @@ namespace oscr
 {
 struct QPainterAdapter
 {
-    QPainter& painter;
-    QGraphicsItem& item;
-    QPainterPath path;
+  QPainter& painter;
+  QGraphicsItem& item;
+  QPainterPath path;
 
-    void begin_path()
-    {
-        path = QPainterPath{};
-    }
-    void close_path()
-    {
-        path.closeSubpath();
-    }
-    void stroke()
-    {
-        painter.strokePath(path, painter.pen());
-    }
-    void fill()
-    {
-        painter.fillPath(path, painter.brush());
-    }
-    void update()
-    {
-        item.update();
-    }
+  void begin_path()
+  {
+    path = QPainterPath{};
+  }
+  void close_path()
+  {
+    path.closeSubpath();
+  }
+  void stroke()
+  {
+    painter.strokePath(path, painter.pen());
+  }
+  void fill()
+  {
+    painter.fillPath(path, painter.brush());
+  }
+  void update()
+  {
+    item.update();
+  }
 
-    void move_to(double x, double y)
-    {
-        path.moveTo(x, y);
-    }
-    void line_to(double x, double y)
-    {
-        path.lineTo(x, y);
-    }
-    void arc_to(double x, double y, double w, double h, double startAngle, double arcLength)
-    {
-        path.arcTo(x, y, w, h, startAngle, arcLength);
-    }
+  void move_to(double x, double y)
+  {
+    path.moveTo(x, y);
+  }
+  void line_to(double x, double y)
+  {
+    path.lineTo(x, y);
+  }
+  void arc_to(double x, double y, double w, double h, double startAngle, double arcLength)
+  {
+    path.arcTo(x, y, w, h, startAngle, arcLength);
+  }
 
 
-    void cubic_to(double c1x, double c1y, double c2x, double c2y, double endx, double endy)
-    {
-        path.cubicTo(c1x, c1y, c2x, c2y, endx, endy);
-    }
-    void quad_to(double x1, double y1, double x2, double y2)
-    {
-        path.quadTo(x1, y1, x2, y2);
-    }
+  void cubic_to(double c1x, double c1y, double c2x, double c2y, double endx, double endy)
+  {
+    path.cubicTo(c1x, c1y, c2x, c2y, endx, endy);
+  }
+  void quad_to(double x1, double y1, double x2, double y2)
+  {
+    path.quadTo(x1, y1, x2, y2);
+  }
 
-    void translate(double x, double y)
-    {
-        painter.translate(x, y);
-    }
-    void scale(double x, double y)
-    {
-        painter.scale(x, y);
-    }
-    void rotate(double a)
-    {
-        painter.rotate(a);
-    }
-    void reset_transform()
-    {
-        painter.resetTransform();
-    }
+  void translate(double x, double y)
+  {
+    painter.translate(x, y);
+  }
+  void scale(double x, double y)
+  {
+    painter.scale(x, y);
+  }
+  void rotate(double a)
+  {
+    painter.rotate(a);
+  }
+  void reset_transform()
+  {
+    painter.resetTransform();
+  }
 
-    // Colors:
-    //                  R    G    B    A
-    void set_stroke_color(halp::rgba_color c)
-    {
-      QPen p = painter.pen();
-      p.setColor(qRgba(c.r, c.g, c.b, c.a));
-      painter.setPen(p);
-    }
+  // Colors:
+  //                  R    G    B    A
+  void set_stroke_color(halp::rgba_color c)
+  {
+    QPen p = painter.pen();
+    p.setColor(qRgba(c.r, c.g, c.b, c.a));
+    painter.setPen(p);
+  }
 
-    void set_stroke_width(double w)
-    {
-      QPen p = painter.pen();
-      p.setWidth(w);
-      painter.setPen(p);
-    }
+  void set_stroke_width(double w)
+  {
+    QPen p = painter.pen();
+    p.setWidth(w);
+    painter.setPen(p);
+  }
 
-    void set_fill_color(halp::rgba_color c)
-    {
-      painter.setBrush(QColor(qRgba(c.r, c.g, c.b, c.a)));
-    }
+  void set_fill_color(halp::rgba_color c)
+  {
+    painter.setBrush(QColor(qRgba(c.r, c.g, c.b, c.a)));
+  }
 
-    //          x1, y1, x2, y2, c1, c2
-    void set_linear_gradient(double x1, double y1, double x2, double y2, halp::rgba_color c1, halp::rgba_color c2){
-        QLinearGradient gradient(QPointF(x1, y1),QPointF(x2, y2));
-        gradient.setColorAt(0,QColor(qRgba(c1.r, c1.g, c1.b, c1.a)));
-        gradient.setColorAt(1,QColor(qRgba(c2.r, c2.g, c2.b, c2.a)));
-        painter.setBrush(gradient);
-    }
+  //          x1, y1, x2, y2, c1, c2
+  void set_linear_gradient(double x1, double y1, double x2, double y2, halp::rgba_color c1, halp::rgba_color c2){
+    QLinearGradient gradient(QPointF(x1, y1),QPointF(x2, y2));
+    gradient.setColorAt(0,QColor(qRgba(c1.r, c1.g, c1.b, c1.a)));
+    gradient.setColorAt(1,QColor(qRgba(c2.r, c2.g, c2.b, c2.a)));
+    painter.setBrush(gradient);
+  }
 
-    //          cx, cy, radius, c1, c2
-    void set_radial_gradient(double cx, double cy, double cr, halp::rgba_color c1, halp::rgba_color c2){
-        QRadialGradient gradient(cx, cy, cr);
-        gradient.setColorAt(0,QColor(qRgba(c1.r, c1.g, c1.b, c1.a)));
-        gradient.setColorAt(1,QColor(qRgba(c2.r, c2.g, c2.b, c2.a)));
-        painter.setBrush(gradient);
-    }
+  //          cx, cy, radius, c1, c2
+  void set_radial_gradient(double cx, double cy, double cr, halp::rgba_color c1, halp::rgba_color c2){
+    QRadialGradient gradient(cx, cy, cr);
+    gradient.setColorAt(0,QColor(qRgba(c1.r, c1.g, c1.b, c1.a)));
+    gradient.setColorAt(1,QColor(qRgba(c2.r, c2.g, c2.b, c2.a)));
+    painter.setBrush(gradient);
+  }
 
-    //          x, y, angle, c1, c2
-    void set_conical_gradient(double x, double y, double a, halp::rgba_color c1, halp::rgba_color c2){
-        QConicalGradient gradient(x, y, a);
-        gradient.setColorAt(0,QColor(qRgba(c1.r, c1.g, c1.b, c1.a)));
-        gradient.setColorAt(1,QColor(qRgba(c2.r, c2.g, c2.b, c2.a)));
-        painter.setBrush(gradient);
-    }
+  //          x, y, angle, c1, c2
+  void set_conical_gradient(double x, double y, double a, halp::rgba_color c1, halp::rgba_color c2){
+    QConicalGradient gradient(x, y, a);
+    gradient.setColorAt(0,QColor(qRgba(c1.r, c1.g, c1.b, c1.a)));
+    gradient.setColorAt(1,QColor(qRgba(c2.r, c2.g, c2.b, c2.a)));
+    painter.setBrush(gradient);
+  }
 
-    // Text:
-    void set_font(std::string_view f)
-    {
-      auto font = painter.font();
-      font.setFamily(QString::fromUtf8(f.data(), f.size()));
-      painter.setFont(font);
-    }
+  // Text:
+  void set_font(std::string_view f)
+  {
+    auto font = painter.font();
+    font.setFamily(QString::fromUtf8(f.data(), f.size()));
+    painter.setFont(font);
+  }
 
-    void set_font_size(double f)
-    {
-        auto font = painter.font();
-        font.setPointSize(f);
-        painter.setFont(font);
-    }
+  void set_font_size(double f)
+  {
+    auto font = painter.font();
+    font.setPointSize(f);
+    painter.setFont(font);
+  }
 
-    void draw_text(double x, double y, std::string_view str)
-    {
-        path.addText(x, y, painter.font(), QString::fromUtf8(str.data(), str.size()));
-    }
+  void draw_text(double x, double y, std::string_view str)
+  {
+    path.addText(x, y, painter.font(), QString::fromUtf8(str.data(), str.size()));
+  }
 
-    // Drawing
-    //          x1, y1, x2 , y2
-    void draw_line(double x1, double y1, double x2, double y2)
-    {
-        path.moveTo(x1, y1);
-        path.lineTo(x2, y2);
-    }
+  // Drawing
+  //          x1, y1, x2 , y2
+  void draw_line(double x1, double y1, double x2, double y2)
+  {
+    path.moveTo(x1, y1);
+    path.lineTo(x2, y2);
+  }
 
-    //          x1, y1, x2 , y2, x3, y3
-    void draw_triangle(double x1, double y1, double x2, double y2, double x3, double y3)
-    {
-        path.moveTo(x1, y1);
-        path.lineTo(x2, y2);
-        path.lineTo(x3, y3);
-        path.lineTo(x1, y1);
-        painter.drawPath(path);
-    }
+  //          x1, y1, x2 , y2, x3, y3
+  void draw_triangle(double x1, double y1, double x2, double y2, double x3, double y3)
+  {
+    path.moveTo(x1, y1);
+    path.lineTo(x2, y2);
+    path.lineTo(x3, y3);
+    path.lineTo(x1, y1);
+    painter.drawPath(path);
+  }
 
-    //          x , y , w  , h
-    void draw_rect(double x, double y, double w, double h)
-    {
-        path.addRect(x, y, w, h);
-    }
+  //          x , y , w  , h
+  void draw_rect(double x, double y, double w, double h)
+  {
+    path.addRect(x, y, w, h);
+  }
 
-    //                  x , y , w  , h
-    void draw_rounded_rect(double x, double y, double w, double h, double r)
-    {
-        path.addRoundedRect(x, y, w, h, r, r);
-    }
+  //                  x , y , w  , h
+  void draw_rounded_rect(double x, double y, double w, double h, double r)
+  {
+    path.addRoundedRect(x, y, w, h, r, r);
+  }
 
-    //            x , y , filename
-    void draw_pixmap(double x, double y, QString str)
-    {
-        painter.drawPixmap(x, y, str);
-    }
+  //            x , y , filename
+  void draw_pixmap(double x, double y, const QString& str)
+  {
+    painter.drawPixmap(x, y, str);
+  }
 
-    //             x , y , w  , h
-    void draw_ellipse(double x, double y, double w, double h)
-    {
-        path.addEllipse(x, y, w, h);
-    }
+  //             x , y , w  , h
+  void draw_ellipse(double x, double y, double w, double h)
+  {
+    path.addEllipse(x, y, w, h);
+  }
 
-    //            cx, cy, radius
-    void draw_circle(double cx, double cy, double cr)
-    {
-        path.addEllipse(QPointF{cx, cy}, cr, cr);
-    }
+  //            cx, cy, radius
+  void draw_circle(double cx, double cy, double cr)
+  {
+    path.addEllipse(QPointF{cx, cy}, cr, cr);
+  }
 
-    //          tab, count
-    void draw_polygon(double* tab, int count)
-    {
-        QPolygonF poly;
-        double x, y;
-        for (int i = 0; i < count*2; i+=2){
-            x = tab[i];
-            y = tab[i+1];
-            poly << QPointF(x, y);
-        }
-        path.addPolygon(poly);
+  //          tab, count
+  void draw_polygon(double* tab, int count)
+  {
+    QPolygonF poly;
+    double x, y;
+    for (int i = 0; i < count*2; i+=2){
+      x = tab[i];
+      y = tab[i+1];
+      poly << QPointF(x, y);
     }
-
+    path.addPolygon(poly);
+  }
 };
 static_assert(avnd::painter<QPainterAdapter>);
 
@@ -259,18 +260,21 @@ class CustomItem : public QGraphicsItem
       if constexpr(requires { impl.mouse_press(0, 0); })
         if(impl.mouse_press(event->pos().x(), event->pos().y()))
           event->accept();
+      update();
     }
     void mouseMoveEvent(QGraphicsSceneMouseEvent* event) override
     {
       if constexpr(requires { impl.mouse_move(0, 0); })
         impl.mouse_move(event->pos().x(), event->pos().y());
           event->accept();
+      update();
     }
     void mouseReleaseEvent(QGraphicsSceneMouseEvent* event) override
     {
       if constexpr(requires { impl.mouse_release(0, 0); })
         impl.mouse_release(event->pos().x(), event->pos().y());
           event->accept();
+      update();
     }
 
   private:
