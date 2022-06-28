ff6048daaa146c0f7dfebe5e9d0fe65ac80a1f29
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2022-06-28 09:52:01 +0200

    [ci] Try to fix jammy package build

--- ci/jammy.build.sh
+++ ci/jammy.build.sh
@@ -5,6 +5,8 @@ cd build
 
 cmake .. \
   -GNinja \
+  -DSCORE_DEPLOYMENT_BUILD=1 \
+  -DBUILD_SHARED_LIBS=0 \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=install \
   -DCMAKE_UNITY_BUILD=1
