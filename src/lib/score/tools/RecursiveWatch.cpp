bb700f6ecbaf3f0559f8aba9a746addea10cef47
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2022-07-20 21:09:36 +0200

    [build] Fix build with gcc-12

--- src/lib/score/tools/RecursiveWatch.cpp
+++ src/lib/score/tools/RecursiveWatch.cpp
@@ -30,6 +30,14 @@
   #define LLFIO_EXPERIMENTAL_STATUS_CODE 1
   #define LLFIO_DISABLE_OPENSSL 1
   #define QUICKCPPLIB_USE_STD_SPAN 1
+
+  #if !defined(__has_feature)
+    #define __has_feature(T) 0
+  #endif
+  #if !defined(__has_extension)
+    #define __has_extension(T) 0
+  #endif
+
   #include <llfio.hpp>
   #include <QApplication>
 #elif SCORE_HAS_STD_FILESYSTEM
