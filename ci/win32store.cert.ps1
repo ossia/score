2ea15706b8ef84a8e2ff08ef9785b4643cdad3d4
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2022-07-31 17:50:17 +0200

    [ci] Fixes for windows

--- ci/win32store.cert.ps1
+++ ci/win32store.cert.ps1
@@ -1,11 +1,11 @@
 $cert = New-SelfSignedCertificate `
   -Type Custom `
-  -Subject $Env:OSSIA_WIN32_CERT_SUBJECT `
+  -Subject "$Env:OSSIA_WIN32_CERT_SUBJECT" `
   -KeyUsage DigitalSignature `
   -FriendlyName "ossia.io" `
   -CertStoreLocation "Cert:\CurrentUser\My" `
   -TextExtension @("2.5.29.37={text}1.3.6.1.5.5.7.3.3", "2.5.29.19={text}")
-  
+
 $password = ConvertTo-SecureString -String $Env:OSSIA_WIN32_CERT_PASSWORD -Force -AsPlainText
 
 Export-PfxCertificate `
