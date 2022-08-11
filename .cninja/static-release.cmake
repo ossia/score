1cf60768a0219d6528cc42e312cc95c1030b7abe
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2022-08-11 13:48:54 +0200

    [ci] Flags to greatly reduce binary size and improve performance

--- .cninja/static-release.cmake
+++ .cninja/static-release.cmake
@@ -7,5 +7,6 @@ set_cache(SCORE_STATIC_PLUGINS True)
 set_cache(CMAKE_UNITY_BUILD True)
 set_cache(SCORE_PCH False)
 
-string(APPEND CMAKE_C_FLAGS_INIT " -fno-stack-protector -Ofast -fno-finite-math-only ")
-string(APPEND CMAKE_CXX_FLAGS_INIT " -fno-stack-protector -Ofast -fno-finite-math-only ")
+# one day: -fsingle-global-definition
+string(APPEND CMAKE_C_FLAGS_INIT " -fno-stack-protector -Ofast -fno-finite-math-only -fnew-infallible -fno-semantic-interposition -fno-plt -Bsymbolic-functions ")
+string(APPEND CMAKE_CXX_FLAGS_INIT " -fno-stack-protector -Ofast -fno-finite-math-only -fnew-infallible -fno-semantic-interposition -fno-plt -Bsymbolic-functions ")
