2f29990026c6c00d5eeab56f1ba02358bb0c12ba
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2022-08-15 19:08:22 +0200

    [avnd] Rework audio plug-ins so that they don't need to link Gamma

--- 3rdparty/gamma.cmake
+++ 3rdparty/gamma.cmake
@@ -1,7 +1,11 @@
+
 add_library(gamma STATIC
   "${CMAKE_CURRENT_LIST_DIR}/Gamma/src/Conversion.cpp"
-  "${CMAKE_CURRENT_LIST_DIR}/Gamma/src/DFT.cpp"
-  "${CMAKE_CURRENT_LIST_DIR}/Gamma/src/Domain.cpp"
+  # "${CMAKE_CURRENT_LIST_DIR}/Gamma/src/DFT.cpp"
+  # "${CMAKE_CURRENT_LIST_DIR}/Gamma/src/FFT.cpp"
+  # "${CMAKE_CURRENT_LIST_DIR}/Gamma/src/fftpack++1.cpp"
+  # "${CMAKE_CURRENT_LIST_DIR}/Gamma/src/fftpack++2.cpp"
+  # "${CMAKE_CURRENT_LIST_DIR}/Gamma/src/Domain.cpp"
   )
 
 target_include_directories(
