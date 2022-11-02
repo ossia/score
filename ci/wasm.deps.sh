0d504c4029a1afbc2463fb9c8ee8332692dc441c
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2022-11-02 13:30:12 -0400

    [ci] Fix wasm build failing due to ICU update on arch

--- ci/wasm.deps.sh
+++ ci/wasm.deps.sh
@@ -7,6 +7,10 @@ pacman -S wget ninja cmake which python tar git double-conversion libb2 icu glib
 export SDK_ARCHIVE=sdk-wasm.tar.xz
 wget -nv https://github.com/ossia/score-sdk/releases/download/sdk25/$SDK_ARCHIVE -O $SDK_ARCHIVE
 
+wget -nv https://github.com/ossia/score-sdk/releases/download/sdk25/icu71.tar.gz -O icu71.tar.gz
+tar xaf icu71.tar.gz
+mv libicu* /usr/lib/
+
 mkdir -p /opt/ossia-sdk-wasm
 chown -R $(whoami) /opt/ossia-sdk-wasm
 chmod -R a+rwx /opt/ossia-sdk-wasm
