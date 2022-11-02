ed7ac09d679ce87a692c7570bcd3321ff9fba7c9
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2022-11-02 16:20:27 -0400

    [ci] Try to bump wasm to avx

--- ci/wasm.build.sh
+++ ci/wasm.build.sh
@@ -13,14 +13,14 @@ source /opt/ossia-sdk-wasm/emsdk/emsdk_env.sh
    -DCMAKE_CXX_STANDARD=20 \
    -DCMAKE_BUILD_TYPE=Release \
    -DCMAKE_UNITY_BUILD=1 \
-   -DCPU_ARCH=sse2 \
+   -DCPU_ARCH=avx \
    -DQT_VERSION="Qt6;6.4" \
    -DOSSIA_PCH=0 \
    -DSCORE_PCH=0 \
-   -DCMAKE_C_FLAGS='-pthread -O3 -ffast-math -msimd128 -msse -msse2 ' \
-   -DCMAKE_CXX_FLAGS='-pthread -O3 -ffast-math -msimd128 -msse -msse2  '  \
-   -DCMAKE_EXE_LINKER_FLAGS='-sASYNCIFY -pthread -O3 -ffast-math -msimd128 -msse -msse2  ' \
-   -DCMAKE_SHARED_LINKER_FLAGS='-sASYNCIFY -pthread -O3 -ffast-math -msimd128 -msse -msse2  '
+   -DCMAKE_C_FLAGS='-pthread -O3 -ffast-math -msimd128 -msse -msse2 -msse3 -mssse3 -msse4 -msse4.1 -msse4.2 -mavx ' \
+   -DCMAKE_CXX_FLAGS='-pthread -O3 -ffast-math -msimd128 -msse -msse2 -msse3 -mssse3 -msse4 -msse4.1 -msse4.2 -mavx  '  \
+   -DCMAKE_EXE_LINKER_FLAGS='-sASYNCIFY -pthread -O3 -ffast-math -msimd128 -msse -msse2 -msse3 -mssse3 -msse4 -msse4.1 -msse4.2 -mavx  ' \
+   -DCMAKE_SHARED_LINKER_FLAGS='-sASYNCIFY -pthread -O3 -ffast-math -msimd128 -msse -msse2 -msse3 -mssse3 -msse4 -msse4.1 -msse4.2 -mavx '
 
 cmake --build .
 
