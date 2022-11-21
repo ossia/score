919e010545fe056dafb7cff2a6a4a29a0e49c0bb
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2022-11-20 23:22:47 -0500

    [sweeper] Add various sweeping modes

--- src/plugins/score-plugin-avnd/AvndProcesses/Sweeper.hpp
+++ src/plugins/score-plugin-avnd/AvndProcesses/Sweeper.hpp
@@ -10,6 +10,7 @@
 #include <halp/audio.hpp>
 #include <halp/controls.hpp>
 #include <halp/meta.hpp>
+#include <rnd/random.hpp>
 
 namespace avnd_tools
 {
@@ -32,8 +33,10 @@ struct PatternSweeper : PatternObject
   struct
   {
     halp::val_port<"Input", ossia::value> input;
-    halp::time_chooser<"Interval"> time;
     PatternSelector pattern;
+
+    halp::time_chooser<"Interval"> time;
+    struct { halp__enum("Mode", Upwards, Upwards, Downwards, UpDown, RandomWalk, Random); } mode;
   } inputs;
 
   struct
@@ -41,9 +44,83 @@ struct PatternSweeper : PatternObject
 
   } outputs;
 
-
   std::size_t current_parameter = 0;
   double last_message_sent_pos = std::numeric_limits<double>::lowest();
+  enum { up, down } direction{up};
+  rnd::pcg pcg_state;
+
+  void prepare()
+  {
+    std::random_device dev;
+    pcg_state.seed(dev);
+  }
+
+  void next()
+  {
+    if(this->roots.size() <= 1)
+    {
+      current_parameter = 0;
+      return;
+    }
+    using mode = decltype(inputs.mode)::enum_type;
+    switch(inputs.mode)
+    {
+    case mode::Upwards:
+      current_parameter++;
+      current_parameter = current_parameter % this->roots.size();
+      break;
+    case mode::Downwards:
+      if(current_parameter == 0)
+        current_parameter = this->roots.size() - 1;
+      else
+        current_parameter--;
+      break;
+    case mode::UpDown:
+    {
+      case_updown:
+      if(direction == up)
+      {
+        if(current_parameter + 1 == this->roots.size())
+        {
+          direction = down;
+          current_parameter--;
+        }
+        else
+        {
+          current_parameter++;
+        }
+      }
+      else
+      {
+        if(current_parameter == 0)
+        {
+          direction = up;
+          current_parameter++;
+        }
+        else
+        {
+          current_parameter--;
+        }
+      }
+      break;
+    }
+    case mode::RandomWalk:
+    {
+      if(std::bernoulli_distribution{}(pcg_state))
+        direction = up;
+      else
+        direction = down;
+      goto case_updown;
+      break;
+    }
+    case mode::Random:
+    {
+      auto dist = std::uniform_int_distribution<int>(0, std::ssize(this->roots) - 1);
+      current_parameter = dist(pcg_state);
+      break;
+    }
+    }
+  }
 
   using tick = halp::tick_musical;
   void operator()(const halp::tick_musical& tk)
@@ -63,8 +140,8 @@ struct PatternSweeper : PatternObject
       {
         QTimer::singleShot(1, qApp, [p, v] { p->push_value(v); });
       }
-      current_parameter++;
-      current_parameter = current_parameter % this->roots.size();
+
+      next();
     }
   }
 };
