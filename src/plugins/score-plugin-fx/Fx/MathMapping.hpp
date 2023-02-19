879f3ad05ef6f8eb81bf18a64762ee6a9d6bf4c7
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2023-02-19 18:08:22 -0500

    [pico] code writing for exprtk

--- src/plugins/score-plugin-fx/Fx/MathMapping.hpp
+++ src/plugins/score-plugin-fx/Fx/MathMapping.hpp
@@ -231,7 +231,7 @@ struct Node
 {
   struct Metadata : Control::Meta_base
   {
-    using code_writer = MathMappingCodeWriter<Node>;
+    using code_writer = MathMappingCodeWriter;
     static const constexpr auto prettyName = "Expression Value Filter";
     static const constexpr auto objectKey = "MathMapping";
     static const constexpr auto category = "Control/Mappings";
@@ -526,7 +526,7 @@ struct Node
 {
   struct Metadata : Control::Meta_base
   {
-    using code_writer = MathMappingCodeWriter<Node>;
+    using code_writer = MathMappingCodeWriter;
     static const constexpr auto prettyName = "Micromap";
     static const constexpr auto objectKey = "MicroMapping";
     static const constexpr auto category = "Control/Mappings";
