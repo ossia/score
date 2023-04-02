5651a6a59803d2f67ac08ce501613275dce06e5e
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2023-04-02 15:27:06 -0400

    [ci] bullseye qt6 work

--- ci/bullseye-qt6.deps.sh
+++ ci/bullseye-qt6.deps.sh
@@ -6,21 +6,21 @@ echo 'deb http://deb.debian.org/debian bullseye-backports main' | $SUDO tee -a /
 # libsdl2-dev libsdl2-2.0-0
 $SUDO apt-get update -qq
 $SUDO apt-get install -qq --force-yes \
-     binutils gcc g++ clang cmake \
+     binutils gcc g++ clang-13 cmake \
      libasound-dev \
      ninja-build \
      libfftw3-dev \
      libsuil-dev liblilv-dev lv2-dev \
-     libclang-dev llvm-dev \
+     libclang-13-dev llvm-13-dev \
      libdrm-dev libgbm-dev \
      qt6-base-dev qt6-base-dev-tools qt6-base-private-dev \
      qt6-declarative-dev qt6-declarative-dev-tools qt6-declarative-private-dev \
      qt6-scxml-dev \
-     libqt6core5compat6-dev \
+     qt6-5compat-dev \
      libqt6opengl6-dev \
-     libqt6websockets6-dev \
-     libqt6serialport6-dev \
-     libqt6shadertools6-dev \
+     qt6-websockets-dev \
+     qt6-serialport-dev \
+     qt6-shadertools-dev \
      libbluetooth-dev \
      libglu1-mesa-dev libglu1-mesa libgles2-mesa-dev \
      libavahi-compat-libdnssd-dev libsamplerate0-dev \
