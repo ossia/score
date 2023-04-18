b0c2f079105aec620f4e4034aa6c73bac5d034b5
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2023-04-18 22:39:02 +0200

    [ci] More RPi fixes

--- ci/raspberrypi-aarch64.deps.sh
+++ ci/raspberrypi-aarch64.deps.sh
@@ -2,7 +2,7 @@
 
 # Note: this is run from an ArchLinux container due to too recent glibc
 pacman -Syyu --noconfirm
-pacman -S wget ninja cmake tar sed clang lld pkgconf --noconfirm
+pacman -S wget ninja cmake tar sed clang lld pkgconf double-conversion libb2 --noconfirm
 
 export SDK_ARCHIVE=sdk-rpi-aarch64.tar.xz
 wget -nv https://github.com/ossia/score-sdk/releases/download/sdk28/$SDK_ARCHIVE -O $SDK_ARCHIVE
