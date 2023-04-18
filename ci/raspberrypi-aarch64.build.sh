22e768a3efe4efec0dc758606319cebd68a80a0f
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2023-04-18 12:52:28 -0400

    [ci] try to understand rpi4 ci error

--- ci/raspberrypi-aarch64.build.sh
+++ ci/raspberrypi-aarch64.build.sh
@@ -6,6 +6,40 @@ mkdir -p /build || true
 chown -R $(whoami) /build
 cd /build
 
+echo '#include <thread>
+#include <cstdio>
+
+int main() { 
+  std::thread t{ [] { printf("hello"); } };
+  t.join();
+}' > thread.cpp
+echo "add_executable(thread thread.cpp)" > CMakeLists.txt
+mkdir bb
+cd bb
+clang++ -v -std=c++20 ../thread.cpp
+
+cmake .. \
+ -GNinja \
+ -DQT_VERSION=Qt6 \
+ -DCMAKE_UNITY_BUILD=1 \
+ -DCMAKE_BUILD_TYPE=Release \
+ -DCMAKE_TOOLCHAIN_FILE=/opt/ossia-sdk-rpi-aarch64/toolchain.aarch64.llvm-host.cmake \
+ -DCMAKE_PREFIX_PATH=/opt/ossia-sdk-rpi-aarch64/pi/sysroot/opt/ossia-sdk-rpi/qt6-static/lib/cmake \
+ -DOSSIA_SDK=/opt/ossia-sdk-rpi-aarch64/pi/sysroot/opt/ossia-sdk-rpi \
+ -DSCORE_DEPLOYMENT_BUILD=1 \
+ -DOSSIA_ENABLE_KFR=1 \
+ -DOSSIA_ENABLE_FFTW=0 \
+ -DCMAKE_INSTALL_PREFIX=install || true
+
+ls CMakeFiles
+echo " ============= "
+cat  CMakeFiles/CMakeConfigureLog.yaml  || true
+
+echo " ============= "
+cat  CMakeFiles/*.log  || true
+cmake --build .
+
+
 cmake $SCORE_DIR \
  -GNinja \
  -DQT_VERSION=Qt6 \
