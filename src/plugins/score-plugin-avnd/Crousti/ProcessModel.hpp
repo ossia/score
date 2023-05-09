932e8a90f4136de689fe8454a6402429ce7167b4
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2023-05-09 16:26:02 -0400

    [pico] Add some useful objects

--- src/plugins/score-plugin-avnd/Crousti/ProcessModel.hpp
+++ src/plugins/score-plugin-avnd/Crousti/ProcessModel.hpp
@@ -636,7 +636,7 @@ public:
       {
         int index = ossia::index_in_container(this->self.inlets(), id);
         return fmt::format(
-            "(avnd::parameter_input_introspection<decltype({})>::get<{}>({}.inputs)."
+            "(avnd::input_introspection<decltype({})>::get<{}>({}.inputs)."
             "value)",
             variable, index, variable);
       }
@@ -644,7 +644,7 @@ public:
       {
         int index = ossia::index_in_container(this->self.outlets(), id);
         return fmt::format(
-            "(avnd::parameter_output_introspection<decltype({})>::get<{}>({}.outputs)."
+            "(avnd::output_introspection<decltype({})>::get<{}>({}.outputs)."
             "value)",
             variable, index, variable);
       }
