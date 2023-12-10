98d4ee67a5f0e2f708c0d1ef17d370882c6ceb4b
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2023-12-10 15:18:52 -0500

    [ci] Fix build on GCC 12 due to https://gcc.gnu.org/bugzilla/show_bug.cgi?id=55971

--- src/plugins/score-plugin-gfx/Gfx/Graph/decoders/GPUVideoDecoder.hpp
+++ src/plugins/score-plugin-gfx/Gfx/Graph/decoders/GPUVideoDecoder.hpp
@@ -10,18 +10,16 @@ extern "C" {
 }
 
 #define SCORE_GFX_VIDEO_UNIFORMS \
-  R"_(
-layout(std140, binding = 0) uniform renderer_t {
-  mat4 clipSpaceCorrMatrix;
-  
-  vec2 renderSize;
-} renderer;
-
-layout(std140, binding = 2) uniform material_t {
-  vec2 scale;
-  vec2 textureSize;
-} mat;
-)_"
+"layout(std140, binding = 0) uniform renderer_t {\n" \
+"  mat4 clipSpaceCorrMatrix;\n" \
+"  \n" \
+"  vec2 renderSize;\n" \
+"} renderer;\n" \
+"\n" \
+"layout(std140, binding = 2) uniform material_t {\n" \
+"  vec2 scale;\n" \
+"  vec2 textureSize;\n" \
+"} mat;\n"
 
 namespace score::gfx
 {
