c0747195a49e91a0f553b933e5257d23dfe8b5e0
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-03-24 11:52:27 -0400

    [pico] Implement writing of OSC code backing a Raw I/O device for esp32 as well as basic import from Wokwi

--- src/plugins/score-plugin-protocols/Protocols/SimpleIO/SimpleIOSpecificSettings.hpp
+++ src/plugins/score-plugin-protocols/Protocols/SimpleIO/SimpleIOSpecificSettings.hpp
@@ -4,10 +4,10 @@
 #include <score/tools/std/StringHash.hpp>
 
 #include <ossia/detail/variant.hpp>
+#include <ossia/protocols/osc/osc_factory.hpp>
 
 #include <QString>
 
-#include <utility>
 #include <vector>
 #include <verdigris>
 
@@ -23,12 +23,13 @@ struct GPIO
   int32_t flags{};
   int32_t events{};
   int32_t state{};
-  bool direction{};
+  bool direction{}; // false: input, true: output
 };
 struct PWM
 {
   int32_t chip{};
   int32_t channel{};
+  bool direction{true}; // false: input, true: output
 };
 struct ADC
 {
@@ -63,6 +64,8 @@ struct Port
 struct SimpleIOSpecificSettings
 {
   std::vector<SimpleIO::Port> ports;
+  QString board;
+  std::optional<ossia::net::osc_protocol_configuration> osc_configuration;
 };
 }
 
