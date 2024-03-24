bfcc081e1e6fe8f65583e044683ac60f3f118548
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-03-24 11:38:24 -0400

    [osc] Make OSC widget independent of OSC protocol

--- src/plugins/score-plugin-protocols/Protocols/OSC/OSCProtocolSettingsWidget.cpp
+++ src/plugins/score-plugin-protocols/Protocols/OSC/OSCProtocolSettingsWidget.cpp
@@ -31,7 +31,7 @@ using framing_type = decltype(ossia::net::osc_protocol_configuration::framing);
 class UDPWidget : public QWidget
 {
 public:
-  UDPWidget(OSCProtocolSettingsWidget* parent)
+  UDPWidget(Device::ProtocolSettingsWidget& proto, QWidget* parent)
       : QWidget{parent}
   {
     auto layout = new QFormLayout{this};
@@ -43,7 +43,7 @@ public:
     m_remotePort->setWhatsThis(
         tr("This is where the other software listens from incoming messages. Score will "
            "send packets to this port."));
-    parent->checkForChanges(m_remotePort);
+    proto.checkForChanges(m_remotePort);
 
     m_localPort = new QSpinBox(this);
     m_localPort->setRange(0, 65535);
@@ -51,7 +51,7 @@ public:
     m_localPort->setWhatsThis(
         tr("This is where the other software sends feedback messages to. Score will "
            "listen for incoming OSC messages on this port."));
-    parent->checkForChanges(m_localPort);
+    proto.checkForChanges(m_localPort);
 
     m_broadcast = new QCheckBox{this};
     m_broadcast->setCheckState(Qt::Unchecked);
@@ -108,7 +108,7 @@ private:
 class TCPWidget : public QWidget
 {
 public:
-  TCPWidget(OSCProtocolSettingsWidget* parent)
+  TCPWidget(Device::ProtocolSettingsWidget& proto, QWidget* parent)
       : QWidget{parent}
   {
     auto layout = new QFormLayout{this};
@@ -167,7 +167,7 @@ private:
 class UnixDatagramWidget : public QWidget
 {
 public:
-  UnixDatagramWidget(OSCProtocolSettingsWidget* parent)
+  UnixDatagramWidget(Device::ProtocolSettingsWidget& proto, QWidget* parent)
       : QWidget{parent}
   {
     auto layout = new QFormLayout{this};
@@ -175,7 +175,7 @@ public:
 
     m_remotePort = new QLineEdit(this);
     m_remotePort->setText("/tmp/ossia.a.socket");
-    parent->checkForChanges(m_remotePort);
+    proto.checkForChanges(m_remotePort);
 
     m_localPort = new QLineEdit(this);
     m_localPort->setText("/tmp/ossia.b.socket");
@@ -212,7 +212,7 @@ private:
 class UnixStreamWidget : public QWidget
 {
 public:
-  UnixStreamWidget(OSCProtocolSettingsWidget* parent)
+  UnixStreamWidget(Device::ProtocolSettingsWidget& proto, QWidget* parent)
       : QWidget{parent}
   {
     auto layout = new QFormLayout{this};
@@ -256,7 +256,7 @@ private:
 class WebsocketClientWidget : public QWidget
 {
 public:
-  WebsocketClientWidget(OSCProtocolSettingsWidget* parent)
+  WebsocketClientWidget(Device::ProtocolSettingsWidget& proto, QWidget* parent)
       : QWidget{parent}
   {
     auto layout = new QFormLayout{this};
@@ -287,7 +287,7 @@ private:
 class WebsocketServerWidget : public QWidget
 {
 public:
-  WebsocketServerWidget(OSCProtocolSettingsWidget* parent)
+  WebsocketServerWidget(Device::ProtocolSettingsWidget& proto, QWidget* parent)
       : QWidget{parent}
   {
     auto layout = new QFormLayout{this};
@@ -296,7 +296,7 @@ public:
     m_remotePort = new QSpinBox(this);
     m_remotePort->setRange(0, 65535);
     m_remotePort->setValue(9996);
-    parent->checkForChanges(m_remotePort);
+    proto.checkForChanges(m_remotePort);
 
     layout->addRow(tr("Port"), m_remotePort);
   }
@@ -319,7 +319,7 @@ private:
 class SerialWidget : public QWidget
 {
 public:
-  SerialWidget(OSCProtocolSettingsWidget* parent)
+  SerialWidget(Device::ProtocolSettingsWidget& proto, QWidget* parent)
       : QWidget{parent}
   {
     auto layout = new QFormLayout{this};
@@ -369,16 +369,38 @@ private:
   QComboBox* m_framing{};
 };
 
-enum OscProtocols
+OSCTransportWidget::OSCTransportWidget(
+    Device::ProtocolSettingsWidget& proto, QWidget* parent)
+    : QWidget{parent}
 {
-  UDP = 0,
-  TCP = 1,
-  Serial = 2,
-  UnixDatagram = 3,
-  UnixStream = 4,
-  WSClient = 5,
-  WSServer = 6
-};
+  m_transportLayout = new QStackedLayout{this};
+  m_transportLayout->setContentsMargins(0, 0, 0, 0);
+  m_udp = new UDPWidget{proto, this};
+  m_transportLayout->addWidget(m_udp);
+
+  m_tcp = new TCPWidget{proto, this};
+  m_transportLayout->addWidget(m_tcp);
+
+  m_serial = new SerialWidget{proto, this};
+  m_transportLayout->addWidget(m_serial);
+
+  m_unix_dgram = new UnixDatagramWidget{proto, this};
+  m_transportLayout->addWidget(m_unix_dgram);
+
+  m_unix_stream = new UnixStreamWidget{proto, this};
+  m_transportLayout->addWidget(m_unix_stream);
+
+  m_ws_client = new WebsocketClientWidget{proto, this};
+  m_transportLayout->addWidget(m_ws_client);
+
+  m_ws_server = new WebsocketServerWidget{proto, this};
+  m_transportLayout->addWidget(m_ws_server);
+}
+
+void OSCTransportWidget::setCurrentProtocol(OscProtocol index)
+{
+  m_transportLayout->setCurrentIndex(index);
+}
 
 OSCProtocolSettingsWidget::OSCProtocolSettingsWidget(QWidget* parent)
     : ProtocolSettingsWidget(parent)
@@ -402,40 +424,109 @@ OSCProtocolSettingsWidget::OSCProtocolSettingsWidget(QWidget* parent)
 
   m_oscVersion = new QComboBox{this};
   m_oscVersion->addItems({"1.0", "1.1", "Extended"});
-  m_transportLayout = new QStackedLayout{};
-  m_transportLayout->setContentsMargins(0, 0, 0, 0);
-  m_udp = new UDPWidget{this};
-  m_transportLayout->addWidget(m_udp);
 
-  m_tcp = new TCPWidget{this};
-  m_transportLayout->addWidget(m_tcp);
-
-  m_serial = new SerialWidget{this};
-  m_transportLayout->addWidget(m_serial);
-
-  m_unix_dgram = new UnixDatagramWidget{this};
-  m_transportLayout->addWidget(m_unix_dgram);
-
-  m_unix_stream = new UnixStreamWidget{this};
-  m_transportLayout->addWidget(m_unix_stream);
-
-  m_ws_client = new WebsocketClientWidget{this};
-  m_transportLayout->addWidget(m_ws_client);
-
-  m_ws_server = new WebsocketServerWidget{this};
-  m_transportLayout->addWidget(m_ws_server);
-
-  connect(
+  m_transportWidget = new OSCTransportWidget{*this, this};
+  QObject::connect(
       m_transport, qOverload<int>(&QComboBox::currentIndexChanged), this,
-      [this](int idx) { m_transportLayout->setCurrentIndex(idx); });
+      [this](int idx) { m_transportWidget->setCurrentProtocol((OscProtocol)idx); });
 
   auto layout = new QFormLayout{this};
   layout->addRow(tr("Name"), m_deviceNameEdit);
-  layout->addRow(tr("OSC Version"), m_oscVersion);
   layout->addRow(tr("Rate limit"), m_rate);
   layout->addRow(tr("Bonjour"), m_bonjour);
+  layout->addRow(tr("OSC Version"), m_oscVersion);
   layout->addRow(tr("Protocol"), m_transport);
-  layout->addRow(m_transportLayout);
+  layout->addRow(m_transportWidget);
+}
+
+ossia::net::osc_protocol_configuration
+OSCTransportWidget::configuration(OscProtocol index) const noexcept
+{
+  ossia::net::osc_protocol_configuration conf;
+  switch(index)
+  {
+    case UDP:
+      conf.transport = m_udp->settings();
+      conf.mode = ossia::net::osc_protocol_configuration::MIRROR;
+      break;
+    case TCP:
+      conf.transport = m_tcp->settings();
+      conf.framing = m_tcp->framing();
+      conf.mode = ossia::net::osc_protocol_configuration::MIRROR;
+      break;
+    case Serial:
+      conf.transport = m_serial->settings();
+      conf.framing = m_serial->framing();
+      conf.mode = ossia::net::osc_protocol_configuration::MIRROR;
+      break;
+    case UnixDatagram:
+      conf.transport = m_unix_dgram->settings();
+      conf.mode = ossia::net::osc_protocol_configuration::MIRROR;
+      break;
+    case UnixStream:
+      conf.transport = m_unix_stream->settings();
+      conf.framing = m_unix_stream->framing();
+      conf.mode = ossia::net::osc_protocol_configuration::MIRROR;
+      break;
+    case WSClient:
+      conf.transport = m_ws_client->settings();
+      conf.mode = ossia::net::osc_protocol_configuration::MIRROR;
+      break;
+    case WSServer:
+      conf.transport = m_ws_server->settings();
+      conf.mode = ossia::net::osc_protocol_configuration::HOST;
+      break;
+  }
+  return conf;
+}
+
+OscProtocol OSCTransportWidget::setConfiguration(
+    const ossia::net::osc_protocol_configuration& osc_conf)
+{
+  struct
+  {
+    OSCTransportWidget& self;
+    const ossia::net::osc_protocol_configuration& osc_conf;
+    OscProtocol proto{};
+    void operator()(const ossia::net::udp_configuration& conf)
+    {
+      self.m_udp->setSettings(conf);
+      proto = UDP;
+    }
+    void operator()(const ossia::net::tcp_configuration& conf)
+    {
+      self.m_tcp->setSettings(osc_conf, conf);
+      proto = TCP;
+    }
+    void operator()(const ossia::net::unix_dgram_configuration& conf)
+    {
+      self.m_unix_dgram->setSettings(conf);
+      proto = UnixDatagram;
+    }
+    void operator()(const ossia::net::unix_stream_configuration& conf)
+    {
+      self.m_unix_stream->setSettings(osc_conf, conf);
+      proto = UnixStream;
+    }
+    void operator()(const ossia::net::serial_configuration& conf)
+    {
+      self.m_serial->setSettings(osc_conf, conf);
+      proto = Serial;
+    }
+    void operator()(const ossia::net::ws_client_configuration& conf)
+    {
+      self.m_ws_client->setSettings(conf);
+      proto = WSClient;
+    }
+    void operator()(const ossia::net::ws_server_configuration& conf)
+    {
+      self.m_ws_server->setSettings(conf);
+      proto = WSServer;
+    }
+  } vis{*this, osc_conf};
+
+  ossia::visit(vis, osc_conf.transport);
+  return vis.proto;
 }
 
 Device::DeviceSettings OSCProtocolSettingsWidget::getSettings() const
@@ -445,42 +536,10 @@ Device::DeviceSettings OSCProtocolSettingsWidget::getSettings() const
   s.protocol = OSCProtocolFactory::static_concreteKey();
 
   OSCSpecificSettings osc = m_settings;
-  switch((OscProtocols)m_transport->currentIndex())
-  {
-    case UDP:
-      osc.configuration.transport = m_udp->settings();
-      osc.configuration.mode = ossia::net::osc_protocol_configuration::MIRROR;
-      break;
-    case TCP:
-      osc.configuration.transport = m_tcp->settings();
-      osc.configuration.framing = m_tcp->framing();
-      osc.configuration.mode = ossia::net::osc_protocol_configuration::MIRROR;
-      break;
-    case Serial:
-      osc.configuration.transport = m_serial->settings();
-      osc.configuration.framing = m_serial->framing();
-      osc.configuration.mode = ossia::net::osc_protocol_configuration::MIRROR;
-      break;
-    case UnixDatagram:
-      osc.configuration.transport = m_unix_dgram->settings();
-      osc.configuration.mode = ossia::net::osc_protocol_configuration::MIRROR;
-      break;
-    case UnixStream:
-      osc.configuration.transport = m_unix_stream->settings();
-      osc.configuration.framing = m_unix_stream->framing();
-      osc.configuration.mode = ossia::net::osc_protocol_configuration::MIRROR;
-      break;
-    case WSClient:
-      osc.configuration.transport = m_ws_client->settings();
-      osc.configuration.mode = ossia::net::osc_protocol_configuration::MIRROR;
-      break;
-    case WSServer:
-      osc.configuration.transport = m_ws_server->settings();
-      osc.configuration.mode = ossia::net::osc_protocol_configuration::HOST;
-      break;
-  }
 
   using osc_version_t = decltype(ossia::net::osc_protocol_configuration::version);
+  osc.configuration
+      = m_transportWidget->configuration((OscProtocol)m_transport->currentIndex());
   osc.configuration.version = static_cast<osc_version_t>(m_oscVersion->currentIndex());
   osc.rate = m_rate->rate();
   osc.bonjour = m_bonjour->isChecked();
@@ -523,48 +582,8 @@ void OSCProtocolSettingsWidget::setSettings(const Device::DeviceSettings& settin
     m_oscVersion->setCurrentIndex(m_settings.configuration.version);
     m_rate->setRate(m_settings.rate);
     m_bonjour->setChecked(m_settings.bonjour);
-    struct vis
-    {
-      OSCProtocolSettingsWidget& self;
-      void operator()(const ossia::net::udp_configuration& conf) const
-      {
-        self.m_udp->setSettings(conf);
-        self.m_transport->setCurrentIndex(UDP);
-      }
-      void operator()(const ossia::net::tcp_configuration& conf) const
-      {
-        self.m_tcp->setSettings(self.m_settings.configuration, conf);
-        self.m_transport->setCurrentIndex(TCP);
-      }
-      void operator()(const ossia::net::unix_dgram_configuration& conf) const
-      {
-        self.m_unix_dgram->setSettings(conf);
-        self.m_transport->setCurrentIndex(UnixDatagram);
-      }
-      void operator()(const ossia::net::unix_stream_configuration& conf) const
-      {
-        self.m_unix_stream->setSettings(self.m_settings.configuration, conf);
-        self.m_transport->setCurrentIndex(UnixStream);
-      }
-      void operator()(const ossia::net::serial_configuration& conf) const
-      {
-        self.m_serial->setSettings(self.m_settings.configuration, conf);
-        self.m_transport->setCurrentIndex(Serial);
-      }
-      void operator()(const ossia::net::ws_client_configuration& conf) const
-      {
-        self.m_ws_client->setSettings(conf);
-        self.m_transport->setCurrentIndex(WSClient);
-      }
-      void operator()(const ossia::net::ws_server_configuration& conf) const
-      {
-        self.m_ws_server->setSettings(conf);
-        self.m_transport->setCurrentIndex(WSServer);
-      }
-    };
-
-    using namespace std;
-    ossia::visit(vis{*this}, m_settings.configuration.transport);
+    auto proto = m_transportWidget->setConfiguration(m_settings.configuration);
+    m_transport->setCurrentIndex(proto);
   }
 }
 }
