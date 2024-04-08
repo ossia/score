4ca2e5eddc64b57eda858dff5e7c5e4253631eba
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-04-07 21:16:18 -0400

    [recorder] Make it output the filename and change bang into toggle

--- src/plugins/score-plugin-avnd/AvndProcesses/AudioRecorder.hpp
+++ src/plugins/score-plugin-avnd/AvndProcesses/AudioRecorder.hpp
@@ -19,28 +19,38 @@ struct AudioRecorder
   halp_meta(uuid, "4463dddc-6acf-4106-a680-fed67d8030da")
 
   using audio_buffer = boost::container::vector<double>;
+
   // Threaded worker
   struct recorder_thread
   {
     ossia::drwav_write_handle f;
     std::string filename;
+    std::string actual_filename;
     int channels = 0;
     int rate = 0;
+    bool must_record = false;
 
-    void reopen()
+    bool reopen()
     {
-      f.close();
-      if(channels <= 0)
-        return;
-      if(rate <= 0)
-        return;
-
-      if(filename.empty())
-        return;
-
-      auto fname = QByteArray::fromStdString(this->filename);
-      fname.replace("%t", QDateTime::currentDateTimeUtc().toString().toUtf8());
-      f.open(fname.toStdString(), channels, rate, 16);
+      if(must_record)
+      {
+        // Open the file with the correct substitutions
+        auto fname = QByteArray::fromStdString(this->filename);
+        fname.replace("%t", QDateTime::currentDateTimeUtc().toString().toUtf8());
+        actual_filename = fname.toStdString();
+        f.open(actual_filename, channels, rate, 16);
+        return false;
+      }
+      else
+      {
+        bool must_reply = false;
+        if(f.is_open() && f.written_frames() > 0)
+        {
+          must_reply = true;
+        }
+        f.close();
+        return must_reply;
+      }
     }
 
     void write(int frames, audio_buffer& data)
@@ -55,9 +65,13 @@ struct AudioRecorder
       if(!f.is_open())
         return;
 
+      // Put addresses to the first sample of each channel in an array,
+      // e.g. arr[0] points to the first channel, arr[1] to the second, etc.
       const double** arr = (const double**)alloca(channels * sizeof(double*));
       for(int c = 0; c < channels; ++c)
         arr[c] = data.data() + c * frames;
+
+      // Write the data
       f.write_pcm_frames(frames, arr);
     }
   };
@@ -67,12 +81,14 @@ struct AudioRecorder
   {
     std::string path;
     int rate{};
-    void operator()(recorder_thread& self)
+    bool must_record{};
+    bool operator()(recorder_thread& self)
     {
       using namespace std;
       swap(self.filename, path);
       self.rate = rate;
-      self.reopen();
+      self.must_record = must_record;
+      return self.reopen();
     }
   };
 
@@ -80,17 +96,34 @@ struct AudioRecorder
   {
     int frames{};
     audio_buffer data;
-    void operator()(recorder_thread& self) { self.write(frames, data); }
+    bool operator()(recorder_thread& self)
+    {
+      self.write(frames, data);
+      return false;
+    }
   };
   using worker_message = ossia::variant<reset_message, process_message>;
 
   struct
   {
     std::function<void(std::shared_ptr<recorder_thread>, worker_message)> request;
-    static void work(std::shared_ptr<recorder_thread> t, worker_message&& mess)
+    static std::function<void(AudioRecorder&)>
+    work(std::shared_ptr<recorder_thread> t, worker_message&& mess)
     {
-      ossia::visit(
-          [&]<typename M>(M&& msg) { std::forward<M>(msg)(*t); }, std::move(mess));
+      bool reply = ossia::visit(
+          [&]<typename M>(M&& msg) { return std::forward<M>(msg)(*t); },
+          std::move(mess));
+      if(reply)
+      {
+        return [filename = t->actual_filename](AudioRecorder& self) mutable {
+          using namespace std;
+          swap(self.filename_to_output, filename);
+        };
+      }
+      else
+      {
+        return {};
+      }
     }
   } worker;
 
@@ -103,27 +136,29 @@ struct AudioRecorder
       void update(AudioRecorder& self) { self.update(); }
     } filename;
 
-    struct : halp::impulse_button_t<"Update">
+    struct : halp::toggle<"Record">
     {
       void update(AudioRecorder& self) { self.update(); }
-    } update;
+    } record;
   } inputs;
 
   struct
   {
+    halp::callback<"Filename", std::string> finished;
   } outputs;
 
   int current_rate = 0;
   void prepare(halp::setup s) { current_rate = s.rate; }
 
-  void update() { worker.request(impl, reset_message{inputs.filename, current_rate}); }
+  void update()
+  {
+    worker.request(impl, reset_message{inputs.filename, current_rate, inputs.record});
+  }
 
   void operator()(int frames)
   {
     if(!std::exchange(started, true))
-    {
       update();
-    }
 
     data.resize(inputs.audio.channels * frames, boost::container::default_init);
     for(int c = 0; c < inputs.audio.channels; c++)
@@ -135,6 +170,12 @@ struct AudioRecorder
     }
 
     worker.request(impl, process_message{frames, std::move(data)});
+
+    if(!filename_to_output.empty())
+    {
+      outputs.finished(filename_to_output);
+      filename_to_output.clear();
+    }
   }
 
   bool started{};
@@ -142,6 +183,7 @@ struct AudioRecorder
   std::optional<int64_t> last_message_sent_pos;
 
   audio_buffer data;
+  std::string filename_to_output;
 };
 
 }
