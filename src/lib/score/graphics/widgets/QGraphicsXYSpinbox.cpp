65384554de4821dd848f44a27c4187f3c795da55
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-04-22 23:41:07 -0400

    [ui] Add an XY int spinbox

--- src/lib/score/graphics/widgets/QGraphicsXYSpinbox.cpp
+++ src/lib/score/graphics/widgets/QGraphicsXYSpinbox.cpp
@@ -11,6 +11,7 @@
 #include <wobjectimpl.h>
 
 W_OBJECT_IMPL(score::QGraphicsXYSpinboxChooser);
+W_OBJECT_IMPL(score::QGraphicsIntXYSpinboxChooser);
 
 namespace score
 {
@@ -19,7 +20,6 @@ QGraphicsXYSpinboxChooser::QGraphicsXYSpinboxChooser(bool isRange, QGraphicsItem
     : m_x{this}
     , m_y{this}
     , m_isRange{isRange}
-
 {
   auto& skin = score::Skin::instance();
   setCursor(skin.CursorPointingHand);
@@ -104,4 +104,95 @@ QRectF QGraphicsXYSpinboxChooser::boundingRect() const
   return m_rect;
 }
 
+QGraphicsIntXYSpinboxChooser::QGraphicsIntXYSpinboxChooser(
+    bool isRange, QGraphicsItem* parent)
+    : m_x{this}
+    , m_y{this}
+    , m_isRange{isRange}
+
+{
+  auto& skin = score::Skin::instance();
+  setCursor(skin.CursorPointingHand);
+  setRange();
+  this->setAcceptedMouseButtons(Qt::LeftButton | Qt::RightButton);
+  this->setFlag(ItemHasNoContents);
+
+  m_x.setPos(0, 2);
+  m_y.setPos(45, 2);
+
+  if(m_isRange)
+  {
+    connect(&m_x, &QGraphicsIntSpinbox::sliderMoved, this, [this]() {
+      if(m_x.value() >= m_y.value())
+        m_y.setValue(m_x.value() + 0.0000001);
+      sliderMoved();
+    });
+    connect(&m_y, &QGraphicsIntSpinbox::sliderMoved, this, [this]() {
+      if(m_y.value() <= m_x.value())
+        m_x.setValue(m_y.value() - 0.0000001);
+      sliderMoved();
+    });
+  }
+  else
+  {
+    connect(
+        &m_x, &QGraphicsIntSpinbox::sliderMoved, this,
+        &QGraphicsIntXYSpinboxChooser::sliderMoved);
+    connect(
+        &m_y, &QGraphicsIntSpinbox::sliderMoved, this,
+        &QGraphicsIntXYSpinboxChooser::sliderMoved);
+  }
+  connect(
+      &m_x, &QGraphicsIntSpinbox::sliderReleased, this,
+      &QGraphicsIntXYSpinboxChooser::sliderReleased);
+  connect(
+      &m_y, &QGraphicsIntSpinbox::sliderReleased, this,
+      &QGraphicsIntXYSpinboxChooser::sliderReleased);
+}
+
+QGraphicsIntXYSpinboxChooser::~QGraphicsIntXYSpinboxChooser() = default;
+
+void QGraphicsIntXYSpinboxChooser::paint(
+    QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget)
+{
+}
+
+std::array<float, 2> QGraphicsIntXYSpinboxChooser::value() const noexcept
+{
+  return {float(m_x.value()), float(m_y.value())};
+}
+
+std::array<float, 2> QGraphicsIntXYSpinboxChooser::getMin() const noexcept
+{
+  return {(float)m_x.min, (float)m_y.min};
+}
+std::array<float, 2> QGraphicsIntXYSpinboxChooser::getMax() const noexcept
+{
+  return {(float)m_x.max, (float)m_y.max};
+}
+
+ossia::vec2f QGraphicsIntXYSpinboxChooser::scaledValue(float x, float y) const noexcept
+{
+  return {
+      (float)(m_x.min + x * (m_x.max - m_x.min)),
+      (float)(m_y.min + y * (m_y.max - m_y.min))};
+}
+
+void QGraphicsIntXYSpinboxChooser::setValue(ossia::vec2f v)
+{
+  m_x.setValue(v[0]);
+  m_y.setValue(v[1]);
+  update();
+}
+
+void QGraphicsIntXYSpinboxChooser::setRange(ossia::vec2f min, ossia::vec2f max)
+{
+  m_x.setRange(min[0], max[0]);
+  m_y.setRange(min[1], max[1]);
+}
+
+QRectF QGraphicsIntXYSpinboxChooser::boundingRect() const
+{
+  return m_rect;
+}
 }
--- src/lib/score/graphics/widgets/QGraphicsXYSpinbox.hpp
+++ src/lib/score/graphics/widgets/QGraphicsXYSpinbox.hpp
@@ -51,4 +51,42 @@ private:
   void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget)
       override;
 };
+
+class SCORE_LIB_BASE_EXPORT QGraphicsIntXYSpinboxChooser final
+    : public QObject
+    , public QGraphicsItem
+{
+  W_OBJECT(QGraphicsIntXYSpinboxChooser)
+  Q_INTERFACES(QGraphicsItem)
+  friend struct DefaultGraphicsSpinboxImpl;
+  QRectF m_rect{0., 0., 150., 24.};
+  QGraphicsIntSpinbox m_x, m_y;
+
+  bool m_grab{};
+  bool m_isRange{};
+
+public:
+  explicit QGraphicsIntXYSpinboxChooser(bool isRange, QGraphicsItem* parent);
+  ~QGraphicsIntXYSpinboxChooser();
+
+  void setPoint(const QPointF& r);
+  void setValue(ossia::vec2f v);
+  void setRange(ossia::vec2f min = {0.f, 0.f}, ossia::vec2f max = {1.f, 1.f});
+
+  ossia::vec2f value() const noexcept;
+  ossia::vec2f getMin() const noexcept;
+  ossia::vec2f getMax() const noexcept;
+
+  bool moving = false;
+
+public:
+  void sliderMoved() E_SIGNAL(SCORE_LIB_BASE_EXPORT, sliderMoved)
+  void sliderReleased() E_SIGNAL(SCORE_LIB_BASE_EXPORT, sliderReleased)
+
+private:
+  ossia::vec2f scaledValue(float x, float y) const noexcept;
+  QRectF boundingRect() const override;
+  void paint(QPainter* painter, const QStyleOptionGraphicsItem* option, QWidget* widget)
+      override;
+};
 }
