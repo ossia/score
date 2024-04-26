122c3ad428846b5d7afef40715dfef3015e42c0d
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-04-26 11:38:28 -0400

    [ci] Add a ubuntu 24.04 build

---
+++ ci/noble.deps.sh
@@ -0,0 +1,38 @@
+#!/bin/bash -eux
+
+source ci/common.setup.sh
+
+echo 'debconf debconf/frontend select Noninteractive' | $SUDO debconf-set-selections
+
+$SUDO apt-get update -qq
+$SUDO apt-get install -y \
+    --allow-change-held-packages \
+    --allow-downgrades \
+    --allow-remove-essential \
+    --allow-unauthenticated \
+     binutils gcc g++ clang lld \
+     software-properties-common wget \
+     libasound-dev \
+     ninja-build cmake \
+     libfftw3-dev \
+     libsuil-dev liblilv-dev lv2-dev \
+     qt6-base-dev qt6-base-dev-tools qt6-base-private-dev \
+     qt6-declarative-dev qt6-declarative-dev-tools qt6-declarative-private-dev \
+     qt6-scxml-dev \
+     libqt6core5compat6-dev \
+     libqt6opengl6-dev \
+     libqt6websockets6-dev \
+     libqt6serialport6-dev \
+     libqt6shadertools6-dev \
+     libbluetooth-dev libsdl2-dev libsdl2-2.0-0 \
+     libglu1-mesa-dev libglu1-mesa libgles2-mesa-dev \
+     libavahi-compat-libdnssd-dev libsamplerate0-dev \
+     portaudio19-dev \
+     libpipewire-0.3-dev \
+     libclang-dev llvm-dev \
+     libvulkan-dev \
+     libavcodec-dev libavdevice-dev libavutil-dev libavfilter-dev libavformat-dev libswresample-dev \
+     file \
+     dpkg-dev
+
+source ci/common.deps.sh
