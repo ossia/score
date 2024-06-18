a8426180bedfca79e095b51cbe71c650c4741ccf
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-06-18 15:27:58 -0400

    [ci] Try to understand mingw build failure

--- ci/common.deps.sh
+++ ci/common.deps.sh
@@ -3,26 +3,26 @@
 (
 cd src/addons
 
-if [[ ! -d iscore-addon-network ]]; then
-  git clone --recursive -j16 https://github.com/ossia/iscore-addon-network
-fi
-
-if [[ ! -d score-addon-synthimi ]]; then
-  git clone --recursive -j16 https://github.com/ossia/score-addon-synthimi
-fi
-
-if [[ ! -d score-addon-threedim ]]; then
-  git clone --recursive -j16 https://github.com/ossia/score-addon-threedim
-fi
-
-if [[ ! -d score-addon-jk ]]; then
-  git clone --recursive -j16 https://github.com/ossia/score-addon-jk
-fi
-
-if [[ ! -d score-addon-ndi ]]; then
-  git clone --recursive -j16 https://github.com/ossia/score-addon-ndi
-fi
-
+# if [[ ! -d iscore-addon-network ]]; then
+#   git clone --recursive -j16 https://github.com/ossia/iscore-addon-network
+# fi
+# 
+# if [[ ! -d score-addon-synthimi ]]; then
+#   git clone --recursive -j16 https://github.com/ossia/score-addon-synthimi
+# fi
+# 
+# if [[ ! -d score-addon-threedim ]]; then
+#   git clone --recursive -j16 https://github.com/ossia/score-addon-threedim
+# fi
+# 
+# if [[ ! -d score-addon-jk ]]; then
+#   git clone --recursive -j16 https://github.com/ossia/score-addon-jk
+# fi
+# 
+# if [[ ! -d score-addon-ndi ]]; then
+#   git clone --recursive -j16 https://github.com/ossia/score-addon-ndi
+# fi
+# 
 if [[ ! -d score-avnd-granola ]]; then
   git clone --recursive -j16 https://github.com/bltzr/score-avnd-granola
 fi
