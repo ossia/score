8847ec431abd28ca7ca1c478ab3ef8a104face2d
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-06-22 06:38:45 -0400

    disable threedim

--- ci/common.deps.sh
+++ ci/common.deps.sh
@@ -11,9 +11,9 @@ if [[ ! -d score-addon-synthimi ]]; then
   git clone --recursive -j16 https://github.com/ossia/score-addon-synthimi
 fi
 
-if [[ ! -d score-addon-threedim ]]; then
-  git clone --recursive -j16 https://github.com/ossia/score-addon-threedim
-fi
+# if [[ ! -d score-addon-threedim ]]; then
+#   git clone --recursive -j16 https://github.com/ossia/score-addon-threedim
+# fi
 
 # if [[ ! -d score-addon-jk ]]; then
 #   git clone --recursive -j16 https://github.com/ossia/score-addon-jk
