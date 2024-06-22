d3f7439c919cd9522025c454454dc9c33bbc8428
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-06-22 06:38:59 -0400

    disable synthimi

--- ci/common.deps.sh
+++ ci/common.deps.sh
@@ -7,9 +7,9 @@ if [[ ! -d iscore-addon-network ]]; then
   git clone --recursive -j16 https://github.com/ossia/iscore-addon-network
 fi
 
-if [[ ! -d score-addon-synthimi ]]; then
-  git clone --recursive -j16 https://github.com/ossia/score-addon-synthimi
-fi
+# if [[ ! -d score-addon-synthimi ]]; then
+#  git clone --recursive -j16 https://github.com/ossia/score-addon-synthimi
+# fi
 
 # if [[ ! -d score-addon-threedim ]]; then
 #   git clone --recursive -j16 https://github.com/ossia/score-addon-threedim
