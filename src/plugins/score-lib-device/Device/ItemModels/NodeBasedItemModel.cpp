e9c4bb87141e8bdb9c88594d0bb8a32a61ef725c
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-10-31 15:50:58 -0400

    [Device] Do not try to infer the min / max if things are patterns

--- src/plugins/score-lib-device/Device/ItemModels/NodeBasedItemModel.cpp
+++ src/plugins/score-lib-device/Device/ItemModels/NodeBasedItemModel.cpp
@@ -8,6 +8,32 @@ namespace Device
 {
 NodeBasedItemModel::~NodeBasedItemModel() = default;
 
+static bool is_pattern(QStringView address)
+{
+  if(address.startsWith(QStringLiteral("//")))
+    return true;
+  static const auto lit = QStringLiteral("?*[]{}!");
+  for(QChar c : address)
+  {
+    if(std::any_of(lit.begin(), lit.end(), [c](QChar lit) { return lit == c; }))
+      return true;
+  }
+
+  return false;
+}
+
+static bool is_pattern(const State::Address& address)
+{
+  if(is_pattern(address.device))
+    return true;
+
+  for(auto& d : address.path)
+    if(is_pattern(d))
+      return true;
+
+  return false;
+}
+
 Device::FullAddressAccessorSettings makeFullAddressAccessorSettings(
     const State::AddressAccessor& addr, const Device::NodeBasedItemModel& deviceexplorer,
     ossia::value min, ossia::value max, ossia::value val)
@@ -17,26 +43,36 @@ Device::FullAddressAccessorSettings makeFullAddressAccessorSettings(
   auto newpath = newval.path;
   newpath.prepend(newval.device);
 
-  // First try to find if there is a matching address
-  // in the device explorer
-  auto new_n = Device::try_getNodeFromString(deviceexplorer.rootNode(), newpath);
-  if(new_n && new_n->is<Device::AddressSettings>())
+  if(!is_pattern(addr.address))
   {
-    return Device::FullAddressAccessorSettings{
-        addr, new_n->get<Device::AddressSettings>()};
+    // First try to find if there is a matching address
+    // in the device explorer
+    auto new_n = Device::try_getNodeFromString(deviceexplorer.rootNode(), newpath);
+    if(new_n && new_n->is<Device::AddressSettings>())
+    {
+      return Device::FullAddressAccessorSettings{
+          addr, new_n->get<Device::AddressSettings>()};
+    }
+    else
+    {
+      // TODO Try also with the OSSIA conversions.
+      // But this requires refactoring quite a bit...
+    }
+
+    // If there is none, build with some default settings
+    Device::FullAddressAccessorSettings s;
+    s.address = addr;
+    s.value = std::move(val);
+    s.domain = ossia::make_domain(std::move(min), std::move(max));
+    return s;
   }
   else
   {
-    // TODO Try also with the OSSIA conversions.
-    // But this requires refactoring quite a bit...
+    Device::FullAddressAccessorSettings s;
+    s.address = addr;
+    s.value = std::move(val);
+    return s;
   }
-
-  // If there is none, build with some default settings
-  Device::FullAddressAccessorSettings s;
-  s.address = addr;
-  s.value = std::move(val);
-  s.domain = ossia::make_domain(std::move(min), std::move(max));
-  return s;
 }
 
 Device::FullAddressAccessorSettings
