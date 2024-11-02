1dd10868d277e941898949d965925d2e729fe6da
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-11-02 14:14:28 -0400

    [core] Rework is_template
    See https://gcc.godbolt.org/z/a517Y5bh4

--- src/lib/score/serialization/IsTemplate.hpp
+++ src/lib/score/serialization/IsTemplate.hpp
@@ -68,33 +68,10 @@ struct is_template<T<A1, A2, A3, A4, A5, A6, A7, A8>> : std::true_type
 {
 };
 
-// TODO check where this causes a conflict with gcc
-#if defined(__clang__) || defined(_MSC_VER)
-template <template <class, std::size_t> class T, class X, std::size_t u>
+template <template <class, auto> class T, class X, auto u>
 struct is_template<T<X, u>> : std::true_type
 {
 };
-
-#if defined(__clang__) && (__clang_major__ >= 19)
-// https://gcc.godbolt.org/z/v115ch8oe
-template <
-    template <class, std::size_t, class, class> class T, class X, std::size_t u, class A,
-    class B>
-struct is_template<T<X, u, A, B>> : std::true_type
-{
-};
-
-#endif
-#endif
-
-// For IdContainer
-
-#if !defined(__clang__) || (__clang_major__ < 19)
-template <template <class, bool> class T, class A, bool C>
-struct is_template<T<A, C>> : std::true_type
-{
-};
-#endif
 
 template <template <class, class, bool> class T, class A, class B, bool C>
 struct is_template<T<A, B, C>> : std::true_type
