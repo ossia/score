4b4bb5e0ea6b580f0a725cbd354c13342b04203c
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-11-08 09:16:33 -0500

    [ci] Disable modules when the ecosystem cant handle it yet

--- ci/archlinux-qt6.build.sh
+++ ci/archlinux-qt6.build.sh
@@ -10,7 +10,8 @@ cmake $SCORE_DIR \
   -DCMAKE_BUILD_TYPE=Release \
   -DCMAKE_INSTALL_PREFIX=install \
   -DSCORE_DYNAMIC_PLUGINS=1 \
-  -DSCORE_PCH=1
+  -DSCORE_PCH=1 \
+  -DCMAKE_CXX_SCAN_FOR_MODULES=0
 
 cmake --build .
 cmake --build . --target install
