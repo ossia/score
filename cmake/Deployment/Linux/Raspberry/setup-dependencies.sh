292868943391ca4088b14fd1ea3224ff7f1d75d6
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-12-13 20:32:45 -0500

    rpi: further improvement for non-debian aarch64

--- cmake/Deployment/Linux/Raspberry/setup-dependencies.sh
+++ cmake/Deployment/Linux/Raspberry/setup-dependencies.sh
@@ -2,15 +2,21 @@
 
 if [[ -f /etc/redhat-release ]]; then
   # Fedora does not ship this for some obscure reason
+  if [[ ! -f "/usr/lib/libbz2.so.1.0" ]]; then
+  if [[ ! -f "/usr/lib64/libbz2.so.1.0" ]]; then
+  if [[ ! -f "/usr/lib/aarch64-linux-gnu/libbz2.so.1.0" ]]; then
   if [[ ! -f "$DIR/lib/libbz2.so.1.0" ]]; then
     find /usr -name libbz2.so.1 -exec cp {} "$DIR/lib/libbz2.so.1.0" \; || true
   fi
+  fi
+  fi
+  fi
 
   # Fedora has a recent enough libstdc++
-  rm "$DIR/lib/libstdc++.so.6"
+  rm -f "$DIR/lib/libstdc++.so.6"
 fi
 
 if [[ -f /etc/arch-release ]]; then
   # Arch has a recent enough libstdc++
-  rm "$DIR/lib/libstdc++.so.6"
+  rm -f "$DIR/lib/libstdc++.so.6"
 fi
