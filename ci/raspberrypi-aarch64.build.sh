3544bf2efeef5b53443869f06c6dea5869cf6255
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2024-12-20 14:07:09 -0500

    rpi: do not remove lib/ wholesale

--- ci/raspberrypi-aarch64.build.sh
+++ ci/raspberrypi-aarch64.build.sh
@@ -27,8 +27,8 @@ cmake --build . --target install/strip
 
 (
   cd install
-  rm -rf include lib share/doc
-  mkdir lib
+  rm -rf include lib/*.a lib/*.settings lib/cmake lib/pkgconfig share/doc
+  mkdir -p lib
   cp /opt/ossia-sdk-rpi-aarch64/aarch64-rpi3-linux-gnu/aarch64-rpi3-linux-gnu/lib64/libstdc++.so.6 lib/
   cp /opt/ossia-sdk-rpi-aarch64/pi/sysroot/usr/lib/aarch64-linux-gnu/libicu*.so.67 lib/
   cp $SCORE_DIR/cmake/Deployment/Linux/Raspberry/* .
