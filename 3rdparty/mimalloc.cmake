522cd681c87c3428f710e5da70d77c1c3757b8b5
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-01-18 16:22:46 -0500

    snmalloc: disable when _FORTIFY_SOURCE or _GLIBCXX_ASSERTIONS are there.
    See https://github.com/microsoft/snmalloc/issues/732

--- 3rdparty/mimalloc.cmake
+++ 3rdparty/mimalloc.cmake
@@ -10,11 +10,20 @@ if(SCORE_HAS_SANITIZERS)
   return()
 endif()
 
+if("${CMAKE_CXX_FLAGS}" MATCHES ".*_FORTIFY_SOURCE.*")
+  return()
+endif()
+if("${CMAKE_CXX_FLAGS}" MATCHES ".*_GLIBCXX_ASSERTIONS.*")
+  return()
+endif()
+
 if(SCORE_USE_SYSTEM_LIBRARIES)
   find_package(snmalloc GLOBAL CONFIG)
 else()
   block()
-    set(SNMALLOC_BUILD_TESTING OFF CACHE INTERNAL "" FORCE)
-    add_subdirectory(3rdparty/snmalloc SYSTEM)
+  set(SNMALLOC_BUILD_TESTING
+      OFF
+      CACHE INTERNAL "" FORCE)
+  add_subdirectory(3rdparty/snmalloc SYSTEM)
   endblock()
 endif()
