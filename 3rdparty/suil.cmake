edaa3b7fd812b7c6c2cd9c929b5a44a1499281ee
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-01-18 16:48:41 -0500

    suil: fix build on system with no suil at all

--- 3rdparty/suil.cmake
+++ 3rdparty/suil.cmake
@@ -1,5 +1,5 @@
 if(OSSIA_USE_SYSTEM_LIBRARIES)
-    return()
+  return()
 endif()
 
 if(NOT LV2_PATH)
@@ -13,42 +13,39 @@ endif()
 
 set(Suil_INCLUDE_DIR "${3RDPARTY_FOLDER}/suil/include")
 
-add_library(Suil SHARED
-    "${3RDPARTY_FOLDER}/suil/src/host.c"
-    "${3RDPARTY_FOLDER}/suil/src/instance.c"
-)
+add_library(Suil SHARED "${3RDPARTY_FOLDER}/suil/src/host.c"
+                        "${3RDPARTY_FOLDER}/suil/src/instance.c")
 add_library(Suil::Suil ALIAS Suil)
-set_target_properties(Suil PROPERTIES
-    LIBRARY_OUTPUT_NAME "suil-0"
-    VERSION 0
-    SOVERSION 0
-)
+set_target_properties(
+  Suil
+  PROPERTIES LIBRARY_OUTPUT_NAME "suil-0"
+             VERSION 0
+             SOVERSION 0)
 
-target_compile_definitions(Suil PRIVATE "SUIL_MODULE_DIR=\"${CMAKE_BINARY_DIR}/lib/suil-0\"")
+target_compile_definitions(
+  Suil PRIVATE "SUIL_MODULE_DIR=\"${CMAKE_BINARY_DIR}/lib/suil-0\"")
 
-target_include_directories(Suil
-    PUBLIC
-      "${Suil_INCLUDE_DIR}"
-    PRIVATE
-      "${3RDPARTY_FOLDER}/suil/src"
-      "${LV2_PATH}"
-)
+target_include_directories(
+  Suil
+  PUBLIC "${Suil_INCLUDE_DIR}" "${CMAKE_CURRENT_BINARY_DIR}/include"
+  PRIVATE "${3RDPARTY_FOLDER}/suil/src" "${LV2_PATH}")
 
-add_library(suil_x11_in_qt6 MODULE
-    "${3RDPARTY_FOLDER}/suil/src/x11.c"
-    "${3RDPARTY_FOLDER}/suil/src/x11_in_qt.cpp"
-)
-set_target_properties(suil_x11_in_qt6 PROPERTIES
-    LIBRARY_OUTPUT_DIRECTORY "${CMAKE_BINARY_DIR}/lib/suil-0"
-)
+add_library(suil_x11_in_qt6 MODULE "${3RDPARTY_FOLDER}/suil/src/x11.c"
+                                   "${3RDPARTY_FOLDER}/suil/src/x11_in_qt.cpp")
+set_target_properties(
+  suil_x11_in_qt6 PROPERTIES LIBRARY_OUTPUT_DIRECTORY
+                             "${CMAKE_BINARY_DIR}/lib/suil-0")
+
+file(
+  GENERATE
+  OUTPUT "include/suil-0/suil/suil.h"
+  INPUT "${3RDPARTY_FOLDER}/suil/include/suil/suil.h")
 
 target_include_directories(suil_x11_in_qt6
-    PRIVATE "${3RDPARTY_FOLDER}/suil/src"
-)
+                           PRIVATE "${3RDPARTY_FOLDER}/suil/src")
 
-target_link_libraries(suil_x11_in_qt6
-    PRIVATE Suil Qt6::Core Qt6::Gui Qt6::Widgets X11::X11
-)
+target_link_libraries(suil_x11_in_qt6 PRIVATE Suil Qt6::Core Qt6::Gui
+                                              Qt6::Widgets X11::X11)
 
 set(Suil_LIBRARY Suil)
-set(Suil_FOUND 1 )
+set(Suil_FOUND 1)
