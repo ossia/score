ef92ef1d029cc0de2e5b5cc87c13e9f8c6ccedea
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-02-03 22:08:21 -0500

    dmx: work on enabling led fixtures

---
+++ src/plugins/score-plugin-protocols/Protocols/Artnet/LEDDialog.cpp
@@ -0,0 +1,7 @@
+#include <ossia/detail/config.hpp>
+#if defined(OSSIA_PROTOCOL_ARTNET)
+namespace Protocols
+{
+
+}
+#endif
---
+++ src/plugins/score-plugin-protocols/Protocols/Artnet/LEDDialog.hpp
@@ -0,0 +1,9 @@
+#pragma once
+#include <ossia/detail/config.hpp>
+#if defined(OSSIA_PROTOCOL_ARTNET)
+#include <QDialog>
+namespace Protocols
+{
+
+}
+#endif
