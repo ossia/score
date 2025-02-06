431bc31e9c66c1600fc774a9400e81a3c7666fd6
samamou <sarahalamoun@gmail.com>
2025-02-06 10:15:56 -0500

    Add hardcoded categories

--- src/plugins/score-plugin-packagemanager/PackageManager/PluginItemModel.cpp
+++ src/plugins/score-plugin-packagemanager/PackageManager/PluginItemModel.cpp
@@ -255,6 +255,7 @@ std::optional<Package> Package::fromJson(const QJsonObject& obj) noexcept
       {"long", [&](QJsonValue v) { add.longDescription = v.toString(); }},
       {"small", [&](QJsonValue v) { add.smallImagePath = v.toString(); }},
       {"large", [&](QJsonValue v) { add.largeImagePath = v.toString(); }},
+      {"category", [&](QJsonValue v) { add.category = v.toString(); }},
       {"size", [&](QJsonValue v) { add.size = v.toString(); }},
       {"key", [&](QJsonValue v) {
     auto str = v.toString();
