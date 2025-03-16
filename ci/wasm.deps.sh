c2d86d20ed6127f83ab7fab9f5cda1abdd6c38c0
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-03-16 16:52:13 +0100

    mac: win32: sdk32

--- ci/wasm.deps.sh
+++ ci/wasm.deps.sh
@@ -5,9 +5,9 @@ pacman -Syyu --noconfirm
 pacman -S wget ninja cmake which python tar git double-conversion libb2 icu glib2 pcre2 --noconfirm
 
 export SDK_ARCHIVE=sdk-wasm.tar.xz
-wget -nv https://github.com/ossia/score-sdk/releases/download/sdk27/$SDK_ARCHIVE -O $SDK_ARCHIVE
+wget -nv https://github.com/ossia/sdk/releases/download/sdk27/$SDK_ARCHIVE -O $SDK_ARCHIVE
 
-wget -nv https://github.com/ossia/score-sdk/releases/download/sdk25/icu71.tar.gz -O icu71.tar.gz
+wget -nv https://github.com/ossia/sdk/releases/download/sdk25/icu71.tar.gz -O icu71.tar.gz
 tar xaf icu71.tar.gz
 mv libicu* /usr/lib/
 
