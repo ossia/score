4903d1ec7c1c8028a38404375d8a4170374c51e0
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-03-16 22:18:47 +0100

    win32: fix push/pop-state with clang

--- cmake/ScoreConfiguration.cmake
+++ cmake/ScoreConfiguration.cmake
@@ -177,46 +177,56 @@ elseif(has_fopenmp_simd_flag)
   add_compile_options(-fopenmp-simd)
 endif()
 
-if(EMSCRIPTEN)
-# Ported from Modules/Platform/Linux.cmake
-# Features for LINK_LIBRARY generator expression
-## check linker capabilities
-if(NOT DEFINED _CMAKE_LINKER_PUSHPOP_STATE_SUPPORTED)
-  execute_process(COMMAND "${CMAKE_LINKER}" --help
-                  OUTPUT_VARIABLE __linker_help
-                  ERROR_VARIABLE __linker_help)
-  if(__linker_help MATCHES "--push-state" AND __linker_help MATCHES "--pop-state")
-    set(_CMAKE_LINKER_PUSHPOP_STATE_SUPPORTED TRUE CACHE INTERNAL "linker supports push/pop state")
-  else()
-    set(_CMAKE_LINKER_PUSHPOP_STATE_SUPPORTED FALSE CACHE INTERNAL "linker supports push/pop state")
-  endif()
-  unset(__linker_help)
-endif()
-
 # https://github.com/llvm/llvm-project/issues/131007
 if(WIN32 AND ("${CMAKE_CXX_COMPILER_ID}" MATCHES Clang) AND ("${CMAKE_CXX_COMPILER_VERSION}" VERSION_GREATER_EQUAL "20"))
-  set(_CMAKE_CXX_LINKER_PUSHPOP_STATE_SUPPORTED FALSE CACHE INTERNAL "linker supports push/pop state")
-  set(_CMAKE_LINKER_PUSHPOP_STATE_SUPPORTED FALSE CACHE INTERNAL "linker supports push/pop state")
-endif()
-
-## WHOLE_ARCHIVE: Force loading all members of an archive
-if(_CMAKE_LINKER_PUSHPOP_STATE_SUPPORTED)
-  set(CMAKE_LINK_LIBRARY_USING_WHOLE_ARCHIVE "LINKER:--push-state,--whole-archive"
+  set(CMAKE_C_LINK_LIBRARY_USING_WHOLE_ARCHIVE "LINKER:--whole-archive"
                                              "<LINK_ITEM>"
-                                             "LINKER:--pop-state")
-else()
+                                             "LINKER:--no-whole-archive")
+  set(CMAKE_CXX_LINK_LIBRARY_USING_WHOLE_ARCHIVE "LINKER:--whole-archive"
+                                             "<LINK_ITEM>"
+                                             "LINKER:--no-whole-archive")
   set(CMAKE_LINK_LIBRARY_USING_WHOLE_ARCHIVE "LINKER:--whole-archive"
                                              "<LINK_ITEM>"
                                              "LINKER:--no-whole-archive")
-endif()
-set(CMAKE_LINK_LIBRARY_USING_WHOLE_ARCHIVE "${CMAKE_LINK_LIBRARY_USING_WHOLE_ARCHIVE}" CACHE INTERNAL "")
-set(CMAKE_LINK_LIBRARY_USING_WHOLE_ARCHIVE_SUPPORTED TRUE)
 
-# Features for LINK_GROUP generator expression
-## RESCAN: request the linker to rescan static libraries until there is
-## no pending undefined symbols
-set(CMAKE_LINK_GROUP_USING_RESCAN "LINKER:--start-group" "LINKER:--end-group")
-set(CMAKE_LINK_GROUP_USING_RESCAN_SUPPORTED TRUE)
+  set(_CMAKE_CXX_LINKER_PUSHPOP_STATE_SUPPORTED FALSE CACHE INTERNAL "linker supports push/pop state")
+  set(_CMAKE_LINKER_PUSHPOP_STATE_SUPPORTED FALSE CACHE INTERNAL "linker supports push/pop state")
+  set(CMAKE_LINK_LIBRARY_USING_WHOLE_ARCHIVE_SUPPORTED TRUE)
+endif()
+
+if(EMSCRIPTEN)
+  # Ported from Modules/Platform/Linux.cmake
+  # Features for LINK_LIBRARY generator expression
+  ## check linker capabilities
+  if(NOT DEFINED _CMAKE_LINKER_PUSHPOP_STATE_SUPPORTED)
+    execute_process(COMMAND "${CMAKE_LINKER}" --help
+                    OUTPUT_VARIABLE __linker_help
+                    ERROR_VARIABLE __linker_help)
+    if(__linker_help MATCHES "--push-state" AND __linker_help MATCHES "--pop-state")
+      set(_CMAKE_LINKER_PUSHPOP_STATE_SUPPORTED TRUE CACHE INTERNAL "linker supports push/pop state")
+    else()
+      set(_CMAKE_LINKER_PUSHPOP_STATE_SUPPORTED FALSE CACHE INTERNAL "linker supports push/pop state")
+    endif()
+    unset(__linker_help)
+  endif()
+  
+  ## WHOLE_ARCHIVE: Force loading all members of an archive
+  if(_CMAKE_LINKER_PUSHPOP_STATE_SUPPORTED)
+    set(CMAKE_LINK_LIBRARY_USING_WHOLE_ARCHIVE "LINKER:--push-state,--whole-archive"
+                                               "<LINK_ITEM>"
+                                               "LINKER:--pop-state")
+  else()
+    set(CMAKE_LINK_LIBRARY_USING_WHOLE_ARCHIVE "LINKER:--whole-archive"
+                                               "<LINK_ITEM>"
+                                               "LINKER:--no-whole-archive")
+  endif()
+  set(CMAKE_LINK_LIBRARY_USING_WHOLE_ARCHIVE_SUPPORTED TRUE)
+  
+  # Features for LINK_GROUP generator expression
+  ## RESCAN: request the linker to rescan static libraries until there is
+  ## no pending undefined symbols
+  set(CMAKE_LINK_GROUP_USING_RESCAN "LINKER:--start-group" "LINKER:--end-group")
+  set(CMAKE_LINK_GROUP_USING_RESCAN_SUPPORTED TRUE)
 endif()
 
 if(CMAKE_UNITY_BUILD)
