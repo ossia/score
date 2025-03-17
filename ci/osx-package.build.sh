d206eed21de7f5dc5d14c72ee7d3b636ade23995
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-03-17 11:37:02 +0100

    mac: update to macos 12 for everyone due to jthread

--- ci/osx-package.build.sh
+++ ci/osx-package.build.sh
@@ -5,7 +5,13 @@ export SCORE_DIR="$PWD"
 export SDK_DIR="$PWD/SDK"
 export PATH=/usr/local/bin:/opt/homebrew/bin:$PATH
 
-if [[ -d /Applications/Xcode_16.2.app ]]; then
+if [[ -d /Applications/Xcode_16.4.app ]]; then
+  export XCODE_ROOT=/Applications/Xcode_16.4.app
+  sudo xcode-select -s "$XCODE_ROOT"
+elif [[ -d /Applications/Xcode_16.3.app ]]; then
+  export XCODE_ROOT=/Applications/Xcode_16.3.app
+  sudo xcode-select -s "$XCODE_ROOT"
+elif [[ -d /Applications/Xcode_16.2.app ]]; then
   export XCODE_ROOT=/Applications/Xcode_16.2.app
   sudo xcode-select -s "$XCODE_ROOT"
 elif [[ -d /Applications/Xcode_16.1.app ]]; then
@@ -17,34 +23,18 @@ elif [[ -d /Applications/Xcode_16.app ]]; then
 elif [[ -d /Applications/Xcode_16.0.app ]]; then
   export XCODE_ROOT=/Applications/Xcode_16.0.app
   sudo xcode-select -s "$XCODE_ROOT"
-elif [[ -d /Applications/Xcode_15.4.app ]]; then
-  export XCODE_ROOT=/Applications/Xcode_15.4.app
-  sudo xcode-select -s "$XCODE_ROOT"
-elif [[ -d /Applications/Xcode_15.3.app ]]; then
-  export XCODE_ROOT=/Applications/Xcode_15.3.app
-  sudo xcode-select -s "$XCODE_ROOT"
-elif [[ -d /Applications/Xcode_15.2.app ]]; then
-  export XCODE_ROOT=/Applications/Xcode_15.2.app
-  sudo xcode-select -s "$XCODE_ROOT"
-elif [[ -d /Applications/Xcode_15.1.app ]]; then
-  export XCODE_ROOT=/Applications/Xcode_15.1.app
-  sudo xcode-select -s "$XCODE_ROOT"
-elif [[ -d /Applications/Xcode_15.0.app ]]; then
-  export XCODE_ROOT=/Applications/Xcode_15.0.app
-  sudo xcode-select -s "$XCODE_ROOT"
 else
   export XCODE_ROOT=/Applications/Xcode.app
 fi
 
 if [[ "$MACOS_ARCH" = "x86_64" ]]; then
-  export CNINJA_TOOLCHAIN=macos-release-10.15
   export CMAKE_OSX_ARCH=x86_64
 else
-  export CNINJA_TOOLCHAIN=macos-release-11.0
   export CMAKE_OSX_ARCH=arm64
 fi
 
-xcrun /usr/local/bin/cninja -S "$PWD" -B build "$CNINJA_TOOLCHAIN" -- \
+xcrun /usr/local/bin/cninja -S "$PWD" -B build \
+  macos-release-12.0 -- \
   -DOSSIA_SDK="$OSSIA_SDK" \
   -DCMAKE_INSTALL_PREFIX="$PWD/install" \
   -DCMAKE_UNITY_BUILD=1 \
