fd840576d12217323459b83a545d6ee1515b03c9
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-03-17 16:35:58 +0100

    appiamge: generate appimages in CI

--- ci/appimage.deploy.sh
+++ ci/appimage.deploy.sh
@@ -2,4 +2,4 @@
 export TAG="$GITTAGNOV"
 
 mv "Score.AppImage" "$BUILD_ARTIFACTSTAGINGDIRECTORY/ossia score-$TAG-linux-$CPU_ARCH.AppImage"
-mv "linux-sdk.zip" "$BUILD_ARTIFACTSTAGINGDIRECTORY/"
+mv "linux-sdk.zip" "$BUILD_ARTIFACTSTAGINGDIRECTORY/linux-sdk-$CPU_ARCH.zip"
