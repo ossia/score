68f6640516d10c06966baef2efb91a80daafe5f4
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-04-13 23:49:55 -0400

    gfx: rework vertical order

--- src/plugins/score-plugin-gfx/Gfx/Graph/ScreenNode.cpp
+++ src/plugins/score-plugin-gfx/Gfx/Graph/ScreenNode.cpp
@@ -578,7 +578,11 @@ public:
 
       void main()
       {
+#if defined(QSHADER_SPIRV)
+        fragColor = texture(tex, vec2(v_texcoord.x, 1. - v_texcoord.y));
+#else
         fragColor = texture(tex, vec2(v_texcoord.x, v_texcoord.y));
+#endif
       }
       )_";
 
