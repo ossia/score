a4a00ae86e4473f5f2cda99a7ca7d824627c7670
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-04-30 19:36:07 -0400

    vstpuppet: fix on msvc

--- ci/mingw.deps.sh
+++ ci/mingw.deps.sh
@@ -2,17 +2,13 @@
 
 # Done in the GH action for caching:
 # pacboy -S --needed --noconfirm \
-#     cmake:p ninja:p toolchain:p \
-#     qt6-base:p qt6-declarative:p qt6-websockets:p qt6-serialport:p \
-#     qt6-shadertools:p qt6-scxml:p qt6-tools:p \
-#     boost:p portaudio:p fftw:p ffmpeg:p \
-#     SDL2:p
+#     cmake:p ninja:p toolchain:p cppwinrt:p qt6-base:p qt6-declarative:p qt6-websockets:p qt6-serialport:p qt6-shadertools:p qt6-scxml:p qt6-tools:p boost:p portaudio:p fftw:p ffmpeg:p SDL2:p
 
 # TODO:
 # jack2:p   not available on clang?
 # lv2 suil lilv
 
 
-# if c++ --version | grep clang; then 
+# if c++ --version | grep clang; then
   source ci/common.deps.sh
 #fi
