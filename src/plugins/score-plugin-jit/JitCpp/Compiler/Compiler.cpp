06d1fc8ff19b8b6a15309aa60057a9b4fe1910da
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-07-19 16:19:36 -0400

    jit: port to jitlink to enable more jit features

--- src/plugins/score-plugin-jit/JitCpp/Compiler/Compiler.cpp
+++ src/plugins/score-plugin-jit/JitCpp/Compiler/Compiler.cpp
@@ -5,6 +5,12 @@
 #include "SectionMemoryManager.cpp"
 #endif
 
+// Add JITLink-specific headers
+#include <llvm/ExecutionEngine/JITLink/EHFrameSupport.h>
+#include <llvm/ExecutionEngine/JITLink/JITLink.h>
+#include <llvm/ExecutionEngine/Orc/EHFrameRegistrationPlugin.h>
+#include <llvm/ExecutionEngine/Orc/ObjectLinkingLayer.h>
+
 namespace Jit
 {
 // TODO investigate https://stackoverflow.com/questions/1839965/dynamically-creating-functions-in-c
@@ -22,7 +28,8 @@ static void jitAtExit(void (*f)())
 
 void setTargetOptions(llvm::TargetOptions& opts)
 {
-  opts.EmulatedTLS = false;
+  opts.EmulatedTLS = true;
+
 #if LLVM_VERSION_MAJOR < 17
   opts.ExplicitEmulatedTLS = false;
 #endif
@@ -32,12 +39,161 @@ void setTargetOptions(llvm::TargetOptions& opts)
   opts.NoTrappingFPMath = true;
   opts.NoSignedZerosFPMath = true;
   opts.HonorSignDependentRoundingFPMathOption = false;
-  opts.EnableIPRA = true;
+  opts.EnableIPRA = false;
   opts.setFPDenormalMode(llvm::DenormalMode::getPositiveZero());
   opts.setFP32DenormalMode(llvm::DenormalMode::getPositiveZero());
+  opts.EnableFastISel = false;
+  opts.EnableGlobalISel = false;
+}
 }
 
-static std::unique_ptr<llvm::orc::LLJIT> jitBuilder()
+namespace Jit
+{
+
+// Custom ObjectLinkingLayer plugin for handling atexit and other runtime functions
+class RuntimeInterposePlugin : public llvm::orc::ObjectLinkingLayer::Plugin
+{
+public:
+  RuntimeInterposePlugin(llvm::orc::MangleAndInterner& mangler)
+      : m_mangler(mangler)
+  {
+  }
+
+  void modifyPassConfig(
+      llvm::orc::MaterializationResponsibility& MR, llvm::jitlink::LinkGraph& G,
+      llvm::jitlink::PassConfiguration& Config) override
+  {
+    // Add a pass to handle runtime interposes
+    Config.PreFixupPasses.push_back([this](llvm::jitlink::LinkGraph& G) -> llvm::Error {
+      // Look for calls to atexit and redirect them
+      for(auto* Sym : G.external_symbols())
+      {
+        if(Sym->getName() == m_mangler("atexit"))
+        {
+          // Create or find our jitAtExit symbol
+          auto& atExitSym = G.addAbsoluteSymbol(
+              "_jitAtExit", llvm::orc::ExecutorAddr::fromPtr(&jitAtExit),
+              0, // size
+              llvm::jitlink::Linkage::Strong, llvm::jitlink::Scope::Default,
+              false // not callable
+          );
+
+          // Redirect references
+          for(auto* B : G.blocks())
+          {
+            for(auto& E : B->edges())
+            {
+              if(&E.getTarget() == Sym)
+              {
+                E.setTarget(atExitSym);
+              }
+            }
+          }
+        }
+      }
+      return llvm::Error::success();
+    });
+  }
+
+  llvm::Error notifyFailed(llvm::orc::MaterializationResponsibility& MR) override
+  {
+    qDebug() << "JITLink failed for " + MR.getTargetJITDylib().getName();
+    return llvm::Error::success();
+  }
+
+  llvm::Error
+  notifyRemovingResources(llvm::orc::JITDylib& JD, llvm::orc::ResourceKey K) override
+  {
+    return llvm::Error::success();
+  }
+
+  void notifyTransferringResources(
+      llvm::orc::JITDylib& JD, llvm::orc::ResourceKey DstKey,
+      llvm::orc::ResourceKey SrcKey) override
+  {
+  }
+
+private:
+  llvm::orc::MangleAndInterner& m_mangler;
+};
+
+class EagerLinkingPlugin : public llvm::orc::ObjectLinkingLayer::Plugin
+{
+public:
+  explicit EagerLinkingPlugin(llvm::orc::ExecutionSession& ES)
+      : ES{ES}
+  {
+  }
+
+  void modifyPassConfig(
+      llvm::orc::MaterializationResponsibility& MR, llvm::jitlink::LinkGraph& G,
+      llvm::jitlink::PassConfiguration& Config) override
+  {
+
+    // Add a pass that collects all external symbols
+    Config.PreFixupPasses.push_back(
+        [this, &MR](llvm::jitlink::LinkGraph& G) -> llvm::Error {
+      // Collect external symbols that need resolution
+      llvm::orc::SymbolLookupSet ExternalSymbols;
+
+      for(auto* Sym : G.external_symbols())
+      {
+        if((*Sym->getName()).starts_with("__lljit"))
+          continue;
+        if((*Sym->getName()).starts_with("atexit"))
+          continue;
+        ExternalSymbols.add(ES.intern(*Sym->getName()));
+      }
+
+      if(!ExternalSymbols.empty())
+      {
+        // Force resolution of external symbols now
+        auto SearchOrder = makeJITDylibSearchOrder(&MR.getTargetJITDylib());
+        auto Result = ES.lookup(SearchOrder, ExternalSymbols);
+
+        if(!Result)
+        {
+          return Result.takeError();
+        }
+
+        //// Apply the resolved addresses
+        for(auto* Sym : G.external_symbols())
+        {
+          auto It = Result->find(ES.intern(*Sym->getName()));
+          if(It != Result->end())
+          {
+
+            //Sym->setAddress(It->second.getAddress());
+          }
+        }
+      }
+
+      return llvm::Error::success();
+    });
+  }
+
+  llvm::Error notifyFailed(llvm::orc::MaterializationResponsibility& MR) override
+  {
+    qDebug() << "JITLink failed for " + MR.getTargetJITDylib().getName();
+    return llvm::Error::success();
+  }
+
+  llvm::Error
+  notifyRemovingResources(llvm::orc::JITDylib& JD, llvm::orc::ResourceKey K) override
+  {
+    return llvm::Error::success();
+  }
+
+  void notifyTransferringResources(
+      llvm::orc::JITDylib& JD, llvm::orc::ResourceKey DstKey,
+      llvm::orc::ResourceKey SrcKey) override
+  {
+  }
+
+  llvm::orc::ExecutionSession& ES;
+};
+
+static std::unique_ptr<llvm::orc::LLJIT> jitBuilder(JitCompiler& self)
 {
   auto JTMB = llvm::orc::JITTargetMachineBuilder::detectHost();
   SCORE_ASSERT(JTMB);
@@ -49,11 +205,29 @@ static std::unique_ptr<llvm::orc::LLJIT> jitBuilder()
 #else
   JTMB->setCodeGenOptLevel(llvm::CodeGenOptLevel::Aggressive);
 #endif
-  
+
   setTargetOptions(JTMB->getOptions());
 
   builder.setJITTargetMachineBuilder(std::move(*JTMB));
   builder.setNumCompileThreads(4);
+
+  // Configure to use JITLink via ObjectLinkingLayer
+  builder.setObjectLinkingLayerCreator(
+      [&](llvm::orc::ExecutionSession& ES, const llvm::Triple& TT) {
+    // Create ObjectLinkingLayer with JITLink
+    auto ObjLinkingLayer
+        = std::make_unique<llvm::orc::ObjectLinkingLayer>(ES, *self.m_memmgr);
+
+    ObjLinkingLayer->addPlugin(std::make_shared<RuntimeInterposePlugin>(self.m_mangler));
+    ObjLinkingLayer->addPlugin(std::make_shared<EagerLinkingPlugin>(ES));
+
+    // crash in deregisterEHFrames
+    //ObjLinkingLayer->addPlugin(
+    //    std::make_shared<llvm::orc::EHFrameRegistrationPlugin>(
+    //        ES, std::make_unique<llvm::jitlink::InProcessEHFrameRegistrar>()));
+
+    return ObjLinkingLayer;
+  });
 
   auto p = builder.create();
   SCORE_ASSERT(p);
@@ -62,8 +236,10 @@ static std::unique_ptr<llvm::orc::LLJIT> jitBuilder()
   SCORE_ASSERT(p.get());
   return std::move(p.get());
 }
+
 JitCompiler::JitCompiler()
-    : m_jit{jitBuilder()}
+    : m_memmgr{std::move(*llvm::jitlink::InProcessMemoryManager::Create())}
+    , m_jit{jitBuilder(*this)}
     , m_mangler{m_jit->getExecutionSession(), m_jit->getDataLayout()}
 {
   using namespace llvm;
@@ -80,42 +256,6 @@ JitCompiler::JitCompiler()
     });
   });
   auto& JD = JIT.getMainJITDylib();
-
-  {
-    llvm::orc::SymbolMap RuntimeInterposes;
-
-#if LLVM_VERSION_MAJOR < 17
-#define toAddress pointerToJITTargetAddress
-#else
-    static const auto toAddress = [](auto ptr) {
-      auto res = pointerToJITTargetAddress(&jitAtExit);
-      return llvm::orc::ExecutorAddr(res);
-    };
-#endif
-
-    RuntimeInterposes[m_mangler("atexit")] = llvm::orc::SymbolMap::mapped_type{
-        toAddress(&jitAtExit), JITSymbolFlags::Exported};
-
-#if defined(_WIN64)
-    RuntimeInterposes[m_mangler("fprintf")]
-        = {toAddress(&::fprintf), JITSymbolFlags::Exported};
-    RuntimeInterposes[m_mangler("vfprintf")]
-        = {toAddress(&::vfprintf), JITSymbolFlags::Exported};
-    RuntimeInterposes[m_mangler("__mingw_vfprintf")]
-        = {toAddress(&::vfprintf), JITSymbolFlags::Exported};
-
-    // RuntimeInterposes[m_mangler("_CxxThrowException")] = {toAddress(&SEHFrameHandler::RaiseSEHException), JITSymbolFlags::Exported};
-#endif
-
-    if(!RuntimeInterposes.empty())
-    {
-      auto s = absoluteSymbols(std::move(RuntimeInterposes));
-      (void)JD.define(std::move(s));
-    }
-  }
-
-  (void)m_overrides.enable(JD, m_mangler);
-
   {
     auto gen = DynamicLibrarySearchGenerator::GetForCurrentProcess(
         m_jit->getDataLayout().getGlobalPrefix(),
