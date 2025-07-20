d60c1001d7cb7228c2f556677d30588678ef95d7
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-07-20 15:00:18 -0400

    ubuntu: try to fix gcc build due to OOM on github actions

--- ci/ubuntu.plucky.deps.sh
+++ ci/ubuntu.plucky.deps.sh
@@ -4,13 +4,22 @@ source ci/common.setup.sh
 
 echo 'debconf debconf/frontend select Noninteractive' | $SUDO debconf-set-selections
 
+# Plucky has clang-20.1.2 which has an unfixed crash when building LLFIO.
+# It is necessary to download clang from upstream which has
+# 20.1.8 with the crash fixed.
+$SUDO apt-get update -qq
+$SUDO apt-get install -qq --force-yes wget lsb-release software-properties-common gnupg
+wget -nv https://apt.llvm.org/llvm.sh
+chmod +x llvm.sh
+$SUDO ./llvm.sh 20
+
 $SUDO apt-get update -qq
 $SUDO apt-get install -y \
     --allow-change-held-packages \
     --allow-downgrades \
     --allow-remove-essential \
     --allow-unauthenticated \
-     binutils gcc g++ clang lld \
+     binutils gcc g++ \
      software-properties-common wget \
      libasound-dev \
      ninja-build cmake \
