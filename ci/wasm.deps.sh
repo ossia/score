fcaf36f74b858e59b34a3f79183e64814e895bc0
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-07-29 23:43:09 -0400

    wasm: more fixes

--- ci/wasm.deps.sh
+++ ci/wasm.deps.sh
@@ -5,11 +5,11 @@ pacman -Syyu --noconfirm
 pacman -S wget ninja cmake which python tar git double-conversion libb2 icu glib2 pcre2 --noconfirm
 
 export SDK_ARCHIVE=sdk-wasm.tar.xz
-wget -nv https://github.com/ossia/sdk/releases/download/sdk27/$SDK_ARCHIVE -O $SDK_ARCHIVE
+wget -nv https://github.com/ossia/sdk/releases/download/sdk33/$SDK_ARCHIVE -O $SDK_ARCHIVE
 
-wget -nv https://github.com/ossia/sdk/releases/download/sdk25/icu71.tar.gz -O icu71.tar.gz
-tar xaf icu71.tar.gz
-mv libicu* /usr/lib/
+#wget -nv https://github.com/ossia/sdk/releases/download/sdk25/icu71.tar.gz -O icu71.tar.gz
+#tar xaf icu71.tar.gz
+#mv libicu* /usr/lib/
 
 mkdir -p /opt/ossia-sdk-wasm
 chown -R $(whoami) /opt/ossia-sdk-wasm
