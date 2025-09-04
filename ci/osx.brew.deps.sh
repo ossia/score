96e8f42d6f51593ceef11b4fe6a967dbbebb8fd6
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-09-04 15:27:56 +0200

    ci: fix failing macOS CI builds due to actions/runner-images#12912

--- ci/osx.brew.deps.sh
+++ ci/osx.brew.deps.sh
@@ -3,7 +3,8 @@
 set +e
 
 export HOMEBREW_NO_AUTO_UPDATE=1
-brew install ninja qt boost cmake ffmpeg fftw portaudio jack sdl lv2 lilv suil freetype
-brew uninstall --ignore-dependencies qt@5
+brew update && (brew list cmake || brew install cmake)
+brew install ninja qt boost ffmpeg fftw portaudio jack sdl lv2 lilv suil freetype
+brew uninstall --ignore-dependencies qt@5 || true
 
 source ci/common.deps.sh
