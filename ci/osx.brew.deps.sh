708bb05cb0ee56ae8a958aa22d0dcc6cc5a30599
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-09-11 13:29:54 -0400

    ci: macos: try to forceffmpeg 7? seems like ffmpeg 8 does not export symbols

--- ci/osx.brew.deps.sh
+++ ci/osx.brew.deps.sh
@@ -4,7 +4,7 @@ set +e
 
 export HOMEBREW_NO_AUTO_UPDATE=1
 brew update && (brew list cmake || brew install cmake)
-brew install ninja qt boost ffmpeg fftw portaudio jack sdl lv2 lilv suil freetype
+brew install ninja qt boost ffmpeg@7 fftw portaudio jack sdl lv2 lilv suil freetype
 brew uninstall --ignore-dependencies qt@5 || true
 
 source ci/common.deps.sh
