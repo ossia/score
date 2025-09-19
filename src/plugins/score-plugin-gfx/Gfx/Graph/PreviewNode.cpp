535c0ce5120866bd5e71bc587e373a9723b78294
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2025-09-19 11:34:08 -0400

    rhi: js: fix texturesource being inverted in metal

--- src/plugins/score-plugin-gfx/Gfx/Graph/PreviewNode.cpp
+++ src/plugins/score-plugin-gfx/Gfx/Graph/PreviewNode.cpp
@@ -157,12 +157,129 @@ public:
   void release(score::gfx::RenderList&) override { }
 };
 
+class PreviewRendererInvertY final : public score::gfx::OutputNodeRenderer
+{
+  score::gfx::TextureRenderTarget m_inputTarget;
+  score::gfx::TextureRenderTarget m_renderTarget;
+
+  QShader m_vertexS, m_fragmentS;
+
+  std::vector<score::gfx::Sampler> m_samplers;
+
+  score::gfx::Pipeline m_p;
+
+  score::gfx::MeshBuffers m_mesh{};
+
+public:
+  explicit PreviewRendererInvertY(
+      const score::gfx::Node& n, score::gfx::TextureRenderTarget rt)
+      : score::gfx::OutputNodeRenderer{n}
+      , m_renderTarget{std::move(rt)}
+  {
+  }
+
+  score::gfx::TextureRenderTarget
+  renderTargetForInput(const score::gfx::Port& p) override
+  {
+    return m_inputTarget;
+  }
+
+  void init(score::gfx::RenderList& renderer, QRhiResourceUpdateBatch& res) override
+  {
+    m_inputTarget = score::gfx::createRenderTarget(
+        renderer.state, QRhiTexture::Format::RGBA8, m_renderTarget.texture->pixelSize(),
+        renderer.samples(), renderer.requiresDepth());
+
+    const auto& mesh = renderer.defaultTriangle();
+    m_mesh = renderer.initMeshBuffer(mesh, res);
+
+    // Note that unlike the "InvertYRenderer", here we leverage the
+    // coordinate inversion inherent to metal rendering (e.g. y direction is
+    // different in viewport vs texture, thus displaying a texture "inverts" it)
+    static const constexpr auto gl_filter = R"_(#version 450
+    layout(location = 0) in vec2 v_texcoord;
+    layout(location = 0) out vec4 fragColor;
+
+    layout(binding = 3) uniform sampler2D tex;
+
+    void main()
+    {
+      fragColor = texture(tex, vec2(v_texcoord.x,  v_texcoord.y));
+    }
+    )_";
+    std::tie(m_vertexS, m_fragmentS)
+        = score::gfx::makeShaders(renderer.state, mesh.defaultVertexShader(), gl_filter);
+
+    // Put the input texture, where all the input nodes are rendering, in a sampler.
+    {
+      auto sampler = renderer.state.rhi->newSampler(
+          QRhiSampler::Linear, QRhiSampler::Linear, QRhiSampler::None,
+          QRhiSampler::ClampToEdge, QRhiSampler::ClampToEdge);
+
+      sampler->setName("FullScreenImageNode::sampler");
+      sampler->create();
+
+      m_samplers.push_back({sampler, this->m_inputTarget.texture});
+    }
+
+    m_p = score::gfx::buildPipeline(
+        renderer, mesh, m_vertexS, m_fragmentS, m_renderTarget, nullptr, nullptr,
+        m_samplers);
+  }
+  void update(
+      score::gfx::RenderList& renderer, QRhiResourceUpdateBatch& res,
+      score::gfx::Edge* edge) override
+  {
+  }
+  void release(score::gfx::RenderList&) override
+  {
+    m_p.release();
+    for(auto& s : m_samplers)
+    {
+      delete s.sampler;
+    }
+    m_samplers.clear();
+    m_inputTarget.release();
+  }
+
+  void finishFrame(
+      score::gfx::RenderList& renderer, QRhiCommandBuffer& cb,
+      QRhiResourceUpdateBatch*& res) override
+  {
+    cb.beginPass(m_renderTarget.renderTarget, Qt::black, {1.0f, 0}, res);
+    res = nullptr;
+    {
+      const auto sz = renderer.state.renderSize;
+      cb.setGraphicsPipeline(m_p.pipeline);
+      cb.setShaderResources(m_p.srb);
+      cb.setViewport(QRhiViewport(0, 0, sz.width(), sz.height()));
+
+      const auto& mesh = renderer.defaultTriangle();
+      mesh.draw(this->m_mesh, cb);
+    }
+    cb.endPass(nullptr);
+  }
+};
+
 score::gfx::OutputNodeRenderer*
 PreviewNode::createRenderer(score::gfx::RenderList& r) const noexcept
 {
   score::gfx::TextureRenderTarget rt{
       m_texture, nullptr, nullptr, m_renderState->renderPassDescriptor, m_renderTarget};
-  return new score::gfx::PreviewRenderer{*this, rt};
+  switch(r.state.api)
+  {
+    default:
+    case score::gfx::GraphicsApi::OpenGL:
+    case score::gfx::GraphicsApi::Vulkan:
+      return new score::gfx::PreviewRenderer{*this, rt};
+      break;
+    case score::gfx::GraphicsApi::Metal:
+    case score::gfx::GraphicsApi::D3D11:
+    case score::gfx::GraphicsApi::D3D12:
+      return new score::gfx::PreviewRendererInvertY{*this, rt};
+      break;
+  }
+  return nullptr;
 }
 
 }
