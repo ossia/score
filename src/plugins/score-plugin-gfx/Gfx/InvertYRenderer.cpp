65b3b86e9073187e5362b9d884d30fb6a839c4f6
Jean-Michaël Celerier <jeanmichael.celerier+github@gmail.com>
2025-12-11 22:25:17 -0500

    spout: d3d11/12 works as expected

--- src/plugins/score-plugin-gfx/Gfx/InvertYRenderer.cpp
+++ src/plugins/score-plugin-gfx/Gfx/InvertYRenderer.cpp
@@ -17,6 +17,7 @@ InvertYRenderer::InvertYRenderer(
 void InvertYRenderer::init(
     score::gfx::RenderList& renderer, QRhiResourceUpdateBatch& res)
 {
+  // FIXME RGBA32F for hdr ?
   m_renderTarget = score::gfx::createRenderTarget(
       renderer.state, QRhiTexture::Format::RGBA8, m_inputTarget.texture->pixelSize(),
       renderer.samples(), renderer.requiresDepth(*this->node.input[0]));
@@ -98,5 +99,124 @@ void InvertYRenderer::finishFrame(
 
   next = nullptr;
 }
+
+
+
+ScaledRenderer::ScaledRenderer(score::gfx::TextureRenderTarget outputTarget, const score::gfx::RenderState &state, const score::gfx::Node &parent)
+    : score::gfx::OutputNodeRenderer{parent}
+    , m_renderTarget{outputTarget}
+{
+}
+
+ScaledRenderer::~ScaledRenderer() { }
+
+void ScaledRenderer::init(score::gfx::RenderList &renderer, QRhiResourceUpdateBatch &res)
+{
+  // FIXME RGBA32F for hdr ?
+  m_inputTarget = score::gfx::createRenderTarget(
+      renderer.state, QRhiTexture::Format::RGBA8, renderer.state.renderSize,
+      renderer.samples(), renderer.requiresDepth(*this->node.input[0]));
+
+  const auto& mesh = renderer.defaultTriangle();
+  m_mesh = renderer.initMeshBuffer(mesh, res);
+  static const constexpr auto gl_filter = R"_(#version 450
+      layout(location = 0) in vec2 v_texcoord;
+      layout(location = 0) out vec4 fragColor;
+
+      layout(binding = 3) uniform sampler2D tex;
+
+      void main()
+      {
+#if defined(QSHADER_SPIRV)
+        fragColor = texture(tex, vec2(v_texcoord.x, 1. - v_texcoord.y));
+#else
+        fragColor = texture(tex, vec2(v_texcoord.x, v_texcoord.y));
+#endif
+      }
+      )_";
+
+  std::tie(m_vertexS, m_fragmentS)
+      = score::gfx::makeShaders(renderer.state, mesh.defaultVertexShader(), gl_filter);
+
+  m_samplers = {};
+  // Put the input texture, where all the input nodes are rendering, in a sampler.
+  {
+    auto sampler = renderer.state.rhi->newSampler(
+        QRhiSampler::Linear, QRhiSampler::Linear, QRhiSampler::None,
+        QRhiSampler::ClampToEdge, QRhiSampler::ClampToEdge);
+
+    sampler->setName("FullScreenImageNode::sampler");
+    sampler->create();
+
+    m_samplers[0] = {sampler, this->m_inputTarget.texture};
+  }
+
+  m_p = score::gfx::buildPipeline(
+      renderer, mesh, m_vertexS, m_fragmentS, m_renderTarget, nullptr, nullptr,
+      m_samplers);
+}
+
+void ScaledRenderer::update(score::gfx::RenderList &renderer, QRhiResourceUpdateBatch &res, score::gfx::Edge *edge) {
+}
+
+void ScaledRenderer::runRenderPass(score::gfx::RenderList &, QRhiCommandBuffer &commands, score::gfx::Edge &e)
+{
+  // m_rt.renderTarget = parent.m_swapChain->currentFrameRenderTarget();
+  // m_rt.renderPass = state->renderPassDescriptor;
+}
+
+void ScaledRenderer::finishFrame(score::gfx::RenderList &renderer, QRhiCommandBuffer &cb, QRhiResourceUpdateBatch *&res)
+{
+  cb.beginPass(m_renderTarget.renderTarget, Qt::black, {1.0f, 0}, res);
+  res = nullptr;
+  {
+    const auto sz = renderer.state.outputSize;
+
+    cb.setGraphicsPipeline(m_p.pipeline);
+    cb.setShaderResources(m_p.srb);
+    cb.setViewport(QRhiViewport(0, 0, sz.width(), sz.height()));
+
+    const auto& mesh = renderer.defaultTriangle();
+    mesh.draw(this->m_mesh, cb);
+  }
+  cb.endPass();
+}
+
+void ScaledRenderer::release(score::gfx::RenderList &)
+{
+  m_p.release();
+  m_inputTarget.release();
+  for(auto& s : m_samplers)
+  {
+    delete s.sampler;
+  }
+  m_samplers = {};
+  //m_renderTarget.release();
+}
+
+score::gfx::TextureRenderTarget ScaledRenderer::renderTargetForInput(const score::gfx::Port &p)
+{
+  return m_inputTarget;
+}
+
+
+score::gfx::TextureRenderTarget BasicRenderer::renderTargetForInput(const score::gfx::Port &p) { return m_rt; }
+
+BasicRenderer::BasicRenderer(score::gfx::TextureRenderTarget outputTarget, const score::gfx::RenderState &state, const score::gfx::Node &parent)
+    : score::gfx::OutputNodeRenderer{parent}
+    , m_rt{outputTarget}
+{
+}
+
+BasicRenderer::~BasicRenderer() { }
+
+void BasicRenderer::init(score::gfx::RenderList &renderer, QRhiResourceUpdateBatch &res) { }
+
+void BasicRenderer::update(score::gfx::RenderList &renderer, QRhiResourceUpdateBatch &res, score::gfx::Edge *edge) {
+}
+
+void BasicRenderer::runRenderPass(score::gfx::RenderList &, QRhiCommandBuffer &commands, score::gfx::Edge &e) { }
+
+void BasicRenderer::release(score::gfx::RenderList &) { }
 
 }
