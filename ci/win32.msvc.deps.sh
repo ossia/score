dd309497194d9c8755610aa5d57fb0eb7e042956
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2026-01-04 12:27:34 -0500

    ci: try to fix msvc build

--- ci/win32.msvc.deps.sh
+++ ci/win32.msvc.deps.sh
@@ -10,6 +10,12 @@ cd /c/ossia-sdk-msvc
 curl -L https://github.com/ossia/sdk/releases/download/sdk25/sdk-msvc.7z --output sdk-msvc.7z
 7z x sdk-msvc.7z
 rm sdk-msvc.7z
+
+curl -L https://github.com/ossia/sdk/releases/download/sdk31/boost_1_90_0.tar.gz --output boost.tar.gz
+tar -xzf boost.tar.gz
+rm boost.tar.gz
+mv boost_1_90_0 boost
+
 ls
 )
 
