fe09e203e59b675f4c4a0963509ed278b531600a
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2026-01-11 18:50:47 -0500

    ci: update to sdk35 for switch to ysfx from @JoepVanlier

--- ci/win32.deps.sh
+++ ci/win32.deps.sh
@@ -8,7 +8,7 @@ choco install -y nsis
 set -x
 mkdir /c/ossia-sdk-x86_64
 cd /c/ossia-sdk-x86_64
-curl -L https://github.com/ossia/sdk/releases/download/sdk34/sdk-mingw-x86_64.7z --output sdk-mingw-x86_64.7z
+curl -L https://github.com/ossia/sdk/releases/download/sdk35/sdk-mingw-x86_64.7z --output sdk-mingw-x86_64.7z
 7z x sdk-mingw-x86_64.7z
 rm sdk-mingw-x86_64.7z
 ls
