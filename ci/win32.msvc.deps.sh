b61aa7493b8c807eafa1b6f731295540e396f321
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2026-01-19 22:04:10 -0500

    msvc: update sdk

--- ci/win32.msvc.deps.sh
+++ ci/win32.msvc.deps.sh
@@ -7,7 +7,7 @@ choco install -y rsync
 set -x
 mkdir /c/ossia-sdk-msvc
 cd /c/ossia-sdk-msvc
-curl -L https://github.com/ossia/sdk/releases/download/sdk25/sdk-msvc.7z --output sdk-msvc.7z
+curl -L https://github.com/ossia/sdk/releases/download/sdk35/sdk-msvc-x86_64.7z --output sdk-msvc.7z
 7z x sdk-msvc.7z
 rm sdk-msvc.7z
 ls
