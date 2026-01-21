358df1deb5645323bfe900c22b3e28cff4af2c26
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2026-01-20 19:04:32 -0500

    windows: handle more video pixel formats

--- src/plugins/score-plugin-gfx/Gfx/CameraDevice.win32.cpp
+++ src/plugins/score-plugin-gfx/Gfx/CameraDevice.win32.cpp
@@ -16,35 +16,124 @@ extern "C" {
 #include <dvdmedia.h>
 #include <wmcodecdsp.h>
 #include <oleauto.h>
+
+static constexpr const GUID MEDIASUBTYPE_Y800             = { 0x30303859, 0x0000, 0x0010, { 0x80, 0x00, 0x00, 0xaa, 0x00, 0x38, 0x9b, 0x71}};
+static constexpr const GUID MEDIASUBTYPE_MICROSOFT_IR8    = { 0x00000032, 0x0000, 0x0010, { 0x80, 0x00, 0x00, 0xaa, 0x00, 0x38, 0x9b, 0x71}};
+static constexpr const GUID MEDIASUBTYPE_Y8               = { 0x20203859, 0x0000, 0x0010, { 0x80, 0x00, 0x00, 0xaa, 0x00, 0x38, 0x9b, 0x71}};
+static constexpr const GUID MEDIASUBTYPE_Z16              = { 0x2036315A, 0x0000, 0x0010, { 0x80, 0x00, 0x00, 0xaa, 0x00, 0x38, 0x9b, 0x71}};
+static constexpr const GUID MEDIASUBTYPE_Y160             = { 0x30363159, 0x0000, 0x0010, { 0x80, 0x00, 0x00, 0xaa, 0x00, 0x38, 0x9b, 0x71}};
+static constexpr const GUID MEDIASUBTYPE_YV16             = { 0x32315659, 0x0000, 0x0010, { 0x80, 0x00, 0x00, 0xaa, 0x00, 0x38, 0x9b, 0x71}};
+static constexpr const GUID MEDIASUBTYPE_Y422             = { 0x32323459, 0x0000, 0x0010, { 0x80, 0x00, 0x00, 0xaa, 0x00, 0x38, 0x9b, 0x71}};
+static constexpr const GUID MEDIASUBTYPE_GREY             = { 0x59455247, 0x0000, 0x0010, { 0x80, 0x00, 0x00, 0xaa, 0x00, 0x38, 0x9b, 0x71}};
 namespace Gfx
 {
 
 static int guidToPixelFormat(const GUID& subtype)
 {
-  if(subtype == MEDIASUBTYPE_YUY2)
-    return AV_PIX_FMT_YUYV422;
-  else if(subtype == MEDIASUBTYPE_RGB24)
+  if(subtype == MEDIASUBTYPE_RGB24)
     return AV_PIX_FMT_BGR24;
   else if(subtype == MEDIASUBTYPE_RGB32)
     return AV_PIX_FMT_BGR0;
+  else if(subtype == MEDIASUBTYPE_ARGB32)
+    return AV_PIX_FMT_BGRA;
+  else if(subtype == MEDIASUBTYPE_RGB565)
+    return AV_PIX_FMT_BGR565LE;
+  else if(subtype == MEDIASUBTYPE_RGB555)
+    return AV_PIX_FMT_BGR555LE;
+  else if(subtype == MEDIASUBTYPE_ARGB1555)
+    return AV_PIX_FMT_BGR555LE;
+  else if(subtype == MEDIASUBTYPE_RGB8)
+    return AV_PIX_FMT_PAL8;
+  else if(subtype == MEDIASUBTYPE_RGB4)
+    return AV_PIX_FMT_RGB4_BYTE;
+
+  else if (subtype == MEDIASUBTYPE_A2R10G10B10)
+    return AV_PIX_FMT_X2RGB10LE;
+  else if (subtype == MEDIASUBTYPE_A2B10G10R10)
+    return AV_PIX_FMT_X2BGR10LE;
+
+  else if(subtype == MEDIASUBTYPE_AYUV)
+    return AV_PIX_FMT_AYUV;
+  else if(subtype == MEDIASUBTYPE_NV24)
+    return AV_PIX_FMT_NV24;
+//   else if(subtype == MEDIASUBTYPE_NV42)
+//     return AV_PIX_FMT_NV42;
+//   else if(subtype == MEDIASUBTYPE_Y410)
+//     return AV_PIX_FMT_XV30LE;
+//   else if(subtype == MEDIASUBTYPE_Y416)
+//     return AV_PIX_FMT_AYUV64LE;
+
+  else if(subtype == MEDIASUBTYPE_YUY2)
+    return AV_PIX_FMT_YUYV422;
+  else if(subtype == MEDIASUBTYPE_YUYV)
+    return AV_PIX_FMT_YUYV422;
   else if(subtype == MEDIASUBTYPE_UYVY)
     return AV_PIX_FMT_UYVY422;
-  else if(subtype == MEDIASUBTYPE_I420)
-    return AV_PIX_FMT_YUV420P;
+  else if(subtype == MEDIASUBTYPE_YVYU)
+    return AV_PIX_FMT_YVYU422;
+
   else if(subtype == MEDIASUBTYPE_IYUV)
+    return AV_PIX_FMT_YUV420P;
+  else if(subtype == MEDIASUBTYPE_I420)
     return AV_PIX_FMT_YUV420P;
   else if(subtype == MEDIASUBTYPE_YV12)
     return AV_PIX_FMT_YUV420P;
   else if(subtype == MEDIASUBTYPE_NV12)
     return AV_PIX_FMT_NV12;
+//  else if(subtype == MEDIASUBTYPE_NV21)
+//    return AV_PIX_FMT_NV21;
+//  else if(subtype == MEDIASUBTYPE_YV12)
+//    return AV_PIX_FMT_YUV420P;
+  else if (subtype == MEDIASUBTYPE_IMC1 || subtype == MEDIASUBTYPE_IMC2 ||
+          subtype == MEDIASUBTYPE_IMC3 || subtype == MEDIASUBTYPE_IMC4)
+    return AV_PIX_FMT_YUV420P;
+
+  else if(subtype == MEDIASUBTYPE_P010)
+    return AV_PIX_FMT_P010LE;
+  else if(subtype == MEDIASUBTYPE_P016)
+    return AV_PIX_FMT_P016LE;
+
+//  else if(subtype == MEDIASUBTYPE_NV16)
+//    return AV_PIX_FMT_NV16;
+  else if(subtype == MEDIASUBTYPE_P210)
+    return AV_PIX_FMT_P210LE;
+  else if(subtype == MEDIASUBTYPE_P216)
+    return AV_PIX_FMT_P216LE;
+
+  else if(subtype == MEDIASUBTYPE_Y210)
+    return AV_PIX_FMT_Y210LE;
+  else if(subtype == MEDIASUBTYPE_Y216)
+    return AV_PIX_FMT_Y216LE;
+  else if(subtype == MEDIASUBTYPE_V216) // not sure
+    return AV_PIX_FMT_Y216LE;
+
+/////   Not supported in dshow as of ffmpeg 8, causes hangs in avformat_find_stream_info
+  else if(subtype == MEDIASUBTYPE_Y8) {
+     return AV_PIX_FMT_GRAY8;
+  }
+  else if(subtype == MEDIASUBTYPE_Z16) {
+     return AV_PIX_FMT_GRAY16;
+  }
+  else if(subtype == MEDIASUBTYPE_Y800) {
+    return AV_PIX_FMT_GRAY8;
+  }
+  else if(subtype == MEDIASUBTYPE_MICROSOFT_IR8) {
+    return AV_PIX_FMT_GRAY8;
+  }
+  else if (subtype == MEDIASUBTYPE_YVU9 || subtype == MEDIASUBTYPE_IF09)
+    return AV_PIX_FMT_YUV410P;
+
+  else if (subtype == MEDIASUBTYPE_Y411 || subtype == MEDIASUBTYPE_Y41P)
+    return AV_PIX_FMT_UYYVYY411;
+
   else if(subtype == MEDIASUBTYPE_MJPG)
     return AV_PIX_FMT_YUVJ420P;
-  else if(subtype == MEDIASUBTYPE_RGB555)
-    return AV_PIX_FMT_BGR555LE;
-  else if(subtype == MEDIASUBTYPE_RGB565)
-    return AV_PIX_FMT_BGR565LE;
-  else if(subtype == MEDIASUBTYPE_ARGB32)
-    return AV_PIX_FMT_BGRA;
+  else if(subtype == MEDIASUBTYPE_TVMJ)
+    return AV_PIX_FMT_YUVJ420P;
+  else if(subtype == MEDIASUBTYPE_WAKE)
+    return AV_PIX_FMT_YUVJ420P;
+  else if(subtype == MEDIASUBTYPE_Plum)
+    return AV_PIX_FMT_YUVJ420P;
   else if(subtype == MEDIASUBTYPE_YVU9)
     return AV_PIX_FMT_YUV410P;
   else
@@ -52,24 +141,37 @@ static int guidToPixelFormat(const GUID& subtype)
 }
 
 static void enumerateCameraFormat(
-    const AM_MEDIA_TYPE* pmt, const VIDEOINFOHEADER* pVih,
-    const VIDEO_STREAM_CONFIG_CAPS& caps, CameraSettings& source,
+    const GUID& subtype,
+    int width,
+    int height,
+    REFERENCE_TIME avgTimePerFrame,
+    const VIDEO_STREAM_CONFIG_CAPS& caps,
+    CameraSettings& source,
     std::function<void()> f)
 {
-  const auto w = pVih->bmiHeader.biWidth;
-  const auto h = pVih->bmiHeader.biHeight;
   source.codec = AV_CODEC_ID_RAWVIDEO;
-  source.pixelformat = guidToPixelFormat(pmt->subtype);
-  if(w <= 0 || h <= 0 || source.pixelformat == AV_PIX_FMT_NONE)
+  source.pixelformat = guidToPixelFormat(subtype);
+
+  // Basic validation
+  if(width <= 0 || height <= 0 || source.pixelformat == AV_PIX_FMT_NONE)
+  {
+    // OLECHAR* guidString;
+    // StringFromCLSID(subtype, &guidString);
+    // qDebug() << "Unsupported PixelFormat GUID: " << guidString;
+    // CoTaskMemFree(guidString);
     return;
+  }
+
+  // MJPEG special case
   if(source.pixelformat == AV_PIX_FMT_YUVJ420P)
   {
     source.codec = AV_CODEC_ID_MJPEG;
     source.pixelformat = AV_PIX_FMT_NONE;
   }
 
-  source.size = {w, h};
+  source.size = {width, height};
 
+  // FPS Enumeration Logic
   if(caps.MinFrameInterval > 0 && caps.MaxFrameInterval > 0)
   {
     const auto maxFps = 10000000.0 / caps.MinFrameInterval;
@@ -88,13 +190,14 @@ static void enumerateCameraFormat(
       }
     }
   }
-  else if(pVih->AvgTimePerFrame > 0)
+  else if(avgTimePerFrame > 0)
   {
-    source.fps = 10000000.0 / pVih->AvgTimePerFrame;
+    source.fps = 10000000.0 / avgTimePerFrame;
     f();
   }
   else
   {
+    // Fallback if no timing info is available
     if(caps.MaxFrameInterval > 0)
     {
       source.fps = 10000000.0 / caps.MaxFrameInterval;
@@ -148,16 +251,33 @@ static void enumerateCameraFormats(
             hr = pStreamConfig->GetStreamCaps(i, &pmt, (BYTE*)&caps);
             if(SUCCEEDED(hr) && pmt)
             {
-              if(pmt->majortype == MEDIATYPE_Video
-                 && pmt->formattype == FORMAT_VideoInfo)
+              if(pmt->majortype == MEDIATYPE_Video)
               {
-                if(VIDEOINFOHEADER* pVih = (VIDEOINFOHEADER*)pmt->pbFormat)
+                if(pmt->formattype == FORMAT_VideoInfo)
                 {
-                  // Finally we reach the camera formats
-                  enumerateCameraFormat(pmt, pVih, caps, source, f);
+                  if(VIDEOINFOHEADER* pVih = (VIDEOINFOHEADER*)pmt->pbFormat)
+                  {
+                    enumerateCameraFormat(
+                        pmt->subtype,
+                        pVih->bmiHeader.biWidth,
+                        pVih->bmiHeader.biHeight,
+                        pVih->AvgTimePerFrame,
+                        caps, source, f);
+                  }
+                }
+                else if(pmt->formattype == FORMAT_VideoInfo2)
+                {
+                  if(VIDEOINFOHEADER2* pVih = (VIDEOINFOHEADER2*)pmt->pbFormat)
+                  {
+                    enumerateCameraFormat(
+                        pmt->subtype,
+                        pVih->bmiHeader.biWidth,
+                        pVih->bmiHeader.biHeight,
+                        pVih->AvgTimePerFrame,
+                        caps, source, f);
+                  }
                 }
               }
-
               if(pmt->cbFormat != 0)
                 CoTaskMemFree((PVOID)pmt->pbFormat);
               if(pmt->pUnk != nullptr)
