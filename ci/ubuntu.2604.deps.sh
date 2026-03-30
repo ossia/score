f7b2d18885cdb85b6d4546c9b9d380b349d1ed06
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2026-03-30 15:22:10 +0200

    ci: ubuntu: proper deps

--- ci/ubuntu.2604.deps.sh
+++ ci/ubuntu.2604.deps.sh
@@ -22,10 +22,9 @@ $SUDO apt-get install -y \
      qt6-base-dev qt6-base-dev-tools qt6-base-private-dev \
      qt6-declarative-dev qt6-declarative-dev-tools qt6-declarative-private-dev \
      qt6-scxml-dev qt6-svg-dev qt6-connectivity-dev \
-     libqt6opengl6-dev \
-     libqt6websockets6-dev \
-     libqt6serialport6-dev \
-     libqt6shadertools6-dev \
+     qt6-websockets-dev \
+     qt6-serialport-dev \
+     qt6-shadertools-dev \
      libbluetooth-dev libsdl2-dev libsdl2-2.0-0 \
      libglu1-mesa-dev libglu1-mesa libgles2-mesa-dev \
      libavahi-compat-libdnssd-dev libsamplerate0-dev \
