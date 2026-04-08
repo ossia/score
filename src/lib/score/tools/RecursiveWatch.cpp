ab68c48e10d9f753c0c0b2e06b864f00df498b63
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2026-04-08 09:45:41 -0400

    llfio: more tries

--- src/lib/score/tools/RecursiveWatch.cpp
+++ src/lib/score/tools/RecursiveWatch.cpp
@@ -17,7 +17,6 @@
   && (__cpp_lib_concepts >= 202002L) \
   && (__cpp_lib_span >= 202002L) && __has_include(<span>) \
   && !defined(__EMSCRIPTEN__) \
-  && !defined(__APPLE__) \
   && (!defined(__clang_major__) || (__clang_major__ >= 14))
 #define SCORE_HAS_LLFIO 1
 #elif defined(__APPLE__)
