d068c6a182c44b936ab5d4732cf136f2daa006f2
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2026-04-09 12:23:31 +0200

    lv2: put scanning in thread pool as it is pretty slow

--- src/plugins/score-plugin-lv2/LV2/Library.hpp
+++ src/plugins/score-plugin-lv2/LV2/Library.hpp
@@ -4,6 +4,8 @@
 #include <Library/LibraryInterface.hpp>
 #include <Library/ProcessesItemModel.hpp>
 
+#include <score/tools/ThreadPool.hpp>
+
 namespace LV2
 {
 class LibraryHandler final : public Library::LibraryInterface
@@ -12,43 +14,53 @@ class LibraryHandler final : public Library::LibraryInterface
   void setup(Library::ProcessesItemModel& model, const score::GUIApplicationContext& ctx)
       override
   {
-    const auto& key = LV2::ProcessFactory{}.concreteKey();
-    QModelIndex node = model.find(key);
-    if(node == QModelIndex{})
-    {
-      return;
-    }
-    auto& parent = *reinterpret_cast<Library::ProcessNode*>(node.internalPointer());
+    score::TaskPool::instance().post([model = QPointer{&model}, &ctx] {
+      auto& plug = ctx.applicationPlugin<LV2::ApplicationPlugin>();
+      auto& world = plug.lilv;
 
-    auto& plug = ctx.applicationPlugin<LV2::ApplicationPlugin>();
-    auto& world = plug.lilv;
+      // This part can take a few seconds so we do it in threadpool
+      auto plugs = world.get_all_plugins();
 
-    auto plugs = world.get_all_plugins();
+      ossia::flat_map<QString, QVector<QString>> categories;
+      categories.reserve(50);
 
-    ossia::flat_map<QString, QVector<QString>> categories;
-
-    auto it = plugs.begin();
-    while(!plugs.is_end(it))
-    {
-      auto plug = plugs.get(it);
-      const auto class_name = plug.get_class().get_label().as_string();
-      const auto plug_name = get_lv2_plugin_name(plug);
-      categories[class_name].push_back(plug_name);
-      it = plugs.next(it);
-    }
-
-    for(auto& category : categories)
-    {
-      // Already sorted through the map
-      auto& cat = parent.emplace_back(
-          Library::ProcessData{Process::ProcessData{{}, category.first, {}}, {}},
-          &parent);
-      for(auto& plug : category.second)
+      auto it = plugs.begin();
+      while(!plugs.is_end(it))
       {
-        Library::addToLibrary(
-            cat, Library::ProcessData{Process::ProcessData{key, plug, plug}, {}});
+        auto plug = plugs.get(it);
+        const auto class_name = plug.get_class().get_label().as_string();
+        const auto plug_name = get_lv2_plugin_name(plug);
+        categories[class_name].push_back(plug_name);
+        it = plugs.next(it);
       }
-    }
+
+      // Back to main thread
+      QMetaObject::invokeMethod(
+          QCoreApplication::instance(), [model, categories = std::move(categories)] {
+        if(!model)
+          return;
+
+        const auto& key = LV2::ProcessFactory{}.concreteKey();
+        QModelIndex node = model->find(key);
+        if(node == QModelIndex{})
+          return;
+
+        auto& parent = *reinterpret_cast<Library::ProcessNode*>(node.internalPointer());
+
+        for(auto& category : categories)
+        {
+          // Already sorted through the map
+          auto& cat = parent.emplace_back(
+              Library::ProcessData{Process::ProcessData{{}, category.first, {}}, {}},
+              &parent);
+          for(auto& plug : category.second)
+          {
+            Library::addToLibrary(
+                cat, Library::ProcessData{Process::ProcessData{key, plug, plug}, {}});
+          }
+        }
+      });
+    });
   }
 };
 }
