4dc3bdf5c5c1e9f7ff7b4019b567397c22951d99
Jean-Michaël Celerier <jeanmichael.celerier+github@gmail.com>
2026-05-14 09:18:20 -0400

    addons: allow to clone with a ref

--- ci/common.deps.sh
+++ ci/common.deps.sh
@@ -5,11 +5,18 @@ cd src/addons
 
 clone_addon() {
   local url=${1}
+  local ref=${2:-}
   local folder=$(echo "${url}" | awk -F'/' '{print $NF}')
 
   (
   if [[ ! -d "$folder" ]]; then
     git clone --recursive -j16 "$url"
+    if [[ "x${ref}" != "x" ]]; then
+    (
+      cd "$folder"
+      git checkout "${ref}"
+    )
+    fi
   else
     # Try to update the submodule if it's really super clean
     cd "$folder"
@@ -17,6 +24,12 @@ clone_addon() {
     if output=$(git status --porcelain  --untracked-files=no) && [ -z "$output" ]; then
       if output=$(git diff-index --quiet HEAD) && [ -z "$output" ]; then
         git pull || true
+        if [[ "x${ref}" != "x" ]]; then
+        (
+          cd "$folder"
+          git checkout "${ref}"
+        )
+        fi
       fi
     fi
   fi
@@ -33,20 +46,20 @@ CI_PLATFORM="${1:-DEFAULT}"
 
 if [[ "$CI_PLATFORM" != "WASM" ]];
 then
-  clone_addon https://github.com/ossia/score-addon-ltc
-  clone_addon https://github.com/ossia/score-addon-ndi
-  clone_addon https://github.com/bltzr/score-avnd-granola
-  clone_addon https://github.com/ossia/score-addon-ultraleap
+  clone_addon         https://github.com/ossia/score-addon-ltc
+  clone_addon         https://github.com/ossia/score-addon-ndi
+  clone_addon      https://github.com/bltzr/score-avnd-granola
+  clone_addon   https://github.com/ossia/score-addon-ultraleap
   clone_addon https://github.com/ossia/score-addon-contextfree
-  clone_addon https://github.com/ossia/score-addon-ble
-#  clone_addon https://github.com/ossia/score-addon-led
-  clone_addon https://github.com/ossia/score-addon-spatgris
-  clone_addon https://github.com/ossia/score-addon-hdf5
-  clone_addon https://github.com/ossia/score-addon-onnx
-  clone_addon https://github.com/ossia/score-addon-deuterium
-  clone_addon https://github.com/ossia/score-addon-airwindows
-  clone_addon https://github.com/ossia/score-addon-lsl
-  clone_addon https://github.com/jcelerier/bendage
+  clone_addon         https://github.com/ossia/score-addon-ble
+  clone_addon         https://github.com/ossia/score-addon-led
+  clone_addon    https://github.com/ossia/score-addon-spatgris
+  clone_addon        https://github.com/ossia/score-addon-hdf5
+  clone_addon        https://github.com/ossia/score-addon-onnx
+  clone_addon   https://github.com/ossia/score-addon-deuterium
+  clone_addon  https://github.com/ossia/score-addon-airwindows
+  clone_addon         https://github.com/ossia/score-addon-lsl
+  clone_addon             https://github.com/jcelerier/bendage
 fi
 
 if [[ "$CI_PLATFORM" == "LINUX" || "$CI_PLATFORM" == "WIN32" ]]; then
