b3feac0d8195d7ede5e1a1e4208d314fd45af021
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2026-06-05 11:10:09 -0400

    ci: skip tarball GPG signing when no key is available

--- ci/tarball.deploy.sh
+++ ci/tarball.deploy.sh
@@ -3,4 +3,7 @@
 export TAG=$GITTAGNOV
 
 mv "ossia-score.tar.xz" "$BUILD_ARTIFACTSTAGINGDIRECTORY/ossia score-$TAG-src.tar.xz"
-mv "ossia-score.tar.xz.asc" "$BUILD_ARTIFACTSTAGINGDIRECTORY/ossia score-$TAG-src.tar.xz.asc"
+
+if [[ -f "ossia-score.tar.xz.asc" ]]; then
+  mv "ossia-score.tar.xz.asc" "$BUILD_ARTIFACTSTAGINGDIRECTORY/ossia score-$TAG-src.tar.xz.asc"
+fi
