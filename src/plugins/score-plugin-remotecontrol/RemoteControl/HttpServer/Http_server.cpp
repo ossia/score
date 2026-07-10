cc8991377e7ed1ffbaa4f4f5131c558dd19b6d5b
thibaudk <thibaud.keller@aquilenet.fr>
2026-07-10 15:44:48 +0100

    [Http_server] re-format

--- src/plugins/score-plugin-remotecontrol/RemoteControl/HttpServer/Http_server.cpp
+++ src/plugins/score-plugin-remotecontrol/RemoteControl/HttpServer/Http_server.cpp
@@ -12,8 +12,6 @@
 
 #include <cstdlib>
 #include <iostream>
-#include <fstream>
-
 
 //------------------------------------------------------------------------------
 
@@ -29,267 +27,235 @@ Http_server::~Http_server()
 }
 
 // Return a reasonable mime type based on the extension of a file.
-beast::string_view
-Http_server::mime_type(beast::string_view path)
+beast::string_view Http_server::mime_type(beast::string_view path)
 {
-    using beast::iequals;
-    auto const ext = [&path]
-    {
-        auto const pos = path.rfind(".");
-        if(pos == beast::string_view::npos)
-            return beast::string_view{};
-        return path.substr(pos);
-    }();
-    if(iequals(ext, ".htm"))  return "text/html";
-    if(iequals(ext, ".html")) return "text/html";
-    if(iequals(ext, ".php"))  return "text/html";
-    if(iequals(ext, ".css"))  return "text/css";
-    if(iequals(ext, ".txt"))  return "text/plain";
-    if(iequals(ext, ".js"))   return "application/javascript";
-    if(iequals(ext, ".json")) return "application/json";
-    if(iequals(ext, ".xml"))  return "application/xml";
-    if(iequals(ext, ".swf"))  return "application/x-shockwave-flash";
-    if(iequals(ext, ".wasm")) return "application/wasm";
-    if(iequals(ext, ".flv"))  return "video/x-flv";
-    if(iequals(ext, ".png"))  return "image/png";
-    if(iequals(ext, ".jpe"))  return "image/jpeg";
-    if(iequals(ext, ".jpeg")) return "image/jpeg";
-    if(iequals(ext, ".jpg"))  return "image/jpeg";
-    if(iequals(ext, ".gif"))  return "image/gif";
-    if(iequals(ext, ".bmp"))  return "image/bmp";
-    if(iequals(ext, ".ico"))  return "image/vnd.microsoft.icon";
-    if(iequals(ext, ".tiff")) return "image/tiff";
-    if(iequals(ext, ".tif"))  return "image/tiff";
-    if(iequals(ext, ".svg"))  return "image/svg+xml";
-    if(iequals(ext, ".svgz")) return "image/svg+xml";
-    return "application/text";
+  using beast::iequals;
+  auto const ext = [&path]
+  {
+    auto const pos = path.rfind(".");
+    if(pos == beast::string_view::npos)
+      return beast::string_view{};
+    return path.substr(pos);
+  }();
+  if(iequals(ext, ".htm"))  return "text/html";
+  if(iequals(ext, ".html")) return "text/html";
+  if(iequals(ext, ".php"))  return "text/html";
+  if(iequals(ext, ".css"))  return "text/css";
+  if(iequals(ext, ".txt"))  return "text/plain";
+  if(iequals(ext, ".js"))   return "application/javascript";
+  if(iequals(ext, ".json")) return "application/json";
+  if(iequals(ext, ".xml"))  return "application/xml";
+  if(iequals(ext, ".swf"))  return "application/x-shockwave-flash";
+  if(iequals(ext, ".wasm")) return "application/wasm";
+  if(iequals(ext, ".flv"))  return "video/x-flv";
+  if(iequals(ext, ".png"))  return "image/png";
+  if(iequals(ext, ".jpe"))  return "image/jpeg";
+  if(iequals(ext, ".jpeg")) return "image/jpeg";
+  if(iequals(ext, ".jpg"))  return "image/jpeg";
+  if(iequals(ext, ".gif"))  return "image/gif";
+  if(iequals(ext, ".bmp"))  return "image/bmp";
+  if(iequals(ext, ".ico"))  return "image/vnd.microsoft.icon";
+  if(iequals(ext, ".tiff")) return "image/tiff";
+  if(iequals(ext, ".tif"))  return "image/tiff";
+  if(iequals(ext, ".svg"))  return "image/svg+xml";
+  if(iequals(ext, ".svgz")) return "image/svg+xml";
+  return "application/text";
 }
 
 // Append an HTTP rel-path to a local filesystem path.
 // The returned path is normalized for the platform.
-std::string
-Http_server::path_cat(
-    beast::string_view base,
-    beast::string_view path)
+std::string Http_server::path_cat(
+    beast::string_view base
+    , beast::string_view path)
 {
-    if (base.empty())
-        return std::string(path);
-    std::string result(base);
+  if (base.empty())
+    return std::string(path);
+  std::string result(base);
 #ifdef BOOST_MSVC
-    char constexpr path_separator = '\\';
-    if(result.back() == path_separator)
-        result.resize(result.size() - 1);
-    result.append(path.data(), path.size());
-    for(auto& c : result)
-        if(c == '/')
-            c = path_separator;
+  char constexpr path_separator = '\\';
+  if(result.back() == path_separator)
+    result.resize(result.size() - 1);
+  result.append(path.data(), path.size());
+  for(auto& c : result)
+    if(c == '/')
+      c = path_separator;
 #else
-    char constexpr path_separator = '/';
-    if(result.back() == path_separator)
-        result.resize(result.size() - 1);
-    result.append(path.data(), path.size());
+  char constexpr path_separator = '/';
+  if(result.back() == path_separator)
+    result.resize(result.size() - 1);
+  result.append(path.data(), path.size());
 #endif
-    return result;
+  return result;
 }
 
 // This function produces an HTTP response for the given
 // request. The type of the response object depends on the
 // contents of the request, so the interface requires the
 // caller to pass a generic lambda for receiving the response.
-template<
-    class Body, class Allocator,
-    class Send>
-void
-Http_server::handle_request(
-    beast::string_view doc_root,
-    http::request<Body, http::basic_fields<Allocator>>&& req,
-    Send&& send)
+template<class Body, class Allocator, class Send>
+void Http_server::handle_request(
+    beast::string_view doc_root
+    , http::request<Body, http::basic_fields<Allocator>>&& req
+    , Send&& send)
 {
-    // Returns a bad request response
-    auto const bad_request =
-    [&req](beast::string_view why)
-    {
-        http::response<http::string_body> res{http::status::bad_request, req.version()};
-        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-        res.set(http::field::content_type, "text/html");
-        res.keep_alive(req.keep_alive());
-        res.body() = std::string(why);
-        res.prepare_payload();
-        return res;
-    };
+  // Returns a bad request response
+  auto const bad_request =
+      [&req](beast::string_view why)
+  {
+    http::response<http::string_body> res{http::status::bad_request, req.version()};
+    res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+    res.set(http::field::content_type, "text/html");
+    res.keep_alive(req.keep_alive());
+    res.body() = std::string(why);
+    res.prepare_payload();
+    return res;
+  };
 
-    // Returns a not found response
-    auto const not_found =
-    [&req](beast::string_view target)
-    {
-        http::response<http::string_body> res{http::status::not_found, req.version()};
-        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-        res.set(http::field::content_type, "text/html");
-        res.keep_alive(req.keep_alive());
-        res.body() = "The resource '" + std::string(target) + "' was not found.<br> Go to the following address : http://ip_address:port/ossia_remote.html.";
-        res.prepare_payload();
-        return res;
-    };
+  // Returns a not found response
+  auto const not_found =
+      [&req](beast::string_view target)
+  {
+    http::response<http::string_body> res{http::status::not_found, req.version()};
+    res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+    res.set(http::field::content_type, "text/html");
+    res.keep_alive(req.keep_alive());
+    res.body() = "The resource '" + std::string(target) + "' was not found.<br> Go to the following address : http://ip_address:port/ossia_remote.html.";
+    res.prepare_payload();
+    return res;
+  };
 
-    // Returns a server error response
-    auto const server_error =
-    [&req](beast::string_view what)
-    {
-        http::response<http::string_body> res{http::status::internal_server_error, req.version()};
-        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-        res.set(http::field::content_type, "text/html");
-        res.keep_alive(req.keep_alive());
-        res.body() = "An error occurred: '" + std::string(what) + "'";
-        res.prepare_payload();
-        return res;
-    };
+  // Returns a server error response
+  auto const server_error =
+      [&req](beast::string_view what)
+  {
+    http::response<http::string_body> res{http::status::internal_server_error, req.version()};
+    res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+    res.set(http::field::content_type, "text/html");
+    res.keep_alive(req.keep_alive());
+    res.body() = "An error occurred: '" + std::string(what) + "'";
+    res.prepare_payload();
+    return res;
+  };
 
-    // Make sure we can handle the method
-    if( req.method() != http::verb::get &&
-        req.method() != http::verb::head)
-        return send(bad_request("Unknown HTTP-method"));
+  // Make sure we can handle the method
+  if( req.method() != http::verb::get &&
+     req.method() != http::verb::head)
+    return send(bad_request("Unknown HTTP-method"));
 
-    // Request path must be absolute and not contain "..".
-    if( req.target().empty() ||
-        req.target()[0] != '/' ||
-        req.target().find("..") != beast::string_view::npos)
-        return send(bad_request("Illegal request-target"));
+  // Request path must be absolute and not contain "..".
+  if( req.target().empty() ||
+     req.target()[0] != '/' ||
+     req.target().find("..") != beast::string_view::npos)
+    return send(bad_request("Illegal request-target"));
 
-    // Build the path to the requested file
-    std::string path = path_cat(doc_root, req.target());
-    if(req.target().back() == '/')
-        path.append("index.html");
+  // Build the path to the requested file
+  std::string path = path_cat(doc_root, req.target());
+  if(req.target().back() == '/')
+    path.append("index.html");
 
-    // Attempt to open the file
-    beast::error_code ec;
-    http::file_body::value_type body;
+  // Attempt to open the file
+  beast::error_code ec;
+  http::file_body::value_type body;
 
-    body.open(path.c_str(), beast::file_mode::scan, ec);
+  body.open(path.c_str(), beast::file_mode::scan, ec);
 
-    // Handle the case where the file doesn't exist
-    if(ec == beast::errc::no_such_file_or_directory)
-        return send(not_found(req.target()));
+  // Handle the case where the file doesn't exist
+  if(ec == beast::errc::no_such_file_or_directory)
+    return send(not_found(req.target()));
 
-    // Handle an unknown error
-    if(ec)
-        return send(server_error(ec.message()));
+  // Handle an unknown error
+  if(ec)
+    return send(server_error(ec.message()));
 
-    // Cache the size since we need it after the move
-    auto const size = body.size();
+  // Cache the size since we need it after the move
+  auto const size = body.size();
 
-    // Respond to HEAD request
-    if(req.method() == http::verb::head)
-    {
-        http::response<http::empty_body> res{http::status::ok, req.version()};
-        res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
-        res.set(http::field::content_type, mime_type(path));
-        res.content_length(size);
-        res.keep_alive(false);
-        return send(std::move(res));
-    }
-
-    // Respond to GET request
-    http::response<http::file_body> res{
-        std::piecewise_construct,
-        std::make_tuple(std::move(body)),
-        std::make_tuple(http::status::ok, req.version())};
-
-    // Allow Cross-Origin Resource Sharing (CORS).
-    res.set(http::field::access_control_allow_headers, "*");
-    res.set(http::field::access_control_allow_origin, "*");
-    res.set(http::field::access_control_allow_methods, "GET");
-
+  // Respond to HEAD request
+  if(req.method() == http::verb::head)
+  {
+    http::response<http::empty_body>
+        res{http::status::ok, req.version()};
     res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
     res.set(http::field::content_type, mime_type(path));
     res.content_length(size);
     res.keep_alive(false);
     return send(std::move(res));
+  }
+
+  // Respond to GET request
+  http::response<http::file_body>
+      res{std::piecewise_construct
+          , std::make_tuple(std::move(body))
+          , std::make_tuple(http::status::ok, req.version())};
+
+  // Allow Cross-Origin Resource Sharing (CORS).
+  res.set(http::field::access_control_allow_headers, "*");
+  res.set(http::field::access_control_allow_origin, "*");
+  res.set(http::field::access_control_allow_methods, "GET");
+
+  res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
+  res.set(http::field::content_type, mime_type(path));
+  res.content_length(size);
+  res.keep_alive(false);
+  return send(std::move(res));
 }
 
 //------------------------------------------------------------------------------
 
 // Report a failure
-void
-Http_server::fail(beast::error_code ec, char const* what)
+void Http_server::fail(beast::error_code ec, char const* what)
 {
-    std::cerr << what << ": " << ec.message() << "\n";
+  std::cerr << what << ": " << ec.message() << "\n";
 }
 
 // Handles an HTTP server connection
-void
-Http_server::do_session(
-    tcp::socket& socket,
-    std::shared_ptr<std::string const> const& doc_root)
+void Http_server::do_session(
+    tcp::socket& socket
+    , std::shared_ptr<std::string const> const& doc_root)
 {
-    bool close = false;
-    beast::error_code ec;
+  bool close = false;
+  beast::error_code ec;
 
-    // This buffer is required to persist across reads
-    beast::flat_buffer buffer;
+  // This buffer is required to persist across reads
+  beast::flat_buffer buffer;
 
-    // This lambda is used to send messages
-    send_lambda<tcp::socket> lambda{socket, close, ec};
+  // This lambda is used to send messages
+  send_lambda<tcp::socket> lambda{socket, close, ec};
 
-    for(;;)
+  for(;;)
+  {
+    // Read a request
+    http::request<http::string_body> req;
+    http::read(socket, buffer, req, ec);
+    if(ec == http::error::end_of_stream)
+      break;
+    if(ec)
+      return Http_server::fail(ec, "read");
+
+    // Send the response
+    Http_server::handle_request(*doc_root, std::move(req), lambda);
+    if(ec)
+      return Http_server::fail(ec, "write");
+    if(close)
     {
-        // Read a request
-        http::request<http::string_body> req;
-        http::read(socket, buffer, req, ec);
-        if(ec == http::error::end_of_stream)
-            break;
-        if(ec)
-            return Http_server::fail(ec, "read");
-
-        // Send the response
-        Http_server::handle_request(*doc_root, std::move(req), lambda);
-        if(ec)
-            return Http_server::fail(ec, "write");
-        if(close)
-        {
-            // This means we should close the connection, usually because
-            // the response indicated the "Connection: close" semantic.
-            break;
-        }
+      // This means we should close the connection, usually because
+      // the response indicated the "Connection: close" semantic.
+      break;
     }
+  }
 
-    // Send a TCP shutdown
-    socket.shutdown(tcp::socket::shutdown_send, ec);
+  // Send a TCP shutdown
+  socket.shutdown(tcp::socket::shutdown_send, ec);
 
-    // At this point the connection is closed gracefully
+  // At this point the connection is closed gracefully
 }
-
-//------------------------------------------------------------------------------
-
-// // Set the IP address in the remote.html file
-// void
-// Http_server::set_ip_address(std::string address)
-// {
-//     qDebug() << "buildWasmPath :" << QString::fromStdString(m_buildWasmPath);
-//     std::rename((m_buildWasmPath + "remote.html").c_str(), (m_buildWasmPath + "remote.html~").c_str());
-
-//     std::ifstream old_file(m_buildWasmPath + "remote.html~");
-//     std::ofstream new_file(m_buildWasmPath + "remote.html");
-
-//     std::string addr = "\"" + m_ipAddress + "\"";
-
-//     for( std::string contents_of_file; std::getline(old_file, contents_of_file); ) {
-//       std::string::size_type position = contents_of_file.find("%SCORE_IP_ADDRESS%");
-//       if( position != std::string::npos )
-//       {
-//         //contents_of_file = contents_of_file.replace(position, 18, addr);
-//         contents_of_file = contents_of_file.replace(position, 18, "%SCORE_IP_ADDRESS%");
-//       }
-//       new_file << contents_of_file << '\n';
-//     }
-// }
 
 //------------------------------------------------------------------------------
 
 // Launch the open_server function in a thread
-void
-Http_server::start_thread()
+void Http_server::start_thread()
 {
-    m_serverThread = std::thread{[this] { open_server(); }};
+  m_serverThread = std::thread{[this] { open_server(); }};
 }
 
 //------------------------------------------------------------------------------
@@ -306,46 +272,45 @@ void Http_server::stop_thread()
 //------------------------------------------------------------------------------
 
 // Open a server using sockets
-int
-Http_server::open_server()
+int Http_server::open_server()
 {
-    try
+  try
+  {
+    auto const address2 = net::ip::make_address("0.0.0.0");
+    auto const port = static_cast<unsigned short>(std::atoi("8080"));
+    std::string packagesPath = score::AppContext().settings<Library::Settings::Model>().getPackagesPath().toStdString();
+    m_buildWasmPath = packagesPath + "/wasm-remote/";
+    auto const m_docRoot = std::make_shared<std::string>(m_buildWasmPath);
+
+    bool is_ip_address_set = false;
+
+    // The acceptor receives incoming connections
+    tcp::acceptor acceptor{ioc, {address2, port}};
+    m_listenSocket = acceptor.native_handle();
+    for(;;)
     {
-        auto const address2 = net::ip::make_address("0.0.0.0");
-        auto const port = static_cast<unsigned short>(std::atoi("8080"));
-        std::string packagesPath = score::AppContext().settings<Library::Settings::Model>().getPackagesPath().toStdString();
-        m_buildWasmPath = packagesPath + "/wasm-remote/";
-        auto const m_docRoot = std::make_shared<std::string>(m_buildWasmPath);
+      // This will receive the new connection
+      tcp::socket socket{ioc};
 
-        bool is_ip_address_set = false;
+      // Block until we get a connection
+      acceptor.accept(socket);
 
-        // The acceptor receives incoming connections
-        tcp::acceptor acceptor{ioc, {address2, port}};
-        m_listenSocket = acceptor.native_handle();
-        for(;;)
-        {
-            // This will receive the new connection
-            tcp::socket socket{ioc};
+      // Set ip address
+      if(!is_ip_address_set)
+      {
+        m_ipAddress = socket.local_endpoint().address().to_string();
+        is_ip_address_set = true;
+      }
 
-            // Block until we get a connection
-            acceptor.accept(socket);
-
-            // Set ip address
-            if(!is_ip_address_set)
-            {
-                m_ipAddress = socket.local_endpoint().address().to_string();
-                is_ip_address_set = true;
-            }
-
-            // Launch the session, transferring ownership of the socket
-            do_session(socket, m_docRoot);
-        }
+      // Launch the session, transferring ownership of the socket
+      do_session(socket, m_docRoot);
     }
-    catch (const std::exception& e)
-    {
-        std::cerr << "Error: " << e.what() << '\n';
-        return EXIT_FAILURE;
-    }
+  }
+  catch (const std::exception& e)
+  {
+    std::cerr << "Error: " << e.what() << '\n';
+    return EXIT_FAILURE;
+  }
 }
 
 }
--- src/plugins/score-plugin-remotecontrol/RemoteControl/HttpServer/Http_server.hpp
+++ src/plugins/score-plugin-remotecontrol/RemoteControl/HttpServer/Http_server.hpp
@@ -44,116 +44,84 @@ namespace RemoteControl
 class Http_server
 {
 public:
-    net::io_context ioc;
+  Http_server();
+  ~Http_server();
 
-    //std::thread th1;
+  // Launch the open_server function in a thread
+  void start_thread();
+  void stop_thread();
 
-    Http_server();
+private:
+  // Return a reasonable mime type based on the extension of a file.
+  beast::string_view mime_type(beast::string_view path);
 
-    ~Http_server();
+  // Append an HTTP rel-path to a local filesystem path.
+  // The returned path is normalized for the platform.
+  std::string path_cat(beast::string_view base
+                       , beast::string_view path);
 
-    //------------------------------------------------------------------------------
+  // This function produces an HTTP response for the given
+  // request. The type of the response object depends on the
+  // contents of the request, so the interface requires the
+  // caller to pass a generic lambda for receiving the response.
+  template<class Body, class Allocator, class Send>
+  void handle_request(
+      beast::string_view doc_root,
+      http::request<Body, http::basic_fields<Allocator>>&& req,
+      Send&& send);
 
-    // Return a reasonable mime type based on the extension of a file.
-    beast::string_view
-    mime_type(beast::string_view path);
+  //------------------------------------------------------------------------------
 
-    // Append an HTTP rel-path to a local filesystem path.
-    // The returned path is normalized for the platform.
-    std::string
-    path_cat(
-        beast::string_view base,
-        beast::string_view path);
+  // Report a failure
+  void fail(beast::error_code ec, char const* what);
 
-    // This function produces an HTTP response for the given
-    // request. The type of the response object depends on the
-    // contents of the request, so the interface requires the
-    // caller to pass a generic lambda for receiving the response.
-    template<
-        class Body, class Allocator,
-        class Send>
-    void
-    handle_request(
-        beast::string_view doc_root,
-        http::request<Body, http::basic_fields<Allocator>>&& req,
-        Send&& send);
+  // This is the C++11 equivalent of a generic lambda.
+  // The function object is used to send an HTTP message.
+  template<class Stream>
+  struct send_lambda
+  {
+    Stream& stream_;
+    bool& close_;
+    beast::error_code& ec_;
 
-    //------------------------------------------------------------------------------
+    explicit send_lambda(
+        Stream& stream,
+        bool& close,
+        beast::error_code& ec)
+        : stream_(stream)
+        , close_(close)
+        , ec_(ec)
+    { }
 
-    // Report a failure
-    void
-    fail(beast::error_code ec, char const* what);
-
-    // This is the C++11 equivalent of a generic lambda.
-    // The function object is used to send an HTTP message.
-    template<class Stream>
-    struct send_lambda
+    template<bool isRequest, class Body, class Fields>
+    void operator()
+        (http::message<isRequest, Body, Fields>&& msg) const
     {
-        Stream& stream_;
-        bool& close_;
-        beast::error_code& ec_;
+      // Determine if we should close the connection after
+      close_ = msg.need_eof();
 
-        explicit
-        send_lambda(
-            Stream& stream,
-            bool& close,
-            beast::error_code& ec)
-            : stream_(stream)
-            , close_(close)
-            , ec_(ec)
-        {
-        }
+      // We need the serializer here because the serializer requires
+      // a non-const file_body, and the message oriented version of
+      // http::write only works with const messages.
+      http::serializer<isRequest, Body, Fields> sr{msg};
+      http::write(stream_, sr, ec_);
+    }
+  };
 
-        template<bool isRequest, class Body, class Fields>
-        void
-        operator()(http::message<isRequest, Body, Fields>&& msg) const
-        {
-            // Determine if we should close the connection after
-            close_ = msg.need_eof();
+  // Handles an HTTP server connection
+  void do_session(
+      tcp::socket& socket
+      , std::shared_ptr<std::string const> const& doc_root);
 
-            // We need the serializer here because the serializer requires
-            // a non-const file_body, and the message oriented version of
-            // http::write only works with const messages.
-            http::serializer<isRequest, Body, Fields> sr{msg};
-            http::write(stream_, sr, ec_);
-        }
-    };
+  //------------------------------------------------------------------------------
 
-    // Handles an HTTP server connection
-    void
-    do_session(
-        tcp::socket& socket,
-        std::shared_ptr<std::string const> const& doc_root);
+  // Open a server using sockets
+  int open_server();
 
-    //------------------------------------------------------------------------------
-
-    // // Set the IP address in the remote.html file
-    // set_ip_address(std::string address);
-    // void
-
-    //------------------------------------------------------------------------------
-
-    // Launch the open_server function in a thread
-    void
-    start_thread();
-
-    //------------------------------------------------------------------------------
-
-    // Launch the open_server function in a thread
-    void
-    stop_thread();
-
-    //------------------------------------------------------------------------------
-
-    // Open a server using sockets
-    int
-    open_server();
-
-    //------------------------------------------------------------------------------
-
-    std::thread m_serverThread;
-    int m_listenSocket{};
-    std::string m_buildWasmPath;
-    std::string m_ipAddress;
+  net::io_context ioc;
+  std::thread m_serverThread;
+  int m_listenSocket{};
+  std::string m_buildWasmPath;
+  std::string m_ipAddress;
 };
 }
