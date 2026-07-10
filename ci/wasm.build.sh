9b3d05a0b17091e869e1d8cea2e2df1de4018d22
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2026-07-10 19:03:06 +0200

    wasm: raise initial memory + allow growth (link error)
    Everything compiles (331/331) but wasm-ld failed: 'initial memory too small,
    53718048 bytes needed' -- score's static data exceeds the 16MB default. Set
    INITIAL_MEMORY=256MB, ALLOW_MEMORY_GROWTH=1, MAXIMUM_MEMORY=2GB.

--- ci/wasm.build.sh
+++ ci/wasm.build.sh
@@ -23,8 +23,8 @@ source /opt/ossia-sdk-wasm/emsdk/emsdk_env.sh
    -DOSSIA_SDK=/opt/ossia-sdk-wasm/ \
    -DCMAKE_C_FLAGS='-pthread -O3 -ffast-math -msimd128 -msse -msse2 -msse3 -mssse3 -msse4 -msse4.1 -msse4.2 -flto -g0 ' \
    -DCMAKE_CXX_FLAGS='-fexperimental-library -DBOOST_ASIO_DISABLE_EPOLL=1 -pthread -O3 -ffast-math -msimd128 -msse -msse2 -msse3 -mssse3 -msse4 -msse4.1 -msse4.2 -flto -g0 '  \
-   -DCMAKE_EXE_LINKER_FLAGS='-sASYNCIFY -pthread -Oz -ffast-math -msimd128 -msse -msse2 -msse3 -mssse3 -msse4 -msse4.1 -msse4.2 -flto -g0 ' \
-   -DCMAKE_SHARED_LINKER_FLAGS='-sASYNCIFY -pthread -Oz -ffast-math -msimd128 -msse -msse2 -msse3 -mssse3 -msse4 -msse4.1 -msse4.2 -flto -g0 '
+   -DCMAKE_EXE_LINKER_FLAGS='-sASYNCIFY -sALLOW_MEMORY_GROWTH=1 -sINITIAL_MEMORY=268435456 -sMAXIMUM_MEMORY=2147483648 -pthread -Oz -ffast-math -msimd128 -msse -msse2 -msse3 -mssse3 -msse4 -msse4.1 -msse4.2 -flto -g0 ' \
+   -DCMAKE_SHARED_LINKER_FLAGS='-sASYNCIFY -sALLOW_MEMORY_GROWTH=1 -pthread -Oz -ffast-math -msimd128 -msse -msse2 -msse3 -mssse3 -msse4 -msse4.1 -msse4.2 -flto -g0 '
 
 cmake --build .
 
