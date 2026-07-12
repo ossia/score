c852c1158e77f89213f601905db0a8ed6e2f4fa9
thibaudk <thibaud.keller@aquilenet.fr>
2026-07-12 22:15:34 +0100

    [remotecontrol] run clang-format

--- src/plugins/score-plugin-remotecontrol/RemoteControl/HttpServer/HttpServer.cpp
+++ src/plugins/score-plugin-remotecontrol/RemoteControl/HttpServer/HttpServer.cpp
@@ -8,6 +8,7 @@
 //
 
 #include "HttpServer.hpp"
+
 #include <Library/LibrarySettings.hpp>
 
 #include <cstdlib>
@@ -26,35 +27,56 @@ HttpServer::~HttpServer()
 beast::string_view HttpServer::mime_type(beast::string_view path)
 {
   using beast::iequals;
-  auto const ext = [&path]
-  {
+  auto const ext = [&path] {
     auto const pos = path.rfind(".");
     if(pos == beast::string_view::npos)
       return beast::string_view{};
     return path.substr(pos);
   }();
-  if(iequals(ext, ".htm"))  return "text/html";
-  if(iequals(ext, ".html")) return "text/html";
-  if(iequals(ext, ".php"))  return "text/html";
-  if(iequals(ext, ".css"))  return "text/css";
-  if(iequals(ext, ".txt"))  return "text/plain";
-  if(iequals(ext, ".js"))   return "application/javascript";
-  if(iequals(ext, ".json")) return "application/json";
-  if(iequals(ext, ".xml"))  return "application/xml";
-  if(iequals(ext, ".swf"))  return "application/x-shockwave-flash";
-  if(iequals(ext, ".wasm")) return "application/wasm";
-  if(iequals(ext, ".flv"))  return "video/x-flv";
-  if(iequals(ext, ".png"))  return "image/png";
-  if(iequals(ext, ".jpe"))  return "image/jpeg";
-  if(iequals(ext, ".jpeg")) return "image/jpeg";
-  if(iequals(ext, ".jpg"))  return "image/jpeg";
-  if(iequals(ext, ".gif"))  return "image/gif";
-  if(iequals(ext, ".bmp"))  return "image/bmp";
-  if(iequals(ext, ".ico"))  return "image/vnd.microsoft.icon";
-  if(iequals(ext, ".tiff")) return "image/tiff";
-  if(iequals(ext, ".tif"))  return "image/tiff";
-  if(iequals(ext, ".svg"))  return "image/svg+xml";
-  if(iequals(ext, ".svgz")) return "image/svg+xml";
+  if(iequals(ext, ".htm"))
+    return "text/html";
+  if(iequals(ext, ".html"))
+    return "text/html";
+  if(iequals(ext, ".php"))
+    return "text/html";
+  if(iequals(ext, ".css"))
+    return "text/css";
+  if(iequals(ext, ".txt"))
+    return "text/plain";
+  if(iequals(ext, ".js"))
+    return "application/javascript";
+  if(iequals(ext, ".json"))
+    return "application/json";
+  if(iequals(ext, ".xml"))
+    return "application/xml";
+  if(iequals(ext, ".swf"))
+    return "application/x-shockwave-flash";
+  if(iequals(ext, ".wasm"))
+    return "application/wasm";
+  if(iequals(ext, ".flv"))
+    return "video/x-flv";
+  if(iequals(ext, ".png"))
+    return "image/png";
+  if(iequals(ext, ".jpe"))
+    return "image/jpeg";
+  if(iequals(ext, ".jpeg"))
+    return "image/jpeg";
+  if(iequals(ext, ".jpg"))
+    return "image/jpeg";
+  if(iequals(ext, ".gif"))
+    return "image/gif";
+  if(iequals(ext, ".bmp"))
+    return "image/bmp";
+  if(iequals(ext, ".ico"))
+    return "image/vnd.microsoft.icon";
+  if(iequals(ext, ".tiff"))
+    return "image/tiff";
+  if(iequals(ext, ".tif"))
+    return "image/tiff";
+  if(iequals(ext, ".svg"))
+    return "image/svg+xml";
+  if(iequals(ext, ".svgz"))
+    return "image/svg+xml";
   return "application/text";
 }
 
@@ -62,7 +84,7 @@ beast::string_view HttpServer::mime_type(beast::string_view path)
 // The returned path is normalized for the platform.
 std::string HttpServer::path_cat(beast::string_view path)
 {
-  if (m_buildWasmPath.empty())
+  if(m_buildWasmPath.empty())
     return std::string(path);
   std::string result(m_buildWasmPath);
 #ifdef BOOST_MSVC
@@ -86,15 +108,12 @@ std::string HttpServer::path_cat(beast::string_view path)
 // request. The type of the response object depends on the
 // contents of the request, so the interface requires the
 // caller to pass a generic lambda for receiving the response.
-template<class Body, class Allocator, class Send>
+template <class Body, class Allocator, class Send>
 void HttpServer::handle_request(
-    http::request<Body, http::basic_fields<Allocator>>&& req
-    , const Send& send)
+    http::request<Body, http::basic_fields<Allocator>>&& req, const Send& send)
 {
   // Returns a bad request response
-  auto const bad_request =
-      [&req](beast::string_view why)
-  {
+  auto const bad_request = [&req](beast::string_view why) {
     http::response<http::string_body> res{http::status::bad_request, req.version()};
     res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
     res.set(http::field::content_type, "text/html");
@@ -105,9 +124,7 @@ void HttpServer::handle_request(
   };
 
   // Returns a not found response
-  auto const not_found =
-      [&req](beast::string_view target)
-  {
+  auto const not_found = [&req](beast::string_view target) {
     http::response<http::string_body> res{http::status::not_found, req.version()};
     res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
     res.set(http::field::content_type, "text/html");
@@ -118,10 +135,9 @@ void HttpServer::handle_request(
   };
 
   // Returns a server error response
-  auto const server_error =
-      [&req](beast::string_view what)
-  {
-    http::response<http::string_body> res{http::status::internal_server_error, req.version()};
+  auto const server_error = [&req](beast::string_view what) {
+    http::response<http::string_body> res{
+        http::status::internal_server_error, req.version()};
     res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
     res.set(http::field::content_type, "text/html");
     res.keep_alive(req.keep_alive());
@@ -131,14 +147,12 @@ void HttpServer::handle_request(
   };
 
   // Make sure we can handle the method
-  if( req.method() != http::verb::get &&
-     req.method() != http::verb::head)
+  if(req.method() != http::verb::get && req.method() != http::verb::head)
     return send(bad_request("Unknown HTTP-method"));
 
   // Request path must be absolute and not contain "..".
-  if( req.target().empty() ||
-     req.target()[0] != '/' ||
-     req.target().find("..") != beast::string_view::npos)
+  if(req.target().empty() || req.target()[0] != '/'
+     || req.target().find("..") != beast::string_view::npos)
     return send(bad_request("Illegal request-target"));
 
   // Build the path to the requested file
@@ -166,8 +180,7 @@ void HttpServer::handle_request(
   // Respond to HEAD request
   if(req.method() == http::verb::head)
   {
-    http::response<http::empty_body>
-        res{http::status::ok, req.version()};
+    http::response<http::empty_body> res{http::status::ok, req.version()};
     res.set(http::field::server, BOOST_BEAST_VERSION_STRING);
     res.set(http::field::content_type, mime_type(path));
     res.content_length(size);
@@ -176,10 +189,9 @@ void HttpServer::handle_request(
   }
 
   // Respond to GET request
-  http::response<http::file_body>
-      res{std::piecewise_construct
-          , std::make_tuple(std::move(body))
-          , std::make_tuple(http::status::ok, req.version())};
+  http::response<http::file_body> res{
+      std::piecewise_construct, std::make_tuple(std::move(body)),
+      std::make_tuple(http::status::ok, req.version())};
 
   // Allow Cross-Origin Resource Sharing (CORS).
   res.set(http::field::access_control_allow_headers, "*");
@@ -209,12 +221,8 @@ void HttpServer::do_session(tcp::socket& socket)
   beast::flat_buffer buffer;
 
   // This lambda is used to send messages
-  const auto lambda{[&close, &socket, &ec]
-              <bool isRequest
-               , class Body
-               , class Fields>
-              (http::message<isRequest, Body, Fields>&& msg)
-  {
+  const auto lambda{[&close, &socket, &ec]<bool isRequest, class Body, class Fields>(
+                        http::message<isRequest, Body, Fields>&& msg) {
     // Determine if we should close the connection after
     close = msg.need_eof();
 
@@ -238,7 +246,7 @@ void HttpServer::do_session(tcp::socket& socket)
     // Send the response
     HttpServer::handle_request(std::move(req), lambda);
 
-    if (ec)
+    if(ec)
       return HttpServer::fail(ec, "write");
     if(close)
     {
@@ -262,7 +270,8 @@ void HttpServer::start_thread()
 
 void HttpServer::stop_thread()
 {
-  if (!running()) return;
+  if(!running())
+    return;
 
   shutdown(m_listenSocket, SHUT_RDWR);
   m_ioc.stop();
@@ -278,17 +287,21 @@ void HttpServer::set_path(const std::string& str)
 void HttpServer::set_address(const std::string& str)
 {
   bool is_runnig{running()};
-  if (is_runnig) stop_thread();
+  if(is_runnig)
+    stop_thread();
   m_endpoint.address(net::ip::make_address(str.c_str()));
-  if (is_runnig) start_thread();
+  if(is_runnig)
+    start_thread();
 }
 
 void HttpServer::set_port(unsigned short prt)
 {
   bool is_runnig{running()};
-  if (is_runnig) stop_thread();
+  if(is_runnig)
+    stop_thread();
   m_endpoint.port(prt);
-  if (is_runnig) start_thread();
+  if(is_runnig)
+    start_thread();
 }
 
 // Open a server using sockets
@@ -311,7 +324,7 @@ int HttpServer::open_server()
       do_session(socket);
     }
   }
-  catch (const std::exception& e)
+  catch(const std::exception& e)
   {
     std::cerr << "Error: " << e.what() << '\n';
     return EXIT_FAILURE;
