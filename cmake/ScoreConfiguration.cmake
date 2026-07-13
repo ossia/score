1b91047fac23f0321db931c4b90628d9eecf54ad
Jean-Michaël Celerier <jeanmichael.celerier+github@gmail.com>
2026-07-13 14:09:35 -0400

    win32: force utf8 by adding the manifest to all builds

--- cmake/ScoreConfiguration.cmake
+++ cmake/ScoreConfiguration.cmake
@@ -304,3 +304,67 @@ install(
   DESTINATION include/score
   COMPONENT Devel
   OPTIONAL)
+
+# Win32 codepage stuff
+if(WIN32)
+  file(CONFIGURE
+    OUTPUT
+      "${CMAKE_BINARY_DIR}/score.exe.manifest"
+    CONTENT
+    [=[<?xml version="1.0" encoding="UTF-8" standalone="yes"?>
+    <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
+      <application xmlns="urn:schemas-microsoft-com:asm.v3">
+         <windowsSettings> <dpiAware       xmlns="http://schemas.microsoft.com/SMI/2005/WindowsSettings">true/PM</dpiAware>                     </windowsSettings>
+         <windowsSettings> <dpiAwareness   xmlns="http://schemas.microsoft.com/SMI/2016/WindowsSettings">PerMonitorV2,PerMonitor</dpiAwareness> </windowsSettings>
+         <windowsSettings> <longPathAware  xmlns="http://schemas.microsoft.com/SMI/2016/WindowsSettings">true</longPathAware>                   </windowsSettings>
+         <windowsSettings> <activeCodePage xmlns="http://schemas.microsoft.com/SMI/2019/WindowsSettings">UTF-8</activeCodePage>                 </windowsSettings>
+      </application>
+      <trustInfo xmlns="urn:schemas-microsoft-com:asm.v2">
+        <security>
+          <requestedPrivileges xmlns="urn:schemas-microsoft-com:asm.v3">
+            <requestedExecutionLevel level="asInvoker" uiAccess="false" />
+          </requestedPrivileges>
+        </security>
+      </trustInfo>
+      <compatibility xmlns="urn:schemas-microsoft-com:compatibility.v1">
+        <application>
+          <supportedOS Id="{8e0f7a12-bfb3-4fe8-b9a5-48fd50a15a9a}" />
+          <supportedOS Id="{1f676c76-80e1-4239-95bb-83d0f6d0da78}" />
+          <supportedOS Id="{4a2f28e3-53b9-4441-ba9c-d69d4a4a6e38}" />
+          <supportedOS Id="{35138b9a-5d96-4fbd-8e2d-a2440225f93a}" />
+        </application>
+      </compatibility>
+    </assembly>
+    ]=]
+  )
+
+  if(MINGW)
+    enable_language(RC)
+    # 1 = CREATEPROCESS_MANIFEST_RESOURCE_ID, 24 = RT_MANIFEST (no includes needed)
+    file(WRITE "${CMAKE_BINARY_DIR}/score.exe.rc"
+         "1 24 \"${CMAKE_BINARY_DIR}/score.exe.manifest\"\n")
+  endif()
+endif()
+
+function(target_enable_utf8 tgt)
+  if(NOT SCORE_MSSTORE_DEPLOYMENT) # c.f. ScoreDeploymentWindowsStore
+    if(MSVC)
+      target_link_options(${tgt} PRIVATE
+        /MANIFEST:EMBED
+        "/MANIFESTINPUT:${CMAKE_BINARY_DIR}/score.exe.manifest")
+    elseif(MINGW)
+      # Compile the manifest resource in a dedicated, flag-free object library.
+      # Adding score.exe.rc straight to ${tgt} makes windres inherit the target's
+      # hundreds of -I/-D flags and overflow the Windows command-line length limit
+      # ("windres: preprocessing failed"). The resource (`1 24 "…manifest"`) needs
+      # no include dirs or defines at all, so strip them.
+      if(NOT TARGET score_win_manifest)
+        add_library(score_win_manifest OBJECT "${CMAKE_BINARY_DIR}/score.exe.rc")
+        set_target_properties(score_win_manifest PROPERTIES
+          INCLUDE_DIRECTORIES ""
+          COMPILE_DEFINITIONS "")
+      endif()
+      target_sources(${tgt} PRIVATE $<TARGET_OBJECTS:score_win_manifest>)
+    endif()
+  endif()
+endfunction()
