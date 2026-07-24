7129d6f26ecb82586244f2735853ed620717be47
Jean-Michaël Celerier <jeanmichael.celerier@gmail.com>
2026-07-24 17:17:02 -0400

    pd: fix build on trixie with system libs

--- 3rdparty/libpd.cmake
+++ 3rdparty/libpd.cmake
@@ -8,10 +8,31 @@ if(OSSIA_USE_SYSTEM_LIBRARIES AND LINUX)
   )
 
   if(LIBPD_LIBRARY AND LIBPD_INCLUDE_DIR)
-    add_library(libpd INTERFACE IMPORTED GLOBAL)
-    target_link_libraries(libpd INTERFACE "${LIBPD_LIBRARY}")
-    target_include_directories(libpd INTERFACE "$<BUILD_INTERFACE:${LIBPD_INCLUDE_DIR}>")
-    return()
+    include(CheckCXXSourceCompiles)
+
+    set(CMAKE_REQUIRED_LIBRARIES "${LIBPD_LIBRARY}")
+    check_cxx_source_compiles("
+#if defined(_WIN32)
+extern \"C\" { __declspec(dllimport) extern void* pd_getinstance(void); }
+int main() {
+    void* ptr = (void*)&pd_getinstance;
+    return (ptr != 0) ? 0 : 1;
+}
+#else
+extern \"C\" { extern void* pd_this; }
+int main() {
+    void* ptr = (void*)&pd_this;
+    return (ptr != 0) ? 0 : 1;
+}
+#endif
+    " LIBPD_LIBRARY_IS_MULTI)
+
+    if(LIBPD_LIBRARY_IS_MULTI)
+      add_library(libpd INTERFACE IMPORTED GLOBAL)
+      target_link_libraries(libpd INTERFACE "${LIBPD_LIBRARY}")
+      target_include_directories(libpd INTERFACE "$<BUILD_INTERFACE:${LIBPD_INCLUDE_DIR}>")
+      return()
+    endif()
   endif()
 endif()
 
