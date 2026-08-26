491f6c691de9cc2a8960a776f53d75703526a98e
Jean-Michaël Celerier <jeanmichael.celerier+github@gmail.com>
2026-08-26 12:45:10 -0400

    ci: install SDL3 in system dependency builds

--- ci/archlinux.deps.sh
+++ ci/archlinux.deps.sh
@@ -21,7 +21,7 @@ $SUDO pacman -S --noconfirm --needed \
    $PACKAGES \
    cmake ninja gcc llvm clang boost \
    icu \
-   ffmpeg portaudio lv2 suil lilv sdl2 alsa-lib \
+   ffmpeg portaudio lv2 suil lilv sdl3 alsa-lib \
    avahi fftw bluez-libs dbus \
    tar xz wget \
    brotli double-conversion zstd glib2 libb2 pcre2 \
