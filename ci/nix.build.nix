7e9e382115fb5d415d4ad0b6137bab7f16830828
Jean-Michaël Celerier <jeanmichael.celerier+github@gmail.com>
2026-08-27 21:14:07 -0400

    nix: make SDL3 input optional

--- ci/nix.build.nix
+++ ci/nix.build.nix
@@ -34,7 +34,7 @@
 , re2
 , rubberband
 , snappy
-, SDL3
+, SDL3 ? null
 , spdlog
 , suil
 , udev
@@ -89,7 +89,6 @@ clangStdenv.mkDerivation (finalAttrs: {
     re2
     rubberband
     snappy
-    SDL3
     spdlog
     suil
     udev
@@ -97,7 +96,7 @@ clangStdenv.mkDerivation (finalAttrs: {
     xorg.libXext
     xorg.libXcomposite
     xorg.libXrandr
-  ];
+  ] ++ lib.optional (SDL3 != null) SDL3;
 
   cmakeFlags = [
     "-Wno-dev"
